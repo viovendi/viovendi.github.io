1aca04d819a624d052c2a22756dae0733d8da258
oleh.stetsko <oleh.stetsko@gmail.com>
2020-08-14 16:17:39 +0300

    JSON.stringify

--- customizations/organization/13060/oleg-test-customization.js
+++ customizations/organization/13060/oleg-test-customization.js
@@ -3,16 +3,16 @@ console.log('Start working, Google Tag Manager');
 async function getAccessToken(){
     const headers = new Headers();
     const user = {
-        "username":"oleg.stetsko@doo.net", 
-        "password":"canon450",
-        "client_id": "viovendi_web",
-        "grant_type":"password"
+        username: 'oleg.stetsko@doo.net', 
+        password:'canon450',
+        client_id: 'viovendi_web',
+        grant_type: 'password'
       }
     headers.append("Accept", "application/json")
     const response = await fetch("https://api.doo.net/v1/oauth", {
         headers,
         method: "POST",
-        body: user
+        body: JSON.stringify(user)
       })
       return response;
 }
