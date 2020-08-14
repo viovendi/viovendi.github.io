444a1848ea56246e20a6433ead32288ba276634d
oleh.stetsko <oleh.stetsko@gmail.com>
2020-08-14 16:09:38 +0300

    handler

--- customizations/organization/13060/oleg-test-customization.js
+++ customizations/organization/13060/oleg-test-customization.js
@@ -13,17 +13,25 @@ async function getAccessToken(){
         method: "POST",
         body: user
       })
-      return  response;
+      return response;
 }
-const accessToken = await getAccessToken();
-console.log(accessToken)
 
-const headers = new Headers();
-headers.append("Content-Type", "application/json")
-headers.append("Authorization", `Bearer ${accessToken}`)
-fetch("https://api.doo.net/v1/organizers/current/contacts?sort_order=asc&sort_by=last_name&page_size=50&page=1", {
-  headers,
-  method: "GET",
-}).then((contacts) => {
-    console.log(contacts)
-})
+async function getContacts(){
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json")
+    headers.append("Authorization", `Bearer ${accessToken}`)
+    fetch("https://api.doo.net/v1/organizers/current/contacts?sort_order=asc&sort_by=last_name&page_size=50&page=1", {
+      headers,
+      method: "GET",
+    }).then((contacts) => {
+        console.log(contacts);
+    })
+}
+
+async function handler(){
+    await getContacts()
+    const accessToken = await getAccessToken();
+    console.log(accessToken)
+}
+
+handler()
