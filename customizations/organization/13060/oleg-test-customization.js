72beee8feaca893540b802d1ef63fa5ca440aa28
oleh.stetsko <oleh.stetsko@gmail.com>
2020-08-14 16:50:11 +0300

    response.json()

--- customizations/organization/13060/oleg-test-customization.js
+++ customizations/organization/13060/oleg-test-customization.js
@@ -23,19 +23,19 @@ async function getContacts(accessToken){
     const headers = new Headers();
     headers.append("Content-Type", "application/json")
     headers.append("Authorization", `Bearer ${accessToken}`)
-    fetch("https://api.doo.net/v1/organizers/current/contacts?sort_order=asc&sort_by=last_name&page_size=50&page=1", {
+    const response = await  fetch("https://api.doo.net/v1/organizers/current/contacts?sort_order=asc&sort_by=last_name&page_size=50&page=1", {
       headers,
       method: "GET",
-    }).then((contacts) => {
-        console.log(contacts);
     })
+    return response.json();
 }
 
 async function handler(){
 
     const response = await getAccessToken();
     console.log(response)
-    await getContacts(response.data.access_token)
+    const contacts = await getContacts(response.data.access_token)
+    console.log(contacts)
 }
 
 handler()
