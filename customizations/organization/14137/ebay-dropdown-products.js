d72092c6493f9a48d00ee7a5b6279f61090a796b
vbodend <69503064+vbodend@users.noreply.github.com>
2020-08-17 14:50:31 +0200

    Update ebay-dropdown-products.js

--- customizations/organization/14137/ebay-dropdown-products.js
+++ customizations/organization/14137/ebay-dropdown-products.js
@@ -1,1 +1,30 @@
-&doNothingForNow
+console.log('Start working, Google Tag Manager');
+ 
+  function productShow(shortName){
+    $('vv-question-simple-product').each(function(i, element) {
+    var item = $(this).find('label').text();
+        if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) == 6){
+        $(this).find('.row.question-simple-product').toggleClass('show');
+        }
+    });
+  } 
+ 
+  function productAddTitle(shortName, title, indexShowFunction){
+    $('vv-question-simple-product').each(function(i, element) {
+    var item = $(this).find('label').text();
+    if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) == 6){
+          var div = document.createElement("div");
+              div.setAttribute("class","products-title");
+              div.textContent = title;
+              div.setAttribute("onclick","javascript:workshopShow"+indexShowFunction+"();");
+              element.before(div);
+          return false;
+       }
+  })
+  }
+ 
+productAddTitle("Uhr: SD1 BayBG", "Do. 08. Oktober 2020", "SD1");
+ 
+function workshopShowSD1(){
+productShow("Uhr: SD1 BayBG");
+}
