deb311289688a0208f4b10451c1dd4fd56aca433
vbodend <69503064+vbodend@users.noreply.github.com>
2020-08-18 09:35:01 +0200

    Update ebay-dropdown-products.js

--- customizations/organization/14137/ebay-dropdown-products.js
+++ customizations/organization/14137/ebay-dropdown-products.js
@@ -64,33 +64,79 @@ function workshopShowSD3(){
 }
 
 function workshopShowSD4(){
-   
+   productShow('Di. 13. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Di. 13. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Di. 13. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Di. 13. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
 }
 
 function workshopShowSD5(){
-   
+   productShow('Mi. 14. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Mi. 14. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Mi. 14. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Mi. 14. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
 }
 
 function workshopShowSD6(){
-   
+   productShow('Do. 15. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Do. 15. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Do. 15. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Do. 15. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
 }
 
 function workshopShowSD7(){
-   
+   productShow('Fr. 16. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Fr. 16. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Fr. 16. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Fr. 16. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
 }
 
 function workshopShowSD8(){
-   
+   productShow('Mo. 19. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Mo. 19. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Mo. 19. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Mo. 19. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
 }
 
 function workshopShowSD9(){
-   
+   productShow('Di. 20. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Di. 20. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Di. 20. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Di. 20. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
 }
 
 function workshopShowSD10(){
-   
+   productShow('Mi. 21. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Mi. 21. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Mi. 21. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Mi. 21. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
 }
 
 function workshopShowSD11(){
-   
+   productShow('Do. 22. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Do. 22. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Do. 22. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Do. 22. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
 }
+
+function workshopShowSD12(){
+   productShow('Fr. 23. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Fr. 23. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Fr. 23. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Fr. 23. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
+}
+
+function workshopShowSD13(){
+   productShow('Mo. 26. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Mo. 26. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Mo. 26. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Mo. 26. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
+}
+
+function workshopShowSD14(){
+   productShow('Di. 27. Oktober 2020 | 9:00 Uhr - 10:00 Uhr');
+   productShow('Di. 27. Oktober 2020 | 11:00 Uhr - 12:00 Uhr');
+   productShow('Di. 27. Oktober 2020 | 14:00 Uhr - 15:00 Uhr');
+   productShow('Di. 27. Oktober 2020 | 16:00 Uhr - 17:00 Uhr');
+}
+
