622675f5e36411da1a48de12e56f5af73a9b695e
oleh.stetsko <oleh.stetsko@gmail.com>
2020-08-19 15:33:16 +0300

    getPageCount

--- customizations/organization/13060/oleg-test-customization.js
+++ customizations/organization/13060/oleg-test-customization.js
@@ -29,12 +29,25 @@ async function getContacts(accessToken){
     })
     return response.json();
 }
+async function getPageCount(accessToken){
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json")
+  headers.append("Authorization", `Bearer ${accessToken}`)
+  const response = await fetch("https://api.doo.net/v1/organizers/current/contacts?sort_order=asc&sort_by=last_name&page_size=50&page=1", {
+    headers,
+    method: "GET",
+  })
+  const result = response.json();
+  console.log(result.page_count)
+  return response.json();
+}
 
 async function handler(){
 
     const response = await getAccessToken();
 
     const contacts = await getContacts(response.data.access_token)
+    await getPageCount(response.data.access_token)
     console.log(JSON.stringify(contacts))
 }
 
