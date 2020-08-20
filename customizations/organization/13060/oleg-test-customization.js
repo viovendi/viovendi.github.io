63c99aba9e7e0820437770d63cac70070ca83c98
oleh.stetsko <oleh.stetsko@gmail.com>
2020-08-20 17:31:36 +0300

    getInput

--- customizations/organization/13060/oleg-test-customization.js
+++ customizations/organization/13060/oleg-test-customization.js
@@ -48,14 +48,37 @@ async function checkCode(code, pageCount, accessToken) {
   for (let i = 0; i < pageCount; i += 1) {
     const page = pageCount - i;
     const contacts = await getContacts(accessToken, page)
-    console.log(page)
     for (let j = 0; j < contacts.length; j += 1) {
-      if (contacts[j].external_customer_id) {
+      if (contacts[j].external_customer_id && contacts[j].external_customer_id.includes(code)) {
         console.log(contacts[j].external_customer_id)
       }
     }
   }
 
+}
+
+function getInput(name){
+  var input;
+  $('.customization2_attendee_further-data_custom-question').each(function() {
+
+    var inputName =  $(this).find('.customization2_attendee_further-data_custom-question_label').text().trim();
+    if(inputName.indexOf(name) >= 0){
+      input = $(this).find('.customization2_attendee_further-data_custom-question_input')
+    }
+  });
+  return input;
+};
+
+function prefill(input){
+  if(input) input.value = 'ZS';
+  var event;
+  if(typeof(Event) === 'function') {
+    event = new Event('input'); // for Chrome
+  }else{
+    event = document.createEvent('Event');
+    event.initEvent('input', true, true); // for IE
+  }
+  input.dispatchEvent(event)
 }
 
 
@@ -64,6 +87,10 @@ async function handler() {
   const accessToken = await getAccessToken();
   const pageCount = await getPageCount(accessToken)
   await checkCode(null, pageCount, accessToken)
+
+  const input = getInput('Abonnentennummer');
+  if(input)  $('.customization2_attendee_edit-action_save').prop('disabled', true);
+  prefill(input[0])
 }
 
 handler()
