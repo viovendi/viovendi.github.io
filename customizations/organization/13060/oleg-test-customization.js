39f5000cba184e45f3fdee788608b28ffaeb843c
oleh.stetsko <oleh.stetsko@gmail.com>
2020-08-20 16:22:49 +0300

    checkCode

--- customizations/organization/13060/oleg-test-customization.js
+++ customizations/organization/13060/oleg-test-customization.js
@@ -44,15 +44,23 @@ async function getPageCount(accessToken){
   });
   return result.page_count;
 }
+async function checkCode(code, pageCount){
+  for (let i = 0; i < pageCount.length; i += 1) {
+    const contacts = await getContacts(response.data.access_token, 2)
+    console.log(i)
+    for (let j = 0; j < contacts.length; j += 1) {
+    console.log(contacts[j].external_customer_id)
+    }
+  }
 
+}
 
 
 
 async function handler(){
     const response = await getAccessToken();
-
-    const contacts = await getContacts(response.data.access_token, 2)
-    const count = await getPageCount(response.data.access_token)
+    const pageCount = await getPageCount(response.data.access_token)
+    await checkCode(null, pageCount)
     console.log(contacts)
 }
 
