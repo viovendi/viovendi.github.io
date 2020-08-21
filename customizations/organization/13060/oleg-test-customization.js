cd0c5069679c1279ee2873a979e5bc15a27fea0c
oleh.stetsko <oleh.stetsko@gmail.com>
2020-08-21 12:30:31 +0300

    test

--- customizations/organization/13060/oleg-test-customization.js
+++ customizations/organization/13060/oleg-test-customization.js
@@ -98,7 +98,7 @@ async function checkCode() {
         continue;
       }
       console.log(contacts[j].external_customer_id.indexOf(code))
-      if (contacts[j].external_customer_id.indexOf(code) >= 0) {
+      if (contacts[j].external_customer_id == code) {
         console.log(contacts[j].external_customer_id)
         $('.customization2_attendee_edit-action_save').prop('disabled', false);
         $('.code-message').text('')
