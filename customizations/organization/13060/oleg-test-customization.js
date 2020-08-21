fd4c5be8d412da192fb05f72c336adc5c7473706
oleh.stetsko <oleh.stetsko@gmail.com>
2020-08-21 12:10:38 +0300

    test

--- customizations/organization/13060/oleg-test-customization.js
+++ customizations/organization/13060/oleg-test-customization.js
@@ -95,7 +95,7 @@ async function checkCode() {
     const contacts = await getContacts(accessToken, page)
     for (let j = 0; j < contacts.length; j += 1) {
       console.log(contacts[j].external_customer_id)
-      console.log(contacts[j].external_customer_id.includes(code))
+      console.log(code)
       if (contacts[j].external_customer_id && contacts[j].external_customer_id.includes(code)) {
         $('.customization2_attendee_edit-action_save').prop('disabled', false);
         return false;
