29173fdddd094802dd2ba4ddb400516aa6fdfe29
vbodend <69503064+vbodend@users.noreply.github.com>
2020-09-21 13:58:07 +0200

    Update onlineprint2021secondpage.js

--- customizations/organization/11581/onlineprint2021secondpage.js
+++ customizations/organization/11581/onlineprint2021secondpage.js
@@ -1,1 +1,56 @@
-.
+
+  console.log('Start working, Google Tag Manager Second Page');
+
+  function translateTicketCategory(categoryName, translation, description){
+    $(".customization2_attendee").each(function(i, element) {
+      var attendee = $(this);
+      var ticketCategoryName = attendee.find(".customization2_attendee_title")
+       var ticketDescription = attendee.find(".customization-category-description")
+      if(ticketCategoryName.text().trim().indexOf(categoryName) == 0){
+        ticketCategoryName.text(translation)
+        ticketDescription.text(description)
+      }
+    });   
+  }
+
+
+  
+
+  /*
+ //
+ // Translate Ticket Category
+ //
+ */
+
+  function translateAll(){
+
+
+
+translateTicketCategory('Frühbucher - Anmeldung zum Praxisforum Kunststoffrezyklate 2021', 'Early Bird Ticket - Registration for Praxisforum Kunststoffrezyklate 2021', 'The Early Bird Ticket can be booked until 09.02.2021.')
+translateTicketCategory('Anmeldung zum Praxisforum Kunststoffrezyklate 2021', 'Registration for Praxisforum Kunststoffrezyklate 2021', '')
+translateTicketCategory('Frühbucher Abonnententicket - Anmeldung zum Praxisforum Kunststoffrezyklate 2021', 'Early Bird Subscriber Price - Registration for Praxisforum Kunststoffrezyklate 2021', '10% discount for subscribers of "Kunststoffe" or "Kunststoffe international". The Early Bird Ticket can be booked until 09.02.2021.')
+ }
+
+
+ 
+
+  translateAll();
+
+
+
+
+  var mutationObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if(mutation){
+        if(mutation){
+          console.log('mutation')
+          setTimeout(function(){
+            callAll();
+          }, 500);
+
+        }
+      };
+    });
+  });
+
+ 
