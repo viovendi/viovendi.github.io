f02707d501377bd788d9ee9195ba0c923f826489
vbodend <69503064+vbodend@users.noreply.github.com>
2020-11-11 09:48:03 +0100

    Update dropdown-hide-questions.js

--- customizations/organization/14176/dropdown-hide-questions.js
+++ customizations/organization/14176/dropdown-hide-questions.js
@@ -15,7 +15,6 @@ function handler(){
                 if ( state == "andere") {
                     $('.customization2_attendee_further-data_custom-question-4').css("display","block");
                     var input = inputPosition.val().replace(/^\s+|\s+$/g, "")
-                    console.log(input)
                       if ( input == "") {
                           $('.customization2_attendee_edit-action_save').attr('disabled', true);
                       }
@@ -34,7 +33,6 @@ function handler(){
         
         inputPosition.change(function(){
                 var input = inputPosition.val().replace(/^\s+|\s+$/g, "")
-                console.log(input)
                   if ( input == "") {
                       $('.customization2_attendee_edit-action_save').attr('disabled', true);
                   }
@@ -64,7 +62,6 @@ function handler(){
         if ( state == "andere") {
             $('.customization2_attendee_further-data_custom-question-5').css("display","block");
             var input = inputPosition.val().replace(/^\s+|\s+$/g, "")
-                  console.log(input)
                     if ( input == "") {
                         $('.customization2_attendee_edit-action_save').attr('disabled', true);
                     }
@@ -82,7 +79,6 @@ function handler(){
     
     inputPosition.change(function(){
                 var input = inputPosition.val().replace(/^\s+|\s+$/g, "")
-                console.log(input)
                   if ( input == "") {
                       $('.customization2_attendee_edit-action_save').attr('disabled', true);
                   }
@@ -103,7 +99,6 @@ function handler(){
         if ( state == "andere") {
             $('.customization2_attendee_further-data_custom-question-7').css("display","block")
                 var input = inputFunktion.val().replace(/^\s+|\s+$/g, "")
-                console.log(input)
                   if ( input == "") {
                     $('.customization2_attendee_edit-action_save').attr('disabled', true);
                   }
@@ -120,7 +115,6 @@ function handler(){
     
     inputFunktion.change(function(){
                 var input = inputFunktion.val().replace(/^\s+|\s+$/g, "")
-                console.log(input)
                   if ( input == "") {
                      $('.customization2_attendee_edit-action_save').attr('disabled', true);
                   }
@@ -136,99 +130,8 @@ function handler(){
       
  }
 
-
-
-
-/*
-//fuction to get value of input field
-function getValue(shortName, wrapperClass){
- var value;
- $('.'+wrapperClass+' .customization2_attendee_further-data .vv-nl-mb-lg').each(function(i, element) {
-   var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
-    
-   if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) > -1){
-     value = $(this).closest('.vv-nl-mb-lg').find('.customization2_attendee_further-data_custom-question_input').val().replace(/^\s+|\s+$/g, "");
-     console.log("Value of " + shortName +": " +value);
-
-     // Disable Button if input-field is empty
-     if (value != ''){
-              console.log("Value of " + shortName +": " +value);
-              console.log("disable == false");
-            $('.customization2_attendee_edit-action_save').attr('disabled', false);
-
-           }
-     //enable button if input field is NOT empty
-           else {
-              console.log("Value of " + shortName +": " +value);
-              console.log("disable == true");
-              $('.customization2_attendee_edit-action_save').attr('disabled', true);
-           }
-   }
- });
-    
-}
-//function to get value of Dropdown Question
-function getStatePosition(shortName, wrapperClass){
-  var state;
- $('.'+wrapperClass+' .customization2_attendee_further-data .vv-nl-mb-lg').each(function(i, element) {
-   var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
-
-   if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) > -1){
-     state = $(this).closest('.vv-nl-mb-lg').find('.customization2_attendee_further-data_custom-question_dropdown').val().replace(/^\s+|\s+$/g, "");
-     
-     // if in dropdown "andere" is selected check the value of the input field
-           if (state == 'andere'){
-             console.log("State Position is: " +state)
-             getValue('Welche Position haben Sie?', 'customization2_attendee-1');  
-           }
-     // else (so if anything other than "andere" is selected , enable the button
-           else {
-              console.log("State Position is: " +state)
-              console.log("disable == false");
-              $('.customization2_attendee_edit-action_save').attr('disabled', false);
-           }
-     }
-
-   
- });
-}
-
-function getStateFunktion(shortName, wrapperClass){
-  var state;
- $('.'+wrapperClass+' .customization2_attendee_further-data .vv-nl-mb-lg').each(function(i, element) {
-   var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
-
-   if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) > -1){
-     state = $(this).closest('.vv-nl-mb-lg').find('.customization2_attendee_further-data_custom-question_dropdown').val();
-           if (state == 'andere'){
-             console.log("State Funktion: "+state)
-             getValue('Welche Funktion haben Sie?', 'customization2_attendee-1');  
-           }
-           else {
-              console.log("State Funktion: "+state)
-              console.log("disable == false");
-              $('.customization2_attendee_edit-action_save').attr('disabled', false);
-           }
-     }
-
-   
- });
-}
-
-
-
-function init2(name){
-  
-  $('.'+name+' .customization2_attendee_further-data_custom-question').on('change', function(){
-    getStatePosition('Position im Unternehmen', name);
-    getStateFunktion('Funktion im Unternehmen', name);
-
-   
-  });
-  
-}
-init2('customization2_attendee-1');*/
-  
+//    $('SOMECLASS').after('<span class="button-error-message error-text error-text--multiple">Bitte tragen Sie etwas in das Feld ein</span>');
+//    $('.button-error-message').remove();
 
   handler();
   document.addEventListener("animationstart", insertionListener, false); // standard + firefox
