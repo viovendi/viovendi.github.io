e35cc0c3e79008c083bd8dfb02f9c07a16021f50
vbodend <69503064+vbodend@users.noreply.github.com>
2020-11-23 16:04:18 +0100

    Update ewe.js

--- customizations/organization/14010/ewe.js
+++ customizations/organization/14010/ewe.js
@@ -7,11 +7,8 @@
        
       if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) > -1){
           
-          console.log("short name is found (in if)")
-        $('.customization2_attendee_further-data_custom-question_checkbox-line_checkbox .vv-checkbox').each(function(){
-            $(this).trigger('click');
-            console.log("vv-checkbox");
-        });
+        console.log("short name is found (in if)")
+       
         $('.check-boxes-wrapper .customization2_attendee_further-data_custom-question_checkbox-group .vv-checkbox').each(function(){
             $(this).trigger('click');
             console.log("click");
