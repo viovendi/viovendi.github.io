49b60962c7a76d0d9d73befffbb3e1fe8eadbd35
vbodend <69503064+vbodend@users.noreply.github.com>
2020-11-23 14:11:20 +0100

    Update ewe.js

--- customizations/organization/14010/ewe.js
+++ customizations/organization/14010/ewe.js
@@ -1,1 +1,56 @@
-TEst
+function getAttendee(){
+    $('.customization-attendee-questions').each(function(i, element) {
+      var attendee = $(this);
+      var checkbox = attendee.find('input[type="checkbox"]')
+      var inputs = attendee.find('vv-question-text');
+      var labels = attendee.find('customization2_attendee_further-data_custom-question_label');
+      console.log(inputs)
+      console.log(labels)
+      checkbox.change(function(){
+ 
+        if(checkbox.is(':checked')){
+          //showQuestionText('Firma/Company',  inputs);
+          console.log('checked');
+        }else{
+          //hideQuestionText('Firma/Company', inputs);
+          console.log("not checked")  
+ 
+        }
+      });
+    });
+  }
+ 
+  getAttendee();
+ 
+ 
+  function hideQuestionText(shortName, inputs){
+    $(inputs).each(function(i, element) {
+      var item = $(this).find('label').text().replace(/^\s+|\s+$/g, "");
+ 
+      if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) == 0){
+        $(this).find('.ew-billing-information__field').css( "display", "none" );
+      }
+    });
+  }
+ 
+  function hideImputs(shortName){
+    $('vv-question-text').each(function(i, element) {
+      var item = $(this).find('label').text().replace(/^\s+|\s+$/g, "");
+ 
+      if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) == 0){
+        $(this).find('.ew-billing-information__field').css( "display", "none" );
+      }
+    });
+  }
+ 
+  function showQuestionText(shortName, inputs){
+    $(inputs).each(function(i, element) {
+      var item = $(this).find('label').text().replace(/^\s+|\s+$/g, "");
+ 
+      if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) == 0){
+        $(this).find('.ew-billing-information__field').css( "display", "block" );
+      }
+    });
+  }
+ 
+  hideImputs('Einwilligungserklärung kostenlos')
