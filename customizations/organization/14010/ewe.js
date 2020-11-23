7a89b530704bb7869fa7d9bd626e9f505717569f
vbodend <69503064+vbodend@users.noreply.github.com>
2020-11-23 15:54:22 +0100

    Update ewe.js

--- customizations/organization/14010/ewe.js
+++ customizations/organization/14010/ewe.js
@@ -12,6 +12,10 @@
       if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) > -1){
           
           console.log("shortname passt")
+        $('.vv-checkbox__input .customization2_attendee_further-data_custom-question_checkbox-line_checkbox').each(function(){
+            $(this).trigger('click');
+            console.log("vv-checkbox");
+        });
         $('.check-boxes-wrapper .customization2_attendee_further-data_custom-question_checkbox-group .vv-checkbox').each(function(){
             $(this).trigger('click');
             console.log("click");
@@ -42,24 +46,7 @@ markAsChecked('Einwilligungserklärung','check-boxes-wrapper','customization2_at
  var insertionListener = function(event) {
     if (event.animationName === "nodeInserted") {
       console.log("Node has been inserted: ", event.target);
-function markAsChecked(shortName, className, wrapperClass){
-    $('.'+wrapperClass+' .customization2_attendee_further-data .vv-nl-mb-lg').each(function(i, element) {
-      var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
-      console.log(item);
-       $('.check-boxes-wrapper .customization2_attendee_further-data_custom-question_checkbox-group .vv-checkbox').each(function(){
-            $(this).trigger('click');
-            console.log("click");
-        });
-      if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) > -1){
-          
-          console.log("shortname passt")
-        $('.check-boxes-wrapper .customization2_attendee_further-data_custom-question_checkbox-group .vv-checkbox').each(function(){
-            $(this).trigger('click');
-            console.log("click");
-        });
-      }
-    });
-  }
+
       markAsChecked('Einwilligungserklärung','check-boxes-wrapper','customization2_attendee-1')
 
 
