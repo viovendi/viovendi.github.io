f926b923c8d931b0f4cd8b8dd23ec7f06808f339
vbodend <69503064+vbodend@users.noreply.github.com>
2020-11-23 14:32:46 +0100

    Update ewe.js

--- customizations/organization/14010/ewe.js
+++ customizations/organization/14010/ewe.js
@@ -6,14 +6,12 @@ function getAttendee(){
       var labels = attendee.find('customization2_attendee_further-data_custom-question_label');
       var labels2 = attendee.find('vv-control-label').text().replace(/^\s+|\s+$/g, "");
 
-      var text = labels.find('vv-control-label vv-control-label--regular customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "")
-      var text2 =labels.find('customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "")
+      
       hideQuestionText('Einwilligungserklärung',labels)
         
       console.log(labels)
+      console.log(inputs)
       console.log(labels2)
-      console.log(text)
-      console.log(text2)
 
       checkbox.change(function(){
  
@@ -35,6 +33,7 @@ function getAttendee(){
   function hideQuestionText(shortName, inputs){
     $(inputs).each(function(i, element) {
       var item = $(this).find('label').text().replace(/^\s+|\s+$/g, "");
+        console.log(item);
  
       if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) == 0){
         $(this).find('.customization2_attendee_further-data_custom-question_label').css( "display", "none" );
