4d004fd399066ae41fb06efe62cdf77831b7356e
vbodend <69503064+vbodend@users.noreply.github.com>
2020-11-23 16:01:41 +0100

    Update ewe.js

--- customizations/organization/14010/ewe.js
+++ customizations/organization/14010/ewe.js
@@ -4,15 +4,11 @@
    function markAsChecked(shortName, className, wrapperClass){
     $('.'+wrapperClass+' .customization2_attendee_further-data .vv-nl-mb-lg').each(function(i, element) {
       var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
-      console.log(item);
-       $('.check-boxes-wrapper .customization2_attendee_further-data_custom-question_checkbox-group .vv-checkbox').each(function(){
-            $(this).trigger('click');
-            console.log("click");
-        });
+       
       if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) > -1){
           
-          console.log("shortname passt")
-        $('.vv-checkbox__input .customization2_attendee_further-data_custom-question_checkbox-line_checkbox').each(function(){
+          console.log("short name is found (in if)")
+        $('.customization2_attendee_further-data_custom-question_checkbox-line_checkbox .vv-checkbox').each(function(){
             $(this).trigger('click');
             console.log("vv-checkbox");
         });
@@ -26,13 +22,12 @@
 markAsChecked('Einwilligungserklärung','check-boxes-wrapper','customization2_attendee-1')
 
 
-
+//funktion to hide Label of question
   function hide(tag, name){
    $(tag).each(function (i, element) {
      var item = $(this);
      var label = item.find("label");
      if(label.text().replace(/^\s+|\s+$/g, "").indexOf(name) == 0){
-       console.log("displayNone");
        $(item).find('.customization2_attendee_further-data_custom-question_label').css( "display", "none" );
      }
       
