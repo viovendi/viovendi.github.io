38acb996951f9cfc10f22943a834e0026e9a8752
vbodend <69503064+vbodend@users.noreply.github.com>
2020-11-24 10:20:41 +0100

    Update ewe.js

--- customizations/organization/14010/ewe.js
+++ customizations/organization/14010/ewe.js
@@ -36,8 +36,10 @@ function init(name){
   markAsChecked(name);
  //Add Text after Checkbox
   $('.'+name+' .check-boxes-wrapper .vv-checkbox__label--md').after('<span class="text-after-ewe" style="font-size:12px"> Der Verwendung meiner Kontaktdaten für werbliche Zwecke gem. § 7 Abs. 3 UWG kann ich jederzeit widersprechen. Eine einfache Mitteilung über unsere <a href="https://contact.vogel.de/ target="_blank"">Support-Seite</a> genügt.</span>');
-//add link to checkboxtext
- $('.'+name+' .customization2_attendee_further-data_custom-question_checkbox-line_label').wrap('<a href="https://privacy.vogel.de/" target="_blank"/>');
+
+ 
+ //add link to checkboxtext
+ $('.'+name+' customization2_attendee_further-data_custom-question_checkbox-line_label').wrap('<a href="https://privacy.vogel.de/" target="_blank"/>');
 }
 
 init('customization2_attendee-1');
