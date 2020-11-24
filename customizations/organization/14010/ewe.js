4bf04803bb07a20cb0566a100603d489006829b8
vbodend <69503064+vbodend@users.noreply.github.com>
2020-11-24 11:04:29 +0100

    Update ewe.js

--- customizations/organization/14010/ewe.js
+++ customizations/organization/14010/ewe.js
@@ -36,7 +36,7 @@ function init(name){
   addClassToField('Einwilligungserklärung', 'check-boxes-wrapper', name);
   markAsChecked(name);
  //Add Text after Checkbox
-  $('.'+name+' .check-boxes-wrapper .vv-checkbox__label--md').after('<span class="text-after-ewe"> Der Verwendung meiner Kontaktdaten für werbliche Zwecke gem. § 7 Abs. 3 UWG kann ich jederzeit widersprechen. Eine einfache Mitteilung über unsere <a href="https://contact.vogel.de/ target="_blank"">Support-Seite</a> genügt.</span>');
+  $('.'+name+' .check-boxes-wrapper .vv-checkbox__label--md').after('<span class="text-after-ewe"> Der Verwendung meiner Kontaktdaten für werbliche Zwecke gem. § 7 Abs. 3 UWG kann ich jederzeit widersprechen. Eine einfache Mitteilung über unsere <a href="https://contact.vogel.de/" target="_blank">Support-Seite</a> genügt.</span>');
 
  
  //add link to checkboxtext
@@ -47,7 +47,6 @@ function init(name){
 }
 
 init('customization2_attendee-1');
-$('.customization2_booking-terms_list').after('<span class="text-after-ewe"> Bitte beachten Sie: Mit Ihrer Registrierung zu dieser Veranstaltung erhalten Sie zusätzlich den kostenlosen redaktionellen Newsletter, den Sie jederzeit über den Abmelde-Link im Newsletter abbestellen können.</span>');
 
 
 var insertionListener = function(event) {
