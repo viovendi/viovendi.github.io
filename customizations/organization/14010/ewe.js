ea28d68a574d63dd3c1d60dd2461ab34c7821f78
vbodend <69503064+vbodend@users.noreply.github.com>
2021-01-12 09:19:09 +0100

    Update ewe.js

--- customizations/organization/14010/ewe.js
+++ customizations/organization/14010/ewe.js
@@ -77,7 +77,7 @@ function init(name){
       $('.'+name+' .customization2_attendee_further-data_custom-question_checkbox-line_label').addClass('with-links');
     }
   }
-  if(name == 'customization2_booker'){
+  else{
     if( !$('.'+name+' .customization2_booker_further-data_custom-question_checkbox-line_label').hasClass('with-links')){
       var textString = $('.'+name+' .customization2_booker_further-data_custom-question_checkbox-line_label').html();
       var res = textString.replace("Einwilligungserklärung", "<a target='_blank' href='https://privacy.vogel.de/'>Einwilligungserklärung</a>");
