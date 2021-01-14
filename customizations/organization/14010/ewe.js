8c20c96b6dcd1da68a6c4b438db0b797269cbdb4
oleh.stetsko <oleh.stetsko@gmail.com>
2021-01-14 21:51:33 +0200

    test

--- customizations/organization/14010/ewe.js
+++ customizations/organization/14010/ewe.js
@@ -95,7 +95,10 @@ function init(name){
 
 init('customization2_attendee');
 init('customization2_booker');
-$('.customization2_booking-terms_list').after('<span class="text-after-ewe"> Bitte beachten Sie: Mit Ihrer Registrierung zu dieser Veranstaltung erhalten Sie zusätzlich den kostenlosen redaktionellen Newsletter, den Sie jederzeit über den Abmelde-Link im Newsletter abbestellen können.</span>');
+if(!$('.text-after-ewe').length){
+  $('.customization2_booking-terms_list').after('<span class="text-after-ewe"> Bitte beachten Sie: Mit Ihrer Registrierung zu dieser Veranstaltung erhalten Sie zusätzlich den kostenlosen redaktionellen Newsletter, den Sie jederzeit über den Abmelde-Link im Newsletter abbestellen können.</span>');
+}
+
 
 /*
 var insertionListener = function(event) {
