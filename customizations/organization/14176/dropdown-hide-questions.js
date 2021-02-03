00fbd0cfafd059a2d4c098ccdf6bfe30350ae8a2
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-02-03 14:26:25 +0200

    fix for new  ticket category

--- customizations/organization/14176/dropdown-hide-questions.js
+++ customizations/organization/14176/dropdown-hide-questions.js
@@ -51,12 +51,12 @@ function handler(){
         
       }
   
-  else{
+  else if(name == "Ich bin ein Student / Trainee im IT-Umfeld" ) {
 
   
      //Hide "Position im Unternehmen" and "Funktion im Unternehmen"
      
-     $('.customization2_attendee_further-data_custom-question-5').css("display","none") //position
+     // $('.customization2_attendee_further-data_custom-question-5').css("display","none") //position
      $('.customization2_attendee_further-data_custom-question-7').css("display","none") //Funkton
 
     
@@ -147,7 +147,103 @@ function handler(){
                         )
     
     
-    }
+    } else{
+
+  
+      //Hide "Position im Unternehmen" and "Funktion im Unternehmen"
+      
+      $('.customization2_attendee_further-data_custom-question-5').css("display","none") //position
+      $('.customization2_attendee_further-data_custom-question-7').css("display","none") //Funkton
+ 
+     
+      var ddPosition = $('.customization2_attendee_further-data_custom-question-4 .customization2_attendee_further-data_custom-question_dropdown')
+      var inputPosition = $('.customization2_attendee_further-data_custom-question-5 .customization2_attendee_further-data_custom-question_input')
+ 
+      ddPosition.change(function(){
+          var state = ddPosition.val().replace(/^\s+|\s+$/g, "")
+         if ( state == "andere") {
+             $('.customization2_attendee_further-data_custom-question-5').css("display","block");
+             $('.customization2_attendee_further-data_custom-question-5').find('vv-optional-text').css("display","none");
+             inputPosition.after('<span class="input-error-message error-text error-text--multiple">Bitte tragen Sie etwas in das Feld ein</span>');
+ 
+ 
+             var input = inputPosition.val().replace(/^\s+|\s+$/g, "")
+                     if ( input == "") {
+                         $('.customization2_attendee_edit-action_save').attr('disabled', true);
+                     }
+                     else {    
+                         $('.customization2_attendee_edit-action_save').attr('disabled', false);
+                         $('.input-error-message').remove();
+                     }
+         }
+         else {    
+             $('.customization2_attendee_further-data_custom-question-5').css("display","none");
+         }
+        
+ 
+      }
+                       )
+     
+     inputPosition.change(function(){
+                 var input = inputPosition.val().replace(/^\s+|\s+$/g, "")
+                   if ( input == "") {
+                       $('.customization2_attendee_edit-action_save').attr('disabled', true);
+                       inputPosition.after('<span class="input-error-message error-text error-text--multiple">Bitte tragen Sie etwas in das Feld ein</span>');
+ 
+                   }
+                   else {    
+                       $('.customization2_attendee_edit-action_save').attr('disabled', false);
+                       $('.input-error-message').remove();
+                   }
+ 
+            }
+                             )
+          
+                        
+      var ddFunktion = $('.customization2_attendee_further-data_custom-question-6 .customization2_attendee_further-data_custom-question_dropdown')
+      var inputFunktion = $('.customization2_attendee_further-data_custom-question-7 .customization2_attendee_further-data_custom-question_input')
+ 
+                        
+      ddFunktion.change(function(){
+          var state = ddFunktion.val().replace(/^\s+|\s+$/g, "")
+         if ( state == "andere") {
+             $('.customization2_attendee_further-data_custom-question-7').css("display","block")
+             $('.customization2_attendee_further-data_custom-question-7').find('vv-optional-text').css("display","none");
+             inputFunktion.after('<span class="input-error-message error-text error-text--multiple">Bitte tragen Sie etwas in das Feld ein</span>');
+ 
+                 var input = inputFunktion.val().replace(/^\s+|\s+$/g, "")
+                   if ( input == "") {
+                     $('.customization2_attendee_edit-action_save').attr('disabled', true);
+                   }
+                   else {    
+                     $('.customization2_attendee_edit-action_save').attr('disabled', false);
+                     $('.input-error-message').remove();
+                   }
+ 
+         }
+         else {  
+             $('.customization2_attendee_further-data_custom-question-7').css("display","none")
+         }
+      }
+                       )
+     
+     inputFunktion.change(function(){
+                 var input = inputFunktion.val().replace(/^\s+|\s+$/g, "")
+                   if ( input == "") {
+                      $('.customization2_attendee_edit-action_save').attr('disabled', true);
+                      inputFunktion.after('<span class="input-error-message error-text error-text--multiple">Bitte tragen Sie etwas in das Feld ein</span>');
+ 
+                   }
+                   else {    
+                       $('.customization2_attendee_edit-action_save').attr('disabled', false);
+                       $('.input-error-message').remove();
+                   }
+ 
+            }    
+                         )
+     
+     
+     }
       $('customization2_attendee_view-action_edit').on( "click", function() {
   console.log( "clicked" );
 });
