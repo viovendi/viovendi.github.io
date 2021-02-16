7a6a96db301cfb3de26254ca0871712ea73ad9de
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-02-16 15:06:45 +0200

    fix

--- customizations/organization/14176/einwilligungserklaerung.js
+++ customizations/organization/14176/einwilligungserklaerung.js
@@ -96,10 +96,28 @@ var insertionListener = function(event) {
 }
 
 
+
 document.addEventListener("animationstart", insertionListener, false); // standard + firefox
 document.addEventListener("MSAnimationStart", insertionListener, false); // IE
 document.addEventListener("webkitAnimationStart", insertionListener, false); // 
 */
 
+//funktion to hide Label of question
+function hide(tag, name){
+  $(tag).each(function (i, element) {
+    var item = $(this);
+    var label = item.find("label");
+     
+    if(label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1){
+      console.log(label.text())
+      $(item).find('.customization2_attendee_further-data_custom-question_label').css( "display", "none" );
+      $(item).find('.customization2_booker_further-data_custom-question_label').css( "display", "none" );
+    }
+     
+  });             
+ }
+
+ hide(".customization2_attendee_further-data_custom-question","Einwilligungserklärung");
+ hide(".customization2_booker_further-data_custom-question","Einwilligungserklärung");
 
 
