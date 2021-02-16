78ea6601ff8b2b0c3b979cd06f7af76605e8b9aa
vbodend <69503064+vbodend@users.noreply.github.com>
2021-02-16 10:12:52 +0100

    Update einwilligungserklaerung.js

--- customizations/organization/14176/einwilligungserklaerung.js
+++ customizations/organization/14176/einwilligungserklaerung.js
@@ -1,5 +1,7 @@
+
+
 /*
-function addClassToField2(shortName, className, wrapperClass){
+function addClassToField(shortName, className, wrapperClass){
  $('.'+wrapperClass+' .customization2_attendee_further-data .vv-nl-mb-lg').each(function(i, element) {
    var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
    if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) > -1){
@@ -9,9 +11,29 @@ function addClassToField2(shortName, className, wrapperClass){
 }
 
 
+//funktion to hide Label of question
+  function hide(tag, name){
+   $(tag).each(function (i, element) {
+     var item = $(this);
+     var label = item.find("label");
+      
+     if(label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1){
+       $(item).find('.customization2_attendee_further-data_custom-question_label').css( "display", "none" );
+       $(item).find('.customization2_booker_further-data_custom-question_label').css( "display", "none" );
+     }
+      
+   });             
+  }
+
+  hide(".customization2_attendee_further-data_custom-question","Einwilligungserklärung");
+  hide(".customization2_booker_further-data_custom-question","Einwilligungserklärung");
+
+
+
  
-function init2(name){
-  addClassToField2('Einwilligungserklärung', 'check-boxes-wrapper', name);
+function init(name){
+  addClassToField('Einwilligungserklärung', 'check-boxes-wrapper', name);
+  
 
   
   if(name == 'customization2_attendee'){
@@ -42,15 +64,17 @@ function init2(name){
 init2('customization2_attendee');
 init2('customization2_booker');
 
-*/
 
+*/
 
 /*
 var insertionListener = function(event) {
  if (event.animationName === "nodeInserted") {
   console.log("noteInserted test");
-      init2('customization2_booker');
-      init2('customization2_attendee');
+      hide(".customization2_attendee_further-data_custom-question","Einwilligungserklärung");
+      hide(".customization2_booker_further-data_custom-question","Einwilligungserklärung");
+      init('customization2_booker');
+      init('customization2_attendee');
       console.log(!$('.customization2_attendee .customization2_attendee_further-data_custom-question_checkbox-line_label').hasClass('with-links'))
 
    var classNames = event.target.classList;
