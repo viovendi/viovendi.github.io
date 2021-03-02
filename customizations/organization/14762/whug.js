f72a8c947dbb36d5013d0e678bee40a167fa30c7
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-02 13:54:28 +0100

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -1,8 +1,9 @@
 console.log('Start working, Google Tag Manager (WHUG)');
     
 
-  var checked = 0;
+  
   function calcCheckboxes(name){
+    var checked = 0;
     $('.'+name+' .customization2_attendee_further-data_product').each(function(i, element) {
       var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
       var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
@@ -19,6 +20,9 @@ console.log('Start working, Google Tag Manager (WHUG)');
 function init(name){
  $('.'+name+' .customization2_attendee_further-data_product_checkbox').on('change', function(){
    console.log("letsgo");
+   if (calcCheckboxes(name) == 0 ){
+   console.log("calc is 0");
+   }
    handler();
  });
 }
