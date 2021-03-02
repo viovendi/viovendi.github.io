11e2f06e23756b2f51080ac446514ea947505c61
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-02 11:06:45 +0100

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -16,17 +16,15 @@ console.log('Start working, Google Tag Manager (WHUG)');
   }
   
   
-   function init(name){
-     $('.'+name+' .customization2_attendee_further-data_product_checkbox').on('change', function(){
-       console.log("letsgo");
-     });
-   }
-   init('customization2_attendee');
+function init(name){
+ $('.'+name+' .customization2_attendee_further-data_product_checkbox').on('change', function(){
+   console.log("letsgo");
+   handler();
+ });
+}
+init('customization2_attendee');
   
-  
-  
-  
-/*-------------------*/
+
 function disableProd(disable){
  $('.customization2_attendee_further-data_product').each(function(i, element) {
        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
@@ -36,7 +34,8 @@ function disableProd(disable){
           }
  });
 }
-  
+
+
 function uncheckProd(disable){
  $('.customization2_attendee_further-data_product').each(function(i, element) {
        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
@@ -49,13 +48,20 @@ function uncheckProd(disable){
   
   function hideProduct(shortName){
   $('.customization2_attendee_further-data_product').each(function(i, element) {
-      var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+      var checkbox = $(this).find('.customization2_attendee_further-data_product_name')
       var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
           if(checkboxName.indexOf(shortName) >= 0){
-             console.log("hide product");
-             $(this).find('.customization2_attendee_further-data_product_name').css("display", "none");
-             $(this).css("display", "none");
-             //checkbox.prop( "display", "none" )
+             checkbox.css("display", "none");
+          }
+ });
+  }
+
+ function showProduct(shortName){
+  $('.customization2_attendee_further-data_product').each(function(i, element) {
+      var checkbox = $(this).find('.customization2_attendee_further-data_product_name')
+      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+          if(checkboxName.indexOf(shortName) >= 0){
+             checkbox.css("display", "block");
           }
  });
   }
@@ -78,18 +84,17 @@ function enableProd(disable){
       checkbox.change(function(){
         if(checkbox.is(':checked')){
           if(checkboxName.indexOf(shortName) >= 0){
-             disableProd(disable)
+             hideProduct(disable)
           }
         }else if(!checkbox.is(':checked')){
           if(checkboxName.indexOf(shortName) >= 0){
-             enableProd(disable)
+             showProduct(disable)
           }
           }
       })
     })
   }
-  
-  
+ 
     function prodcheck(shortName, disable){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
       var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
@@ -126,10 +131,29 @@ function handler(){
     //get Ticket name:
   var ticketname = $('.customization2_attendee_title')
   var name = ticketname.text().replace(/^\s+|\s+$/g, "")
+  
+  //pseudo code
+  /*
+  if any day ticket is selected:
+   by clicking one day (eg. Monday), hide all other day
+   show only the workshops that are on the selected day
+   #
+   first hide all workshops
+    
+  */
   if (name == "Day Ticket Adult" ){
-    console.log("ticketname is:");
+    hideProduct("Workshop Montag");
+    hideProduct("Workshop Dienstag");
     hideProduct("Workshop Mittwoch");
+    hideProduct("Workshop Donnerstag");
+      
+      prodCheked("Montag", "Dienstag" )
+      prodCheked("Montag", "Mittwoch" )
+      prodCheked("Montag", "Donnerstag" )
+    
    }
+    
+    
   
 
   
@@ -139,12 +163,10 @@ function handler(){
 
   /*---------------*/
 
-    prodCheked("Kreatives Zeichnen", "Song- und Poesiewerkstatt" )
     
 
     prodcheck("Kreatives Zeichnen", "Filmpräsentation" )
     
-    prodCheked("Song- und Poesiewerkstatt", "Kreatives Zeichnen" )
     
 
 }
