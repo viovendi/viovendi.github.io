ff607b3502d73c1a4b32d870195b4424fc7d51bc
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-02 15:47:06 +0100

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -105,6 +105,27 @@ function showProdWhenChecked(shortName, show){
     })
   }
 
+function disableProd(disable){
+    $('.customization2_attendee_further-data_product').each(function(i, element) {
+          var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+         var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+             if(checkboxName.indexOf(disable) == -1){
+                checkbox.prop( "disabled", true )
+             }
+    });
+   }
+
+
+   function enableProd(disable){
+   $('.customization2_attendee_further-data_product').each(function(i, element) {
+         var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+        var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+            if(checkboxName){
+               checkbox.prop( "disabled", false )
+            }
+   });
+  }
+
 
  
     function prodcheck(shortName, disable){
@@ -134,8 +155,27 @@ function showProdWhenChecked(shortName, show){
           }
         }
   }
-                                                        )}
-  
+                                                        )};
+
+  function markAsChecked(name){
+       $('.customization2_attendee_further-data_product').each(function(i, element) {
+      var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+       if(checkboxName.indexOf(shortName) >= 0){
+             checkbox.trigger('click');
+          }
+   });
+  }
+                                                               
+   function disableProd(disable){
+    $('.customization2_attendee_further-data_product').each(function(i, element) {
+          var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+         var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+             if(checkboxName.indexOf(disable) == -1){
+                checkbox.prop( "disabled", true )
+             }
+    });
+   }
   
   
 function handler(){
@@ -197,6 +237,15 @@ function handler(){
     if (name == "All Day Ticket" ){
         
         //preselect all days
+       markaschecked("Montag");
+       markaschecked("Dienstag");
+       markaschecked("Mittwoch");
+       markaschecked("Donnerstag");
+        // disable all day
+       disableProd("Montag");
+       disableProd("Dienstag");
+       disableProd("Mittwoch");
+       disableProd("Donnerstag");
     }
     
 
