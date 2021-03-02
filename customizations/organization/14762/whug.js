ce1ba8d037e616b60ba316d1f909efba7117ad07
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-02 14:58:30 +0100

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -1,22 +1,5 @@
 console.log('Start working, Google Tag Manager (WHUG)');
-    
 
-  
-  function calcCheckboxes(name){
-    var checked = 0;
-    $('.'+name+' .customization2_attendee_further-data_product').each(function(i, element) {
-      var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-     
-         if(checkbox.is(':checked')){
-          checked = checked + 1;
-          };
-      
-    });
-     return checked;
-  }
-  
-  
 function init(name){
  $('.'+name+' .customization2_attendee_further-data_product_checkbox').on('change', function(){
       handler();
@@ -24,10 +7,7 @@ function init(name){
  });
 }
 init('customization2_attendee');
-hideProduct("Workshop 1");
-hideProduct("Workshop 2");
-hideProduct("Workshop 3");
-hideProduct("Workshop 4");
+
 handler();
 
 
@@ -174,7 +154,10 @@ function handler(){
     
   */
   if (name == "Day Ticket Adult" ){
-      
+    hideProduct("Workshop 1");
+    hideProduct("Workshop 2");
+    hideProduct("Workshop 3");
+    hideProduct("Workshop 4");
     hideProdWhenChecked("Montag", "Dienstag" )
     hideProdWhenChecked("Montag", "Mittwoch" )
     hideProdWhenChecked("Montag", "Donnerstag" )
@@ -211,8 +194,10 @@ function handler(){
        
    }
     
-    
-  
+    if (name == "All Day Ticket" ){
+        
+        //preselect all days
+    }
     
 
     
