d21afb5785dc7f5e60af6317bd286c96966df104
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-02 13:11:53 +0100

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -25,16 +25,6 @@ function init(name){
 init('customization2_attendee');
   
 
-function disableProd(disable){
- $('.customization2_attendee_further-data_product').each(function(i, element) {
-       var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-          if(checkboxName.indexOf(disable) >= 0){
-             checkbox.prop( "disabled", true )
-          }
- });
-}
-
 
 function uncheckProd(disable){
  $('.customization2_attendee_further-data_product').each(function(i, element) {
@@ -66,15 +56,7 @@ function uncheckProd(disable){
  });
   }
    
-function enableProd(disable){
- $('.customization2_attendee_further-data_product').each(function(i, element) {
-       var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-          if(checkboxName.indexOf(disable) >= 0){
-             checkbox.prop( "disabled", false )
-          }
- });
-}
+
    
   function hideProdWhenChecked(shortName, hide){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
@@ -165,14 +147,15 @@ function handler(){
     
   */
   if (name == "Day Ticket Adult" ){
-    
+    hideProduct("Workshop Montag");
+    hideProduct("Workshop Dienstag");
+    hideProduct("Workshop Mittwoch");
+    hideProduct("Workshop Donnerstag");
       
     hideProdWhenChecked("Montag", "Dienstag" )
     hideProdWhenChecked("Montag", "Mittwoch" )
     hideProdWhenChecked("Montag", "Donnerstag" )
-    hideProdWhenChecked("Montag", "Workshop Dienstag" )
-    hideProdWhenChecked("Montag", "Workshop Mittwoch" )
-    hideProdWhenChecked("Montag", "Workshop Donnerstag" )
+  
     showProdWhenChecked("Montag", "Workshop Montag");
    }
     
