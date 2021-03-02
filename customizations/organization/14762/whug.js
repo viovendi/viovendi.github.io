e23f5548394054603c848ba4a038c7707b082f57
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-02 14:47:40 +0100

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -32,7 +32,17 @@ handler();
 
 
 
-
+function ischecked(shortname){
+  $('.customization2_attendee_further-data_product').each(function(i, element) {
+      var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+      if(checkboxName.indexOf(shortName) >= 0){
+        if(checkbox.is(':checked')){
+            return 1;
+        }
+         else return 0;
+      }
+}
   
   function hideProduct(shortName){
   $('.customization2_attendee_further-data_product').each(function(i, element) {
@@ -187,7 +197,7 @@ function handler(){
     
    }
     
-    if (calcCheckboxes(name) == 0 ){
+    if (ischecked("Montag") == 0 && ischecked("Dienstag") == 0 && ischecked("Mittwoch") == 0 && ischecked("Donnerstag) == 0 && ){
         console.log("hide 4 products");
         hideProduct("Workshop 1");
         hideProduct("Workshop 2");
