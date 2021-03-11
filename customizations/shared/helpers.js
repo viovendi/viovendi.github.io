2aa54bd78f124581592f3ff3f51937a95741abc5
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-03-11 14:39:28 +0200

    innerText

--- customizations/shared/helpers.js
+++ customizations/shared/helpers.js
@@ -5,7 +5,7 @@ function myHelpers() {
       this.lable = lable;
       const field = document.querySelectorAll(this.cssClass);
       field.forEach((field) => {
-        console.log(field.querySelectorAll('label p').innerText.trim())
+        console.log(field.querySelectorAll('label p').innerText)
       });
       return this
     }
