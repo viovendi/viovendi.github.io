93ac3f95fa24f924f340e3f8a2fc35d77412e3f2
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-03-11 12:50:37 +0200

    myHelpers

---
+++ customizations/shared/helpers.js
@@ -0,0 +1,17 @@
+function myHelpers() {
+
+    this.findField = function(cssClass, lable) {
+      this.cssClass = cssClass;
+      this.lable = lable;
+      const field = document.querySelectorAll('cssClass');
+      field.forEach((field) => {
+       console.log(field)
+      });
+      return this
+    }
+    return this
+  }
+  
+  hp = new myHelpers()
+  
+  
