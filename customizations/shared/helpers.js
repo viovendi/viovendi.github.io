cff8c09a7a16ba3a923a97f4d71c335bbcf8b1a7
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-03-11 15:37:24 +0200

    field

--- customizations/shared/helpers.js
+++ customizations/shared/helpers.js
@@ -3,13 +3,13 @@ function myHelpers() {
     this.findField = function(cssClass, lable) {
       this.cssClass = cssClass;
       this.lable = lable;
-      const fields= document.querySelectorAll(this.cssClass);
+      const fields = document.querySelectorAll(this.cssClass);
       fields.forEach((field) => {
         if( field.querySelectorAll('label p')[0].innerText.trim().includes(this.lable) ){
-          console.log(this)
-          return field;
+          this.field = field;
         }
       });
+      return this.field;
     }
      this.hide = function (elem) {
       console.log(elem)
