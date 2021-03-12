edf83b837291843e0dcc4ce10739c3ba165d99f4
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-03-12 13:20:44 +0200

    myHelpers Update

--- customizations/shared/helpers.js
+++ customizations/shared/helpers.js
@@ -1,20 +1,21 @@
-function myHelpers() {
-    this.findField = function(cssClass, lable) {
-      this.cssClass = cssClass;
+function myHelpers(elements) {
+    this.findField = function(lable) {
       this.lable = lable;
       this.field = null;
-      const fields = document.querySelectorAll(this.cssClass);
-      fields.forEach((field) => {
+
+      elements.forEach((field) => {
         if( field.querySelectorAll('label p')[0].innerText.trim().includes(this.lable) ){
           this.field = field;
         }
       });
       return this.field;
     }
-     this.hide = function (elem) {
-      elem.style.display = 'none';
-    };
     return this
   }
+
+  function $$(selector){
+    const elements = document.querySelectorAll(selector);
+		return new myHelpers(elements);
+	}
   
   
