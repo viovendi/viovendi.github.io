52639141c9756a1a272590a05399885566e80bba
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-03-25 18:52:46 +0200

    test

--- customizations/organization/14842/customization-solutions.js
+++ customizations/organization/14842/customization-solutions.js
@@ -33,13 +33,11 @@ async function makeRequest(options) {
   getEventInfo(eventId);
 
 
-  function disableProd(disable){
+  function disableProd(){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
           var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-         var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-             if(checkboxName.indexOf(disable) >= 0){
+         //var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
                 checkbox.prop( "disabled", true )
-             }
     });
    }
       
@@ -47,7 +45,7 @@ async function makeRequest(options) {
     $('.customization2_attendee_further-data_product').each(function(i, element) {
           var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
          var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-             if(checkboxName.indexOf(disable) >= 0){
+             if(checkboxName.includes(disable)){
                 checkbox.prop( "disabled", false )
              }
     });
@@ -61,14 +59,13 @@ async function makeRequest(options) {
  
       checkbox.change(function(){
         if(checkbox.is(':checked')){
-            console.log($(this).parent().find('p').text())
-        //   if(checkboxName.indexOf(shortName) >= 0){
-        //      disableProd(disable)
-        //   }
-        // }else if(!checkbox.is(':checked')){
-        //   if(checkboxName.indexOf(shortName) >= 0){
-        //      enableProd(disable)
-        //   }
+            var shortName = $(this).parent().find('p').text().trim();
+          if(enable){
+             disableProd()
+             enableProd(shortName)
+          }
+        }else if(!checkbox.is(':checked')){
+             enableProd(checkboxName)
   }
       })
     })
