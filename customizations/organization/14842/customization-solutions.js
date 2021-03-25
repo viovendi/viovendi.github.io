04f0fea1eaf68c6804185af44696a3766d8c06ba
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-03-25 18:31:53 +0200

    test

--- customizations/organization/14842/customization-solutions.js
+++ customizations/organization/14842/customization-solutions.js
@@ -31,3 +31,49 @@ async function makeRequest(options) {
   }
 
   getEventInfo(eventId);
+
+
+  function disableProd(disable){
+    $('.customization2_attendee_further-data_product').each(function(i, element) {
+          var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+         var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+             if(checkboxName.indexOf(disable) >= 0){
+                checkbox.prop( "disabled", true )
+             }
+    });
+   }
+      
+   function enableProd(disable){
+    $('.customization2_attendee_further-data_product').each(function(i, element) {
+          var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+         var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+             if(checkboxName.indexOf(disable) >= 0){
+                checkbox.prop( "disabled", false )
+             }
+    });
+   }
+
+
+  function prodCheked(){
+    $('.customization2_attendee_further-data_product').each(function(i, element) {
+      var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+ 
+      checkbox.change(function(){
+        if(checkbox.is(':checked')){
+            console.log($(this))
+        //   if(checkboxName.indexOf(shortName) >= 0){
+        //      disableProd(disable)
+        //   }
+        // }else if(!checkbox.is(':checked')){
+        //   if(checkboxName.indexOf(shortName) >= 0){
+        //      enableProd(disable)
+        //   }
+  }
+      })
+    })
+  }
+
+
+
+  prodCheked()
