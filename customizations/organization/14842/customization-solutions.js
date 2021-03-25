56383c93c92c1923ae5916296d0d4eb1228b7fb8
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-03-25 18:58:19 +0200

    test

--- customizations/organization/14842/customization-solutions.js
+++ customizations/organization/14842/customization-solutions.js
@@ -33,11 +33,10 @@ async function makeRequest(options) {
   getEventInfo(eventId);
 
 
-  function disableProd(){
+  function disableProd(disabled){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
           var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-         //var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-                checkbox.prop( "disabled", true )
+                checkbox.prop( "disabled", disabled )
     });
    }
       
@@ -61,11 +60,11 @@ async function makeRequest(options) {
         if(checkbox.is(':checked')){
             var shortName = $(this).parent().find('p').text().trim();
           if(shortName){
-             disableProd()
+             disableProd(true)
              enableProd(shortName)
           }
         }else if(!checkbox.is(':checked')){
-             enableProd(checkboxName)
+            disableProd(false)
   }
       })
     })
