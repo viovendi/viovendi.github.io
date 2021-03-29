9ebfbff1dcc5a5821bb7f43ced791790fe227d43
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-29 14:53:33 +0200

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -205,8 +205,7 @@ handler();
   }
    
 
-   // this hides or shows another product when 
-checked or unchecked
+   // this hides or shows another product when checked or unchecked
   function hideProdWhenChecked(shortName, hide){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
       var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
