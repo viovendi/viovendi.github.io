2712cd06aa76e1f81c5d300f4113c2a7123a9b9c
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-29 15:20:06 +0200

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -1,169 +1,6 @@
 console.log('Start working, Google Tag Manager (WHUG)');
 
 
-/*
-/------- Code from Oleg ------ 
-
-
-async function makeRequest(options) {
-    let result = null;
-    try {
-        result = await $.ajax(options);
-        return result;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-
-var path = window.location.pathname;
-var eventId = path.split('/')[3]
-
-async function getEventInfo(eventId) {
-    const result = await makeRequest({
-        url: `https://api.doo.net/v1/events/${eventId}`,
-        type: 'get',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        dataType: 'json',
-    })
-    //console.log({"ticket_categories": result.ticket_categories, "attributes": result.event_attributes.attributes})
-    return {
-        "ticket_categories": result.ticket_categories,
-        "attributes": result.event_attributes.attributes
-    };
-}
-
-
-function disableProd(disabled) {
-    $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-        checkbox.prop("disabled", disabled)
-    });
-}
-
-function disableProdByName(shortName) {
-    $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-        var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-        if (checkboxName.includes(shortName)) {
-            checkbox.prop("disabled", true)
-        }
-    });
-}
-
-function enableProd(disable) {
-    $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-        var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-        if (checkboxName.includes(disable)) {
-            checkbox.prop("disabled", false)
-        }
-    });
-}
-
-function chekedAllProd() {
-    $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-        checkbox.prop('checked', true).prop("disabled", true)
-    })
-}
-
-function prodCheked() {
-    $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-
-        checkbox.change(function () {
-            if (checkbox.is(':checked')) {
-                var shortName = $(this).parent().find('p').text().trim();
-                if (shortName) {
-                    disableProd(true)
-                    enableProd(shortName)
-                }
-            } else if (!checkbox.is(':checked')) {
-                disableProd(false)
-            }
-        })
-    })
-}
-
-function getTicketAttributeId(ticketTitle, ticketCategories) {
-    const result = $.grep(ticketCategories, function (v) {
-        return v.name === ticketTitle;
-    });
-    return result[0].event_attribute_ids[0];
-}
-
-
-function getEventAttributeName(attributeId, eventAttributes) {
-    const result = $.grep(eventAttributes, function (v) {
-        return v.id === attributeId;
-    });
-    return result[0].name;
-}
-
-function hideProduct(shortName){
-      $('.customization2_attendee_further-data_product').each(function(i, element) {
-          var checkbox = $(this).find('.customization2_attendee_further-data_product_name')
-          var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-              if(checkboxName.indexOf(shortName) >= 0){
-                 $(this).css(display = "none");
-              }
-     });
- }
-
-function showProduct(shortName){
-      $('.customization2_attendee_further-data_product').each(function(i, element) {
-          var checkbox = $(this).find('.customization2_attendee_further-data_product_name')
-          var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-              if(checkboxName.indexOf(shortName) >= 0){
-                 $(this).css(display = "block");
-              }
-     });
- }
-
- 
-
-
-async function customization() {
-    const ticketTitle = $('.customization2_attendee-state_edit .customization2_attendee_title').text().trim()
-    console.log("ticketTitle: " +ticketTitle)
-    const eventInfo = await getEventInfo(eventId);
-    console.log("eventInfo: " +eventInfo);
-    const attributeId = getTicketAttributeId(ticketTitle, eventInfo.ticket_categories)
-    const attributeName = getEventAttributeName(attributeId, eventInfo.attributes);
-    console.log("attributeName: " +attributeName)
-    if (attributeName === 'Montag') {
-        console.log('Montag');
-        prodCheked()
-    } else if (attributeName === 'Dienstag') {
-    console.log(attributeName)
-        console.log('Customization2Dinestag');
-        chekedAllProd()
-    }
-
-    
-    
-}
-    
-
-customization()
-
-var insertionListener = function (event) {
-    if (event.animationName === "nodeInserted") {
-        console.log('bookerNodeInserted')
-        customization()
-    }
-}
-
-
-document.addEventListener("animationstart", insertionListener, false);
-document.addEventListener("MSAnimationStart", insertionListener, false);
-document.addEventListener("webkitAnimationStart", insertionListener, false);
-
-//----------- End Code from Oleg ------
-*/
 
 // call handler whenever a checkbox is clicked/changed
 function init(name){
@@ -352,7 +189,7 @@ function handler(){
    by clicking one day (eg. Monday), hide all other day
    show only the workshops that are on the selected day
    */
-  console.log(name);
+
    
  
     
@@ -375,15 +212,15 @@ function handler(){
     hideProdWhenChecked("Donnerstag", "Dienstag" )
     hideProdWhenChecked("Donnerstag", "Mittwoch" )
       
-   console.log("ischecked montag:  "+ischecked("Montag"))
+   console.log("is checked montag:  "+ischecked("Montag"))
     if(ischecked("Montag")){
-      console.log("showws1");
+      console.log("show ws1");
       showProduct("Workshop 1");
       
     }
-    if( !ischecked("Montag")){
+    else if(!ischecked("Montag")){
      hideProduct("Workshop 1");
-     console.log("hide");
+     console.log("hide ws1");
     }
    
     if(ischecked("Dienstag")){
