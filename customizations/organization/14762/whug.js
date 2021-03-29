1d7b792de0b4d7566c2695b6d81136685bbd94f2
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-29 15:25:48 +0200

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -141,10 +141,11 @@ function showProdWhenChecked(shortName, show){
 
 
 function ischecked(shortName){
+ var bool = false;
    $('.customization2_attendee_further-data_product').each(function(i, element) {
       var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
       var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-      var bool = false;
+      
       
         if(checkbox.is(':checked')){
             console.log("checkbox is checked + name:" + checkboxName)
