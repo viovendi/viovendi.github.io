377cacee4ca23c46fe1c7aca0e9f02801dfd5dce
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-29 11:28:17 +0200

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -126,17 +126,17 @@ function showProduct(shortName){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
       var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
       var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
- 
-      checkbox.change(function(){
-        if(checkbox.is(':checked')){
-          if(checkboxName.indexOf(shortName) >= 0){
-             hideProduct(hide)
-          }
-        }else if(!checkbox.is(':checked')){
-          if(checkboxName.indexOf(shortName) >= 0){
-             showProduct(hide);
-          }
-          }
+        console.log("hidewhenchecked");
+      
+            if(checkbox.is(':checked')){
+              if(checkboxName.indexOf(shortName) >= 0){
+                 hideProduct(hide)
+              }
+            }else if(!checkbox.is(':checked')){
+              if(checkboxName.indexOf(shortName) >= 0){
+                 showProduct(hide);
+              }
+          
       })
     })
   }
@@ -159,6 +159,7 @@ async function customization() {
         console.log('Customization2Dinestag');
         chekedAllProd()
     }*/
+    console.log("cust");
     hideProdWhenChecked("Montag", "Dienstag" )
     hideProdWhenChecked("Montag", "Mittwoch" )
     hideProdWhenChecked("Montag", "Donnerstag" )
