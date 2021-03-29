6b4313923559dc06bc8271abcd0f394753f4a983
vbodend <69503064+vbodend@users.noreply.github.com>
2021-03-29 10:49:49 +0200

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -96,20 +96,21 @@ function getEventAttributeName(attributeId, eventAttributes) {
 
 async function customization() {
     const ticketTitle = $('.customization2_attendee-state_edit .customization2_attendee_title').text().trim()
-    console.log(ticketTitle)
+    console.log("ticketTitle:" +ticketTitle)
     const eventInfo = await getEventInfo(eventId);
-    console.log(eventInfo);
+    
+    console.log("eventInfo:" +eventInfo);
     //const attributeId = getTicketAttributeId(ticketTitle, eventInfo.ticket_categories)
-    //const attributeName = getEventAttributeName(attributeId, eventInfo.attributes);
-    //console.log(attributeName)
-    /*if (attributeName === 'Customization1') {
-        console.log('Customization1');
-        prodCheked()
-    } else if (attributeName === 'Customization2') {
+    const attributeName = getEventAttributeName(attributeId, eventInfo.attributes);
     console.log(attributeName)
-        console.log('Customization2');
+    if (attributeName === 'Montag') {
+        console.log('Montag');
+        prodCheked()
+    } else if (attributeName === 'Dienstag') {
+    console.log(attributeName)
+        console.log('Customization2Dinestag');
         chekedAllProd()
-    }*/
+    }
 }
 
 customization()
