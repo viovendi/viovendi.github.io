d1f8e1ce5fa36d2de5ab2dba93503ed97451699c
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-06 12:02:54 +0200

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -60,6 +60,21 @@ function hideProdWhenChecked(shortName, hide) {
   })
 }
 
+function uncheckProduct2whenProduct1Isunchecked(product1, product2){
+  $('.customization2_attendee_further-data_product').each(function (i, element) {
+    var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+    var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+
+    checkbox.change(function () {
+      if (!checkbox.is(':checked')) {
+        if (checkboxName.includes(product1)) {
+          uncheckProd(product2)
+        }
+      } 
+    })
+  })
+}
+
 function hideProd(shortName, hide) {
   $('.customization2_attendee_further-data_product').each(function (i, element) {
     var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
@@ -128,8 +143,8 @@ function showProdWhenChecked(shortName, show) {
  */
 
 
-/*
-function recheckProd(shortName){
+
+function uncheckProd(shortName){
 $('.customization2_attendee_further-data_product').each(function(i, element) {
      var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
@@ -137,12 +152,13 @@ $('.customization2_attendee_further-data_product').each(function(i, element) {
      if(checkbox.is(':checked')){
          if(checkboxName.indexOf(shortName) >= 0){
             checkbox.trigger('click');
-            checkbox.trigger('click');
          }
        }
  }
                                                       )};
-*/
+
+
+
 
 function markAsChecked(name) {
   $('.customization2_attendee_further-data_product').each(function (i, element) {
@@ -232,9 +248,13 @@ function handler() {
       hideProduct("Workshop 1");
       hideProduct("Workshop 1.1");
       hideProduct("Workshop 1.2");
-      markAsUnChecked("Workshop 1");
-      markAsUnChecked("Workshop 1.1");
-      markAsUnChecked("Workshop 1.2");
+      uncheckProduct2whenProduct1Isunchecked("Workshop 1");
+      uncheckProduct2whenProduct1Isunchecked("Workshop 1.1");
+      uncheckProduct2whenProduct1Isunchecked("Workshop 1.2");
+
+      //markAsUnChecked("Workshop 1");
+      //markAsUnChecked("Workshop 1.1");
+      //markAsUnChecked("Workshop 1.2");
     }
 
     if (ischecked("Dienstag")) {
