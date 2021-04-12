15bfd0f3c8a7ddad1239d04f16b2cef761d37575
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-12 15:23:34 +0200

    Update doi.js

--- customizations/organization/13476/doi.js
+++ customizations/organization/13476/doi.js
@@ -4,9 +4,6 @@ console.log('Start working, Google Tag Manager (DOI)');
 
 // call handler whenever a checkbox is clicked/changed
 function init(name) {
-  $('.' + name + ' .customization2_attendee_further-data_product_checkbox').on('change', function () {
-    handler();
-  });
   $('.customization2_double-opt-in_checkbox').trigger('clicked');
 }
 
@@ -22,25 +19,16 @@ handler();
 
 
 function markAsChecked(name) {
-  $('.customization2_attendee_further-data_product').each(function (i, element) {
-    var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-    var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+  $('.customization2_opt-out-and-opt-in').each(function (i, element) {
+    var checkbox = $(this).find('.customization2_double-opt-in_checkbox')
+    var checkboxName = $(this).find('.customization2_double-opt-in_checkbox-text').text().trim()
     if (checkboxName.indexOf(name) >= 0) {
       checkbox.prop('checked', true);
     }
   });
 }
 
-function markAsUnChecked(name) {
-  $('.customization2_attendee_further-data_product').each(function (i, element) {
-    var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-    var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-    if (checkboxName.indexOf(name) >= 0) {
-      console.log("markasunchecked" + checkboxName);
-      checkbox.prop('checked', false);
-    }
-  });
-}
+
 
 
 
@@ -50,6 +38,7 @@ function handler() {
  console.log('clicked');
 
 $('.customization2_double-opt-in_checkbox').click();
+  markAsChecked("Double Opt in Text");
 
 }
 
@@ -58,6 +47,7 @@ var insertionListener = function (event) {
     console.log("Node has been inserted Git: ", event.target);
     //Insert your code here
     init('customization2_attendee');
+    handler();
    
  
   }
