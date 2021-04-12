e171480adeaece966d7414543ec5347fef65b857
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-12 09:14:02 +0200

    Update customization-solutions.js

--- customizations/organization/14842/customization-solutions.js
+++ customizations/organization/14842/customization-solutions.js
@@ -32,17 +32,21 @@ function calcCheckboxes(name){
 }
 
 function enabledDisabledButton(name){
+  const ticketTitle = $('.customization2_attendee-state_edit .customization2_attendee_title').text().trim()
+  if (ticketTitle != "Tortenwettbewerb") {
+  console.log(ticketTitle)
  if(calcCheckboxes(name) < 1 ){
    $('.'+name+' .customization2_attendee_edit-action_save').attr('disabled', true);
    if( $('.'+name+' .customization2_attendee_edit-action .button-error-message').length < 1 ){     
      
-    $('.'+name+' .customization2_attendee-state_edit .customization2_attendee_further-data_product-1').before('<span class="button-error-message error-text error-text--multiple">Bitte wählen Sie einen Termin an dem Sie die Messe besuchen möchten aus.</span>');
+    $('.'+name+' .customization2_attendee_further-data_product-1').before('<span class="button-error-message error-text error-text--multiple">Bitte wählen Sie einen Termin an dem Sie die Messe besuchen möchten aus.</span>');
     //$('.'+name+' .customization2_attendee_further-data_headline').after('<span class="button-error-message error-text error-text--multiple">Bitte wählen Sie einen Termin an dem Sie die Messe besuchen möchten aus.</span>');
    }
  }else{
    $('.'+name+' .customization2_attendee_edit-action_save').attr('disabled', false);
    $('.'+name+' .button-error-message').remove();
  }
+    }
 }
    
 /* --------- Olegs Code begins -------- */
@@ -152,7 +156,6 @@ function getEventAttributeName(attributeId, eventAttributes) {
 
 async function customization() {
     const ticketTitle = $('.customization2_attendee-state_edit .customization2_attendee_title').text().trim()
-    console.log(ticketTitle)
     const eventInfo = await getEventInfo(eventId);
     const attributeId = getTicketAttributeId(ticketTitle, eventInfo.ticket_categories)
     const attributeName = getEventAttributeName(attributeId, eventInfo.attributes);
