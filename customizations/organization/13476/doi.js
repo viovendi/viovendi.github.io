0574f2ae18b3016a35b2592b4b374158e51b95e9
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-12 15:08:37 +0200

    Update and rename DOI.js to doi.js

---
+++ customizations/organization/13476/doi.js
@@ -0,0 +1,97 @@
+console.log('Start working, Google Tag Manager (WHUG)');
+
+
+
+// call handler whenever a checkbox is clicked/changed
+function init(name) {
+  $('.' + name + ' .customization2_attendee_further-data_product_checkbox').on('change', function () {
+    handler();
+  });
+}
+
+init('customization2_attendee');
+
+handler();
+
+
+
+
+
+
+
+function uncheckProduct2whenProduct1Isunchecked(product1, product2){
+  $('.customization2_attendee_further-data_product').each(function (i, element) {
+    var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+    var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+
+    checkbox.change(function () {
+      if (!checkbox.is(':checked')) {
+        if (checkboxName.includes(product1)) {
+          
+          uncheckProd(product2)
+        }
+      } 
+    })
+  })
+}
+
+function hideProd(shortName, hide) {
+  $('.customization2_attendee_further-data_product').each(function (i, element) {
+    var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+    var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+
+    if (checkbox.is(':checked')) {
+      if (checkboxName.includes(shortName)) {
+        hideProduct(hide)
+      }
+    } 
+  })
+}
+                                              )};
+
+
+
+
+function markAsChecked(name) {
+  $('.customization2_attendee_further-data_product').each(function (i, element) {
+    var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+    var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+    if (checkboxName.indexOf(name) >= 0) {
+      checkbox.prop('checked', true);
+    }
+  });
+}
+
+function markAsUnChecked(name) {
+  $('.customization2_attendee_further-data_product').each(function (i, element) {
+    var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+    var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+    if (checkboxName.indexOf(name) >= 0) {
+      console.log("markasunchecked" + checkboxName);
+      checkbox.prop('checked', false);
+    }
+  });
+}
+
+
+
+
+// handler is called every time a checkbox is "changed" 
+function handler() {
+
+
+}
+
+var insertionListener = function (event) {
+  if (event.animationName === "nodeInserted") {
+    console.log("Node has been inserted Git: ", event.target);
+    //Insert your code here
+    init('customization2_attendee');
+   
+ 
+  }
+};
+
+document.addEventListener("animationstart", insertionListener, false); // standard + firefox
+document.addEventListener("MSAnimationStart", insertionListener, false); // IE
+document.addEventListener("webkitAnimationStart", insertionListener, false); // 
