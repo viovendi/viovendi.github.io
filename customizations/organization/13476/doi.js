167cbd0d15ae5b14a4b0ea52d322dfe705cd0925
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-12 15:10:53 +0200

    Update doi.js

--- customizations/organization/13476/doi.js
+++ customizations/organization/13476/doi.js
@@ -19,37 +19,6 @@ handler();
 
 
 
-function uncheckProduct2whenProduct1Isunchecked(product1, product2){
-  $('.customization2_attendee_further-data_product').each(function (i, element) {
-    var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-    var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-
-    checkbox.change(function () {
-      if (!checkbox.is(':checked')) {
-        if (checkboxName.includes(product1)) {
-          
-          uncheckProd(product2)
-        }
-      } 
-    })
-  })
-}
-
-function hideProd(shortName, hide) {
-  $('.customization2_attendee_further-data_product').each(function (i, element) {
-    var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-    var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-
-    if (checkbox.is(':checked')) {
-      if (checkboxName.includes(shortName)) {
-        hideProduct(hide)
-      }
-    } 
-  })
-}
-                                              )};
-
-
 
 
 function markAsChecked(name) {
