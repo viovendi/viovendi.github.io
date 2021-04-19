3b9bff242b07c30c2649ccf6f85fc83c07295dff
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-19 09:05:48 +0200

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -217,7 +217,7 @@ function ischecked(shortName) {
 // handler is called every time a checkbox is "changed" 
 function handler() {
   
-    const name = $('.customization2_attendee-state_edit .customization2_attendee-1 .customization2_attendee_title').text().trim();
+    var name = $('.customization2_attendee-state_edit .customization2_attendee-1 .customization2_attendee_title').text().trim();
     console.log("moreattendes ticketname: " + name);
     if (!name.trim()) {
        name = $('.customization2_attendee-state_edit .customization2_attendee_title').text().trim();
