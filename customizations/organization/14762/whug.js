a41c62c60e4f8f264291af0010a509f173237e27
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-19 09:40:43 +0200

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -1,6 +1,6 @@
 console.log('Start working, Google Tag Manager (WHUG)');
 
-/*
+
 
 // call handler whenever a checkbox is clicked/changed
 function init(name) {
@@ -113,7 +113,7 @@ function hideProdWhenUnChecked(shortName, hide){
          }
       })
     })
-  
+  */
 
 function showProdWhenChecked(shortName, show) {
   $('.customization2_attendee_further-data_product').each(function (i, element) {
@@ -152,7 +152,7 @@ function showProdWhenChecked(shortName, show) {
    })
  }
  
-
+*/
 
 
 function uncheckProd(shortName){
@@ -234,7 +234,7 @@ function handler() {
    by clicking one day (eg. Monday), hide all other day
    show only the workshops that are on the selected day (on Monday its Workshop1)
   
-
+*/
 
   if (name == "Erwachsenen Ticket") {
     // hide other day when 1 day is selected
@@ -338,7 +338,7 @@ var insertionListener = function (event) {
     //Insert your code here
     init('customization2_attendee');
     handler()
-    /*
+    
     const name = $('.customization2_attendee-state_edit .customization2_attendee-1 .customization2_attendee_title').text().trim()
     console.log("const name: "+name);
 
@@ -424,206 +424,4 @@ var insertionListener = function (event) {
 document.addEventListener("animationstart", insertionListener, false); // standard + firefox
 document.addEventListener("MSAnimationStart", insertionListener, false); // IE
 document.addEventListener("webkitAnimationStart", insertionListener, false); // 
-*/
 
-console.log('Start working, Google Tag Manager (Load different GTM-Container into the widget for each event)');
-
-
-/*
-Code from https://github.com/viovendi/viovendi.github.io/blob/master/customizations/organization/14176/checkboxes.js
-Preview Link: https://doo.net/de-de/preview/widget.html?isPreview=true&configName=booking-14842-23230&eventId=71456&locale=de-de&organizerId=14842&widgetType=booking
-OID 14842
-
-
-var windowpath = window.location.pathname;
-var language = windowpath.split('/')[1]
-
-function init(name) {
-  enabledDisabledButton(name)
-  $('.' + name + ' .customization2_attendee_further-data_product_checkbox').on('change', function () {
-    enabledDisabledButton(name)
-  });
-}
-
-init('customization2_attendee');
-
-function calcCheckboxes(name){
-  var checked = 0;
-  $('.customization2_attendee_further-data_product').each(function (i, element) {
-    var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-    var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-
-    if ( checkbox.is(':checked') ) {
-      checked = checked + 1;
-    }
-  }
-                                                          )
-  return checked;
-}
-
-function enabledDisabledButton(name){
-  var ticketTitle = $('.customization2_attendee-state_edit .customization2_attendee_title').text().trim()
-  if (!(ticketTitle == "Dauerkarte" || ticketTitle == "Tortenwettbewerb" || ticketTitle == "Multi-day ticket" || ticketTitle == "Cake contest") ) {
-     if(calcCheckboxes(name) < 1 ){
-       $('.'+name+' .customization2_attendee_edit-action_save').attr('disabled', true);
-       if( $('.'+name+' .customization2_attendee_edit-action .button-error-message').length < 1 ){
-        console.log(language)
-         if (language == "en-us"){
-        $('.'+name+' .customization2_attendee_further-data_product-1').before('<span class="button-error-message error-text error-text--multiple">Please select a date</span>');
-        //$('.'+name+' .customization2_attendee_further-data_headline').after('<span class="button-error-message error-text error-text--multiple">Bitte wählen Sie einen Termin an dem Sie die Messe besuchen möchten aus.</span>');
-         }
-         else {
-           $('.'+name+' .customization2_attendee_further-data_product-1').before('<span class="button-error-message error-text error-text--multiple">Bitte wählen Sie einen Termin an dem Sie die Messe besuchen möchten aus.</span>');
-         }
-         // this removes the error message in state_view
-         $('.customization2_attendee-state_view .button-error-message').remove();
-
-       }
-     }else{
-       $('.'+name+' .customization2_attendee_edit-action_save').attr('disabled', false);
-       $('.'+name+' .button-error-message').remove();
-     }
-    }
-}
-*/
-
-/* --------- Olegs Code begins -------- */
-
-var path = window.location.pathname;
-var eventId = path.split('/')[3];
-var locale = path.split('/')[1];;
-//console.log(locale);
-
-async function makeRequest(options) {
-    let result = null;
-    try {
-        result = await $.ajax(options);
-        return result;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-
-
-
-async function getEventInfo(eventId,locale) {
-    const result = await makeRequest({
-        url: `https://api.doo.net/v1/events/${eventId}?locale=${locale}`,
-        type: 'get',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        dataType: 'json',
-    })
-    console.log({result})
-    return {
-        "ticket_categories": result.ticket_categories,
-        "attributes": result.event_attributes.attributes
-    };
-}
-
-
-function disableProd(disabled) {
-    $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-        checkbox.prop("disabled", disabled)
-    });
-}
-
-function enableProd(disable) {
-    $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-        var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-        if (checkboxName.includes(disable)) {
-            checkbox.prop("disabled", false)
-        }
-    });
-}
-
-function chekedAllProd() {
-    $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-        checkbox.prop('checked', true).prop("disabled", true)
-    })
-}
-
-function isCheked(){
-    $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-        if (checkbox.is(':checked')) {
-            var shortName = $(this).parent().find('p').text().trim();
-            if (shortName) {
-                disableProd(true)
-                enableProd(shortName)
-            }
-        }
-    });
-}
-
-function prodCheked() {
-    $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-
-        checkbox.change(function () {
-            if (checkbox.is(':checked')) {
-                var shortName = $(this).parent().find('p').text().trim();
-                if (shortName) {
-                    disableProd(true)
-                    enableProd(shortName)
-                }
-            } else if (!checkbox.is(':checked')) {
-                disableProd(false)
-            }
-        })
-    })
-}
-
-function getTicketAttributeId(ticketTitle, ticketCategories) {
-    const result = $.grep(ticketCategories, function (v) {
-        return v.name.includes(ticketTitle);
-    });
-    console.log(result)
-    return result[0].event_attribute_ids[0];
-}
-
-
-function getEventAttributeName(attributeId, eventAttributes) {
-    const result = $.grep(eventAttributes, function (v) {
-        return v.id === attributeId;
-    });
-    return result[0].name;
-}
-
-
-async function customization() {
-    const ticketTitle = $('.customization2_attendee-state_edit .customization2_attendee_title').text().trim()
-    const eventInfo = await getEventInfo(eventId, locale);
-    const attributeId = getTicketAttributeId(ticketTitle, eventInfo.ticket_categories)
-    const attributeName = getEventAttributeName(attributeId, eventInfo.attributes);
-    if (attributeName === 'Montag') {
-        console.log('Montag');
-        isCheked()
-        prodCheked()
-    } else if (attributeName === 'Dienstag') {
-        console.log('Dienstag');
-        chekedAllProd()
-    }
-}
-
-customization()
-
-var insertionListener = function (event) {
-    if (event.animationName === "nodeInserted") {
-        console.log('bookerNodeInserted')
-        customization()
-
-      // Added this so that this works for every attendee
-      //  init('customization2_attendee');
-    }
-}
-
-
-document.addEventListener("animationstart", insertionListener, false);
-document.addEventListener("MSAnimationStart", insertionListener, false);
-document.addEventListener("webkitAnimationStart", insertionListener, false);
