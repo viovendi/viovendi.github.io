b854121281f8667e4863de06b11b8a9b3aa40d18
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-19 08:35:50 +0200

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -42,8 +42,8 @@ function showProduct(shortName) {
 function disableProd(disable){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
           var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-         var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-             if(checkboxName.indexOf(disable) == -1){
+          var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+             if(checkboxName.indexOf(disable) == 1){
                 checkbox.prop( "disabled", true )
              }
     });
