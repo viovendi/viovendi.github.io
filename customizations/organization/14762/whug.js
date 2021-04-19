153a0707678de71527376708376188f2ed8e4155
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-19 09:04:36 +0200

    Update whug.js

--- customizations/organization/14762/whug.js
+++ customizations/organization/14762/whug.js
@@ -219,7 +219,7 @@ function handler() {
   
     const name = $('.customization2_attendee-state_edit .customization2_attendee-1 .customization2_attendee_title').text().trim();
     console.log("moreattendes ticketname: " + name);
-    if (!name.trim())) {
+    if (!name.trim()) {
        name = $('.customization2_attendee-state_edit .customization2_attendee_title').text().trim();
     console.log("one ticket ticketname: " +name);
   }
