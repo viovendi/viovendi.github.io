c9655bb2fb3685552d33bfab2913a6c018d2570e
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-20 09:42:59 +0200

    Update custom729.js

--- customizations/organization/13709/custom729.js
+++ customizations/organization/13709/custom729.js
@@ -6,8 +6,11 @@
     formValid = false;
     
     checkboxChecked();
-    hide("vv-additional-question-dropdown","25.03.2020");
-    hide("vv-additional-question-dropdown","26.03.2020");
+	hide("vv-additional-question-dropdown","23.06.");
+	hide("vv-additional-question-dropdown","24.06.");
+	  
+	hide("vv-additional-question-dropdown","25.03.2020");
+	hide("vv-additional-question-dropdown","26.03.2020");
   }
   handler();
  
