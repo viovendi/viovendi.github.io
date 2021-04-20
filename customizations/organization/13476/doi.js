37e8488c1eaafec3b7d9f8bae05bfa2f76bbef94
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-20 12:14:21 +0200

    Update doi.js

--- customizations/organization/13476/doi.js
+++ customizations/organization/13476/doi.js
@@ -28,10 +28,9 @@ markAsChecked("Double Opt in Text");
 
 
 //change Datasecurity text
-var obj = $(".customization2_booking-terms_text").text("Ich willige ein, dass die Wealthcap Kapitalverwaltungsgesellschaft mbH die von mir angegebenen personenbezogenen Daten zu Werbezwecken per E-Mail und/oder Briefpost nutzt, um mir Informationen zu Veranstaltungen, zum Unternehmen und zu weiteren Produkten und Angeboten der Wealthcap Kapitalverwaltungsgesellschaft mbH aus dem Bereich der Finanz- und Kapitalanlage sowie zur Immobilienwirtschaft zukommen zu lassen. \n \n Ich nehme zur Kenntnis, dass Wealthcap die Richtigkeit meiner E-Mail-Adresse durch Zusendung eines von mir zu bestätigenden Links überprüft. Nur durch diese Bestätigung schließe ich meine Anmeldung erfolgreich ab. \n " +'<a href="http://www.wealthcap.com/service/datenschutz" target= "_blank">Es gelten die Datenschutzinformationen von Wealthcap.</a>');
+var obj = $(".customization2_booking-terms_text").text("Ich willige ein, dass die Wealthcap Kapitalverwaltungsgesellschaft mbH die von mir angegebenen personenbezogenen Daten zu Werbezwecken per E-Mail und/oder Briefpost nutzt, um mir Informationen zu Veranstaltungen, zum Unternehmen und zu weiteren Produkten und Angeboten der Wealthcap Kapitalverwaltungsgesellschaft mbH aus dem Bereich der Finanz- und Kapitalanlage sowie zur Immobilienwirtschaft zukommen zu lassen. \n \n Ich nehme zur Kenntnis, dass Wealthcap die Richtigkeit meiner E-Mail-Adresse durch Zusendung eines von mir zu bestätigenden Links überprüft. Nur durch diese Bestätigung schließe ich meine Anmeldung erfolgreich ab.\n\n");
+obj.append($('<a/>').attr('href', 'http://www.wealthcap.com/service/datenschutz">Datenschutzunformationen').html("Es gelten die Datenschutzinformationen von Wealthcap.")).html();
 obj.html(obj.html().replace(/\n/g,'<br/>'));
-obj.append($('<a/>').attr('href', 'http://www.google.com">Google').html("some text")).html();
-//html('<a href="http://www.google.com">Google</a>');
 // .customization2_booking-terms_text
 $(".customization2_booking-terms_custom-term-1").hide();
 
