2b5d3777c2a88a8e1240d77a37964435c7dbda28
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-26 11:16:28 +0200

    Update translate.js

--- customizations/organization/14759/translate.js
+++ customizations/organization/14759/translate.js
@@ -1,4 +1,11 @@
 console.log("GTM Git loaded");
+
+// replace placeholder on first page:
 $('div:nth-child(1) vv-input__control-container').attr("placeholder", "newText");
   
+//Start Date
+$('ng-tns-c47-0 p-calendar').attr("placeholder", "newText");
+
+//End Date
+$('ng-tns-c47-1 p-calendar').attr("placeholder", "newText");
  
