db292ffd01b3175a844b4d65ff4d0ce9a721f455
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-26 16:01:45 +0200

    Update translate.js

--- customizations/organization/14759/translate.js
+++ customizations/organization/14759/translate.js
@@ -26,3 +26,83 @@ $('.ng-tns-c47-0').attr("placeholder", "Startdatum");
 //End Date
 $('.ng-tns-c47-1').attr("placeholder", "Slutdatum");
  
+//Organizer contact
+<h2 class="customization-event-info-contact-label">
+              Organizer contact
+            </h2>
+
+
+//99 available
+$('.')
+<p class="event-category__amount">
+        99 available
+      </p>
+
+//Next
+$('.customization-button-next').text('Nästa');
+
+
+//Encrypted connection
+$('.customization-encrypted-connection').text('Krypterad anslutning');
+
+//		Selection
+//$('.progress-point__label').text('');
+
+
+$('.ew-header__progress-container .progress-meter .selection span').text('Urval');
+$('.ew-header__progress-container .progress-meter .details span').text('Deltagarinformation');
+$('.ew-header__progress-container .progress-meter .information span').text('Bekräftelse');
+
+//ATTENDEE DETAILS // BOOKING DETAILS
+$('.customization2_attendees_title').text('BOKNINGSINFORMATION');
+    
+  
+//SUMMARY
+$('.customization2_summary_headline').text('SAMMANFATTNING');
+
+//Contact details
+$('.customization2_attendee_contact-data_headline').text('Contact details1');
+
+//First name
+$('.customization2_attendee_contact-data_first-name_label').text('Förnamn');
+
+
+//Required
+$('.customization2_attendee_contact-data_first-name_error').text('Obligatorisk');
+
+//Last name
+$('.customization2_attendee_contact-data_last-name_label').text('Efternamn');
+
+//   Required
+$('.customization2_attendee_contact-data_last-name_error').text('Obligatorisk');
+
+//Email address
+$('.customization2_attendee_contact-data_email_label').text('E-postadress');
+
+//Please enter valid email address
+$('.customization2_attendee_contact-data_email_error').text('Vänligen fyll i giltig e-postadress');
+
+//Confirm email address
+$('.customization2_attendee_contact-data_email-confirmation_label').text('Bekräfta e-postadress');
+
+//Please enter valid email address
+$('.customization2_attendee_contact-data_email-confirmation_error').text('Vänligen fyll i giltig e-postadress');
+
+//Accept
+$('.customization2_attendee_edit-action_save').text('Jag har fyllt i mina uppgifter');
+
+//I accept the following terms:
+$('.customization2_booking-terms_text').text('');
+
+
+//Email addresses not matching
+$('.customization2_attendee_contact-data_email-confirmation_error').text('E-postadresser stämmer inte överens');
+
+//Change selection
+$('.customization-button-previous').text('Tillbaka till urval');
+
+
+//Book now
+$('.customization-button-next').text('Boka nu');
+  
+
