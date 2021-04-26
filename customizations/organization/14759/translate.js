594a8dc1eb7ad095af6975d6cd6333e6c6ecc8fa
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-26 11:32:28 +0200

    Update translate.js

--- customizations/organization/14759/translate.js
+++ customizations/organization/14759/translate.js
@@ -1,15 +1,17 @@
 console.log("GTM Git loaded");
 
 // replace placeholder on first page:
-$("#event-list-widget > vv-events-search > vv-search > form > div.row.vv-mb-lg > div.col-md-8.col-sm-6 > div > div:nth-child(1) > vv-input > label > div > input").attr("placeholder", "newText");
+
+//
+$("#event-list-widget > vv-events-search > vv-search > form > div.row.vv-mb-lg > div.col-md-8.col-sm-6 > div > div:nth-child(1) > vv-input > label > div > input").attr("placeholder", "Sök efter nyckelord");
 
 //Dropdown
-$('vv-selection-input__value ng-star-inserted').text('some Text');
+$("#event-list-widget > vv-events-search > vv-search > form > div.row.vv-mb-lg > div.col-md-8.col-sm-6 > div > div:nth-child(2) > vv-selection > div > div.vv-selection__value-container > vv-selection-input > div > div > p").text('Välj varuhus');
 
 
 //Start Date
-$('.ng-tns-c47-0').attr("placeholder", "newText");
+$('.ng-tns-c47-0').attr("placeholder", "Startdatum");
 
 //End Date
-$('.ng-tns-c47-1').attr("placeholder", "newText");
+$('.ng-tns-c47-1').attr("placeholder", "Slutdatum");
  
