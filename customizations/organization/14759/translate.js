a26a2f875b1776f1603ee92fb972aed60b3623c1
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-26 11:27:17 +0200

    Update translate.js

--- customizations/organization/14759/translate.js
+++ customizations/organization/14759/translate.js
@@ -4,6 +4,10 @@ console.log("GTM Git loaded");
 $('#event-list-widget > vv-events-search > vv-search > form > div.row.vv-mb-lg > div.col-md-8.col-sm-6 > div > div:nth-child(1) > vv-input > label > div').attr("placeholder", "newText");
 $('#event-list-widget > vv-events-search > vv-search > form > div.row.vv-mb-lg > div.col-md-8.col-sm-6 > div > div:nth-child(1)').find('vv-input__control').attr("placeholder", "newText1");
 
+//Dropdown
+$('vv-selection-input__value ng-star-inserted').text('some Text');
+
+
 //Start Date
 $('.ng-tns-c47-0').attr("placeholder", "newText");
 
