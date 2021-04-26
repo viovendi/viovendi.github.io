59e48e65cd90297603418463e56082053b3f6590
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-26 11:22:43 +0200

    Update translate.js

--- customizations/organization/14759/translate.js
+++ customizations/organization/14759/translate.js
@@ -1,7 +1,7 @@
 console.log("GTM Git loaded");
 
 // replace placeholder on first page:
-$('.ng-untouched ng-pristine ng-valid').find('vv-input__control').attr("placeholder", "newText");
+$('ng-untouched ng-pristine ng-valid').find('vv-input__control').attr("placeholder", "newText");
   
 //Start Date
 $('.ng-tns-c47-0').attr("placeholder", "newText");
