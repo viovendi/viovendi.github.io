b063a5d2ac9bf83f2aa95eaa3970f19bc897c802
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-26 11:27:36 +0200

    Update translate.js

--- customizations/organization/14759/translate.js
+++ customizations/organization/14759/translate.js
@@ -1,8 +1,8 @@
 console.log("GTM Git loaded");
 
 // replace placeholder on first page:
-$('#event-list-widget > vv-events-search > vv-search > form > div.row.vv-mb-lg > div.col-md-8.col-sm-6 > div > div:nth-child(1) > vv-input > label > div').attr("placeholder", "newText");
-$('#event-list-widget > vv-events-search > vv-search > form > div.row.vv-mb-lg > div.col-md-8.col-sm-6 > div > div:nth-child(1)').find('vv-input__control').attr("placeholder", "newText1");
+$('event-list-widget > vv-events-search > vv-search > form > div.row.vv-mb-lg > div.col-md-8.col-sm-6 > div > div:nth-child(1) > vv-input > label > div').attr("placeholder", "newText");
+$('event-list-widget > vv-events-search > vv-search > form > div.row.vv-mb-lg > div.col-md-8.col-sm-6 > div > div:nth-child(1)').find('vv-input__control').attr("placeholder", "newText1");
 
 //Dropdown
 $('vv-selection-input__value ng-star-inserted').text('some Text');
