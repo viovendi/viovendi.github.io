851abecef6526bb4a6fc002d3d0a5e9d9f6ee010
vbodend <69503064+vbodend@users.noreply.github.com>
2021-04-27 12:12:11 +0200

    Update standard143.js

--- customizations/organization/14871/standard143.js
+++ customizations/organization/14871/standard143.js
@@ -93,10 +93,10 @@ console.log('Start working, Google Tag Manager');
   function hide(tag, date){
    $(tag).each(function(){
      	var item = $(this);
-	var label = item.find('customization2_attendee_further-data_custom-question_label').text();
+	var label = item.find('.customization2_attendee_further-data_custom-question_label').text();
 	console.log(label);
 
-     if(label[0].innerText.replace(/^\s+|\s+$/g, "").indexOf(date) != -1){
+     if(label.replace(/^\s+|\s+$/g, "").indexOf(date) != -1){
 	     console.log("display none");
        $(item[0]).css("display", "none");
      }
