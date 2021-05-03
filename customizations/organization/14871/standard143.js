b0b291ab79937ced2c173c1fff61672be8a0748f
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-03 10:19:01 +0200

    Update standard143.js

--- customizations/organization/14871/standard143.js
+++ customizations/organization/14871/standard143.js
@@ -2,6 +2,13 @@ console.log("GTM works");
 
 console.log('Start working, Google Tag Manager');
 
+
+  $(".customization2_attendee_edit-action_save").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".customization2_attendees_title").offset().top
+    }, 1000);
+});
+
   var formValid;
   function handler(){
     formValid = false;
