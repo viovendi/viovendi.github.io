a9532ef2ac99cd905cd2cd7d8199dc786c5b18eb
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-03 10:40:46 +0200

    Update standard143.js

--- customizations/organization/14871/standard143.js
+++ customizations/organization/14871/standard143.js
@@ -6,6 +6,10 @@ console.log('Start working, Google Tag Manager');
   $(".customization2_attendee_edit-action_save").click(function() {
 
 	  console.log("scroll");
+	  setTimeout(function(){
+		  console.log("scrolled");
+  			$('html, body').animate({ scrollTop: $('.customization2_attendees_title').offset().top}, 1000);
+			}, 2000);
 	  $('html, body').animate({ scrollTop: $('.customization2_attendees_title').offset().top}, 1000);
 	
 });
