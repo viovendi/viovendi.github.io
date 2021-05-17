70caeb53e79b117e2590cb9c00e6a4fccc03c0bc
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-17 12:08:04 +0200

    Update doi.js

--- customizations/organization/13476/doi.js
+++ customizations/organization/13476/doi.js
@@ -51,7 +51,7 @@ function prefill(){
     }
     console.log($('.customization2_booker_contact-data_country_input').val().length);
     if ($('.customization2_booker_contact-data_country_input').val().length === 0 ){
-      $(".customization2_booker_contact-data_country_input")[0].selectedIndex = 52;
+      $(".customization2_booker_contact-data_country_input")[0].selectedIndex = 49;
     }
   
   //set focus to first input
