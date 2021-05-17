bc8c10ee1df08beda745ccacadfb5cba9942a4de
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-17 16:41:39 +0200

    Update hidequestion.js

--- customizations/organization/13621/hidequestion.js
+++ customizations/organization/13621/hidequestion.js
@@ -1,1 +1,28 @@
-console.log('works, hide question');
+console.log('hide Eventinsight');
+
+//funktion to hide Label of question
+function hideQuestion(tag, name) {
+  $(tag).each(function (i, element) {
+    var item = $(this);
+    var label = item.find("label");
+
+    if (label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1) {
+      $(item).hide();    }
+  });
+}
+
+hideQuestion(".customization2_booker_further-data_custom-question", "WKZ-Nummer");
+
+var insertionListener = function(event) {
+ if (event.animationName === "nodeInserted") {
+  console.log("noteInserted Eventinsight");
+hideQuestion(".customization2_booker_further-data_custom-question", "WKZ-Nummer");
+     }
+   }
+ 
+
+document.addEventListener("animationstart", insertionListener, false); // standard + firefox
+document.addEventListener("MSAnimationStart", insertionListener, false); // IE
+document.addEventListener("webkitAnimationStart", insertionListener, false); // 
+
+
