8c011110d34b396b5dd26611285869632cdfe423
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-17 15:24:12 +0200

    Update hidequestion.js

--- customizations/organization/14010/hidequestion.js
+++ customizations/organization/14010/hidequestion.js
@@ -1,1 +1,16 @@
-console.log('works');
+console.log('hide Eventinsight');
+
+//funktion to hide Label of question
+function hideQuestion(tag, name) {
+  $(tag).each(function (i, element) {
+    var item = $(this);
+    var label = item.find("label");
+
+    if (label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1) {
+      $(item).find('.customization2_attendee_further-data_custom-question_label').css("display", "none");    }
+  });
+}
+
+hideQuestion(".customization2_attendee_further-data_custom-question", "EventInsight user id");
+
+
