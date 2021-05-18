770f2c43201d9e38ebfde92db6c99c9eb6dde3e4
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-18 17:27:39 +0200

    Update edit-links.js

--- customizations/organization/14801/edit-links.js
+++ customizations/organization/14801/edit-links.js
@@ -7,7 +7,7 @@ var answer2 = '<a href="https://doo-product-consulting-uploads.s3.eu-central-1.a
 function changeText(tag, name, newtext) {
   $(tag).each(function (i, element) {
     var item = $(this);
-    var label = item.find("label");
+    var label = item.find(".customization2_attendee_further-data_custom-question_label");
 //.customization2_attendee_further-data_custom-question_label
 
     if (label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1) {
