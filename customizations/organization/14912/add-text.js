375b5989cf5c84dcc639d4469ccd9843dc95a997
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-18 14:03:58 +0200

    Update add-text.js

--- customizations/organization/14912/add-text.js
+++ customizations/organization/14912/add-text.js
@@ -18,13 +18,13 @@ function addText(tag, name) {
     }
   });
 }
-addText(".customization2_attendee_further-data_custom-question", "Wella Company would also");
+addText(".customization2_attendee_further-data_custom-question", "Yes, I would like");
       //$('.customization2_attendee_further-data_custom-question-3').after('<span class="textandlink">You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</span>');
 
 var insertionListener = function(event) {
  if (event.animationName === "nodeInserted") {
   console.log("noteInserted");
-    addText(".customization2_attendee_further-data_custom-question", "Wella Company would also");
+    addText(".customization2_attendee_further-data_custom-question", "Yes, I would like");
      }
    }
  
