bb4b75422607c036e90f57d0518918a08ba023c0
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-18 14:10:04 +0200

    Update add-text.js

--- customizations/organization/14912/add-text.js
+++ customizations/organization/14912/add-text.js
@@ -11,14 +11,11 @@ function addText(tag, name) {
 
     if (label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1) {
       console.log("after:");
-      $(label).after('<span class="textandlink">You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</span>');
-   
-
-              }
+      $(item).after('<span class="textandlink">You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</span>');
+   }
               else {    
-                  $('.customization2_attendee_edit-action_save').attr('disabled', false);
-                  $('.input-error-message').remove();
-    }
+                  $('.textandlink').remove();
+              }
   });
 }
 addText(".customization2_attendee_further-data_custom-question", "Wella Company would also like to keep");
