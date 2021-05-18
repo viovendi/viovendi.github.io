964c5095c5934ce56ce75c45e84bc98303116ddd
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-18 13:57:34 +0200

    Update add-text.js

--- customizations/organization/14912/add-text.js
+++ customizations/organization/14912/add-text.js
@@ -8,8 +8,14 @@ function addText(tag, name) {
 
     if (label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1) {
       console.log("after:");
-      $(item).after('You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</p>');
-   }
+      $(item).after('<span class="textandlink">You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</span>');
+   
+
+              }
+              else {    
+                  $('.customization2_attendee_edit-action_save').attr('disabled', false);
+                  $('.input-error-message').remove();
+    }
   });
 }
 addText(".customization2_attendee_further-data_custom-question", "Wella Company would also like to keep you informed by email, text messages and other digital communications (including via targeted adverts online and via social media), with exclusive updates on our offers, trends and products. If you would like to hear ");
