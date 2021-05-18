c5c37a49f5256ae2bee1b4e1de7a865c97ff3962
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-18 14:02:43 +0200

    Update add-text.js

--- customizations/organization/14912/add-text.js
+++ customizations/organization/14912/add-text.js
@@ -8,7 +8,7 @@ function addText(tag, name) {
 
     if (label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1) {
       console.log("after:");
-      $(item).after('<span class="textandlink">You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</span>');
+      $(label).after('<span class="textandlink">You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</span>');
    
 
               }
@@ -18,13 +18,13 @@ function addText(tag, name) {
     }
   });
 }
-addText(".customization2_attendee_further-data_custom-question", "Wella Company would also like to keep you informed by email, text messages and other digital communications (including via targeted adverts online and via social media), with exclusive updates on our offers, trends and products. If you would like to hear ");
-      $('.customization2_attendee_further-data_custom-question-3').after('<span class="textandlink">You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</span>');
+addText(".customization2_attendee_further-data_custom-question", "Wella Company would also");
+      //$('.customization2_attendee_further-data_custom-question-3').after('<span class="textandlink">You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</span>');
 
 var insertionListener = function(event) {
  if (event.animationName === "nodeInserted") {
   console.log("noteInserted");
-    addText(".customization2_attendee_further-data_custom-question", "Wella Company would also like to keep you informed by email, text messages and other digital communications (including via targeted adverts online and via social media), with exclusive updates on our offers, trends and products. If you would like to hear ");
+    addText(".customization2_attendee_further-data_custom-question", "Wella Company would also");
      }
    }
  
