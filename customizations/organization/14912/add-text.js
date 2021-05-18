e7acae02f52d3dd62a9b0d52e714921c505cc2bf
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-18 14:05:31 +0200

    Update add-text.js

--- customizations/organization/14912/add-text.js
+++ customizations/organization/14912/add-text.js
@@ -4,7 +4,7 @@ console.log('GTM Works');
 function addText(tag, name) {
   $(tag).each(function (i, element) {
     var item = $(this);
-    var label = item.find("label");
+    var label = item.find("customization2_attendee_further-data_custom-question_label");
     console.log(item);
     console.log(label);
 
@@ -20,13 +20,13 @@ function addText(tag, name) {
     }
   });
 }
-addText(".customization2_attendee_further-data_custom-question", "Yes, I would like");
+addText(".customization2_attendee_further-data_custom-question", "Wella Company would also like to keep");
       //$('.customization2_attendee_further-data_custom-question-3').after('<span class="textandlink">You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</span>');
 
 var insertionListener = function(event) {
  if (event.animationName === "nodeInserted") {
   console.log("noteInserted");
-    addText(".customization2_attendee_further-data_custom-question", "Yes, I would like");
+    addText(".customization2_attendee_further-data_custom-question", "Wella Company would also like to keep");
      }
    }
  
