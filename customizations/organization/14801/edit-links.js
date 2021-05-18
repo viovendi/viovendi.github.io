454ab9a7ab72f3eb3d22148ca2f6df15be07123c
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-18 16:31:54 +0200

    Update edit-links.js

--- customizations/organization/14801/edit-links.js
+++ customizations/organization/14801/edit-links.js
@@ -2,13 +2,12 @@ console.log('GTM Works Standard-203');
 
 
 var answer1 ='<a href="https://doo-product-consulting-uploads.s3.eu-central-1.amazonaws.com/Destatis/20210518_Einvera%CC%88ndniserkl_Speaker_Teil-1_Vortrag_14-WissTag_B21+(1).pdf" target="_blank">datenschutzrechtlichen Hinweise</a>'
-
+var answer2 = '<a href="https://doo-product-consulting-uploads.s3.eu-central-1.amazonaws.com/Destatis/20210518_Einvera%CC%88ndniserkl_Speaker_Teil-2_Video_14-WissTag_B21.pdf" target="_blank">datenschutzrechtlichen Hinweise</a>'
 //funktion to hide Label of question
 function changeText(tag, name, newtext) {
   $(tag).each(function (i, element) {
     var item = $(this);
     var label = item.find("label");
-    console.log(label.html());
 //.customization2_attendee_further-data_custom-question_label
 
     if (label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1) {
@@ -17,12 +16,12 @@ function changeText(tag, name, newtext) {
       var htmlold = label.html()
       var newtest = htmlold.replace('datenschutzrechtlichen Hinweise', answer1);
       label.html(newtest)
-      console.log(newtest);
       
       
   }});
 }
 changeText(".customization2_attendee_further-data_custom-question", "Die datenschutzrechtlichen Hinweise und Informationen nach Art. 13 DS-GVO zur Anmeldung und bei Teilnahme an der Veranstaltung für Moderatoren, ", answer1);
+changeText(".customization2_attendee_further-data_custom-question", "Die datenschutzrechtlichen Hinweise und Informationen nach Art. 13 DS-GVO, hier ergänzend zur Anfertigung, Speicherung und Veröffentlichung", answer2);
 
 var insertionListener = function(event) {
  if (event.animationName === "nodeInserted") {
@@ -30,6 +29,7 @@ var insertionListener = function(event) {
    
    // code here
     changeText(".customization2_attendee_further-data_custom-question", "Die datenschutzrechtlichen Hinweise und Informationen nach Art. 13 DS-GVO zur Anmeldung und bei Teilnahme an der Veranstaltung für Moderatoren, ", answer1);
+    changeText(".customization2_attendee_further-data_custom-question", "Die datenschutzrechtlichen Hinweise und Informationen nach Art. 13 DS-GVO, hier ergänzend zur Anfertigung, Speicherung und Veröffentlichung", answer2);
    
    
      }
