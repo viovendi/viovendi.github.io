ef4343914c1ab86b4de71ed85fb36511d8a51fc5
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-18 13:46:12 +0200

    Create add-text.js

---
+++ customizations/organization/14912/add-text.js
@@ -0,0 +1,26 @@
+console.log('GTM Works);
+
+//funktion to hide Label of question
+function addText(tag, name) {
+  $(tag).each(function (i, element) {
+    var item = $(this);
+    var label = item.find("label");
+
+    if (label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1) {
+      $(item).after('You can change your preferences at any time by unsubscribing from these communications. For further information on how we use and protect your personal data, please click here to view our <a href="https://www.wellacompany.com/privacy-policy" target="_blank">Privacy Notice</a>.</p>');
+   }
+  });
+}
+addText(".customization2_attendee_further-data_custom-question", "Wella Company would also like to keep you informed by email, text messages");
+
+var insertionListener = function(event) {
+ if (event.animationName === "nodeInserted") {
+  console.log("noteInserted");
+    addText(".customization2_attendee_further-data_custom-question", "Wella Company would also like to keep you informed by email, text messages");
+     }
+   }
+ 
+
+document.addEventListener("animationstart", insertionListener, false); // standard + firefox
+document.addEventListener("MSAnimationStart", insertionListener, false); // IE
+document.addEventListener("webkitAnimationStart", insertionListener, false); // 
