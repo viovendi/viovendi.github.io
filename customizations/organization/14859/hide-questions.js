01f5e69dafd35b387710e071a1a1459cd835b0a6
vbodend <69503064+vbodend@users.noreply.github.com>
2021-05-25 17:43:07 +0200

    Update hide-questions.js

--- customizations/organization/14859/hide-questions.js
+++ customizations/organization/14859/hide-questions.js
@@ -3,8 +3,9 @@
  function handler(){
      //Hide all questions except the one needed
     $('.customization2_attendee_further-data_custom-question-4').css("display","none")
-    
+    $('.customization2_attendee_further-data_custom-question-6').css("display","none")
      var ddIndustry = $('.customization2_attendee_further-data_custom-question-3 .customization2_attendee_further-data_custom-question_dropdown')
+     var ddManufacturing = $('.customization2_attendee_further-data_custom-question-5 .customization2_attendee_further-data_custom-question_dropdown')
      
      ddIndustry.change(function(){
        	var state = ddIndustry.val().replace(/^\s+|\s+$/g, "")
@@ -15,6 +16,20 @@
             }
            else{
            $('.customization2_attendee_further-data_custom-question-4').css("display","none");
+            }
+                       
+     }
+                      );
+   
+   ddManufacturing.change(function(){
+       	var state = ddManufacturing.val().replace(/^\s+|\s+$/g, "")
+        	$('.customization2_booker_further-data_custom-question-4').css("display","none")
+              
+            if (state == "Others" ){
+              $('.customization2_attendee_further-data_custom-question-6').css("display","block")
+            }
+           else{
+           $('.customization2_attendee_further-data_custom-question-6').css("display","none");
             }
                        
      }
