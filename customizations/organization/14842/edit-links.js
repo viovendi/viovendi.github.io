2c37535e6fc7c1c54ce1546230d29f7b5a5f081f
fabianhasselbacher <69850626+fabianhasselbacher@users.noreply.github.com>
2021-05-31 12:19:06 +0200

    Update edit-links.js

--- customizations/organization/14842/edit-links.js
+++ customizations/organization/14842/edit-links.js
@@ -1,16 +1,16 @@
 console.log('GTM Works Complex-600');
 
 var answer1 = 'Das vollständige Programm finden Sie auf <a href="https://nachhaltige-beschaffung.org/" target="_blank">www.nachhaltige-beschaffung.org</a>. Weitere organisatorische Informationen bekommen Sie rechtzeitig zugeschickt.'
-$(".customization2_attendee_further-data_product-11").before(answer1);
-$(".customization2_attendee_further-data_product-11:before").css({"font-weight": "600","font-size": "16px","text-align": "justify","color": "#343a3f","padding-bottom": "20px"});
+$(".customization2_attendee_further-data_product-11").before(answer1)
+$(".customization2_attendee_further-data_product-11:before").css({"font-weight": "600","font-size": "16px","text-align": "justify","color": "#343a3f","padding-bottom": "20px"})
 
 var insertionListener = function(event) {
  if (event.animationName === "nodeInserted") {
     console.log("noteInserted");
    
    // code here
-  $(".customization2_attendee_further-data_product-11").before(answer1);
-  $(".customization2_attendee_further-data_product-11:before").css({"font-weight": "600","font-size": "16px","text-align": "justify","color": "#343a3f","padding-bottom": "20px"});
+  $(".customization2_attendee_further-data_product-11").before(answer1)
+  $(".customization2_attendee_further-data_product-11:before").css({"font-weight": "600","font-size": "16px","text-align": "justify","color": "#343a3f","padding-bottom": "20px"})
    
      }
    }
