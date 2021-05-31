fcdd16ae240d314e5c9a937d1bf2b92c5768bacd
fabianhasselbacher <69850626+fabianhasselbacher@users.noreply.github.com>
2021-05-31 14:48:55 +0200

    Update edit-links.js

--- customizations/organization/14859/edit-links.js
+++ customizations/organization/14859/edit-links.js
@@ -1,7 +1,7 @@
 console.log('GTM Works Standard-230');
 
 var answer1 = '<div class="standard-230">I want to have my contact details shared within the Oerlikon Group and with Oerlikon’s partners of the 2021 AMTC conference listed in the <a href="https://doo-product-consulting-uploads.s3.eu-central-1.amazonaws.com/Oerlikon/2021/2021+AMTC+Conference+privacy+noticefor+website.pdf" target="_blank">Privacy Notice</a> in order to receive information about their services, products and invites to their events and conferences.</div>'
-$(".vv-checkbox__label--md").before(answer1)
+$(".customization2_booking-terms_text").before(answer1)
 $(".standard-230").css({
 	"word-break": "break-all"
 })
@@ -11,7 +11,7 @@ var insertionListener = function (event) {
 		console.log("noteInserted");
 
 		// code here
-		$(".vv-checkbox__label--md").before(answer1)
+		$(".customization2_booking-terms_text").before(answer1)
 		$(".standard-230").css({
 			"word-break": "break-all"
 		})
