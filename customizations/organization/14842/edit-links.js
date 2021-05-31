5554763684197c29a465c9b206a2602982e528ba
fabianhasselbacher <69850626+fabianhasselbacher@users.noreply.github.com>
2021-05-31 12:24:15 +0200

    Update edit-links.js

--- customizations/organization/14842/edit-links.js
+++ customizations/organization/14842/edit-links.js
@@ -1,8 +1,8 @@
 console.log('GTM Works Complex-600');
 
-var answer1 = 'Das vollständige Programm finden Sie auf <a href="https://nachhaltige-beschaffung.org/" target="_blank">www.nachhaltige-beschaffung.org</a>. Weitere organisatorische Informationen bekommen Sie rechtzeitig zugeschickt.'
+var answer1 = '<div class="complex-600">Das vollständige Programm finden Sie auf <a href="https://nachhaltige-beschaffung.org/" target="_blank">www.nachhaltige-beschaffung.org</a>. Weitere organisatorische Informationen bekommen Sie rechtzeitig zugeschickt.</div>'
 $(".customization2_attendee_further-data_product-11").before(answer1)
-$(".customization2_attendee_further-data_product-11:before").css({"font-weight": "600","font-size": "16px","text-align": "justify","color": "#343a3f","padding-bottom": "20px"})
+$(".complex-600").css({"font-weight": "600","font-size": "16px","text-align": "justify","color": "#343a3f","padding-bottom": "20px"})
 
 var insertionListener = function(event) {
  if (event.animationName === "nodeInserted") {
@@ -10,8 +10,7 @@ var insertionListener = function(event) {
    
    // code here
   $(".customization2_attendee_further-data_product-11").before(answer1)
-  $(".customization2_attendee_further-data_product-11:before").css({"font-weight": "600","font-size": "16px","text-align": "justify","color": "#343a3f","padding-bottom": "20px"})
-   
+ 
      }
    }
  
