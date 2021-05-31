76fb691c0a45e2bdca497ae231a57d8f6efbc551
fabianhasselbacher <69850626+fabianhasselbacher@users.noreply.github.com>
2021-05-31 12:06:19 +0200

    Update edit-links.js

--- customizations/organization/14842/edit-links.js
+++ customizations/organization/14842/edit-links.js
@@ -1,6 +1,6 @@
 console.log('GTM Works Complex-600');
 
-var answer1 = 'Das vollständige Programm finden Sie auf <a href="https://nachhaltige-beschaffung.org/">www.nachhaltige-beschaffung.org</a> Weitere organisatorische Informationen bekommen Sie rechtzeitig zugeschickt.'
+var answer1 = 'Das vollständige Programm finden Sie auf <a href="https://nachhaltige-beschaffung.org/" target="_blank">www.nachhaltige-beschaffung.org</a>. Weitere organisatorische Informationen bekommen Sie rechtzeitig zugeschickt.'
 $(".customization2_attendee_further-data_product-11").before(answer1)
 
 var insertionListener = function(event) {
