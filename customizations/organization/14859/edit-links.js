02d23d231feda73fcecf0d714ceb1235fb328afd
vbodend <69503064+vbodend@users.noreply.github.com>
2021-06-02 11:03:21 +0200

    Update edit-links.js

--- customizations/organization/14859/edit-links.js
+++ customizations/organization/14859/edit-links.js
@@ -2,7 +2,6 @@ console.log('GTM Works Standard-230');
 
 
 
-
 var newdiv1 = $( "<div id='object1'>I want to have my contact details shared within the Oerlikon Group and with Oerlikon’s partners of the 2021 AMTC conference listed in the <a href='https://doo-product-consulting-uploads.s3.eu-central-1.amazonaws.com/Oerlikon/2021/2021+AMTC+Conference+privacy+noticefor+website.pdf' target='_blank'>Privacy Notice</a> in order to receive information about their services, products and invites to their events and conferences.'</div>" );
 
 //var answer1 = 'I want to have my contact details shared within the Oerlikon Group and with Oerlikon’s partners of the 2021 AMTC conference listed in the <a href="https://doo-product-consulting-uploads.s3.eu-central-1.amazonaws.com/Oerlikon/2021/2021+AMTC+Conference+privacy+noticefor+website.pdf" target="_blank">Privacy Notice</a> in order to receive information about their services, products and invites to their events and conferences.'
