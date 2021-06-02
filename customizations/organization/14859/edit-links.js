405b6a28204f8e85f767ff287b1d219a592c2f6a
vbodend <69503064+vbodend@users.noreply.github.com>
2021-06-02 10:20:32 +0200

    Update edit-links.js

--- customizations/organization/14859/edit-links.js
+++ customizations/organization/14859/edit-links.js
@@ -1,4 +1,12 @@
 console.log('GTM Works Standard-230');
+console.log("1");
+// first page:
+$('.customization-access-code-label').text('test');
+$('.customization-access-code-label.span').text('test2');
+$('*[@id="customization-booking-area-wrapper-page1"]/vv-ticket-selection/div/div[1]/div/vv-code-input/div/vv-button/button/text').text('test3');
+$('.vv-ticket-selection/div/div[1]/div/vv-code-input/div/vv-button/button/text').text('test4');
+
+
 
 var newdiv1 = $( "<div id='object1'>I want to have my contact details shared within the Oerlikon Group and with Oerlikon’s partners of the 2021 AMTC conference listed in the <a href='https://doo-product-consulting-uploads.s3.eu-central-1.amazonaws.com/Oerlikon/2021/2021+AMTC+Conference+privacy+noticefor+website.pdf' target='_blank'>Privacy Notice</a> in order to receive information about their services, products and invites to their events and conferences.'</div>" );
 
@@ -9,11 +17,4 @@ $("#object1").css({
 	"padding-bottom": "10px"	
 })
 
-
-
-// first page:
-$('.customization-access-code-label').text('test');
-$('.customization-access-code-label.span').text('test2');
-$('*[@id="customization-booking-area-wrapper-page1"]/vv-ticket-selection/div/div[1]/div/vv-code-input/div/vv-button/button/text').text('test3');
-$('.vv-ticket-selection/div/div[1]/div/vv-code-input/div/vv-button/button/text').text('test4');
 
