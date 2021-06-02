d8f7f45670d7ab76c5a560c2afbbc58a7850ea1c
vbodend <69503064+vbodend@users.noreply.github.com>
2021-06-02 09:42:28 +0200

    Update edit-links.js

--- customizations/organization/14859/edit-links.js
+++ customizations/organization/14859/edit-links.js
@@ -5,7 +5,17 @@ var newdiv1 = $( "<div id='object1'>I want to have my contact details shared wit
 //var answer1 = 'I want to have my contact details shared within the Oerlikon Group and with Oerlikon’s partners of the 2021 AMTC conference listed in the <a href="https://doo-product-consulting-uploads.s3.eu-central-1.amazonaws.com/Oerlikon/2021/2021+AMTC+Conference+privacy+noticefor+website.pdf" target="_blank">Privacy Notice</a> in order to receive information about their services, products and invites to their events and conferences.'
 $(".customization2_booking-terms_text").prepend(newdiv1);
 $("#object1").css({
-	"margin-top": "-50px"
+	"margin-top": "-50px",
+	//"word-break": "break-word",
+	//"font-size": "14.4px",
+	//"font-family": "'Helvetica Neue LT Std-Th'",
+	"font-weight": "400",
+	//"position": "absolute",
+	//"margin-left": "25px",
+	"padding-bottom": "10px"
+	//"padding-right": "25px",
+	//"height": "auto",
+	//"width": "85%"
 	
 })
 
