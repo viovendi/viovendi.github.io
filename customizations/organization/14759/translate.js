7d8d392306e3e8680fc95390cc0d990f4111152c
vbodend <69503064+vbodend@users.noreply.github.com>
2021-06-07 11:36:31 +0200

    Update translate.js

--- customizations/organization/14759/translate.js
+++ customizations/organization/14759/translate.js
@@ -220,7 +220,7 @@ $('.customization-client-portal-link').css('color', '#F99D27');
   
  }, 1000);
 //99 available Replace the word
-//var available = $('.event-category__amount').text();
+//var available = $('.event-category__amount').text(); 
 //var res = available.replace("available", "tillgängliga platser");
 //$('.event-category__amount').text(res);
   
