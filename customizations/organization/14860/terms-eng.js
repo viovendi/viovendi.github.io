d0bb0a130d0a056c51c82c93c294f6abbce47b6e
vbodend <69503064+vbodend@users.noreply.github.com>
2021-06-08 08:13:15 +0200

    Update terms-eng.js

--- customizations/organization/14860/terms-eng.js
+++ customizations/organization/14860/terms-eng.js
@@ -1,1 +1,6 @@
 console.log("terms-eng");
+
+
+$('.customization2_booking-terms .vv-mb-xxs').prepend('<b class="terms_new">By clicking on the "Send" button, you agree that the data you entered will be used for the purpose of registration for the event. The data is collected by doo GmbH on behalf of SMA Solar Technology AG. The data is acquired by the provider doo GmbH. In addition, you agree to the SMA Data Protection Declaration. Any consent that you have given can be withdrawn at any time with effect for the future. In case of revocation, your data will be deleted immediately. Otherwise, your data will be erased when we have processed your request or the purpose of storage has ceased. You are entitled to request from us anytime information on the data stored about you. Further information on data protection can be found in the SMA Data Protection Declaration of this website.</b>')
+$('.terms_new').css({"font-size": "14px", "line-height": "25px", "max-width": "90%"});
+
