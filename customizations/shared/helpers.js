8c1e70e2a6e5865bb35ba5d8f3e47680a9b8fb0a
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-06-11 16:41:09 +0300

    test

--- customizations/shared/helpers.js
+++ customizations/shared/helpers.js
@@ -23,6 +23,7 @@ function myHelpers(elements) {
     this.field = null;
 
     elements.forEach((field) => {
+      console.log(field.querySelectorAll('label p')[0].innerText.trim())
       if (field.querySelectorAll('label p')[0].innerText.trim().includes(this.lable)) {
         this.field = field;
       }
