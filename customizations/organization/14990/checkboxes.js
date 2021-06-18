daafe6b0ab806e0104a97755001b234cd2b484b4
vbodend <69503064+vbodend@users.noreply.github.com>
2021-06-18 14:27:20 +0200

    Update checkboxes.js

--- customizations/organization/14990/checkboxes.js
+++ customizations/organization/14990/checkboxes.js
@@ -5,10 +5,11 @@ var field = $$('.customization2_booker_further-data_custom-question').findField(
 var radioGroup = $$('.customization2_booker_further-data_custom-question').findRadioGroup('Abstract submission (deadline: 30th June 2021)');
    
    $(field).hide()
+$('.col-xs-4 .col-4 .vv-pr-none').hide()
   var inputs = $(radioGroup).find('input');
   inputs.each(function( i, element ) {
   $(element).change(function(input) {
-    if($(input.currentTarget).siblings('.customization2_booker_further-data_custom-question_radio-line_label').text().replace(/^\s+|\s+$/g, "").indexOf('Yes')){
+    if($(input.currentTarget).siblings('.customization2_booker_further-data_custom-question_radio-line_label').text().replace(/^\s+|\s+$/g, "").indexOf('No')){
        console.log('show');
       $(field).show();
        $('.col-xs-4 .col-4 .vv-pr-none').show()
