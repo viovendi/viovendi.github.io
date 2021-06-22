1a5b2eedbc2ac8e5667e1f20d9b84447610b2cfe
vbodend <69503064+vbodend@users.noreply.github.com>
2021-06-22 14:03:20 +0200

    Update listwidget.js

--- customizations/organization/14231/listwidget.js
+++ customizations/organization/14231/listwidget.js
@@ -1,6 +1,5 @@
 console.log('works');
 //$('.customization-booking-area-wrapper-page1 .event-info__event-description .customization-event-info-description').css({"display":"none"});
 //$('.event-info__event-description.customization-event-info-description').hide();
-var windowpath = window.location.pathname;
-var language = windowpath.split('/')[1]
-console.log(windowpath);
+var page1 = $('.customization-booking-area-wrapper-page1');
+var page2 = $('.customization-booking-area-wrapper-page2');
