4614e1604ee6715735fc35300208394f4e14574a
vbodend <69503064+vbodend@users.noreply.github.com>
2021-06-22 15:19:17 +0200

    Update listwidget.js

--- customizations/organization/14231/listwidget.js
+++ customizations/organization/14231/listwidget.js
@@ -4,13 +4,13 @@ console.log('works');
 var page1 = $('.customization-booking-area-wrapper-page1');
 var page2 = $('.customization-booking-area-wrapper-page2');
 
-//if booker is on first page(page1, Tickets) hide event info
-if( page1.length )         // use this if you are using id to check
+//if booker is on first page(page1, Tickets) -> hide event info
+if( page1.length )  // it exists (page1)
 {
   $('.event-info__event-description.customization-event-info-description').hide();
-     // it exists
+    
 }
-//if booker is on secondpage(page2, Products) show event info
+//if booker is on secondpage(page2, Products) or any other page -> show event info
 else{
  $('.event-info__event-description.customization-event-info-description').show();
 }
