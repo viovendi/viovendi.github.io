dcba090ec760086adae7930c86cf1c5b8fe9f12e
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-06-23 16:28:42 +0300

    test

--- customizations/organization/14856/popup.js
+++ customizations/organization/14856/popup.js
@@ -85,7 +85,7 @@ console.log('GTM github!');
     $('#accept2').on('click', function(){
       $('#checkboxModal2').hide();
       $('#checkboxModal2').remove();
-      $(input).trigger('click');
+      input.trigger('click');
     });
   }
 
