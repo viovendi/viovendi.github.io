466ca80f7d2070a0df34078c538ff5d3b2f1ced1
vbodend <69503064+vbodend@users.noreply.github.com>
2021-06-28 14:04:58 +0200

    Update hide.js

--- customizations/organization/14269/hide.js
+++ customizations/organization/14269/hide.js
@@ -4,6 +4,17 @@ var name = $('.event-info__title').text()
 
 if(name.indexOf("Brennholz")>=0){
   $('.customization-booking-area-wrapper-page1').hide();
+  $('.event-info__date-container').hide();
+  $('.event-info__date-container').css("margin-left:", "-130px");
+  $('.event-info .event-info__details').css("margin-left", "0px");
+/*.event-info__header {
+    margin-left: -130px;
+}
+  .event-info .event-info__details {
+    margin-left: 0px;
+}*/
+
+  
 }
 
 async function makeRequest(options) {
@@ -32,6 +43,9 @@ async function getEventAttributes(eventId) {
   const attribute = $.grep(result.event_attributes.attributes, function (v) {
     if(v.name == 'Forst'){
       $('.customization-booking-area-wrapper-page1').hide();
+      $('.event-info__date-container').hide();
+      $('.event-info__date-container').css("margin-left:", "-130px");
+      $('.event-info .event-info__details').css("margin-left", "0px");
     }
     
     return v.name
