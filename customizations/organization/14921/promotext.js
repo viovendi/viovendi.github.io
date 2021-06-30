4b8193fce9365354c527cd173f38dbaad3250f96
vbodend <69503064+vbodend@users.noreply.github.com>
2021-06-30 09:27:51 +0200

    Update promotext.js

--- customizations/organization/14921/promotext.js
+++ customizations/organization/14921/promotext.js
@@ -5,12 +5,5 @@ var name = $('.event-info__title').text()
 if(name.indexOf("Professional GelColor")>=0){
   var text = $( "<div class='promotextbefore'>If you have a voucher for half price GelColor education please leave out the % sign from the code</div>" )
   $('.ew-selection__promocode-block').before(text)
-  $('.promotextbefore').css("padding-left", "12px");
- /*.ew-selection__promocode-block:before {
-    content: "If you have a voucher for half price GelColor education\A please leave out the % sign from the code";
-    white-space: pre;
-}*/
-
-
-  
+  $('.promotextbefore').css("padding-left", "12px");  
 }
