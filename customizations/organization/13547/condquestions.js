ff148a72fb50575229a9d47a6e1133074809f69c
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-05 11:30:21 +0200

    Update condquestions.js

--- customizations/organization/13547/condquestions.js
+++ customizations/organization/13547/condquestions.js
@@ -22,6 +22,38 @@ function showQuestion(tag, name) {
   });
 }
 
+function hideQuestionExactWording(tag, name) {
+  $(tag).each(function (i, element) {
+    var item = $(this);
+    var label = item.find("label");
+
+    if (label.text().replace(/^\s+|\s+$/g, "") == name) {
+      $(item).hide();    }
+  });
+}
+
+function showQuestionExactWording(tag, name) {
+  $(tag).each(function (i, element) {
+    var item = $(this);
+    var label = item.find("label");
+
+    if (label.text().replace(/^\s+|\s+$/g, "") == name) {
+      $(item).show();    }
+  });
+}
+
+
+function hideAll(){
+//First hide all questions:
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Organisation - Vertretung");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Mitgliedsnummer - Vertretung");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Upload der unterschriebenen Vollmacht - Vertretung");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Mitgliedsnummer");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Upload der unterschriebenen Vollmacht - Vertretung");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Kommen Sie zusätzlich als Vertreter für andere Mitglieder");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Wie viele Mitglieder vertreten Sie");
+     $('.vv-mt-md').hide(); // grey text  "erlaubte Formate: PNG, JPG, PDF"
+}
 
 
 
@@ -35,16 +67,9 @@ var insertionListener = function(event) {
       console.log("noteInserted");
      
      var radioGroupGastorMitglied = $$('.customization2_attendee_further-data_custom-question').findRadioGroup('Sind Sie ein Gast oder ein Stimmberechtigtes Mitglied?');
-
+     hideAll();
      
-     //First hide all questions:
-     hideQuestion(".customization2_attendee_further-data_custom-question", "Organisation - Vertretung");
-     hideQuestion(".customization2_attendee_further-data_custom-question", "Mitgliedsnummer - Vertretung");
-     hideQuestion(".customization2_attendee_further-data_custom-question", "Upload der unterschriebenen Vollmacht - Vertretung");
-     hideQuestion(".customization2_attendee_further-data_custom-question", "Mitgliedsnummer");
-     hideQuestion(".customization2_attendee_further-data_custom-question", "Upload der unterschriebenen Vollmacht - Vertretung");
-     hideQuestion(".customization2_attendee_further-data_custom-question", "Kommen Sie zusätzlich als Vertreter für andere Mitglieder");
-     hideQuestion(".customization2_attendee_further-data_custom-question", "Wie viele Mitglieder vertreten Sie");
+     
 
      
 
@@ -55,14 +80,18 @@ var insertionListener = function(event) {
   var inputs = $(radioGroupGastorMitglied).find('input');
   inputs.each(function( i, element ) {
   $(element).change(function(input) {
+    // this is somehow twisted (maybe because of "change" and currentTarget); if 'Gast', it's actual 'Mitglied'
     if($(input.currentTarget).siblings('.customization2_attendee_further-data_custom-question_radio-line_label').text().replace(/^\s+|\s+$/g, "").indexOf('Gast')){
-       console.log('Gast');
-       showQuestion(".customization2_attendee_further-data_custom-question", "Kommen Sie zusätzlich als Vertreter für andere Mitglieder");
-       showQuestion(".customization2_attendee_further-data_custom-question", "Wie viele Mitglieder vertreten Sie");
+    console.log('Mitglied');
+
     }
     
     else{
-      console.log('Mitglied');
+      console.log('Gast');
+      showQuestion(".customization2_attendee_further-data_custom-question", "Kommen Sie zusätzlich als Vertreter für andere Mitglieder");
+      showQuestionExactWording(".customization2_attendee_further-data_custom-question", "Mitgliedsnummer");
+
+
       }
    });
   });
