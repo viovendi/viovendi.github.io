1404f149c949421a416830a133d75c724ad8b79d
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-05 12:18:30 +0200

    Update condquestions.js

--- customizations/organization/13547/condquestions.js
+++ customizations/organization/13547/condquestions.js
@@ -100,7 +100,7 @@ var insertionListener = function(event) {
                               var state = dropDownQuestion.val().trim()
                               if (state.includes("1")) {
                                  console.log('1');
-                                 showQuestion(".customization2_attendee_further-data_custom-question", "1");
+                                 showQuestion(".customization2_attendee_further-data_custom-question", " 1");
 
                                  //showQuestion(".customization2_attendee_further-data_custom-question", "Organisation - Vertretung 1");
                                  //showQuestion(".customization2_attendee_further-data_custom-question", "Mitgliedsnummer - Vertretung 1");
@@ -109,14 +109,14 @@ var insertionListener = function(event) {
                               }
                               if (state.includes("2")) {
                                 console.log('2');
-                                showQuestion(".customization2_attendee_further-data_custom-question", "1");
-                                showQuestion(".customization2_attendee_further-data_custom-question", "2");
+                                showQuestion(".customization2_attendee_further-data_custom-question", " 1");
+                                showQuestion(".customization2_attendee_further-data_custom-question", " 2");
                               }
                               if (state.includes("3")) {
                                   console.log('3');
-                                  showQuestion(".customization2_attendee_further-data_custom-question", "1");
-                                  showQuestion(".customization2_attendee_further-data_custom-question", "2");
-                                  showQuestion(".customization2_attendee_further-data_custom-question", "3");
+                                  showQuestion(".customization2_attendee_further-data_custom-question", " 1");
+                                  showQuestion(".customization2_attendee_further-data_custom-question", " 2");
+                                  showQuestion(".customization2_attendee_further-data_custom-question", " 3");
                               }
                               if (state.includes("4")) {
                                   console.log('4');
