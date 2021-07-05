fee15767fef855fdc9ef548f2739171af381589c
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-05 12:24:03 +0200

    Update condquestions.js

--- customizations/organization/13547/condquestions.js
+++ customizations/organization/13547/condquestions.js
@@ -98,30 +98,30 @@ var insertionListener = function(event) {
                       var inputDropDown = $(question).find('.customization2_attendee_further-data_custom-question_input')
                           dropDownQuestion.change(function(){
                               var state = dropDownQuestion.val().trim()
-                              if (state.includes(" 1")) {
+                              if (state.includes("1")) {
                                  console.log('1');
-                                 showQuestion(".customization2_attendee_further-data_custom-question", " 1");
+                                 showQuestion(".customization2_attendee_further-data_custom-question", " 1 ");
 
                                  //showQuestion(".customization2_attendee_further-data_custom-question", "Organisation - Vertretung 1");
                                  //showQuestion(".customization2_attendee_further-data_custom-question", "Mitgliedsnummer - Vertretung 1");
                                  //showQuestion(".customization2_attendee_further-data_custom-question", "Upload der unterschriebenen Vollmacht - Vertretung 1");
                                  $('.customization2_attendee_further-data_custom-question-7').parent().find('.vv-mt-md').show();
                               }
-                              if (state.includes(" 2")) {
+                              if (state.includes("2")) {
                                 console.log('2');
-                                showQuestion(".customization2_attendee_further-data_custom-question", " 1");
-                                showQuestion(".customization2_attendee_further-data_custom-question", " 2");
+                                showQuestion(".customization2_attendee_further-data_custom-question", " 1 ");
+                                showQuestion(".customization2_attendee_further-data_custom-question", " 2 ");
                               }
-                              if (state.includes(" 3")) {
+                              if (state.includes("3")) {
                                   console.log('3');
-                                  showQuestion(".customization2_attendee_further-data_custom-question", " 1");
-                                  showQuestion(".customization2_attendee_further-data_custom-question", " 2");
-                                  showQuestion(".customization2_attendee_further-data_custom-question", " 3");
+                                  showQuestion(".customization2_attendee_further-data_custom-question", " 1 ");
+                                  showQuestion(".customization2_attendee_further-data_custom-question", " 2 ");
+                                  showQuestion(".customization2_attendee_further-data_custom-question", " 3 ");
                               }
-                              if (state.includes(" 4")) {
+                              if (state.includes("4")) {
                                   console.log('4');
                               }
-                              if (state.includes(" 5")) {
+                              if (state.includes("5")) {
                                   console.log('5');
                               }
 
