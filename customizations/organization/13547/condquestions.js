dc216db9ccdab11e550d7ecbe162bd6928f1debe
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-05 10:59:29 +0200

    Update condquestions.js

--- customizations/organization/13547/condquestions.js
+++ customizations/organization/13547/condquestions.js
@@ -1,1 +1,35 @@
 console.log("git");
+
+
+//funktion to hide question
+function hideQuestion(tag, name) {
+  $(tag).each(function (i, element) {
+    var item = $(this);
+    var label = item.find("label");
+
+    if (label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1) {
+      $(item).hide();    }
+  });
+}
+
+
+hideQuestion(".customization2_attendee_further-data_custom-question", "Organisation - Vertretung 1");
+
+
+
+
+
+
+
+var insertionListener = function(event) {
+   if (event.animationName === "nodeInserted") {
+      console.log("noteInserted");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Organisation - Vertretung 1");
+
+   }
+}
+   
+
+  document.addEventListener("animationstart", insertionListener, false); // standard + firefox
+  document.addEventListener("MSAnimationStart", insertionListener, false); // IE
+  document.addEventListener("webkitAnimationStart", insertionListener, false); // 
