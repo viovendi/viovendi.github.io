3d72fd19d4332d616fb5c303faae5f1e6831093b
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-05 11:11:47 +0200

    Update condquestions.js

--- customizations/organization/13547/condquestions.js
+++ customizations/organization/13547/condquestions.js
@@ -12,8 +12,17 @@ function hideQuestion(tag, name) {
   });
 }
 
+function showQuestion(tag, name) {
+  $(tag).each(function (i, element) {
+    var item = $(this);
+    var label = item.find("label");
 
-hideQuestion(".customization2_attendee_further-data_custom-question", "Organisation - Vertretung 1");
+    if (label.text().replace(/^\s+|\s+$/g, "").indexOf(name) > -1) {
+      $(item).show();    }
+  });
+}
+
+
 
 
 
@@ -24,7 +33,39 @@ hideQuestion(".customization2_attendee_further-data_custom-question", "Organisat
 var insertionListener = function(event) {
    if (event.animationName === "nodeInserted") {
       console.log("noteInserted");
-     hideQuestion(".customization2_attendee_further-data_custom-question", "Organisation - Vertretung 1");
+     
+     var radioGroupGastorMitglied = $$('.customization2_attendee_further-data_custom-question').findRadioGroup('Sind Sie ein Gast oder ein Stimmberechtigtes Mitglied?');
+
+     
+     //First hide all questions:
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Organisation - Vertretung");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Mitgliedsnummer - Vertretung");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Upload der unterschriebenen Vollmacht - Vertretung");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Mitgliedsnummer");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Upload der unterschriebenen Vollmacht - Vertretung");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Kommen Sie zusätzlich als Vertreter für andere Mitglieder");
+     hideQuestion(".customization2_attendee_further-data_custom-question", "Wie viele Mitglieder vertreten Sie");
+
+     
+
+     
+
+
+
+  var inputs = $(radioGroupGastorMitglied).find('input');
+  inputs.each(function( i, element ) {
+  $(element).change(function(input) {
+    if($(input.currentTarget).siblings('.customization2_attendee_further-data_custom-question_radio-line_label').text().replace(/^\s+|\s+$/g, "").indexOf('Gast')){
+       console.log('Gast');
+       showQuestion(".customization2_attendee_further-data_custom-question", "Kommen Sie zusätzlich als Vertreter für andere Mitglieder");
+       showQuestion(".customization2_attendee_further-data_custom-question", "Wie viele Mitglieder vertreten Sie");
+       }
+    else{
+      console.log('Mitglied');
+    
+    }
+
+     
 
    }
 }
