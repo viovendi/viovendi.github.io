804205ae75a8ac31a6fd5e578c531d99e4eeaaf9
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-06 12:08:27 +0200

    Update termseng.js

--- customizations/organization/14970/termseng.js
+++ customizations/organization/14970/termseng.js
@@ -1,6 +1,9 @@
 console.log('git 14970 ENG');
+//page 3 call only
+if( $('.customization-booking-area-wrapper-page3').length >0 ){
+  	$('h4.vv-heading.vv-heading--4.vv-text-align--left.vv-color--text').text('Paying-informationen');
+  }
 
-$('h4.vv-heading.vv-heading--4.vv-text-align--left.vv-color--text').text('Paying-informationen');
 
 
 $('p.vv-text.vv-text__size--md.vv-text__weight--regular.vv-color--text').first().text("We have saved your binding ticket booking in our system. However, payment for the tickets is not due until around 4 weeks before the start of the fair. You will then receive a straightforward e-mail from us with the link to pay for your booked tickets and further information.");
