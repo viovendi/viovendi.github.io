7c6ef97f40a75ec391a0cb3c3c2374c33ae663b9
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-06 12:07:55 +0200

    Update terms.js

--- customizations/organization/14970/terms.js
+++ customizations/organization/14970/terms.js
@@ -1,6 +1,9 @@
 console.log('git 14970');
+//call page 3 only
+if( $('.customization-booking-area-wrapper-page3').length >0 ){
+  	$('h4.vv-heading.vv-heading--4.vv-text-align--left.vv-color--text').text('Zahlungs-Informationen');
+  }
 
-$('h4.vv-heading.vv-heading--4.vv-text-align--left.vv-color--text').text('Zahlungs-Informationen');
 
 
 $('p.vv-text.vv-text__size--md.vv-text__weight--regular.vv-color--text').first().text("Ihre Ticket-Buchung haben wir verbindlich in unserem System gespeichert. Die Bezahlung der Tickets wird jedoch erst rund 4 Wochen vor dem Messestart fällig. Dann erhalten Sie von uns ganz unkompliziert eine E-Mail mit dem Link zur Bezahlung Ihrer gebuchten Tickets sowie weiteren Informationen.");
