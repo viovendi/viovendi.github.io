2f691396f842aa38b6c6a89934b079b4b8311184
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-06 09:22:36 +0200

    Update terms.js

--- customizations/organization/14970/terms.js
+++ customizations/organization/14970/terms.js
@@ -4,7 +4,7 @@ $('h4.vv-heading.vv-heading--4.vv-text-align--left.vv-color--text').text('Zahlun
 
 
 $('p.vv-text.vv-text__size--md.vv-text__weight--regular.vv-color--text').first().text("Ihre Ticket-Buchung haben wir verbindlich in unserem System gespeichert. Die Bezahlung der Tickets wird jedoch erst rund 4 Wochen vor dem Messestart fällig. Dann erhalten Sie von uns ganz unkompliziert eine E-Mail mit dem Link zur Bezahlung Ihrer gebuchten Tickets sowie weiteren Informationen.");
-$('p.vv-text.vv-text__size--md.vv-text__weight--regular.vv-color--text:nth-child(2)').hide();
+$("#widgetContainer > vv-widget-master > div > div > vv-booking-standard > div:nth-child(3) > div > div > div:nth-child(4) > div > vv-payment-step > div > div.vv-ph-lg.vv-pb-md > form > div.col-md-7 > vv-booking-approval-notice > div > vv-text > p").hide();
 
 $('.vv-pl-lg.customization2_booking-terms_list').after('<div style="font-size: 14px;"><p>Die im Zeitraum der Veranstaltung gültigen Hygienevorschriften sind einzuhalten.</p></div>');
 
