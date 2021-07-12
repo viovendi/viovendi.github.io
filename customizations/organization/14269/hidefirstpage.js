0c77c81a86e305396a46ca1d5664c97178e4fc7f
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-12 09:56:34 +0200

    Update hidefirstpage.js

--- customizations/organization/14269/hidefirstpage.js
+++ customizations/organization/14269/hidefirstpage.js
@@ -1,1 +1,22 @@
 console.log('git first page')
+//hide end day
+  $('div.event-article__date-container__day > span:nth-child(2)').each(function(i, element) {
+       $(this).hide();
+    });
+  //hide end month
+  $('div.event-article__date-container__month > span:nth-child(2)').each(function(i, element) {
+       $(this).hide();
+    });
+  
+  // remove '-' from day
+  $('div.event-article__date-container__day > span.ng-star-inserted').each(function(i, element) {
+       var finalTextDay = $(this).text().replace('-','')
+       $(this).empty().html(finalTextDay);
+    });
+  
+  // remove '-' from month
+  $('div.event-article__date-container__month > span.ng-star-inserted').each(function(i, element) {
+       var finalTextMonth = $(this).text().replace('-','');
+       $(this).empty().html(finalTextMonth);
+    });
+  
