d18fd6e42b035ee79f6da2f9f3bbf90dea5c96ba
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-13 12:34:08 +0200

    Update hidefirstpage.js

--- customizations/organization/14269/hidefirstpage.js
+++ customizations/organization/14269/hidefirstpage.js
@@ -10,7 +10,9 @@ $('.event-article__event-info__title').each(function(i, element) {
       $(this).parents().find('div.event-article__date-container__day > span:nth-child(2)').hide();
       $(this).parents().find('div.event-article__date-container__month > span:nth-child(2)').hide();
       var day = $(this).parents().find('div.event-article__date-container__day > span.ng-star-inserted')
-      console.log(day.text('Ab ' + day.text().replace(/^\s+|\s+$/g, "")));
+      var textDay = day.text().replace('-','');
+       var finalTextDay = textDay.replace(/^\s+|\s+$/g, "")
+       $(this).empty().html(finalTextDay);
 
   }
 });
@@ -26,11 +28,12 @@ $('.event-article__event-info__title').each(function(i, element) {
   
   // remove '-' from day
   $('div.event-article__date-container__day > span.ng-star-inserted').each(function(i, element) {
-    
+    /*
        var newTextDay = $(this).text('Ab ' + $(this).text().replace(/^\s+|\s+$/g, ""));
        var textDay = newTextDay.text().replace('-','');
        var finalTextDay = textDay.replace(/^\s+|\s+$/g, "")
        $(this).empty().html(finalTextDay);
+       */
     });
   
   // remove '-' from month
