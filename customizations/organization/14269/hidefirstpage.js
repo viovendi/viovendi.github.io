75779d9ee1eadd4308c2dcea6c44ed906442533a
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-13 12:26:13 +0200

    Update hidefirstpage.js

--- customizations/organization/14269/hidefirstpage.js
+++ customizations/organization/14269/hidefirstpage.js
@@ -9,13 +9,7 @@ $('.event-article__event-info__title').each(function(i, element) {
    
       $(this).parents().find('div.event-article__date-container__day > span:nth-child(2)').hide();
       $(this).parents().find('div.event-article__date-container__month > span:nth-child(2)').hide();
-      /* var newTextDay = $(this).parents().find('div.event-article__date-container__day > span.ng-star-inserted').text('Ab ' + $(this).parents().find('div.event-article__date-container__day > span.ng-star-inserted').text().replace(/^\s+|\s+$/g, ""));
-       var textDay = newTextDay.text().replace('-','');
-       var finalTextDay = textDay.replace(/^\s+|\s+$/g, "")
-       $(this).parents().find('div.event-article__date-container__day > span.ng-star-inserted').empty().html(finalTextDay);
-    
-*/
-
+     
 
   }
 });
@@ -31,11 +25,11 @@ $('.event-article__event-info__title').each(function(i, element) {
   
   // remove '-' from day
   $('div.event-article__date-container__day > span.ng-star-inserted').each(function(i, element) {
-    /*
+    
        var newTextDay = $(this).text('Ab ' + $(this).text().replace(/^\s+|\s+$/g, ""));
        var textDay = newTextDay.text().replace('-','');
        var finalTextDay = textDay.replace(/^\s+|\s+$/g, "")
-       $(this).empty().html(finalTextDay);*/
+       $(this).empty().html(finalTextDay);
     });
   
   // remove '-' from month
