3fa273a8e7bafda1bd5494cbdf6121439ab47584
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-13 12:41:12 +0200

    Update hidefirstpage.js

--- customizations/organization/14269/hidefirstpage.js
+++ customizations/organization/14269/hidefirstpage.js
@@ -9,10 +9,7 @@ $('.event-article__event-info__title').each(function(i, element) {
    
       $(this).parents().find('div.event-article__date-container__day > span:nth-child(2)').hide();
       $(this).parents().find('div.event-article__date-container__month > span:nth-child(2)').hide();
-    /*  var day = $(this).parents().find('div.event-article__date-container__day > span.ng-star-inserted')
-      var textDay = day.text().replace('-','');
-       var finalTextDay = textDay.replace(/^\s+|\s+$/g, "")
-       day.empty().html(finalTextDay);*/
+    
 
   }
 });
@@ -28,7 +25,9 @@ $('.event-article__event-info__title').each(function(i, element) {
   
   // remove '-' from day
   $('div.event-article__date-container__day > span.ng-star-inserted').each(function(i, element) {
-    
+    if($(this).parents().find('.event-article__event-info__title').text().indexOf('Brennholz') >= 0){
+      console.log('1');
+    }
        var newTextDay = $(this).text('Ab ' + $(this).text().replace(/^\s+|\s+$/g, ""));
        var textDay = newTextDay.text().replace('-','');
        var finalTextDay = textDay.replace(/^\s+|\s+$/g, "")
