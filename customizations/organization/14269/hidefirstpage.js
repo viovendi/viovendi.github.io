14da0e3175265ecc17e80e825d588d75a021b5f9
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-13 13:29:17 +0200

    Update hidefirstpage.js

--- customizations/organization/14269/hidefirstpage.js
+++ customizations/organization/14269/hidefirstpage.js
@@ -4,15 +4,17 @@ console.log('git first page')
 
 
 // --------------------------------------
-$('.event-article__event-info__title').each(function(i, element) {
+
+
+ var forstReplaceText = function() {
+   
+   $('.event-article__event-info__title').each(function(i, element) {
   // check if name contains 'Brennholz');
     if($(this).text().indexOf('Brennholz') >= 0){  
         $(this).parents().find('div.event-article__date-container__day > span:nth-child(2)').hide();
         $(this).parents().find('div.event-article__date-container__month > span:nth-child(2)').hide();
     }
  });
-
- var forstReplaceText = function() {
   //hide end day
  /* $('div.event-article__date-container__day > span:nth-child(2)').each(function(i, element) {
     if($(this).parents().find('.event-article__event-info__title').text().indexOf('Brennholz') >= 0){
