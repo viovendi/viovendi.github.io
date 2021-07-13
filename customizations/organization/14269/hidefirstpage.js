bd6a5777bf782ccb744d701800e2a315ccd029ab
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-13 12:11:10 +0200

    Update hidefirstpage.js

--- customizations/organization/14269/hidefirstpage.js
+++ customizations/organization/14269/hidefirstpage.js
@@ -7,6 +7,10 @@ console.log('git first page')
 $('.event-article__event-info__title').each(function(i, element) {
   if($(this).text().indexOf('Brennholz') >= 0){
   console.log($(this).parent().parent().find('div.event-article__date-container__day > span:nth-child(2)').text())
+      console.log($(this).parent().parent().find('.event-article__date-container__day > span:nth-child(2)').text())
+      console.log($(this).parent().parent().find('span:nth-child(2)').text())
+
+
   }
 });
  var forstReplaceText = function() {
