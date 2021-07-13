9ce805dd0799de24ec2df68c77564d1be4ac3e9f
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-13 12:15:06 +0200

    Update hidefirstpage.js

--- customizations/organization/14269/hidefirstpage.js
+++ customizations/organization/14269/hidefirstpage.js
@@ -7,7 +7,7 @@ console.log('git first page')
 $('.event-article__event-info__title').each(function(i, element) {
   if($(this).text().indexOf('Brennholz') >= 0){
    
-      console.log( $(this).closest('div.event-article__date-container__day > span:nth-child(2)').hide())
+      $(this).closest('div.event-article__date-container__day > span:nth-child(2)').hide();
 
 
   }
