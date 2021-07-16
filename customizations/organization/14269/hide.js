6489b937715d74c832f30d726089641c2261252d
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-16 09:19:56 +0200

    Update hide.js

--- customizations/organization/14269/hide.js
+++ customizations/organization/14269/hide.js
@@ -2,7 +2,37 @@ console.log('14269 Booking wrapper if Brennholz in name or Forst attribute');
 
 var name = $('.event-info__title').text()
 console.log(name);
+if(name.indexOf("Brennholz")>=0){
+      
+      // edit line-heigt of title
+      $('.event-info__title').css('line-height','2.87rem')
+      $('.event-info__date-container__day').css('line-height','2.87rem')
+      
+      //hide end day and month, replace '-' with '' and add 'Ab ' before the text
+      $('.customization-event-info-date-end').hide()
+      $('.customization-booking-area-wrapper-page1').hide();
+      $('div.event-info__date-container__month.customization-event-info-date-month > span:nth-child(2)').hide();
+  
+      
+      var startDay = $('.customization-event-info-date-start')
+      var startMonth = $('div.event-info__date-container__month.customization-event-info-date-month > span:nth-child(1)')
 
+      var day = startDay.text('Ab ' + startDay.text().replace(/^\s+|\s+$/g, ""))
+      var newTextMonth = startMonth.text().replace(/^\s+|\s+$/g, "")
+      var finalTextDay = day.text().replace('-','')
+      
+      var finalTextMonth = newTextMonth.replace('-','')
+
+
+      $('.customization-event-info-date-start').empty().html(finalTextDay.replace(/^\s+|\s+$/g, ""));
+      $('div.event-info__date-container__month.customization-event-info-date-month > span:nth-child(1)').empty().html(finalTextMonth);
+  
+       //hide date
+      $('.customization-event-info-date-details').hide();
+      $('div.m-box.event-info__details > div:nth-child(3)').hide();
+    }
+
+/*
 async function makeRequest(options) {
   let result = null;
   try {
@@ -64,5 +94,5 @@ async function getEventAttributes(eventId) {
 }
 
 getEventAttributes(eventId);
-
+*(
 
