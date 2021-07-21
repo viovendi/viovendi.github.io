a6da762052145f5d62390b546afbfe2928627ca7
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-07-21 13:28:38 +0300

    test

--- customizations/organization/14176/checkboxes.js
+++ customizations/organization/14176/checkboxes.js
@@ -1,14 +1,14 @@
    function addClassToField(shortName, className, wrapperClass){
     $('.'+wrapperClass+' .customization2_attendee_further-data .vv-nl-mb-lg').each(function(i, element) {
       var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
-      
+
       if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) > -1){
         $(this).closest('.vv-nl-mb-lg').addClass(className);
       }
     });
   }
 
-   
+
   function enabledDisabledButton(name){
     if(calcCheckboxes(name) < 5 ){
       $('.'+name+' .customization2_attendee_edit-action_save').attr('disabled', true);
@@ -20,7 +20,7 @@
       $('.'+name+' .button-error-message').remove();
     }
   }
-  
+
    function calcCheckboxes(name){
      var checked = 0;
      $('.'+name+' .check-boxes-wrapper .customization2_attendee_further-data_custom-question_checkbox-group vv-checkbox label').each(function(){
@@ -30,7 +30,7 @@
      });
      return checked;
    }
-   
+
    function markAsChecked(name){
      $('.'+name+' .check-boxes-wrapper .customization2_attendee_further-data_custom-question_checkbox-group .vv-checkbox').each(function(){
        $(this).trigger('click');
@@ -38,11 +38,13 @@
      // scroll page back to top
      $('.customization2_attendee_contact-data_first-name label').trigger('click');
    }
-   
+
    function clickedEdit(name){
       addClassToField('Mit dem Freiticket stimmst Du der Kontaktdaten-Weitergabe', 'check-boxes-wrapper', name);
       addClassToField('Mit dem VIP-Ticket stimmst Du der Kontaktdaten-Weitergabe', 'check-boxes-wrapper', name);
-      
+
+      addClassToField('Mit dem Freiticket stimmst Du der Daten-Weitergabe', name);
+
      //addClassToField('Mit dem Freiticket stimmst Du der Weitergabe Deiner Kontaktdaten', 'check-boxes-wrapper', name);
      //addClassToField('Mit dem Freiticket stimmst Du widerruflich der Weitergabe Deiner Kontaktdaten', 'check-boxes-wrapper', name);
      //addClassToField('Mit dem Ticket stimmst', 'check-boxes-wrapper', name);
@@ -59,32 +61,32 @@
 function addTextUnderQuestion(shortName, className, wrapperClass){
     $('.'+wrapperClass+' .customization2_attendee_further-data .vv-nl-mb-lg').each(function(i, element) {
       var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
-      
+
       if(item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf(shortName) > -1){
         $(this).find('.customization2_attendee_further-data_custom-question_label').css('font-size','12px') ;
-        $(this).after('<div class="addedtext" style="font-size: 12px; color: #343a3f; margin-top: 20px;margin-bottom: 30px;"><p>Die ausgewählten Partner erhalten meine Adress- und Kontaktdaten für die Bewerbung ihrer Produkte auch per E-Mail und Telefon sowie Daten zum Nachweis dieser Einwilligung, die diese in eigener Verantwortung verarbeiten. Du hast das Recht, diese Einwilligung jederzeit mit Wirkung für die Zukunft zu widerrufen. Nutze dafür folgende Kontaktmöglichkeit <a href="mailto:eventservice@vogel.de" target="_blank">eventservice@vogel.de</a>. Sitzt einer der Partner außerhalb der EU, so weisen wir darauf hin, dass dort ggfs. ein niedrigeres Datenschutzniveau als in der EU besteht. Weitere Informationen zum Umgang mit personenbezogenen Daten siehe <a href="https://www.vogelitakademie.de/datenschutz" target="_blank">Datenschutzerklärung</a>.</p></div>');         
+        $(this).after('<div class="addedtext" style="font-size: 12px; color: #343a3f; margin-top: 20px;margin-bottom: 30px;"><p>Die ausgewählten Partner erhalten meine Adress- und Kontaktdaten für die Bewerbung ihrer Produkte auch per E-Mail und Telefon sowie Daten zum Nachweis dieser Einwilligung, die diese in eigener Verantwortung verarbeiten. Du hast das Recht, diese Einwilligung jederzeit mit Wirkung für die Zukunft zu widerrufen. Nutze dafür folgende Kontaktmöglichkeit <a href="mailto:eventservice@vogel.de" target="_blank">eventservice@vogel.de</a>. Sitzt einer der Partner außerhalb der EU, so weisen wir darauf hin, dass dort ggfs. ein niedrigeres Datenschutzniveau als in der EU besteht. Weitere Informationen zum Umgang mit personenbezogenen Daten siehe <a href="https://www.vogelitakademie.de/datenschutz" target="_blank">Datenschutzerklärung</a>.</p></div>');
       }
     });
   }
 
-   
+
    function init(name){
       addClassToField('Mit dem Freiticket stimmst Du der Kontaktdaten-Weitergabe', 'check-boxes-wrapper', name);
       addClassToField('Mit dem VIP-Ticket stimmst Du der Kontaktdaten-Weitergabe', 'check-boxes-wrapper', name);
       addTextUnderQuestion('Mit dem VIP-Ticket stimmst Du der Kontaktdaten-Weitergabe', 'check-boxes-wrapper', name);
       addTextUnderQuestion('Mit dem Ticket stimmst Du der Kontaktdaten-Weitergabe', 'check-boxes-wrapper', name);
-      
+
      //addClassToField('Mit dem Freiticket stimmst Du der Weitergabe Deiner Kontaktdaten', 'check-boxes-wrapper', name);
      //addClassToField('Mit dem Freiticket stimmst Du widerruflich der Weitergabe Deiner Kontaktdaten', 'check-boxes-wrapper', name);
      //addClassToField('Mit dem Ticket stimmst', 'check-boxes-wrapper', name);
      //addClassToField('Mit dem Freiticket stimmst Du jederzeit ', 'check-boxes-wrapper', name);
      //addClassToField('Mit dem VIP-Ticket stimmst Du jederzeit widerruflich', 'check-boxes-wrapper', name);
-      
+
      //addTextUnderQuestion('Mit dem Freiticket stimmst Du jederzeit widerruflich', 'check-boxes-wrapper', name);
      //addTextUnderQuestion('Mit dem Ticket stimmst Du jederzeit widerruflich', 'check-boxes-wrapper', name);
      //addTextUnderQuestion('Hiermit stimmst Du jederzeit widerruflich', 'check-boxes-wrapper', name);
-     
-        
+
+
      markAsChecked(name);
      $('.'+name+' .check-boxes-wrapper .customization2_attendee_further-data_custom-question_checkbox-group').on('change', function(){
        enabledDisabledButton(name);
@@ -92,14 +94,14 @@ function addTextUnderQuestion(shortName, className, wrapperClass){
    }
    init('customization2_attendee-1');
 
-   
+
   var insertionListener = function(event) {
     if (event.animationName === "nodeInserted") {
       var classNames = event.target.classList;
 
       for (var i = 0; i < classNames.length; i++) {
         var className = classNames[i];
-        
+
         if(className.indexOf('customization2_attendee-') > -1){
           clickedEdit(className);
           return false;
@@ -112,4 +114,4 @@ function addTextUnderQuestion(shortName, className, wrapperClass){
 
   document.addEventListener("animationstart", insertionListener, false); // standard + firefox
   document.addEventListener("MSAnimationStart", insertionListener, false); // IE
-  document.addEventListener("webkitAnimationStart", insertionListener, false); // 
+  document.addEventListener("webkitAnimationStart", insertionListener, false); //
