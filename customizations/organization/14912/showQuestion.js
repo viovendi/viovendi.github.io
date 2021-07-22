70a889d91280c10a50fe23bbde60ecf36f9d159f
vbodend <69503064+vbodend@users.noreply.github.com>
2021-07-22 11:14:50 +0200

    Update showQuestion.js

--- customizations/organization/14912/showQuestion.js
+++ customizations/organization/14912/showQuestion.js
@@ -1,4 +1,4 @@
-console.log('git works Wella 14912'):
+console.log('git works Wella 14912');
 /*
 1. hide question mit dem namen (z)
 wenn kunde auf ja bei frage xy drückt -> show question mit dem namen ("z")
