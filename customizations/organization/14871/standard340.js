c6e2c18ac0f42a6c3992e0efa220ba4e430a08ea
vbodend <69503064+vbodend@users.noreply.github.com>
2021-08-11 16:40:14 +0200

    Update standard340.js

--- customizations/organization/14871/standard340.js
+++ customizations/organization/14871/standard340.js
@@ -50,29 +50,34 @@ var product0 = $$('.customization2_attendee_further-data_product').findField('So
   
 $(product0).addClass('show');
 // Workshopblock1
-var workshopblock1 = ["Workshopslot 1 (16:15-16:35) "];
+// Workshopblock1
+var workshopblock1 = ["Value + (16:15 - 16:35)","GTM Excellence (16:15 - 16:35)","Operations Excellence (16:15 - 16:35)","SaaS Acceleration (16:15 - 16:35)","Software Implementation Efficiency (16:15 - 16:35)","36One – Establish ‘one version of the truth’ (16:15 - 16:35)"];
 
-var product1 = $$('.customization2_attendee_further-data_product').findField('Keynote: Stark durch Krisen – von der Kunst, nicht den Kopf zu verlieren');
-$(product1).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock1);">20.09.2021: Keynote: 13.00 - 14.00 Uhr ▼</span>');
+var product1 = $$('.customization2_attendee_further-data_product').findField('Value + (16:15 - 16:35)');
+$(product1).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock1);">Workshopslot 1 (16:15-16:35) ▼</span>');
  
 // Workshopblock2
-var workshopblock2 = ["Round Table: Sport in unseren Locations","Landkarte der Veranstaltungswirtschaft - Studienergebnisse Zähl Dazu", "Neue Pflichten und Datenströme bei hybriden Veranstaltungen für Veranstaltungshäuser", "Digitalisierung ist die Antwort – was war die Frage?"];
-var product2 = $$('.customization2_attendee_further-data_product').findField('Round Table: Sport in unseren Locations');
+var workshopblock2 = ["Value + (16:40 - 17:00)","GTM Excellence (16:40 - 17:00)","Operations Excellence (16:40 - 17:00)","Software Implementation Efficiency (16:40 - 17:00)","SaaS Acceleration (16:40 - 17:00)","36One – Establish ‘one version of the truth’ (16:40 - 17:00)"];
+var product2 = $$('.customization2_attendee_further-data_product').findField('Value + (16:40 - 17:00)');
  
-$(product2).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock2);" >20.09.2021: Parallele Sessions: 14.15 - 15.15 Uhr ▼</span>');
+$(product2).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock2);">Workshopslot 2 (16:40-17:00)▼</span>');
  productChecked(workshopblock2);
+
 // Workshopblock3
-var workshopblock3 = ["Grill the Expert: Sie fragen - unsere Kooperationsanwälte antworten","Die Relevanz von Hygiene- und Sicherheitskonzepten","Digital Leadership: Einblicke in digitale Arbeitsweisen & Führungskulturen","Nachhaltigkeit in der Veranstaltungsbranche"];
-var product3 = $$('.customization2_attendee_further-data_product').findField('Grill the Expert: Sie fragen - unsere Kooperationsanwälte antworten');
-$(product3).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock3);">20.09.2021: Parallele Sessions 16.00 - 17.00 Uhr ▼</span>');
+var workshopblock3 = ["Value + (17:05 - 17:25)","GTM Excellence (17:05 - 17:25)","Software Implementation Efficiency (17:05 - 17:25)","Operations Excellence (17:05 - 17:25)","SaaS Acceleration (17:05 - 17:25)","36One – Establish ‘one version of the truth’ (17:05 - 17:25)"];
+var product3 = $$('.customization2_attendee_further-data_product').findField('Value + (17:05 - 17:25)');
+$(product3).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock3);">Workshopslot 3 (17:05-17:25)▼</span>');
 productChecked(workshopblock3);
+
 // Workshopblock4
-var workshopblock4 = ["Keynote: Tatort WWW - Live Hack"];
-var product4 = $$('.customization2_attendee_further-data_product').findField('Keynote: Tatort WWW - Live Hack');
-$(product4).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock4);">21.09.2021: Keynote 09.15 - 10.30 Uhr ▼</span>');
+var workshopblock4 = ["Digital Roadmap/ New Growth Cases (9:25 - 9:50)","ERP 2025 (9:25 - 9:50)","The Aareon triad: This is what we stand for!  Vision, Mission and Values (9:25 - 9:50)","Employee Engagement Program (9:25 - 9:50)","Innovation Management (9:25 - 9:50)","We support you….GAM, GAB, Group Application Landscape? (9:25 - 9:50)"];
+var product4 = $$('.customization2_attendee_further-data_product').findField('Digital Roadmap/ New Growth Cases (9:25 - 9:50)');
+$(product4).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock4);">Workshopslot 1 (09:25-09:50)▼</span>');
 productChecked(workshopblock4);
-var workshopblock4a = ["Stadtrundgang und Kollegenabend - See the C - im Weltecho"];
-var product4a = $$('.customization2_attendee_further-data_product').findField('Stadtrundgang und Kollegenabend - See the C - im Weltecho');
-$(product4a).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock4a);">20.09.2021 Abendveranstaltung ab 17.45 Uhr ▼</span>');
-productChecked(workshopblock4a);
+
+// Workshopblock5
+var workshopblock5 = ["Digital Roadmap/ New Growth Cases (9:55 - 10:20)","ERP 2025 (9:55 - 10:20)","The Aareon triad: This is what we stand for!  Vision, Mission and Values (9:55 - 10:20)","Employee Engagement Program (9:55 - 10:20)","Innovation Management (9:55 - 10:20)","We support you….GAM, GAB, Group Application Landscape? (9:55 - 10:20)"];
+var product5 = $$('.customization2_attendee_further-data_product').findField('Digital Roadmap/ New Growth Cases (9:55 - 10:20));
+$(product5).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock4a);">Workshopslot 2 (09:55-10:20)▼</span>');
+productChecked(workshopblock5);
 
