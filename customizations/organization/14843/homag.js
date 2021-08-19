14bcd2066b1591ff8df5b70d741a7be632278026
vbodend <69503064+vbodend@users.noreply.github.com>
2021-08-19 09:52:35 +0200

    Update homag.js

--- customizations/organization/14843/homag.js
+++ customizations/organization/14843/homag.js
@@ -82,6 +82,9 @@ var workshopblockWeKnow = ["We support you….GAM, GAB, Group Application Landsc
  //productChecked(workshopblockWeKnow);
 
 function editFunc(){
+  
+  var product11 = $$('.customization2_attendee_further-data_product')
+  $(product11).addClass('show');
   var product11 = $$('.customization2_attendee_further-data_product').findField('Teilnahme | Montag, 25. Oktober 2021');
   var product12 = $$('.customization2_attendee_further-data_product').findField('Dienstag I 26 Oktober 2021');
   var product13 = $$('.customization2_attendee_further-data_product').findField('Value + (17:05 - 17:25)');
