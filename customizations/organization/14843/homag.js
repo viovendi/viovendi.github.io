9b7f74b5bff4f3fbb4b6da165d5e439a1f816662
vbodend <69503064+vbodend@users.noreply.github.com>
2021-08-19 10:09:57 +0200

    Update homag.js

--- customizations/organization/14843/homag.js
+++ customizations/organization/14843/homag.js
@@ -45,8 +45,8 @@ function editFunc(){
   
 $('.customization2_attendee_further-data_custom-question').show()
 $('.customization2_attendee_further-data_product').show()
-  var product11 = $$('.customization2_attendee_further-data_product').findField('Teilnahme | Montag, 25. Oktober 2021');
-  var product12 = $$('.customization2_attendee_further-data_product').findField('Dienstag I 26 Oktober 2021');
+  var product11 = $$('.customization2_attendee_further-data .customization2_attendee_further-data_product').findField('Teilnahme | Montag, 25. Oktober 2021');
+  var product12 = $$('.customization2_attendee_further-data .customization2_attendee_further-data_product').findField('Dienstag I 26 Oktober 2021');
 
   
   $(product11).before('<span class= "workshops" onclick = "showWorkshopblock(workshopblock1);">Teilnahme | Montag, 25. Oktober 2021▼</span>');
