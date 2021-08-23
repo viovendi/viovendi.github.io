0e610f17a278c3003274f7e20535581c1c63a05d
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-08-23 15:28:07 +0300

    test

--- customizations/organization/14137/complex864.js
+++ customizations/organization/14137/complex864.js
@@ -1,5 +1,5 @@
 console.log("COMPLEX-864")
- console.log('GTM file connected');
+console.log('GTM file connected');
 
 // Hide questions
 $('.customization2_attendee_further-data_custom-question-2').hide();
@@ -13,47 +13,50 @@ var radioGroupYesNo = $$('.customization2_attendee_further-data_custom-question'
 var inputs = $(radioGroupYesNo).find('input');
 console.log('inputs');
 console.log(inputs);
-inputs.each(function( i, element ) {
-  $(element).change(function(input) {
+inputs.each(function (i, element) {
+  $(element).change(function (input) {
     //on change show these 2 questions
-const option = $(input.currentTarget).siblings('.customization2_attendee_further-data_custom-question_radio-line_label').text().trim();
-    if(option.includes('Ja')){
+    const option = $(input.currentTarget).siblings('.customization2_attendee_further-data_custom-question_radio-line_label').text().trim();
+    if (option.includes('Ja')) {
       $('.customization2_attendee_further-data_custom-question-2').show();
-      $('.customization2_attendee_edit-action_save').on('click', ()=>{
-       console.log( $('.customization2_attendee_further-data_custom-question-2').val())
-       // $('.customization2_attendee_further-data_custom-question-2').find('vv-input').addClass('ng-invalid ng-dirty');
-      })
-    }else{
+      $('.customization2_attendee_edit-action_save').on('click', () => {
+        if (!$('.customization2_attendee_further-data_custom-question-2').val()) {
+          console.log('empty')
+        } else {
+          console.log('!empty')
+        }
+      });
+    } else {
       console.log('hide!');
       $('.customization2_attendee_further-data_custom-question-2').hide();
     }
-   });
+  });
 });
 
 //show 3 questions if product is checked, else hide
-function productChecked(products){
- $(products).each( function(i, element) {
-  var product = $$('.customization2_attendee_further-data_product').findField(element);
-  var input = $(product).find('input');
-   input.change(function(){
-     if(input.is(':checked')){
-       $('.customization2_attendee_further-data_custom-question-4').show();
-       $('.customization2_attendee_further-data_custom-question-5').show();
-       $('.customization2_attendee_further-data_custom-question-6').show();
-     }
-     if(!input.is(':checked')){
-      $('.customization2_attendee_further-data_custom-question-4').hide();
-      $('.customization2_attendee_further-data_custom-question-5').hide();
-      $('.customization2_attendee_further-data_custom-question-6').hide();
-     }
-   });
- });
+function productChecked(products) {
+  $(products).each(function (i, element) {
+    var product = $$('.customization2_attendee_further-data_product').findField(element);
+    var input = $(product).find('input');
+    input.change(function () {
+      if (input.is(':checked')) {
+        $('.customization2_attendee_further-data_custom-question-4').show();
+        $('.customization2_attendee_further-data_custom-question-5').show();
+        $('.customization2_attendee_further-data_custom-question-6').show();
+      }
+      if (!input.is(':checked')) {
+        $('.customization2_attendee_further-data_custom-question-4').hide();
+        $('.customization2_attendee_further-data_custom-question-5').hide();
+        $('.customization2_attendee_further-data_custom-question-6').hide();
+      }
+    });
+  });
 }
 
-var workshopblockValue = ["Goodie Paket"] ;
+var workshopblockValue = ["Goodie Paket"];
 productChecked(workshopblockValue);
 
 
-$('.customization2_attendee_edit-action_save').on('click', (e)=>{
+$('.customization2_attendee_edit-action_save').on('click', (e) => {
   console.log('click')
 })
