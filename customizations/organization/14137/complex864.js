bea9cd3b9a88bef528d75f4ecc3438854e459875
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-08-23 15:35:30 +0300

    test

--- customizations/organization/14137/complex864.js
+++ customizations/organization/14137/complex864.js
@@ -20,7 +20,7 @@ inputs.each(function (i, element) {
     if (option.includes('Ja')) {
       $('.customization2_attendee_further-data_custom-question-2').show();
       $('.customization2_attendee_edit-action_save').on('click', () => {
-        if ($('.customization2_attendee_further-data_custom-question-2').val().trim().length) {
+        if (!$('.customization2_attendee_further-data_custom-question-2').val().trim().length) {
           console.log('empty')
         } else {
           console.log('!empty')
