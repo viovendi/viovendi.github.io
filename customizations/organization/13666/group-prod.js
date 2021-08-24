512920eda5588d8ee8c1cd658bca3786402b5970
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-08-24 18:53:08 +0300

    fix

--- customizations/organization/13666/group-prod.js
+++ customizations/organization/13666/group-prod.js
@@ -45,5 +45,5 @@ $(field4).hide();
 var checkboxGroup = $$('.customization2_attendee_further-data_custom-question').findCheckboxGroup('Shuttle');
 
 $(checkboxGroup).find('input').change(function(){
-    consol.log($(this).parent('label').text())
+    console.log($(this).parent('label').text())
 })
