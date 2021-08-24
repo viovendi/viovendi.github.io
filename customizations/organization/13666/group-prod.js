0f2ef1ef6de23ce3c294470c136da1ba49ca5043
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-08-24 17:14:51 +0300

    append

--- customizations/organization/13666/group-prod.js
+++ customizations/organization/13666/group-prod.js
@@ -4,9 +4,10 @@ console.log($('.power_talk'))
 $('vv-additional-question-product').each(function (index, value) {
     const prodName = $(this).find('label p').text().trim();
     if(prodName.includes('vor Ort')) {
-$('.power_talk_vor_ort').appendTo($(this))
+$('.power_talk_vor_ort').append($(this))
     }
+
     if(prodName.includes('online')) {
-        $('.power_talk_online').appendTo($(this))
+        $('.power_talk_online').append($(this))
             }
 });
