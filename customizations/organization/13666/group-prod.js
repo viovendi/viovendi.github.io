74820b4595a3d103b58f8bae4fb20d1c353653b8
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-08-24 17:02:45 +0300

    prodName

--- customizations/organization/13666/group-prod.js
+++ customizations/organization/13666/group-prod.js
@@ -1,1 +1,7 @@
 console.log($('.power_talk'))
+
+
+$('vv-additional-question-product').each(function (index, value) {
+    const prodName = $(this).find('label p').text();
+    console.log(prodName)
+});
