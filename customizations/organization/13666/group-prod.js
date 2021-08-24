3751b4509f10371804630eb4be63bbdebe51f95a
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-08-24 18:36:54 +0300

    test

--- customizations/organization/13666/group-prod.js
+++ customizations/organization/13666/group-prod.js
@@ -34,3 +34,12 @@ $('.investment_forum h4').click(function(){
 
 var field1 = $$('.customization2_attendee_further-data_custom-question').findField('Ankunft Datum (optional)');
 $(field1).hide();
+
+var field2 = $$('.customization2_attendee_further-data_custom-question').findField('Ankunft Uhrzeit (optional)');
+$(field2).hide();
+
+var field3 = $$('.customization2_attendee_further-data_custom-question').findField('Ankunft mit Flug Nr. / Bahn Nr. (optional)');
+$(field3).hide();
+
+var field4 = $$('.customization2_attendee_further-data_custom-question').findField('Abfahrts-/Abflugsort (optional)');
+$(field4).hide();
