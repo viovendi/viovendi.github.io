a86efd0460af016c977ef8106191e2b8159ed471
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-08-24 17:33:18 +0300

    test

--- customizations/organization/13666/group-prod.js
+++ customizations/organization/13666/group-prod.js
@@ -3,11 +3,11 @@ console.log($('.power_talk'))
 
 $('vv-additional-question-product').each(function (index, value) {
     const prodName = $(this).find('label p').text().trim();
-    if(prodName.includes('vor Ort')) {
-$('.power_talk_vor_ort').append($(this))
+    if(prodName.includes('Power Talk')) {
+$('.power_talk').append($(this))
     }
 
-    if(prodName.includes('online')) {
-        $('.power_talk_online').append($(this))
+    if(prodName.includes('Investment Forum')) {
+        $('.investment_forum').append($(this))
             }
 });
