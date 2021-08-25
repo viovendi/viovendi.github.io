cccab3406e58eef229c0d4e4c665db02caab7399
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-08-25 17:13:01 +0300

    fix

--- customizations/organization/13666/group-prod.js
+++ customizations/organization/13666/group-prod.js
@@ -1,67 +1,67 @@
-function disableProd(disable){
-    $('.customization2_attendee_further-data_product').each(function(i, element) {
-          var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-         var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-             if(checkboxName.indexOf(disable) >= 0){
-                checkbox.prop( "disabled", true )
-             }
-    });
-   }
-
-function enableProd(disable){
-$('.customization2_attendee_further-data_product').each(function(i, element) {
+function disableProd(disable) {
+    $('.customization2_attendee_further-data_product').each(function (i, element) {
         var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
         var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-            if(checkboxName.indexOf(disable) >= 0){
-            checkbox.prop( "disabled", false )
-            }
-});
+        if (checkboxName.indexOf(disable) >= 0) {
+            checkbox.prop("disabled", true)
+        }
+    });
 }
 
-function prodCheked(shortName, disable){
-    $('.customization2_attendee_further-data_product').each(function(i, element) {
-      var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+function enableProd(disable) {
+    $('.customization2_attendee_further-data_product').each(function (i, element) {
+        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+        var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+        if (checkboxName.indexOf(disable) >= 0) {
+            checkbox.prop("disabled", false)
+        }
+    });
+}
 
-      checkbox.change(function(){
-        if(checkbox.is(':checked')){
-          if(checkboxName.indexOf(shortName) >= 0){
-             disableProd(disable)
-          }
-        }else if(!checkbox.is(':checked')){
-          if(checkboxName.indexOf(shortName) >= 0){
-             enableProd(disable)
-          }
-          }
-      })
+function prodCheked(shortName, disable) {
+    $('.customization2_attendee_further-data_product').each(function (i, element) {
+        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+        var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+
+        checkbox.change(function () {
+            if (checkbox.is(':checked')) {
+                if (checkboxName.indexOf(shortName) >= 0) {
+                    disableProd(disable)
+                }
+            } else if (!checkbox.is(':checked')) {
+                if (checkboxName.indexOf(shortName) >= 0) {
+                    enableProd(disable)
+                }
+            }
+        })
     })
-  }
+}
 
 
 
 $('vv-additional-question-product').each(function (index, value) {
     const prodName = $(this).find('label p').text().trim();
-    if(prodName.includes('Power Talk')) {
+    if (prodName.includes('Power Talk')) {
         $(this).hide()
-$('.power_talk').append($(this))
+        $('.power_talk').append($(this))
     }
 
-    if(prodName.includes('Investment Forum')) {
+    if (prodName.includes('Investment Forum')) {
         $(this).hide()
         $('.investment_forum').append($(this))
-            }
+    }
 });
 
-$('.power_talk h4').click(function(e){
-    if($('.power_talk').find('vv-additional-question-product:visible').length){
+$('.power_talk h4').click(function (e) {
+    if ($('.power_talk').find('vv-additional-question-product:visible').length) {
         $('.power_talk').find('vv-additional-question-product').hide();
-    }else{
+    } else {
         $('.power_talk').find('vv-additional-question-product').show();
     }
 
 })
-$('.investment_forum h4').click(function(){
-    if($('.investment_forum').find('vv-additional-question-product:visible').length){
+$('.investment_forum h4').click(function () {
+    if ($('.investment_forum').find('vv-additional-question-product:visible').length) {
         $('.investment_forum').find('vv-additional-question-product').hide();
     } else {
         $('.investment_forum').find('vv-additional-question-product').show();
@@ -84,27 +84,30 @@ $(field4).hide();
 
 var checkboxGroup = $$('.customization2_attendee_further-data_custom-question').findCheckboxGroup('Shuttle');
 
-$(checkboxGroup).find('input').change(function(){
-    if($(this).is(":checked")){
+$(checkboxGroup).find('input').change(function () {
+    if ($(this).is(":checked")) {
         const option = $(this).parent('label').text().trim();
-        if(option.includes('Ich benötige einen Shuttle-Service vom Flughafen zur Location')){
+        if (option.includes('Ich benötige einen Shuttle-Service vom Flughafen zur Location')) {
             $(field1).show();
             $(field2).show();
             $(field3).show();
             $(field4).show();
+            if (locale === "de-de") {
+                $(checkboxGroup).after('<p style="margin-top: 20px;">Wenn Sie einen Shuttle-Service vom Flughafen zur Location benötigen, geben Sie hier bitte Ihre Anreisedaten an.</p>')
+            }
+
+            if (locale === "en-us") {
+                $(checkboxGroup).after('<p style="margin-top: 20px;">If you need a shuttle servie from the airport to the location, please enter your arrival information here.</p>')
+            }
         }
-    }else {
+    } else {
         $(field1).hide();
         $(field2).hide();
         $(field3).hide();
         $(field4).hide();
     }
 })
-if(locale === "de-de"){
-$(checkboxGroup).after('<p style="margin-top: 20px;">Wenn Sie einen Shuttle-Service vom Flughafen zur Location benötigen, geben Sie hier bitte Ihre Anreisedaten an.</p>')}
 
-if(locale === "en-us"){
-    $(checkboxGroup).after('<p style="margin-top: 20px;">If you need a shuttle servie from the airport to the location, please enter your arrival information here.</p>')}
 
 
 
