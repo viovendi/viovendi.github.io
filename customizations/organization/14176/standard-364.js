c07b3e3f0bd98b842e57060d160d0f659d6a73bb
fabianhasselbacher <69850626+fabianhasselbacher@users.noreply.github.com>
2021-08-31 14:56:05 +0200

    Update standard-364.js

--- customizations/organization/14176/standard-364.js
+++ customizations/organization/14176/standard-364.js
@@ -1,3 +1,6 @@
-/* Activate Data Privacy Checkbox */
-$('.vv-checkbox').trigger('click');
-	console.log('clicked');
+//Activate Data Privacy Checkbox 
+$('.vv-radio').trigger('click');
+console.log('clicked')
+
+//Add text below Data Privacy
+$('.vv-radio__label-text').append('Die ausgewählten Partner erhalten meine Adress- und Kontaktdaten für die Bewerbung ihrer Produkte auch per E-Mail und Telefon sowie Daten zum Nachweis dieser Einwilligung, die diese in eigener Verantwortung verarbeiten. Du hast das Recht, diese Einwilligung jederzeit mit Wirkung für die Zukunft zu widerrufen. Nutze dafür folgende Kontaktmöglichkeit <a href="eventservice@vogel.de" target="_blank">eventservice@vogel.de</a>. Sitzt einer der Partner außerhalb der EU, so weisen wir darauf hin, dass dort ggfs. ein niedrigeres Datenschutzniveau als in der EU besteht. Weitere Informationen zum Umgang mit personenbezogenen Daten siehe <a href="https://www.vogelitakademie.de/datenschutz" target="_blank">Datenschutzerklärung</a>.');
