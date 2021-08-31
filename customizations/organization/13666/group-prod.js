c2d3333b49b7ba3d3cd21aa7e2ebb046e45cb171
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-08-31 20:56:55 +0300

    test

--- customizations/organization/13666/group-prod.js
+++ customizations/organization/13666/group-prod.js
@@ -71,16 +71,16 @@ $('vv-additional-question-product').each(function (index, value) {
 
 
 var field1 = $$('.customization2_attendee_further-data_custom-question').findField('Ankunft Datum (optional)');
-//$(field1).hide();
+$(field1).hide();
 
 var field2 = $$('.customization2_attendee_further-data_custom-question').findField('Ankunft Uhrzeit (optional)');
-//$(field2).hide();
+$(field2).hide();
 
 var field3 = $$('.customization2_attendee_further-data_custom-question').findField('Ankunft mit Flug Nr. / Bahn Nr. (optional)');
-//$(field3).hide();
+$(field3).hide();
 
 var field4 = $$('.customization2_attendee_further-data_custom-question').findField('Abfahrts-/Abflugsort (optional)');
-//$(field4).hide();
+$(field4).hide();
 
 var checkboxGroup = $$('.customization2_attendee_further-data_custom-question').findCheckboxGroup('Shuttle');
 
@@ -88,10 +88,10 @@ $(checkboxGroup).find('input').change(function () {
     if ($(this).is(":checked")) {
         const option = $(this).parent('label').text().trim();
         if (option.includes('Ich benötige einen Shuttle-Service vom Flughafen zur Location')) {
-            // $(field1).show();
-            // $(field2).show();
-            // $(field3).show();
-            // $(field4).show();
+            $(field1).show();
+            $(field2).show();
+            $(field3).show();
+            $(field4).show();
             if (locale === "de-de") {
                 $(checkboxGroup).after(`<p class="service"  style="margin-top: 20px;">Wenn Sie einen Shuttle-Service vom Flughafen zur Location benötigen, geben Sie hier bitte Ihre Anreisedaten an.</p>`)
             }
