a7f3fa1392caba2f7c29bd989620c770f23d60e2
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-08-31 20:37:05 +0300

    test

--- customizations/organization/13666/group-prod.js
+++ customizations/organization/13666/group-prod.js
@@ -52,22 +52,22 @@ $('vv-additional-question-product').each(function (index, value) {
     }
 });
 
-$('.power_talk h4').click(function (e) {
-    if ($('.power_talk').find('vv-additional-question-product:visible').length) {
-        $('.power_talk').find('vv-additional-question-product').hide();
-    } else {
-        $('.power_talk').find('vv-additional-question-product').show();
-    }
+// $('.power_talk h4').click(function (e) {
+//     if ($('.power_talk').find('vv-additional-question-product:visible').length) {
+//         $('.power_talk').find('vv-additional-question-product').hide();
+//     } else {
+//         $('.power_talk').find('vv-additional-question-product').show();
+//     }
 
-})
-$('.investment_forum h4').click(function () {
-    if ($('.investment_forum').find('vv-additional-question-product:visible').length) {
-        $('.investment_forum').find('vv-additional-question-product').hide();
-    } else {
-        $('.investment_forum').find('vv-additional-question-product').show();
-    }
+// })
+// $('.investment_forum h4').click(function () {
+//     if ($('.investment_forum').find('vv-additional-question-product:visible').length) {
+//         $('.investment_forum').find('vv-additional-question-product').hide();
+//     } else {
+//         $('.investment_forum').find('vv-additional-question-product').show();
+//     }
 
-})
+// })
 
 
 var field1 = $$('.customization2_attendee_further-data_custom-question').findField('Ankunft Datum (optional)');
