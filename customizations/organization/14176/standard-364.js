e5ea63913ab8999f1796f2027b45d390d8535bbb
fabianhasselbacher <69850626+fabianhasselbacher@users.noreply.github.com>
2021-08-31 15:33:14 +0200

    Delete standard-364.js

--- customizations/organization/14176/standard-364.js
+++
@@ -1,6 +0,0 @@
-//Activate Data Privacy Checkbox 
-$('.customization2_attendee_further-data_custom-question customization2_attendee_further-data_custom-question-13 .vv-radio').trigger('click');
-console.log('clicked')
-
-//Add text below Data Privacy
-$(item).find('.customization2_attendee_further-data_custom-question customization2_attendee_further-data_custom-question-13 > vv-radio-group > div > div > div > vv-radio > label > div > p').empty().html('.<br/>Die ausgewählten Partner erhalten meine Adress- und Kontaktdaten für die Bewerbung ihrer Produkte auch per E-Mail und Telefon sowie Daten zum Nachweis dieser Einwilligung, die diese in eigener Verantwortung verarbeiten. Du hast das Recht, diese Einwilligung jederzeit mit Wirkung für die Zukunft zu widerrufen. Nutze dafür folgende Kontaktmöglichkeit <a href="eventservice@vogel.de" target="_blank">eventservice@vogel.de</a>. Sitzt einer der Partner außerhalb der EU, so weisen wir darauf hin, dass dort ggfs. ein niedrigeres Datenschutzniveau als in der EU besteht. Weitere Informationen zum Umgang mit personenbezogenen Daten siehe <a href="https://www.vogelitakademie.de/datenschutz" target="_blank">Datenschutzerklärung</a>.');
