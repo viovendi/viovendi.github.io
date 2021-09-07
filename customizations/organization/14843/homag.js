d448253756acd889bea5d5236998c74d5cce8058
vbodend <69503064+vbodend@users.noreply.github.com>
2021-09-07 15:29:06 +0200

    Update homag.js

--- customizations/organization/14843/homag.js
+++ customizations/organization/14843/homag.js
@@ -111,10 +111,10 @@ function main(wrapper){
     console.log(wrapper);
 
     //Add Link to text
-    var element = $("label:contains('live.homag.com')");
-    console.log(element);
-    element.replace("live.homag.com", "<a target='_blank' href='https://live.homag.com/'>live.homag.com</a>");
-    console.log("replaced");
+    //var element = $("label:contains('live.homag.com')");
+    //console.log(element);
+    //element.replace("live.homag.com", "<a target='_blank' href='https://live.homag.com/'>live.homag.com</a>");
+   // console.log("replaced");
     
     
     $('.customization2_attendee-state_edit .customization2_attendee_further-data_custom-question-1').show();
