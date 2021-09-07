379216ff68317df4484b5985afad346d7a258f13
Alex <olexiy.movchan@gmail.com>
2021-09-07 16:52:53 +0300

    14176/checkboxes.js - removed duplicated question

--- customizations/organization/14176/checkboxes.js
+++ customizations/organization/14176/checkboxes.js
@@ -101,7 +101,6 @@ function init(name) {
 	addTextUnderQuestion('Mit dem Ticket stimmst Du der Datenweitergabe', 'check-boxes-wrapper', name);
 	addTextUnderQuestionVIP('Mit dem ermäßigten VIP-Ticket stimmen Sie', 'check-boxes-wrapper', name);
 	
-	addTextUnderQuestionVIP('Mit dem ermäßigten VIP-Ticket stimmen Sie der Datenweitergabe', 'check-boxes-wrapper', name);
 	addTextUnderQuestionVIP('Mit dem ermäßigten VIP-Ticket stimmst Du der Daten-Weitergabe', 'check-boxes-wrapper', name);
 
 	//addClassToField('Mit dem Freiticket stimmst Du der Weitergabe Deiner Kontaktdaten', 'check-boxes-wrapper', name);
