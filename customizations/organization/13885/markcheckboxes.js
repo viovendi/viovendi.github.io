a5e7dc64ba41318528ed4832b9a2fd64d2f6e5ff
fabianhasselbacher <69850626+fabianhasselbacher@users.noreply.github.com>
2021-09-16 12:56:59 +0200

    Update markcheckboxes.js

--- customizations/organization/13885/markcheckboxes.js
+++ customizations/organization/13885/markcheckboxes.js
@@ -2,18 +2,19 @@ function markAsChecked(name) {
 	//search all further questions
 	$('.customization2_attendee_further-data_custom-question').each(function (i, element) {
 
-		//.customization2_attendee .customization2_attendee_further-data .vv-nl-mb-lg
-		// check all labels and check if it contains "Einwilligungserklärung"
-		var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
-		if (item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf('Hiermit erkläre ich mich') > -1) {
-			console.log("Bedingung eingetreten" + item);
-			
-			//get parent element and move to vv-checkbox to click it
-			var parentElement = $(this).parent();
-			console.log(parentElement);
-			$(parentElement + " > div > div > vv-checkbox").trigger('click');
-			console.log("clicked")
-		});
+			//.customization2_attendee .customization2_attendee_further-data .vv-nl-mb-lg
+			// check all labels and check if it contains "Hiermit erkläre ich mich"
+			var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
+			if (item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf('Hiermit erkläre ich mich') > -1) {
+				console.log("Bedingung eingetreten" + item);
+
+				//get parent element and move to vv-checkbox to click it
+				var parentElement = $(this).parent();
+				console.log(parentElement);
+				$(parentElement + " > div > div > vv-checkbox").trigger('click');
+				console.log("clicked");
+			};
+		};
 	};
 };
 
