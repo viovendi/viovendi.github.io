23d7f9062135c2d40083c1b4f15c4ef4220b57bb
fabianhasselbacher <69850626+fabianhasselbacher@users.noreply.github.com>
2021-09-16 11:28:14 +0200

    Update markcheckboxes.js

--- customizations/organization/13885/markcheckboxes.js
+++ customizations/organization/13885/markcheckboxes.js
@@ -5,11 +5,10 @@ function markAsChecked(name) {
 		//.customization2_attendee .customization2_attendee_further-data .vv-nl-mb-lg
 		// check all labels and check if it contains "Einwilligungserklärung"
 		var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
-		console.log(item);
 		if (item !== '' && item.replace(/^\s+|\s+$/g, "").indexOf('Hiermit erkläre ich mich') > -1) {
-			console.log("Bedingung eingetreten" + item)
+			console.log("Bedingung eingetreten" + item);
 			$('.' + name + ' .check-boxes-wrapper .customization2_attendee_further-data_custom-question_checkbox-group .vv-checkbox').each(function () {
-				console.log(this)
+				console.log(this);
 				if (!$(this).hasClass('checked-manually')) {
 					$(this).trigger('click');
 					$(this).addClass('checked-manually');
