b65876fc4e6541b86f333faa30d964dbd22aa4a3
fabianhasselbacher <69850626+fabianhasselbacher@users.noreply.github.com>
2021-09-27 14:43:03 +0200

    Update edit-terms.js

--- customizations/organization/14834/edit-terms.js
+++ customizations/organization/14834/edit-terms.js
@@ -3,4 +3,4 @@ console.log("Standard-436");
 
 $('.customization2_booking-terms_text').replaceWith('<p class="terms_new">AGB: Ich habe die <a src="https://www.rtl2.de/consent?targetUrl=%2Fseite%2Fallgemeine-geschaeftsbedingungen" target="_blank" >Allgemeinen Geschäftsbedingungen zu RTL2 Gewinnspielen</a> sowie die <a src="https://www.rtl2.de/consent?targetUrl=%2Fdatenschutz" target="_blank" >Datenschutzerklärung</a> der RTL2 Fernsehen GmbH & Co. KG zur Kenntnis genommen und bin mit deren Geltung einverstanden.</p>')
 $('.terms_new').css({"font-size": "14px", "line-height": "25px", "max-width": "90%"});
-$('.customization2_booking-terms_list).css({"display": "none"}:);
+$('.customization2_booking-terms_list').css({"display": "none"}:);
