49dee5cc6c69e9828c47f08234a5241ab844b4aa
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-09-30 18:08:54 +0300

    test

--- customizations/organization/13666/group-prod.js
+++ customizations/organization/13666/group-prod.js
@@ -22,7 +22,11 @@ function prodCheked(shortName, disable) {
     $('.customization2_attendee_further-data_product').each(function (i, element) {
         var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
         var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-
+        if (checkbox.is(':checked')) {
+            if (checkboxName.indexOf(shortName) >= 0) {
+                disableProd(disable)
+            }
+        }
         checkbox.change(function () {
             if (checkbox.is(':checked')) {
                 if (checkboxName.indexOf(shortName) >= 0) {
