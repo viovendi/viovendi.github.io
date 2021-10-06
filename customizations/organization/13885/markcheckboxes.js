b06d305560a7b1e047a6ce7fb5c8df50db7281cd
Alex <olexiy.movchan@gmail.com>
2021-10-06 15:21:16 +0300

    markcheckboxes.js - rolled back

--- customizations/organization/13885/markcheckboxes.js
+++ customizations/organization/13885/markcheckboxes.js
@@ -25,10 +25,5 @@ function hideAndMoveCheckbox() {
 	$('.customization2_booking-terms_text').css("left", "24px");
 	console.log('hided')
 }
-// markAsChecked('customization2_attendee');
-// hideAndMoveCheckbox();
-
-if ($('.customization2_booking-terms').length > 0 ){
-	$('.customization2_booking-terms .customization2_booking-terms_checkbox').trigger('click');
-	$('.customization2_booking-terms').hide();
-}
+markAsChecked('customization2_attendee');
+hideAndMoveCheckbox();
