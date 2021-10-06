f73e1a908d57625d6e41de3b20dae4c2ab07eaa3
Alex <olexiy.movchan@gmail.com>
2021-10-06 15:34:21 +0300

    13885/privacy-checkbox.js - cleanup

--- customizations/organization/13885/privacy-checkbox.js
+++ customizations/organization/13885/privacy-checkbox.js
@@ -1,7 +1,5 @@
 function checkAndHidePrivacy(){
-  console.log('checkAndHidePrivacy');
   if ($('.customization2_booking-terms').length > 0 ){
-    console.log('.customization2_booking-terms exists');
     $('.customization2_booking-terms .customization2_booking-terms_checkbox').trigger('click');
     $('.customization2_booking-terms').hide();
   }
