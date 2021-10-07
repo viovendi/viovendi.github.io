4ae08073b7295a0f0f3077fee8f9d8945fce7683
Alex <olexiy.movchan@gmail.com>
2021-10-07 12:37:05 +0300

    13885/markcheckboxes.js - fix

--- customizations/organization/13885/markcheckboxes.js
+++ customizations/organization/13885/markcheckboxes.js
@@ -30,9 +30,13 @@ hideAndMoveCheckbox();
 
 /* check and hide privacy checkbox */
 function checkAndHidePrivacy(){
+  console.log('checkAndHidePrivacy');
   if ($('.customization2_booking-terms').length > 0 ){
     $('.customization2_booking-terms .customization2_booking-terms_checkbox').trigger('click');
-    // $('.customization2_booking-terms').hide();
+    $('.customization2_booking-terms .vv-checkbox__indicator').css('opacity', '0');
+    $('.customization2_booking-terms .vv-checkbox').on('click', function(e){
+      e.preventDefault();
+    });
   }
 }
 checkAndHidePrivacy();
