f6d996b46eda17a024fc021a3d51bb805808b4ec
Alex <olexiy.movchan@gmail.com>
2021-10-07 12:08:09 +0300

    13885/privacy-checkbox.js - debug

--- customizations/organization/13885/privacy-checkbox.js
+++ customizations/organization/13885/privacy-checkbox.js
@@ -1,7 +1,7 @@
 function checkAndHidePrivacy(){
   if ($('.customization2_booking-terms').length > 0 ){
     $('.customization2_booking-terms .customization2_booking-terms_checkbox').trigger('click');
-    $('.customization2_booking-terms').hide();
+    // $('.customization2_booking-terms').hide();
   }
 }
 checkAndHidePrivacy();
