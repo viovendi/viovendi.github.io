05cf17289d688b16cd9990ff20e3e962aaf4b5f4
vbodend <69503064+vbodend@users.noreply.github.com>
2021-10-21 13:37:23 +0200

    Update standard505links.js

--- customizations/organization/14801/standard505links.js
+++ customizations/organization/14801/standard505links.js
@@ -1,1 +1,4 @@
 console.log("Standard-505");
+var textLiveHomag = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').text();
+var newtext = textLiveHomag.replace("Datenschutz-, bzw. Einwilligungserklärung", "<a target='_blank' href='https://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2021/datenschutz-tn.html'>Datenschutz-, bzw. Einwilligungserklärung</a>");
+$('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').empty().html(newtext);
