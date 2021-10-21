6ceae9eb21a7e247cfe9d707d0e5bed7da1f9584
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-10-21 13:53:59 +0200

    Update standard505links.js

--- customizations/organization/14801/standard505links.js
+++ customizations/organization/14801/standard505links.js
@@ -5,6 +5,6 @@ var newtext0 = textLiveHomag0.replace("Datenschutz-, bzw. Einwilligungserklärun
 $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').empty().html(newtext0);
 
 //2. Frage nur für Speaker
-var textLiveHomag2 = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').text();
+var textLiveHomag2 = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-3').find('.customization2_attendee_further-data_custom-question_label').text();
 var newtext2 = textLiveHomag2.replace("Datenschutz-, bzw. Einwilligungserklärung", "<a target='_blank' href='https://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2021/datenschutz-einwilligung-speaker-t2-video-doku.html'>Datenschutz-, bzw. Einwilligungserklärung</a>");
-$('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').empty().html(newtext2);
+$('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-3').find('.customization2_attendee_further-data_custom-question_label').empty().html(newtext2);
