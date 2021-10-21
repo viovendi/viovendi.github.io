76402514de613bad318c228c7fe2c51bb3678fa5
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-10-21 13:49:47 +0200

    Update standard505links.js

--- customizations/organization/14801/standard505links.js
+++ customizations/organization/14801/standard505links.js
@@ -1,4 +1,10 @@
 console.log("Standard-505");
-var textLiveHomag = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').text();
-var newtext = textLiveHomag.replace("Datenschutz-, bzw. Einwilligungserklärung", "<a target='_blank' href='https://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2021/datenschutz-tn.html'>Datenschutz-, bzw. Einwilligungserklärung</a>");
-$('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').empty().html(newtext);
+//erste Frage momentan für alle
+var textLiveHomag0 = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').text();
+var newtext0 = textLiveHomag0.replace("Datenschutz-, bzw. Einwilligungserklärung", "<a target='_blank' href='https://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2021/datenschutz-einwilligung-speaker-t1-vortrag.html'>Datenschutz-, bzw. Einwilligungserklärung</a>");
+$('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').empty().html(newtext0);
+
+//2. Frage nur für Speaker
+var textLiveHomag2 = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').text();
+var newtext2 = textLiveHomag2.replace("Datenschutz-, bzw. Einwilligungserklärung", "<a target='_blank' href='https://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2021/datenschutz-einwilligung-speaker-t2-video-doku.html'>Datenschutz-, bzw. Einwilligungserklärung</a>");
+$('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').empty().html(newtext2);
