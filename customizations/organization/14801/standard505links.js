1f09173f9bf9cd0bc00037a2b11eb8fcd465302e
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-10-21 14:08:34 +0200

    Update standard505links.js

--- customizations/organization/14801/standard505links.js
+++ customizations/organization/14801/standard505links.js
@@ -3,6 +3,7 @@ console.log("Standard-505");
 var textLiveHomag0 = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').text();
 
 console.log(textLiveHomag0);
+if(textLiveHomag0.includes("*Speaker*"))console.log("Speaker Ticket!!!!!!!!!!");
 var newtext0 = textLiveHomag0.replace("Datenschutz-, bzw. Einwilligungserklärung", "<a target='_blank' href='https://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2021/datenschutz-einwilligung-speaker-t1-vortrag.html'>Datenschutz-, bzw. Einwilligungserklärung</a>");
 $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').empty().html(newtext0);
 
