00d807b5579633a3e441db56b4ea2c437c2e2ac2
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-10-21 14:17:07 +0200

    Update standard505links.js

--- customizations/organization/14801/standard505links.js
+++ customizations/organization/14801/standard505links.js
@@ -1,11 +1,16 @@
-console.log("Standard-505");
-//erste Frage momentan für alle
+//console.log("Standard-505");
 var textLiveHomag0 = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').text();
-
-console.log(textLiveHomag0);
-if(textLiveHomag0.includes("Speaker"))console.log("Speaker Ticket!!!!!!!!!!");
-var newtext0 = textLiveHomag0.replace("Datenschutz-, bzw. Einwilligungserklärung", "<a target='_blank' href='https://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2021/datenschutz-einwilligung-speaker-t1-vortrag.html'>Datenschutz-, bzw. Einwilligungserklärung</a>");
+//Wenn es das Speaker Ticket ist
+if(textLiveHomag0.includes("Speaker")){
+  
+  var newtext0 = textLiveHomag0.replace("Datenschutz-, bzw. Einwilligungserklärung", "<a target='_blank' href='https://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2021/datenschutz-einwilligung-speaker-t1-vortrag.html'>Datenschutz-, bzw. Einwilligungserklärung</a>");
 $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').empty().html(newtext0);
+                                      }else{
+                                        //Teilnehmerticket
+                                      var newtext0 = textLiveHomag0.replace("Datenschutz-, bzw. Einwilligungserklärung", "<a target='_blank' href='https://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2021/datenschutz-tn.html'>Datenschutz-, bzw. Einwilligungserklärung</a>");
+$('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-2').find('.customization2_attendee_further-data_custom-question_label').empty().html(newtext0);
+ 
+                                      }
 
 //2. Frage nur für Speaker
 var textLiveHomag2 = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-3').find('.customization2_attendee_further-data_custom-question_label').text();
