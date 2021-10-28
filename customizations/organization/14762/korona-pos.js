99be64914208ebfd3d58b59a27537e7f15380dfd
Oleg Stetsko <oleh.stetsko@gmail.com>
2021-10-28 12:36:07 +0300

    test

--- customizations/organization/14762/korona-pos.js
+++ customizations/organization/14762/korona-pos.js
@@ -1,4 +1,6 @@
 // Korona POS 2.0
 
 
-console.log("Tag Korona POS 2.0", dataLayer)
+console.log("Tag Korona POS 2.0")
+const data = _.find(dataLayer, { event: 'RegistrationConfirmed'});
+console.log(data)
