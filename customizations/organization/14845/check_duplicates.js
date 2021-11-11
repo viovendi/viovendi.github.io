7232194a9d4bd21aeb0aceff46b209f62b5071a2
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-11-11 16:25:16 +0100

    Update check_duplicates.js

--- customizations/organization/14845/check_duplicates.js
+++ customizations/organization/14845/check_duplicates.js
@@ -1,3 +1,3 @@
 console.log("-clicked -> github check_duplicates.js")
 var mail = $('.vv-input__control customization2_attendee_contact-data_email_input');
-console.log(mail[0].value);
+console.log(mail);
