cdfc4475215fb48d35728e66a0dd7c6fd3a1f87b
vbodend <69503064+vbodend@users.noreply.github.com>
2021-11-16 09:44:17 +0100

    Create adituscodes.js

---
+++ customizations/organization/10668/adituscodes.js
@@ -0,0 +1,63 @@
+console.log('Start working, Google Tag Manager')
+  var categoryName = $('.customization2_attendee-state_edit .customization2_attendee_title').text().trim();
+  var pathname = window.location.pathname;
+  var eventId = pathname.split('/')[3];
+
+  console.log(categoryName)
+
+  function getCode(categoryName, eventId){
+    $.ajax({
+      url: 'https://cs.doo.net/v1/integrations/aditus/get-code',
+      headers: {
+        'Accept': '*/*',
+        'Content-Type': 'application/json',
+      },
+      type: 'post',
+      data: JSON.stringify({
+        "event_id": eventId,
+        "event_category_name": categoryName
+      }),
+      dataType: 'json',
+      success: function (res) {
+        if(res.aditus_code){
+          setTimeout(function(){
+            addCode(res.aditus_code, "Aditus Code")
+          }, 5000);
+        } else if(res.payload) {
+          setTimeout(function(){
+            addCode(res.payload, "Aditus Code")
+          }, 5000);
+          addCode(res.payload, "Aditus Code")
+        } else {
+          addCode("no response", "Aditus Code")
+        }
+      }
+    });
+  }
+
+
+  function addCode(code, inputName) {
+
+    var elements = document.querySelectorAll(".customization2_attendee_further-data_custom-question");
+
+    for (var i = 0; i < elements.length; i++) {
+      var label = elements[i].querySelectorAll('.customization2_attendee_further-data_custom-question_label')[0].textContent.trim();
+      if(label.indexOf(inputName) >= 0){
+        var input = elements[i].querySelectorAll('.customization2_attendee_further-data_custom-question_input')[0]
+        input.value = code;
+        var event;
+        if(typeof(Event) === 'function') {
+          event = new Event('input'); // for Chrome
+        }else{
+          event = document.createEvent('Event');
+          event.initEvent('input', true, true); // for IE
+        }
+        input.dispatchEvent(event)
+      }
+    }
+
+  };
+
+  getCode(categoryName, eventId)
+
+
