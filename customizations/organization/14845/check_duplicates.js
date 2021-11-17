11efb18aea013d0564100a03c05e993232a304d1
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-11-17 16:06:40 +0100

    Update check_duplicates.js

--- customizations/organization/14845/check_duplicates.js
+++ customizations/organization/14845/check_duplicates.js
@@ -2,21 +2,26 @@ console.log("--clicked -> github check_duplicates.js")
 attendeeDiv= $(".customization2_attendees");
 mails=[]
 
-attendeeDiv.find("input[type=email][name=email]").each(function() {
+function getAllMails(){
+  attendeeDiv.find("input[type=email][name=email]").each(function() {
     mails.push($(this).val());
-});
-attendeeDiv.find(".customization2_attendee_contact-data_email_value").each(function() {
+  });
+  attendeeDiv.find(".customization2_attendee_contact-data_email_value").each(function() {
     console.log("exists");
     mail = $(this).text();
     mail = mail.replace(/(\r\n|\n|\r)/gm, "");
     mail=mail.replace(/\s/g,'');
-    console.log(mail)
+    //console.log(mail)
     mails.push(mail);
-});
+   });
 console.log(mails);
+}
+
+getAllMails();
 
 attendeeDiv.find("input[type=email][name=email]").each(function() {
-    $(this).on('input', function() { 
+    $(this).on('input', function() {
+        getAllMails();
         console.log($(this).val());
         count =0;
       for(let i=0;i<mails.length;i++){
