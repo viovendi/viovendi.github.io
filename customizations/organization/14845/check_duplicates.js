7a4601985894a9e077d4ea78987a9c6ba9d82c20
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-11-17 13:18:34 +0100

    Update check_duplicates.js

--- customizations/organization/14845/check_duplicates.js
+++ customizations/organization/14845/check_duplicates.js
@@ -1,10 +1,15 @@
 console.log("-clicked -> github check_duplicates.js")
+funct
 mails=[]
 $("input[type=email][name=email]").each(function() {
     mails.push($(this).val());
 });
 $(".customization2_attendee_contact-data_email_value").each(function() {
     console.log("exists");
+    mail = $(this).text();
+    mail.replace(/\s/g,'');
+    mail.replace(/\n/g,'');
+    console.log(mail)
     mails.push($(this).text());
 });
 console.log(mails);
