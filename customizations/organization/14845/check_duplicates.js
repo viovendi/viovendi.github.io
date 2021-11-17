2db6e4ec01a034834059c55157792f6c8b3fbd3f
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-11-17 10:44:29 +0100

    Update check_duplicates.js

--- customizations/organization/14845/check_duplicates.js
+++ customizations/organization/14845/check_duplicates.js
@@ -4,3 +4,9 @@ $("input[type=email][name=email]").each(function() {
     mails.push($(this).val());
 });
 console.log(mails);
+let fehler="";
+for(let i=0;i<mails.length;i++){
+    for(let k=0;k<mails.length;k++){
+        if(mails[k].equals(mails[i])){console.log("Fehler: Mail:"+mails[k]+" kommt mehrfach vor!");}
+ }
+}
