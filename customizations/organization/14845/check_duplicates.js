d8aac59e5ad6cdceb00fe2950f23f5f5a35f03c2
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-11-17 11:28:24 +0100

    Update check_duplicates.js

--- customizations/organization/14845/check_duplicates.js
+++ customizations/organization/14845/check_duplicates.js
@@ -4,7 +4,7 @@ $("input[type=email][name=email]").each(function() {
     mails.push($(this).val());
 });
 $(".vv-text vv-text__size--lg vv-text__weight--regular vv-color--text customization2_attendee_contact-data_email_value").each(function() {
-    mails.push($(this).val());
+    mails.push($(this).text());
 });
 console.log(mails);
 //let fehler="";
