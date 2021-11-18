9047c5adb7fa7dab3c23502e876fa90061ecd365
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-11-18 13:34:58 +0100

    Update check_duplicates.js

--- customizations/organization/14845/check_duplicates.js
+++ customizations/organization/14845/check_duplicates.js
@@ -1,4 +1,4 @@
-console.log("--clicked -> github check_duplicates.js")
+console.log("---> github check_duplicates.js")
 attendeeDiv= $(".customization2_attendees");
 mails=[];
 errorMssge="Diese E-Mail ist bereits für diese Veranstaltung angemeldet";
@@ -22,42 +22,46 @@ console.log(mails);
 getAllMails();
 
 function checkMails(){
-attendeeDiv.find("input[type=email][name=email]").each(function() {
-  if($(this).nextAll('.start:first').attr('id')!="errormail")
-  $( "<p id='errormail' style='color:red;display:none;'>Diese E-Mail ist bereits für diese Veranstaltung angemeldet.</p>" ).insertAfter(this);
-    
+ attendeeDiv.find("input[type=email][name=email]").each(function() {
+ 
+   count =0;
+   for(let i=0;i<mails.length;i++){
+        if(mails[i]==$(this).val()){
+            count ++;
+        }
+    }
+      if(count>1){
+        $(".customization-button-next").prop("disabled",true);
+        $(".customization2_attendee_edit-action_save").prop("disabled",true);
+        $(".customization2_attendee_edit-action_save").before("<span class=error-message>"+errorMssge+"</span>");
+      }else {
+        $("div.customization2_attendee_edit-action").find(".error-message").remove();
+        $(".customization-button-next").prop("disabled",false);
+        $(".customization2_attendee_edit-action_save").prop("disabled",false);
+      }
+   
   $(this).on('input', function() {
-        getAllMails();
-        console.log($(this).val());
-        count =0;
+      getAllMails();
+      count =0;
       for(let i=0;i<mails.length;i++){
           if(mails[i]==$(this).val()){
               count ++;
           }
       }
         if(count>1){
-        //  $(this).prop("aria-invalid",true);
-          console.log("Fehler:  kommt mehrfach vor!");
-        //  $(this).nextAll('.start:first').css("display","inline");
           $(".customization-button-next").prop("disabled",true);
-        //  $( "<p style='color:red;'>Fehler! Die Mail kommt mehrmals vor</p>" ).insertAfter(attendeeDiv);
           $(".customization2_attendee_edit-action_save").prop("disabled",true);
           $(".customization2_attendee_edit-action_save").before("<span class=error-message>"+errorMssge+"</span>");
         }else {
           $("div.customization2_attendee_edit-action").find(".error-message").remove();
-          console.log("kein Fehler");
-         // $(this).nextAll('.start:first').css("display","none");
-        //  $(this).prop("aria-invalid",false);
           $(".customization-button-next").prop("disabled",false);
           $(".customization2_attendee_edit-action_save").prop("disabled",false);
         }
    });
-});
+ });
 }
 
 checkMails();
-$(".customization2_attendee_contact-data_copy-link").click(function() {
-checkMails();
-});
+
 
 
