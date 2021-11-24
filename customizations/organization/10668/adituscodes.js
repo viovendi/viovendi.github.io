f03af8498df4d6a32b4b8f1ea87d5478ee9a2fdd
vbodend <69503064+vbodend@users.noreply.github.com>
2021-11-24 17:54:33 +0100

    Update adituscodes.js

--- customizations/organization/10668/adituscodes.js
+++ customizations/organization/10668/adituscodes.js
@@ -102,8 +102,11 @@ function getTicketcategory(){
       if(label.indexOf(inputName) >= 0){
             console.log(label)
             const element = elements[i].querySelectorAll('.customization2_attendee_further-data_custom-question_input')[0]
+            
             const replaceText = () => {
              // i think here is the mistake... if empty -> put code in ; if not empty do nothing
+             console.log("element.textContent.trim() = "+element.textContent.trim());
+             console.log("textToReplace = " +textToReplace)
                     if (element.textContent.trim() === textToReplace) {
                       element.childNodes.forEach(node => {
                         if (node.textContent.trim() === textToReplace) {
