48ba05213169c8c2a4a6a97ecd9a40c5779ffb03
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-12-02 12:21:34 +0100

    Update aboticket.js

--- customizations/organization/13844/aboticket.js
+++ customizations/organization/13844/aboticket.js
@@ -1,42 +1,44 @@
-console.log('github code loaded')
-hideNumberedAboTickets();
-$('.event-category').each(function(){
-  if(isAboticket($(this))){
-    $(this).find("select").on('change', function() {
-      setNumberedAboTickets($(this).val());
-    });
-  }
-  
-//  console.log("found category");
-//  console.log($(this).find("h3").text());
-});
+console.log('github code loaded customization 13844/aboticket.js');
 
-function setNumberedAboTickets(amount){
-  console.log(amount)
-  $('.event-category').each(function(){
-  if(isAboticketNumbered($(this))){
-  $(this).find("select").val(amount);
-  }
-});
+hideNumberedAboTickets();
+addListenerToAboTicket();
+
+
+function addListenerToAboTicket() {
+    $('.event-category').each(function () {
+        if (isAboticket($(this))) {
+            $(this).find("select").on('change', function () {
+                setNumberedAboTickets($(this).val());
+            });
+        }
+    });
 }
-function hideNumberedAboTickets(){
-  $('.event-category').each(function(){
-  if(isAboticketNumbered($(this))){
-  $(this).css('display','none');
-  }
-});
+function setNumberedAboTickets(amount) {
+    console.log(amount)
+    $('.event-category').each(function () {
+        if (isAboticketNumbered($(this))) {
+            $(this).find("select").val(amount);
+        }
+    });
 }
-function isAboticket(categoryElement){
-  var ticketname= getTicketName(categoryElement);
- return  ticketname=='Abo-Ticket';
+function hideNumberedAboTickets() {
+    $('.event-category').each(function () {
+        if (isAboticketNumbered($(this))) {
+            $(this).css('display', 'none');
+        }
+    });
 }
-function isAboticketNumbered(categoryElement){
-  var ticketname= getTicketName(categoryElement);
- return  ticketname!='Abo-Ticket'&&ticketname.startsWith('Abo-Ticket:');
+function isAboticket(categoryElement) {
+    var ticketname = getTicketName(categoryElement);
+    return ticketname == 'Abo-Ticket';
 }
-function getTicketName(categoryElement){
-  var ticketname= $(categoryElement).find("h3").text();
+function isAboticketNumbered(categoryElement) {
+    var ticketname = getTicketName(categoryElement);
+    return ticketname != 'Abo-Ticket' && ticketname.startsWith('Abo-Ticket:');
+}
+function getTicketName(categoryElement) {
+    var ticketname = $(categoryElement).find("h3").text();
     ticketname = ticketname.replace(/(\r\n|\n|\r)/gm, "");
-    ticketname=ticketname.replace(/\s/g,'');
- return  ticketname;
+    ticketname = ticketname.replace(/\s/g, '');
+    return ticketname;
 }
