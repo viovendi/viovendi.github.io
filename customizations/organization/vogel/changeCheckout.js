8ce0f35d9a619be699c257b17175783d57acacf7
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-12-02 16:01:49 +0100

    Update changeCheckout.js

--- customizations/organization/vogel/changeCheckout.js
+++ customizations/organization/vogel/changeCheckout.js
@@ -3,6 +3,7 @@ const observer = new MutationObserver((mutations, obs) => {
 
     if ($(page4).is(':visible')) {
         console.log("page 4 visible");
+        $('.customization-confirmation-description').text('Vielen Dank für Ihre unverbindliche Vormerkung zum Event! Gerne senden wir Ihnen Neuigkeiten zu der Veranstaltung direkt in Ihr Postfach und informieren Sie, wenn das Programm veröffentlicht wurde oder die Anmeldung freigeschaltet ist.');
         obs.disconnect();
         return;
     }
