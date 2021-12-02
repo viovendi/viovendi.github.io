ac2bffb17c1135bcf20847655023dbe9d85249d4
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-12-02 11:15:32 +0100

    Update aboticket.js

--- customizations/organization/13844/aboticket.js
+++ customizations/organization/13844/aboticket.js
@@ -1,1 +1,8 @@
 console.log('github code loaded')
+$('.event-category').each(function(){
+  
+  $(this).find(".event-category__name customization-category-name").each(function() {
+   console.log($(this).text());
+  });
+  
+});
