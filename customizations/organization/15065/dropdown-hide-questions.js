b0d1b92b08ffe028a740acb6ee3c264b4ec5dd44
vbodend <69503064+vbodend@users.noreply.github.com>
2021-12-03 10:16:40 +0100

    Update dropdown-hide-questions.js

--- customizations/organization/15065/dropdown-hide-questions.js
+++ customizations/organization/15065/dropdown-hide-questions.js
@@ -4,7 +4,7 @@ console.log("in git");
 function handler() {
     //option 1
     var funkton = $$('.customization2_attendee_further-data_custom-question').findField('Welche Position hast Du?');
-    var select = $$('.customization2_attendee_further-data_custom-question').findField('Position im Unternehmen?');
+    var select = $$('.customization2_attendee_further-data_custom-question_label').findField('Position im Unternehmen?');
 
     //option 2
     var funkton2 = $$('.customization2_attendee_further-data_custom-question').findField('Welche Position hast Du? (optional)'); //findField('Welche Funktion hast Du?');
