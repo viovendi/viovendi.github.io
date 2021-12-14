de27454a558869c29197315112cbd8da091e10ce
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2021-12-14 11:52:43 +0100

    Update changeCheckout.js

--- customizations/organization/vogel/changeCheckout.js
+++ customizations/organization/vogel/changeCheckout.js
@@ -23,10 +23,10 @@ const observer2 = new MutationObserver((mutations, obs) => {
 
     if ($(page2).is(':visible')) {
         console.log("page 2 visible");
-        if($('.customization-button-next').text().includes("Jetzt Buchen"))
-        $('.customization-button-next').text('Vielen Dank für Ihre unverbindliche Vormerkung zum Event! Gerne senden wir Ihnen Neuigkeiten zu der Veranstaltung direkt in Ihr Postfach und informieren Sie, wenn das Programm veröffentlicht wurde oder die Anmeldung freigeschaltet ist.');
-        if($('.customization-button-next').text().includes("Order now"))
-        $('.customization-button-next').text('Thank you for your non-binding reservation for the event! We will be happy to send you news about the event directly to your mailbox and inform you when the programme has been published or registration is open.');
+        if($('.customization-button-next').get('content').includes("Jetzt Buchen"))
+        $('.customization-button-next').set('content','42');
+        if($('.customization-button-next').get('content').includes("Order now"))
+        $('.customization-button-next').set('content','42');
         
         obs.disconnect();
         return;
