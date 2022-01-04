111682b3b32785f273de61bd4f1ce9e59ce3eee4
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-01-04 10:44:41 +0100

    Update dropdown-hide-questions.js

--- customizations/organization/14176/dropdown-hide-questions.js
+++ customizations/organization/14176/dropdown-hide-questions.js
@@ -108,7 +108,7 @@ if($(this).find('option').filter(':selected').text().trim() == 'andere'){
     
 function disableWhenEmpty(field){
     
-   $(this).addClass('error-state');
+   $(this).find('.customization2_attendee_further-data_custom-question_input').addClass('error-state');
    $('.customization2_attendee_edit-action_save').prop("disabled", true);
    $(field).find('.customization2_attendee_further-data_custom-question_input').on('input', function() {
        console.log('text:'+ $(this).val());
