f2397ed9eef8abf1e199a6d72ed5c7d15879e30a
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-01-13 12:05:15 +0100

    Update translationFrench.js

--- customizations/organization/14970/translationFrench.js
+++ customizations/organization/14970/translationFrench.js
@@ -1,1 +1,26 @@
-console.log("translating2")
+console.log("translating")
+
+function handler(){
+  $('.customization2_attendee_further-data_custom-question_dropdown option').each(function(){
+    if($(this).text().trim()=="Bitte auswählen"){
+        $(this).text("NEW TEXT");
+    }
+  })
+}
+
+var insertionListener = function (event) {
+    if (event.animationName === "nodeInserted") {
+        console.log("Node has been inserted: ", event.target);
+        
+        handler();
+
+        $('customization2_attendee_view-action_edit').on("click", function () {
+            console.log("clicked");
+        });
+    }
+}
+handler();
+
+document.addEventListener("animationstart", insertionListener, false); // standard + firefox
+document.addEventListener("MSAnimationStart", insertionListener, false); // IE
+document.addEventListener("webkitAnimationStart", insertionListener, false); // 
