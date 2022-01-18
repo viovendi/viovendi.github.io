8b6a3d7bf920b2200fbec4a16ac3eb96fc19af4e
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-01-18 12:16:01 +0100

    Update bannerGenerator.js

--- customizations/organization/14334/bannerGenerator.js
+++ customizations/organization/14334/bannerGenerator.js
@@ -27,31 +27,18 @@ function generator(){
     ctx.drawImage($('img').get(0), 0, 0);
     ctx.font = "18pt Verdana";
     $(document).on('input change keyup paste', '#halle_inp', function() {
-        //redraw image
-       // ctx.clearRect(0, 0, canvas.width, canvas.height);
-      //  ctx.drawImage($('img').get(0), 0, 0);
-        //refill text
-       // ctx.fillStyle = "black";
-       // ctx.fillText($(this).val(), 65, 465);
         setTextsHochkant()
     });
     $(document).on('input change keyup paste', '#stand_inp', function() {
-        //redraw image
-        //ctx.clearRect(0, 0, canvas.width, canvas.height);
-        //ctx.drawImage($('img').get(0), 0, 0);
-        //refill text
+        if($(this).val().length<8)
         setTextsHochkant();
-      //  ctx.fillStyle = "black";
-      //  ctx.fillText($(this).val(), 60, 530);
     });
-    //$('button').click(function() {
-        //console.log(ctx.getImageData(50, 50, 100, 100));
-    //});
+    
     function setTextsHochkant(){
         
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage($('img').get(0), 0, 0);
-        //refill text
+        
         ctx.fillStyle = "black";
         ctx.fillText($('#halle_inp').val(), 65, 465);
         ctx.fillText($('#stand_inp').val(), 60, 530);
