8a7fba3df982e86c7da56e6c70a3c0664f78e3ff
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-01-18 12:05:21 +0100

    Update bannerGenerator.js

--- customizations/organization/14334/bannerGenerator.js
+++ customizations/organization/14334/bannerGenerator.js
@@ -26,25 +26,36 @@ function generator(){
     canvas.height = $('img').height();
     ctx.drawImage($('img').get(0), 0, 0);
     ctx.font = "18pt Verdana";
-    $(document).on('input', '#halle_inp', function() {
+    $(document).on('input change keyup paste', '#halle_inp', function() {
         //redraw image
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage($('img').get(0), 0, 0);
+       // ctx.clearRect(0, 0, canvas.width, canvas.height);
+      //  ctx.drawImage($('img').get(0), 0, 0);
         //refill text
-        ctx.fillStyle = "black";
-        ctx.fillText($(this).val(), 65, 465);
+       // ctx.fillStyle = "black";
+       // ctx.fillText($(this).val(), 65, 465);
+        setTextsHochkant()
     });
     $(document).on('input change keyup paste', '#stand_inp', function() {
         //redraw image
         //ctx.clearRect(0, 0, canvas.width, canvas.height);
         //ctx.drawImage($('img').get(0), 0, 0);
         //refill text
-        ctx.fillStyle = "black";
-        ctx.fillText($(this).val(), 60, 530);
+        setTextsHochkant();
+      //  ctx.fillStyle = "black";
+      //  ctx.fillText($(this).val(), 60, 530);
     });
     //$('button').click(function() {
         //console.log(ctx.getImageData(50, 50, 100, 100));
     //});
+    function setTextsHochkant(){
+        
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage($('img').get(0), 0, 0);
+        //refill text
+        ctx.fillStyle = "black";
+        ctx.fillText($('halle_inp').val(), 65, 465);
+        ctx.fillText($('#stand_inp').val(), 60, 530);
+    }
     function download() {
         var download = document.getElementById("download");
         var image = document.getElementById("canvas").toDataURL("image/png")
