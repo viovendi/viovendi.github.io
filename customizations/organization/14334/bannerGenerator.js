a9d4148d190230f20f7f2ba118d5e8a61138d8e4
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-01-19 12:43:39 +0100

    Update bannerGenerator.js

--- customizations/organization/14334/bannerGenerator.js
+++ customizations/organization/14334/bannerGenerator.js
@@ -73,8 +73,8 @@ $(document).on('input change keyup paste', '#halle_inp', function() {
         ctx.drawImage($('#banner_img').get(0), 0, 0);
         
         ctx.fillStyle = "black";
-        ctx.fillText($('#halle_inp').val(), 235 , 48);
-        ctx.fillText($('#stand_inp').val(), 250 , 48);
+        ctx.fillText($('#halle_inp').val(), 235 , 42);
+        ctx.fillText($('#stand_inp').val(), 265 , 42);
     }
     function setTextsHochkant(){
         
