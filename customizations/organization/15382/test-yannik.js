7417e3c03276ad8d3ba0e7a23af984aa0ace88f3
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-01-27 15:45:26 +0100

    Update test-yannik.js

--- customizations/organization/15382/test-yannik.js
+++ customizations/organization/15382/test-yannik.js
@@ -1,3 +1,11 @@
 console.log("removeCheckbox ");
 $('#isTermsAccepted').click();
-console.log("checkboxClicked");
+
+$(document).ready(function () {
+    function emailConfirmation() {
+        var box1 = $('.customization2_attendee_contact-data_email_input');
+        var box2 = $('.customization2_attendee_contact-data_email-confirmation_input');
+        box2.val(box1.val());
+    }
+    $('.customization2_attendee_contact-data_email_input').on('keyup', emailConfirmation);
+});
