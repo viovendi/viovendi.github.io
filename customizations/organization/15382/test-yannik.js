b0ac15b74533b0362aed9996a7486ee8fc00274e
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-01-28 11:05:06 +0100

    Update test-yannik.js

--- customizations/organization/15382/test-yannik.js
+++ customizations/organization/15382/test-yannik.js
@@ -1,36 +1,50 @@
-$(document).ready(function () {
-    // Email Confirmation 
-    function copyEmail() {
-        var box1 = $('.customization2_booker_contact-data_email_input');
-        var box2 = $('.customization2_booker_contact-data_email-confirmation_input');
-        box2.val(box1.val());
-    };
+// Email Confirmation 
+function copyEmail() {
+    var box1 = $('.customization2_booker_contact-data_email_input');
+    var box2 = $('.customization2_booker_contact-data_email-confirmation_input');
+    box2.val(box1.val());
+};
 
-    function emailConfirmation() {
-        copyEmail();
-        $('.customization2_booker_contact-data_email-confirmation_input').focus();
-    }
+function emailConfirmation() {
+    copyEmail();
+    $('.customization2_booker_contact-data_email-confirmation_input').focus();
+}
 
-    $('body').on('change', '.customization2_booker_contact-data_email_input', emailConfirmation);
-
-    // Hide Email Confirmation
-    //$('.customization2_booker_contact-data_email-confirmation').attr('style', 'display: none');
-
-    
+function replaceDOI() {
     // Replace DOI & Terms
+    console.log("DOI and Terms moved");
     $(".customization2_booking-terms .vv-mb-xxs").appendTo(".customization2_booker_contact-data");
     $(".customization2_booking-terms .vv-pl-lg").appendTo(".customization2_booker_contact-data");
     $("vv-advanced-questions .customization2_opt-out-and-opt-in").appendTo(".customization2_booking-terms");
 
     // click Terms checkbox
-    console.log("checkbox clicked")
-    $('#isTermsAccepted').click()
+    console.log("checkbox clicked");
+    $('#isTermsAccepted').click();
 
     // Remove Terms checkbox
-    console.log("Checkbox removed")
+    console.log("Checkbox removed");
     $("vv-checkbox[name='isTermsAccepted']").attr('style', 'padding-top: 30px');
     $("vv-checkbox[name='isTermsAccepted'] .vv-checkbox .vv-checkbox__label.vv-checkbox__label--center.vv-checkbox__label--md .vv-checkbox__indicator").attr('style', 'display: none');
-});
+}
 
 
+$('body').on('change', '.customization2_booker_contact-data_email_input', emailConfirmation);
 
+// Hide Email Confirmation
+//$('.customization2_booker_contact-data_email-confirmation').attr('style', 'display: none');
+
+replaceDOI();
+
+
+var insertionListener = function (event) {
+    if (event.animationName === "nodeInserted") {
+        //Insert your code here
+
+        replaceDOI();
+
+    }
+};
+
+document.addEventListener("animationstart", insertionListener, false); // standard + firefox
+document.addEventListener("MSAnimationStart", insertionListener, false); // IE
+document.addEventListener("webkitAnimationStart", insertionListener, false); //
