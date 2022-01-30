d8aac9afc18d1838bcadb797f24bc1f02ec58825
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-01-30 12:56:28 +0100

    Update test-yannik.js

--- customizations/organization/15382/test-yannik.js
+++ customizations/organization/15382/test-yannik.js
@@ -19,6 +19,30 @@ function hideEmail() {
     });
 };
 
+var doi = $("vv-advanced-questions .customization2_opt-out-and-opt-in").clone();
+var termsLabel = $(".customization2_booking-terms .vv-mb-xxs").clone();
+var termsLink = $(".customization2_booking-terms .vv-pl-lg").clone();
+
+function replaceDOI() {
+    // Rearrange DOI & Terms
+    console.log("DOI moved");
+    $('.customization2_booking-terms').empty();
+    $(doi).appendTo(".customization2_booking-terms");
+
+    console.log("Terms moved");
+    $("vv-advanced-questions .customization2_opt-out-and-opt-in").remove();
+    $(termsLabel).appendTo(".customization2_booker_contact-data");
+    $(termsLink).appendTo(".customization2_booker_contact-data");
+
+    // click Terms checkbox
+    console.log("checkbox clicked");
+    $('#isTermsAccepted').click();
+
+    // Remove Terms checkbox
+    console.log("Checkbox removed");
+    $("vv-checkbox[name='isTermsAccepted'] .vv-checkbox .vv-checkbox__label.vv-checkbox__label--center.vv-checkbox__label--md .vv-checkbox__indicator").attr('style', 'display: none');
+};
+
 $(document).ready(function () {
 
     $('body').on('change', '.customization2_booker_contact-data_email_input', emailConfirmation);
@@ -26,12 +50,14 @@ $(document).ready(function () {
     //$('body').on('click', 'button.customization2_booker_view-action_edit', hideEmail);
 
     hideEmail();
+    replaceDOI();
 
     var insertionListener = function (event) {
         if (event.animationName === "nodeInserted") {
 
             console.log("event listener");
             hideEmail();
+            replaceDOI();
         }
     };
 
