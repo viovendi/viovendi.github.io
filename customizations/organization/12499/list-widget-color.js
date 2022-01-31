ab13d4d12cc864a16e953c314e281bc62d13b327
christiankrausse7 <86656535+christiankrausse7@users.noreply.github.com>
2022-01-31 16:36:02 +0100

    Update list-widget-color.js

--- customizations/organization/12499/list-widget-color.js
+++ customizations/organization/12499/list-widget-color.js
@@ -1,1 +1,8 @@
-console.log("console feedback");
+console.log("GTM starts git");
+
+function changeColorVeranstalter() {
+        console.log("changeColorVeranstalter");
+        $('.col-sm-7.col-md-8.m-text').css({
+            "display": "none",
+        });
+    };
