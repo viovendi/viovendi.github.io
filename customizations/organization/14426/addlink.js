9b5e2a02ac3ab869ec71797d5c93e59856e94112
Oleg Stetsko <oleh.stetsko@gmail.com>
2022-02-03 14:46:23 +0200

    location test

--- customizations/organization/14426/addlink.js
+++ customizations/organization/14426/addlink.js
@@ -1,5 +1,7 @@
 console.log('Start working, Google Tag Manager, Custom Tag 14426 Link');
 
+console.log(window.location.href)
+
 /*
 // CSS MISSING maybe because of empty()
     var text = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-1').find('.customization2_attendee_further-data_custom-question_radio-line_label').text();
