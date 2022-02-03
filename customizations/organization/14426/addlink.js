ae1a187c38252c99d018bf12ca7fa1021720cc64
vbodend <69503064+vbodend@users.noreply.github.com>
2022-02-03 09:29:00 +0100

    Update addlink.js

--- customizations/organization/14426/addlink.js
+++ customizations/organization/14426/addlink.js
@@ -1,1 +1,7 @@
 console.log('git works');
+
+    var textLiveHomag = $('.customization2_attendee_further-data_custom-question_radio-line.customization2_attendee_further-data_custom-question_radio-line-1').find('.customization2_attendee_further-data_custom-question_radio-line_label').text();
+    var newtext = textLiveHomag.replace("www.westfalenhallen.de/dataprotectionnotice-beretta", "<a target='_blank' href='https://www.westfalenhallen.de/dataprotectionnotice-beretta'>www.westfalenhallen.de/dataprotectionnotice-beretta</a>");
+    $('.customization2_attendee_further-data_custom-question_radio-line.customization2_attendee_further-data_custom-question_radio-line-1').find('.customization2_attendee_further-data_custom-question_radio-line_label').empty().html(newtext);
+
+
