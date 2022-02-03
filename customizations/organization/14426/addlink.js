97cf4700b7d8036e78b420f591dada7bca7247ff
Oleg Stetsko <oleh.stetsko@gmail.com>
2022-02-03 15:53:55 +0200

    test

--- customizations/organization/14426/addlink.js
+++ customizations/organization/14426/addlink.js
@@ -1,9 +1,8 @@
 console.log('Start working, Google Tag Manager, Custom Tag 14426 Link');
 
-const href = window.location.href
-// console.log(href)
 
-if(href.indexOf('88669') !== -1){
+if(window.location.href.indexOf('88669') !== -1){
+  const href = window.location.href
     // Script works only for EID 88669
   if(href.indexOf('de-de') !== -1){
     const radioLineLabel = $('.customization2_attendee_further-data_custom-question-1 .customization2_attendee_further-data_custom-question_radio-line_label p');
