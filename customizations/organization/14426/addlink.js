d0fa57d60691ad5a8cd7fbd48c3741c5a275d077
vbodend <69503064+vbodend@users.noreply.github.com>
2022-02-03 09:39:19 +0100

    Update addlink.js

--- customizations/organization/14426/addlink.js
+++ customizations/organization/14426/addlink.js
@@ -1,7 +1,21 @@
 console.log('git works');
-
-    var textLiveHomag = $('.customization2_attendee_further-data_custom-question_radio-line.customization2_attendee_further-data_custom-question_radio-line-1').find('.customization2_attendee_further-data_custom-question_radio-line_label').text();
+/*
+customization2_attendee_further-data_custom-question customization2_attendee_further-data_custom-question-1
+var fieldDe = $$('.customization2_booker_further-data_custom-question_checkbox-line').findField('Ich bin mit der Verarbeitung')
+      var fieldEn = $$('.customization2_booker_further-data_custom-question_checkbox-line').findField('I consent to the use and processing')
+      if (!$('.' + name + ' .customization2_booker_further-data_custom-question_checkbox-line_label').hasClass('with-links') && $(fieldDe).html()) {
+        var textString = $('.' + name + ' .customization2_booker_further-data_custom-question_checkbox-line_label').html();
+        var textString1 = textString.replace("Einwilligungserklärung", "<a target='_blank' href='https://privacy.vogel.de/'>Einwilligungserklärung</a>");
+        var res = textString1.replace("(privacy.vogel.de)", "<a target='_blank' href='https://privacy.vogel.de/'>(privacy.vogel.de)</a>");
+        $('.' + name + ' .customization2_booker_further-data_custom-question_checkbox-line_label').html(res);
+        $('.' + name + ' .customization2_booker_further-data_custom-question_checkbox-line_label').addClass('with-links');
+      } else if($(fieldEn).html()){
+        var resultString = $(fieldEn).find('.customization2_booker_further-data/*_custom-question_checkbox-line_label').html().replace("declaration of consent (privacy.vogel.de)", "<a target='_blank' href='https://privacy.vogel.de/index_en.html'>declaration of consent (privacy.vogel.de)</a>");
+        $(fieldEn).find('.customization2_booker_further-data_custom-question_checkbox-line_label').html(resultString)
+      }
+*/
+    var textLiveHomag = $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-1').find('.customization2_attendee_further-data_custom-question_radio-line_label').text();
     var newtext = textLiveHomag.replace("www.westfalenhallen.de/dataprotectionnotice-beretta", "<a target='_blank' href='https://www.westfalenhallen.de/dataprotectionnotice-beretta'>www.westfalenhallen.de/dataprotectionnotice-beretta</a>");
-    $('.customization2_attendee_further-data_custom-question_radio-line.customization2_attendee_further-data_custom-question_radio-line-1').find('.customization2_attendee_further-data_custom-question_radio-line_label').empty().html(newtext);
+    $('.customization2_attendee_further-data_custom-question.customization2_attendee_further-data_custom-question-1').find('.customization2_attendee_further-data_custom-question_radio-line_label').empty().html(newtext);
 
 
