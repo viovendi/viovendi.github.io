bf60bd354c272e51a25652e75ba5a54417e98b68
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-02-08 11:07:13 +0100

    Update list-widget-color.js

--- customizations/organization/12499/list-widget-color.js
+++ customizations/organization/12499/list-widget-color.js
@@ -4,9 +4,16 @@ $(document).ready(function () {
 
         function changeColorVeranstalter() {
                 console.log("changeColorVeranstalter");
+                var events = $('.col-12.event-article__event-info')
+                events.each(function() {
+                      var veranstalter =  $(this).find('div.event-article__event-info__details > div:nth-child(2) > div:nth-child(2)');
+                      console.log(veranstalter.text());
+                });
+                
                 $('div.event-article__event-info__details > div:nth-child(2) > div:nth-child(2)').css({
                 "background-color": "#FAB908",
                 });
+                
         };
         
         changeColorVeranstalter();
