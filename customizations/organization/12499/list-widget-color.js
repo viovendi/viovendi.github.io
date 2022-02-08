8f638a394668ae208bab848a1a514ff045183db4
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-02-08 11:24:20 +0100

    Update list-widget-color.js

--- customizations/organization/12499/list-widget-color.js
+++ customizations/organization/12499/list-widget-color.js
@@ -4,14 +4,20 @@ $(document).ready(function () {
 
         function changeColorVeranstalter() {
                 console.log("changeColorVeranstalter");
-                var events = $('.col-12.event-article__event-info')
+                var events = $('.event-article');
                 events.each(function() {
                       var veranstalter =  $(this).find('div.event-article__event-info__details > div:nth-child(2) > div:nth-child(2)');
                       var text = veranstalter.text().trim();
                       if(text!="doo GmbH"){
                        veranstalter.css({
                         "background-color": "#FAB908",
-                        });              
+                        });
+                       $(this).find('.button').css({
+                        "background-color": "#FAB908",
+                        });
+                       $(this).find('event-article__date-container__day > span')..css({
+                        "color": "#FAB908",
+                        });;
                       }
                 });   
         };
