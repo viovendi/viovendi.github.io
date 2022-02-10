cf2719afadf48d71f6b3f0e68d48fee969a4a8be
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-02-10 14:39:20 +0100

    Update sap50years.js

--- customizations/organization/15195/sap50years.js
+++ customizations/organization/15195/sap50years.js
@@ -25,10 +25,6 @@ function setRadioGroup(radioGroup) {
 function isFirstRadioButton(radioGroup) {
     var attendeeField = $(radioGroup).closest('.booking-participant.booking-participant--attendee.customization3_booking-participant_attendee.vv-border-xs.vv-rounded-lg.vv-border-grey-light')[0];
     var firstAttendeeField = $('.booking-participant.booking-participant--attendee.customization3_booking-participant_attendee.vv-border-xs.vv-rounded-lg.vv-border-grey-light').first()[0];
-
-    console.log(attendeeField);
-    console.log(firstAttendeeField);
-
     var isFirst = attendeeField == firstAttendeeField;
     console.log('isFirst: ' + isFirst)
     return isFirst;
