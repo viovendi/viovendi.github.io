355d669ee0f75964c59e87eb9290ed084c97daa0
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-02-10 11:36:51 +0100

    Update sap50years.js

--- customizations/organization/15195/sap50years.js
+++ customizations/organization/15195/sap50years.js
@@ -3,9 +3,13 @@ console.log("github customization loaded");
 var insertionListener = function(event) {
    if (event.animationName === "nodeInserted") {
       console.log("nodeInserted");
+      changesForAttendeeForm();
    }
 }
-   
+function changesForAttendeeForm(){
+ $('.customization2_attendee_contact-data_copy-link').hide();  
+   console.log('hidden copy link')
+}
 
   document.addEventListener("animationstart", insertionListener, false); // standard + firefox
   document.addEventListener("MSAnimationStart", insertionListener, false); // IE
