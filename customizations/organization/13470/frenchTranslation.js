18973f598c8e5e73e0d4f4d321d23c7837042429
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-02-10 16:48:39 +0100

    Update frenchTranslation.js

--- customizations/organization/13470/frenchTranslation.js
+++ customizations/organization/13470/frenchTranslation.js
@@ -10,6 +10,11 @@ function changeTextTo(selector,originalText, newText){
 
 function handler2(){
   changeTextTo('.customization2_attendee_further-data_custom-question_dropdown option','Bitte auswählen', 'Veuillez sélectionner');
+  changeTextTo('span.center-block.vv-mh-auto.ew-selection__ssl--xs.customization-encrypted-connection','Verschlüsselte Verbindung','Connexion chiffrée SSL');
+  changeTextTo('.ew-selection__error','Bitte geben Sie die gewünschte Ticketzahl an.','Veuillez indiquer le nombre de tickets souhaité.');
+  changeTextTo('span.vv-text.vv-text__size--md.vv-text__weight--regular.vv-color--text','Bereits angemeldet?', 'Déjà inscrit ?');
+  changeTextTo('button.vv-button.vv-button-text.vv-button-text-blue.vv-text__size--md','Hier geht es zu Ihrer Buchung','Accédez à votre enregistrement ici');
+  changeTextTo('button.button.pull-right.float-right.col-xs-12.col-12.customization-button-next','Weiter','Suivant');
    
 }
 
