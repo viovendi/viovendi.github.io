dfb3309b74a939d8e0166d3c7e22bd55d0fd35ac
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-02-10 15:07:34 +0100

    Create frenchTranslation.js

---
+++ customizations/organization/13470/frenchTranslation.js
@@ -0,0 +1,37 @@
+console.log("translating")
+
+function changeTextTo(selector,originalText, newText){
+  $(selector).each(function(){
+    if($(this).text().trim()==originalText){
+        $(this).text(newText);
+    }
+  }) 
+}
+
+function handler2(){
+  changeTextTo('.customization2_attendee_further-data_custom-question_dropdown option','Bitte auswählen', 'Veuillez sélectionner');
+   
+}
+
+
+/*
+
+INSERTION LISTENER
+
+*/
+var insertionListener = function (event) {
+    if (event.animationName === "nodeInserted") {
+        console.log("Node has been inserted: ", event.target);
+        
+        handler2();
+
+        $('customization2_attendee_view-action_edit').on("click", function () {
+            console.log("clicked");
+        });
+    }
+}
+handler2();
+
+document.addEventListener("animationstart", insertionListener, false); // standard + firefox
+document.addEventListener("MSAnimationStart", insertionListener, false); // IE
+document.addEventListener("webkitAnimationStart", insertionListener, false); // 
