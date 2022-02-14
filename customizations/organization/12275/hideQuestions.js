f14f0becf3ab38178ad797aa48942ff51f1f283c
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-02-14 14:56:30 +0100

    Update hideQuestions.js

--- customizations/organization/12275/hideQuestions.js
+++ customizations/organization/12275/hideQuestions.js
@@ -45,7 +45,7 @@ function setup(){
     var pathArray = window.location.pathname.split('/');
     
     for (i = 0; i < pathArray.length; i++) {
-        if(pathArray[i].contains("100"))console.log("is right eventID")
+        if(pathArray[i].includes("100"))console.log("is right eventID")
      }
     
 }
