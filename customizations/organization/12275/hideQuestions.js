80d42b25b9b802c692b7d0e2c81699ba081ced14
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-02-15 12:22:27 +0100

    Update hideQuestions.js

--- customizations/organization/12275/hideQuestions.js
+++ customizations/organization/12275/hideQuestions.js
@@ -221,7 +221,7 @@ function disableWhenEmpty(field) {
         
         $(inputOfField).addClass('error-state');
     
-        $(inputOfField).on('input change paste', function () {
+        $(inputOfField).on('input change paste click', function () {
             console.log('input changed')
             if ($(this).val().trim().length == 0) {
                 $(this).addClass('error-state');
@@ -236,20 +236,7 @@ function disableWhenEmpty(field) {
             }
         });
     
-        $(inputOfField).parent().on('input change paste', function () {
-            console.log('input changed')
-            if ($(this).val().trim().length == 0) {
-                $(this).addClass('error-state');
-                $(field).find('.error-message').show();
-                $('.customization2_attendee_edit-action_save').prop("disabled", true);
-
-            } else {
-                console.log('value that isnt empty: '+$(this).val())
-                $(this).removeClass('error-state');
-                $(field).find('.error-message').hide();
-                $('.customization2_attendee_edit-action_save').prop("disabled", false);
-            }
-        });
+       
        
     }
 
