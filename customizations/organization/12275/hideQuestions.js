4b958e02a8944edae6a4709eafb0dbaf106a939f
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-02-15 15:41:58 +0100

    Update hideQuestions.js

--- customizations/organization/12275/hideQuestions.js
+++ customizations/organization/12275/hideQuestions.js
@@ -217,7 +217,7 @@ function disableWhenEmpty(field) {
 
             inputOfField = $(field).find('.customization2_attendee_further-data_custom-question_date');
         }else{
-            if (!$(inputOfField).hasClass("error-state")) {
+            if (!$(inputOfField).hasClass('.error-state')) {
                 $("<div class='error-message'>Erforderlich</div>").insertAfter($(inputOfField));
             }
         }
