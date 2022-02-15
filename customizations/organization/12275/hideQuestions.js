8c9111c7c2c66815a7fd0cd578aea90114183588
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-02-15 12:17:58 +0100

    Update hideQuestions.js

--- customizations/organization/12275/hideQuestions.js
+++ customizations/organization/12275/hideQuestions.js
@@ -236,7 +236,20 @@ function disableWhenEmpty(field) {
             }
         });
     
-        
+        $(inputOfField).parent().on('input change paste', function () {
+            console.log('input changed')
+            if ($(this).val().trim().length == 0) {
+                $(this).addClass('error-state');
+                $(field).find('.error-message').show();
+                $('.customization2_attendee_edit-action_save').prop("disabled", true);
+
+            } else {
+                console.log('value that isnt empty: '+$(this).val())
+                $(this).removeClass('error-state');
+                $(field).find('.error-message').hide();
+                $('.customization2_attendee_edit-action_save').prop("disabled", false);
+            }
+        });
        
     }
 
