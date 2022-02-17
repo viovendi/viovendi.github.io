1fe43810e3e7f184eed553f4f07a17ccbedb3c90
Oleg Stetsko <oleh.stetsko@gmail.com>
2022-02-17 17:39:23 +0200

    test

--- customizations/organization/14426/addlink.js
+++ customizations/organization/14426/addlink.js
@@ -6,7 +6,8 @@ if(window.location.href.indexOf('88669') !== -1){
     // Script works only for EID 88669
   if(href.indexOf('de-de') !== -1){
     const radioLineLabel1 = $('.customization2_attendee_further-data_custom-question-1 .customization2_attendee_further-data_custom-question_radio-line_label p');
-    console.log($$('.customization2_attendee_further-data_custom-question').findRadioGroup('Datenschutzhinweis'))
+    $$('.customization2_attendee_further-data_custom-question').findRadioGroup('Datenschutzhinweis')
+    console.log($$('.customization2_attendee_further-data_custom-question').findRadioGroup('Datenschutzhinweis').find('p'))
   radioLineLabel1.html(`
     <span>Ich bin mit den Bedingungen unter folgendem Link einverstanden </span>
     <a target='_blank' href='https://www.jagd-und-hund.de/en/beretta/'>www.westfalenhallen.de/datenschutzhinweis-beretta.</a>
