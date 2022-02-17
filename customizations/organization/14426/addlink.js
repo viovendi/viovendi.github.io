52fd73448811d4c87e7301171513d3d6d4531d32
Oleg Stetsko <oleh.stetsko@gmail.com>
2022-02-17 17:52:22 +0200

    test

--- customizations/organization/14426/addlink.js
+++ customizations/organization/14426/addlink.js
@@ -5,7 +5,7 @@ if(window.location.href.indexOf('88669') !== -1){
   const href = window.location.href;
     // Script works only for EID 88669
   if(href.indexOf('de-de') !== -1){
-    const radioLineLabel1 = $('.customization2_attendee_further-data_custom-question-1 .customization2_attendee_further-data_custom-question_radio-line_label p');consoele.log
+    const radioLineLabel1 = $('.customization2_attendee_further-data_custom-question-1 .customization2_attendee_further-data_custom-question_radio-line_label p');
    console.log( $$('.customization2_attendee_further-data_custom-question').findRadioGroup('Datenschutzhinweis').querySelectorAll('p.vv-radio__label-text')[0])
 
   radioLineLabel1.html(`
