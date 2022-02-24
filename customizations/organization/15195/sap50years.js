fc53d1fa5ddaf81c12edffccbdfca71c685b729d
vbodend <69503064+vbodend@users.noreply.github.com>
2022-02-24 09:57:56 +0100

    Update sap50years.js

--- customizations/organization/15195/sap50years.js
+++ customizations/organization/15195/sap50years.js
@@ -51,7 +51,7 @@ function hideStuff(name){
   if (ticketTitle == "Partner" ) {
     $('.'+name+' .customization2_attendee_contact-data_email vv-optional-text > vv-text > span').hide();
     $('.'+name+' .customization2_attendee_contact-data_email-confirmation vv-optional-text > vv-text > span').hide();
-    $('.'+name+' .customization2_attendee_contact-data_email').before('<span class= "addedHeading" >Falls ihr Partner auch bei der SAP arbeitet, bitte tragen Sie die SAP-Email-Adresse ein</span>');
+    $('.'+name+' .customization2_attendee_contact-data_email').before('<span class= "addedHeading" style="color:#343a3f">Falls ihr Partner auch bei der SAP arbeitet, bitte tragen Sie die SAP-Email-Adresse ein</span>');
   }
   
   
