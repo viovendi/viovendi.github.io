64b5105c7d3e3041da2ab3faade7adf82fdf1dbd
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-02-28 14:03:33 +0100

    Update translate.js

--- customizations/organization/14759/translate.js
+++ customizations/organization/14759/translate.js
@@ -253,7 +253,7 @@ $('.p-datepicker-prev').click(function () {
 			$('.ew-confirmation__organizer-contact span.text--grey').text('Om du har några frågor, vänligen kontakta');
 		
 			//
-			var bookingId = $(".notice__booking-id span").html();
+			var bookingId = $(".notice__booking-id span").text();
 			$(".notice__booking-id").html("Boknings-ID: " + bookingId);
 
 	}, 3000);
