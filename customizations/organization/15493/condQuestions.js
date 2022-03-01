0fbf455abd9f20408a6d38d13502f8ccd0614707
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-03-01 17:50:51 +0100

    Update condQuestions.js

--- customizations/organization/15493/condQuestions.js
+++ customizations/organization/15493/condQuestions.js
@@ -118,6 +118,12 @@ function addWeitereAngabenTeilnehmerHeader () {
     }
 }
 
+function addWeitereAngabenBegleitungHeader () {
+    if(!$('.weitereAngabenBegleitung').length){
+        console.log("Weitere Angaben added");
+        $($$('.customization2_attendee_further-data_custom-question').findField('Meine Begleitung wählt zum intergalaktischen Dinner')).prepend('<div class="weitereAngabenBegleitung" style="margin-bottom:10px; margin-top: 14px"><p style="font-size:1rem; font-weight: 600; color: #343a3f;">Weitere Angaben</p></div>');
+    }
+}
 
 handler();
 addHotelDescription();
