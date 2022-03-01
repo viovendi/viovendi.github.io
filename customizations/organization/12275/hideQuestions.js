46294e81a9f5165d1ab569667af3f3ab8903fdfb
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-03-01 10:47:50 +0100

    Update hideQuestions.js

--- customizations/organization/12275/hideQuestions.js
+++ customizations/organization/12275/hideQuestions.js
@@ -47,7 +47,7 @@ function setup(){
     
 }
 function hide(field){
-    if($(field[0]).hasClass("error-state"))console.log("error state detected");
+    if(field[0].hasClass("error-state"))console.log("error state detected");
         $(field[0]).removeClass("error-state");
         $(field).hide(); 
 
