b333c4945596574dd1f6a223936ff418afa10c3e
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-03-07 08:55:50 +0100

    Update translate.js

--- customizations/organization/14759/translate.js
+++ customizations/organization/14759/translate.js
@@ -159,7 +159,11 @@ $('.p-datepicker-prev').click(function () {
 			$('.customization2_attendee_edit-action_save').text('Jag har fyllt i mina uppgifter');
 		
 			//Copy details from attendee 1
-			$(".customization2_attendee_contact-data_copy-link").text($(".customization2_attendee_contact-data_copy-link").text().replace("Copy details from attendee 1", "Kopiera information från deltagare 1"));
+			//$(".customization2_attendee_contact-data_copy-link").text($(".customization2_attendee_contact-data_copy-link").text().replace("Copy details from attendee 1", "Kopiera information från deltagare 1"));
+			$('.customization2_attendee_contact-data_copy-link').each(function() {
+    				var text = $(this).text();
+    				$(this).text(text.replace("Copy details from attendee 1", "Kopiera information från deltagare 1")); 
+			});
 
 			//Cancel
 			var canceltext = $(".customization2_attendee_edit-action_cancel .vv-button-text-blue").text();
