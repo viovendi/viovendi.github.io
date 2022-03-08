74e6e8d24a32ef1622bf56242d86818e838d0018
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-03-08 10:11:36 +0100

    Update hideQuestions.js

--- customizations/organization/12275/hideQuestions.js
+++ customizations/organization/12275/hideQuestions.js
@@ -138,7 +138,7 @@ function showField(field){
 }
 function handler() {
 
-    let questionNumber = 8;
+    let questionNumber = 9;
     //Arrival
     var dropdownArrival = $('.customization2_attendee_further-data_custom-question-'+questionNumber++);
     //
