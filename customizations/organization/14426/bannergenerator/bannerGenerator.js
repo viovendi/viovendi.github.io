e582845a9811155d2ea32fdf0ae8c0816ff0857c
christiankrausse7 <86656535+christiankrausse7@users.noreply.github.com>
2022-03-08 12:56:32 +0100

    Update bannerGenerator.js

--- customizations/organization/14426/bannergenerator/bannerGenerator.js
+++ customizations/organization/14426/bannergenerator/bannerGenerator.js
@@ -27,8 +27,8 @@ function setTexts() {
         ctx.fillText($('#halle_inp').val(), 235, 45);
         ctx.fillText($('#stand_inp').val(), 260, 45);
     } else if (value == 'hochkant') {
-        ctx.fillText($('#halle_inp').val(), 65, 485);
-        ctx.fillText($('#stand_inp').val(), 80 - $('#stand_inp').val().length * 6, 535);
+        ctx.fillText($('#halle_inp').val(), 70, 515);
+        ctx.fillText($('#stand_inp').val(), 80 - $('#stand_inp').val().length * 6, 555);
     } else if (value == 'facebook') {
         //
     }
