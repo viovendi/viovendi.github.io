755a95caafb9a3fc5afcf3c4a047660d4436c3e7
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-03-09 16:55:28 +0100

    Update custom-1022.js

--- customizations/organization/vogel/custom-1022.js
+++ customizations/organization/vogel/custom-1022.js
@@ -145,7 +145,7 @@ var categoriesNamesEn = {'Plant Engineering': ['Plant Design', 'Refrigeration / 
       
       if( labelText.indexOf(parentCatNameVisibleLang) != -1){
         console.log("function:changeParentCat -> if ");
-	console.log(parentCatNameVisibleLang);
+	console.log(this);
         $(this).find('.vv-selection-input__value').not('#childCatSelect').on('change', function(){
 		
 		console.log("element found");
