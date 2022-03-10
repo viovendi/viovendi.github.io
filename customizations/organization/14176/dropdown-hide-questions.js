695ed3497dc7d85ce0895357ec283fba20a6f275
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-03-10 14:20:17 +0100

    Update dropdown-hide-questions.js

--- customizations/organization/14176/dropdown-hide-questions.js
+++ customizations/organization/14176/dropdown-hide-questions.js
@@ -77,7 +77,7 @@ function handler(){
    
     const observer1 = new MutationObserver( function(mutations) {
         console.log('dropdown1 changed');
-    }.bind(this));
+    });
    console.log(dropdown1);
    console.log($(dropdown1).get(0));
    console.log(getValueTagFromDropDown($(dropdown1)).get(0));
