219d8e829af1561c417e48a7c10485cf6067abbb
Oleg Stetsko <oleh.stetsko@gmail.com>
2022-03-10 11:49:45 +0200

    test helpers

--- customizations/shared/helpers.js
+++ customizations/shared/helpers.js
@@ -19,14 +19,14 @@ function myHelpers(elements) {
     Note: you are not required to use the entire field name only part of it
   */
   this.findField = function (lable) {
-   
+
     this.lable = lable;
     this.field = null;
 
     elements.forEach((field) => {
-       
-    console.log('field: ' + field);
-    
+
+    console.log(field.querySelector('label p').innerText);
+
       if (field.querySelectorAll('label p')[0].innerText.trim().includes(this.lable)) {
         this.field = field;
       }
