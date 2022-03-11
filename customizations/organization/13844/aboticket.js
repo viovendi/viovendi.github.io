909477db60cfa4db682fd9fb85dfd13ca44c7d61
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-03-11 09:58:42 +0100

    Update aboticket.js

--- customizations/organization/13844/aboticket.js
+++ customizations/organization/13844/aboticket.js
@@ -54,12 +54,13 @@ function setNumberedAboTickets(amount) {
     const updateTicket = async ticket=>{
         
         await  $(this).find(".vv-selection-input__control").click();
-	await  $(this).find(".vv-single-select-option").each(el => {
+		console.log(ticket);
+	    await  $(this).find(".vv-single-select-option").each(el => {
             console.log(el.text());
 	    console.log(el);
 	});
-        await  $(this).find(".vv-single-select-option").find(el => 
-            el.textContent.trim()==amount).click();
+     //   await  $(this).find(".vv-single-select-option").find(el => 
+     //       el.textContent.trim()==amount).click();
   
         return
     }
