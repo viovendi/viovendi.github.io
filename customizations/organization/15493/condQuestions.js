ca7738642e3af3a6f2a05a477c1e43d3f602246a
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-03-14 12:44:21 +0100

    Update condQuestions.js

--- customizations/organization/15493/condQuestions.js
+++ customizations/organization/15493/condQuestions.js
@@ -211,6 +211,7 @@ const observerThisPage = new MutationObserver((mutations, obs) => {
         const page2 = document.getElementsByClassName('customization-booking-area-wrapper-page2');
     
         if ($(page2).is(':visible')) {
+            console.log('page 2 visible')
             editBookingPortal();
             obs.disconnect();
             return;
