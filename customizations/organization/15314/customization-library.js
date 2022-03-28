4e24472ca7025b9147405d69c55a8c5b143e6b8e
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-03-28 14:19:25 +0200

    Update customization-library.js

--- customizations/organization/15314/customization-library.js
+++ customizations/organization/15314/customization-library.js
@@ -1,18 +1,90 @@
 /*
-param: dropDown is a parent element from the dropdown input
+"name": "Google Tag Manager JS Helpers",
+"version": "1.0.0",
+"author": "Stetsko Oleg",
 */
-function getValueFromDropDown(dropDown) {
-    const valueText = $(dropDown).find('.vv-selection-input__value.m-ellipsis');
-    console.log(valueText);
-    const value = $(valueText).text().trim();
-    return value;
+
+/*
+How to Connect
+
+As an external script in GTM
+<script src="https://viovendi.github.io/customizations/shared/helpers.js"></script>
+*/
+
+function myHelpers(elements) {
+  /*
+   label - is the name of the field you want to get the (string: 'Branche (optional)' )
+    for example  var field = $$('.customization2_attendee_further-data_custom-question').findField('Branche (optional)');
+
+    Note: you are not required to use the entire field name only part of it
+  */
+  this.findField = function (lable) {
+
+    this.lable = lable;
+    this.field = null;
+
+    elements.forEach((field) => {
+
+      if (field.querySelector('label p') && field.querySelector('label p').innerText.trim().includes(this.lable)) {
+        this.field = field;
+      }
+    });
+    return this.field;
+  }
+  this.findRadioGroup = function (lable) {
+    this.lable = lable;
+    this.field = null;
+
+    elements.forEach((field) => {
+     if ($(field.querySelectorAll('.vv-radio-group > p')[0]).text().trim().includes(this.lable)) {
+        this.field = field;
+      }
+    });
+    return this.field;
+  }
+  this.findCheckboxGroup = function (lable) {
+    this.lable = lable;
+    this.field = null;
+
+    elements.forEach((field) => {
+     if ($(field.querySelectorAll('.vv-checkbox-group > p')[0]).text().trim().includes(this.lable)) {
+        this.field = field;
+      }
+    });
+    return this.field;
+  }
+    //Maybe useless because findField works now? should be checked
+  this.findDropDown = function (lable) {
+    this.lable = lable;
+    this.field = null;
+    elements.forEach((field) => {
+        const labelText = $(field).find(".vv-control-label").text();
+        if (labelText.trim().includes(lable)) {
+           this.field = field;
+        }
+    });
+    return this.field;
 }
+  return this
+}
+
+/*
+ selector - is the CSS class of the parent input field that you want to change
+ for example $$('.customization2_attendee_further-data_custom-question')
+*/
+function $$(selector) {
+  const elements = document.querySelectorAll(selector);
+  return new myHelpers(elements);
+}
+
+
 /*
 param: dropDown is a parent element from the dropdown input
 */
-function getValueTagFromDropDown(dropDown) {
+this.getValueFromDropDown = function (dropDown) {
     const valueText = $(dropDown).find('.vv-selection-input__value.m-ellipsis');
-    return valueText;
+    const value = $(valueText).text().trim();
+    return value;
 }
 
 function findDropDownByLabel(label) {
