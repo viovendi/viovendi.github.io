036722c651780c4aa90c9ac36757db3e8224ea22
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-03-30 12:23:24 +0200

    Update loginScript.js

--- customizations/organization/15195/loginScript.js
+++ customizations/organization/15195/loginScript.js
@@ -1,5 +1,15 @@
 var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 console.log(CognitoUserPool)
+var poolData = {
+	UserPoolId: 'eu-central-1_N6OlPZKBe', // Your user pool id here
+	ClientId: '132qeevu4fo2biee2gp40t623d', // Your client id here
+};
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+var userData = {
+	Username: 'username',
+	Pool: userPool,
+};
+var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 cognitoUser.getUserAttributes(function(err, result) {
 	if (err) {
 		alert(err.message || JSON.stringify(err));
