967a3de20416ca35fe50ed0179a1cb4b52b69216
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-04-05 14:47:30 +0200

    Update sap50years.js

--- customizations/organization/15195/sap50years.js
+++ customizations/organization/15195/sap50years.js
@@ -126,19 +126,6 @@ document.addEventListener("animationstart", insertionListener, false); // standa
 document.addEventListener("MSAnimationStart", insertionListener, false); // IE
 document.addEventListener("webkitAnimationStart", insertionListener, false); //
 
-
-
-/*var insertionListener2 = function (event) {
-  if (event.animationName === "nodeInserted2") {
-      init('customization2_attendee');
-  }
-};
-
-
-document.addEventListener("animationstart", insertionListener2, false); // standard + firefox
-document.addEventListener("MSAnimationStart", insertionListener2, false); // IE
-document.addEventListener("webkitAnimationStart", insertionListener2, false); // Chrome + Safari */
-
 // CUSTOMIZATION ON PAGE 1
 
 
