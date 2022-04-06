f25f97562dc156eab11d8faef53dbfc02c1167ba
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-06 18:51:23 +0200

    Update translation.js

--- customizations/organization/15517/translation.js
+++ customizations/organization/15517/translation.js
@@ -18,28 +18,29 @@ function hideDefaultOption(selector, originalText) {
         }
     })
 }
+
 var nameArray = {
-    "On-site participation - Steering Committee / General Council": "new name 1", 
-    "Online Participation - Steering Committee / General Council": "new name 2", 
-    "On-site participation - ONLY Commissions  /  Working Groups":"new name 3",
-    "Accompanying person package":"new name 4",
-    "A1) International Notarial Cooperation Commission (CCNI)": "A1)", 
-    "A2) Advisory Commission (CC)": "A2)", 
-    "A3) WG «Partnerships with International Organizations»":"A3)",
-    "B1) Human Rights Commission (CDH)":"B1)",
-    "B2) WG «Prevention of Money Laundering»": "B2)", 
-    "B3) WG «Authentic Act» + WG «New Technologies»": "B3)", 
-    "C1) Notarial Deontology Commission (CDN)":"C1)",
-    "C2) Themes and Congresses Commission (CTC)":"C2)",
-    "C3) Permanent Notarial Office for International Exchanges (ONPI)":"C3)",
-    "D1) Welcome Reception | Thursday, 26 May 2022": "D1)", 
-    "D2) Full-day trip Herrenchiemsee | Friday, 27 May 2022": "D2)", 
-    "D3) Munich City Tour | Saturday, 28 May 2022":"D3)",
-    "D4) Gala Dinner - Delegate | 27 May 2022":"D4)",
-    "D5) I will be accompanied to the Gala Dinner":"D5)",
-    "E1) Welcome Reception | Thursday, 26 May 2022": "E1)", 
-    "E2) Full-day trip Herrenchiemsee Delegates | Friday,": "E2)", 
-    "E3) Munich City Tour | Saturday, 28 May 2022":"E3)"
+    "On-site participation - Steering Committee / General Council": "Participation sur place - Comité directeur / Conseil général", 
+    "Online Participation - Steering Committee / General Council": "Participation en ligne - Comité directeur / Conseil général", 
+    "On-site participation - ONLY Commissions  /  Working Groups":"Participation sur place - UNIQUEMENT Commissions / Groupes de travail",
+    "Accompanying person package":"Forfait pour la personne accompagnante",
+    "A1) International Notarial Cooperation Commission (CCNI)": "A1) Commission de coopération notariale internationale (CCNI)", 
+    "A2) Advisory Commission (CC)": "A2) Commission consultative (CC)", 
+    "A3) WG «Partnerships with International Organizations»":"A3) Groupe de travail 'Partenariats avec les organisations internationales'",
+    "B1) Human Rights Commission (CDH)":"B1) Commission des droits de l'homme (CDH)",
+    "B2) WG «Prevention of Money Laundering»": "B2) GT ' Prévention du blanchiment d'argent'", 
+    "B3) WG «Authentic Act» + WG «New Technologies»": "B3) WG «Authentic Act» + WG «New Technologies»", 
+    "C1) Notarial Deontology Commission (CDN)":"C1) Commission de déontologie notariale (CDN)",
+    "C2) Themes and Congresses Commission (CTC)":"C2) Commission Thèmes et Congrès (CTC)",
+    "C3) Permanent Notarial Office for International Exchanges (ONPI)":"C3) Office notarial permanent pour les échanges internationaux (ONPI)",
+    "D1) Welcome Reception | Thursday, 26 May 2022": "D1) Réception de bienvenue | Jeudi, 26 mai 2022", 
+    "D2) Full-day trip Herrenchiemsee | Friday, 27 May 2022": "D2) Excursion d'une journée à Herrenchiemsee | Vendredi, 27 mai 2022", 
+    "D3) Munich City Tour | Saturday, 28 May 2022":"D3) Visite de la ville de Munich | Samedi, 28 mai 2022",
+    "D4) Gala Dinner - Delegate | 27 May 2022":"D4) Dîner de gala - Délégué | 27 mai 2022",
+    "D5) I will be accompanied to the Gala Dinner":"D5) Je serai accompagné(e) au Dîner de Gala",
+    "E1) Welcome Reception | Thursday, 26 May 2022": "E1) Réception de bienvenue | Jeudi, 26 mai 2022", 
+    "E2) Full-day trip Herrenchiemsee Delegates | Friday,": "E2) Excursion d'une journée à Herrenchiemsee Délégués ", 
+    "E3) Munich City Tour | Saturday, 28 May 2022":"E3) Visite de la ville de Munich | Samedi, 28 mai 2022"
 };
 
 function translateTicketNames() {
