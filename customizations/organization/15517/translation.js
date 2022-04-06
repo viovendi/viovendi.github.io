aa54ed9e6f12111219092f6722eed20dc3a30448
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-06 16:56:14 +0200

    Update translation.js

--- customizations/organization/15517/translation.js
+++ customizations/organization/15517/translation.js
@@ -170,7 +170,7 @@ observer2.observe(document, {
 function startCustomizationPage2(){
    console.log('page 2 visible')
       $('.vv-text.vv-text__size--lg.vv-text__weight--regular.vv-color--text.customization2_summary_item_name').each(function(){
-        console.log($(this).tex())
+        console.log($(this).text())
       });
      
   console.log($('.ew-selection__sum-title').next().html());
