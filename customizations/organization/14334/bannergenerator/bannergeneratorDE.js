3be36ff74cb8029e058b54f04f97aebcbd0ab95b
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-04-07 07:52:57 +0200

    Update bannergeneratorDE.js

--- customizations/organization/14334/bannergenerator/bannergeneratorDE.js
+++ customizations/organization/14334/bannergenerator/bannergeneratorDE.js
@@ -33,8 +33,8 @@ function setTexts() {
         ctx.fillText($('#halle_inp').val(), 110 - $('#halle_inp').val().length * 6, 515);
         ctx.fillText($('#stand_inp').val(), 110 - $('#stand_inp').val().length * 5, 555);
     } else if (value == 'email') {
-        ctx.fillText($('#halle_inp').val(), 28 - $('#halle_inp').val().length * 6, 43);
-        ctx.fillText($('#stand_inp').val(), 49, 43);
+        ctx.fillText($('#halle_inp').val(), 40 - $('#halle_inp').val().length * 6, 30);
+        ctx.fillText($('#stand_inp').val(), 40 - $('#halle_inp').val().length * 6, 47);
     } else if (value == 'instagrampost') {
         ctx.fillText($('#halle_inp').val(), 330 - $('#halle_inp').val().length * 6, 993);
         ctx.fillText($('#stand_inp').val(), 745 - $('#stand_inp').val().length * 9, 993);
@@ -74,7 +74,7 @@ function setupBannerImage() {
     } else if (value == 'vertikal') {
         ctx.font = "13pt Verdana";
     } else if (value == 'email') {
-        ctx.font = "10pt Verdana";
+        ctx.font = "8pt Verdana";
     } else if (value == 'instagrampost') {
         ctx.font = "26pt Verdana";
     } else if (value == 'instagramstory') {
