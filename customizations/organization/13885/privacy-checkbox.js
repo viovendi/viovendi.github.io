4560d80f8934bbee3d646d83c07e2a40501cb14c
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-12 12:16:01 +0200

    Update privacy-checkbox.js

--- customizations/organization/13885/privacy-checkbox.js
+++ customizations/organization/13885/privacy-checkbox.js
@@ -3,7 +3,7 @@ function checkAndHidePrivacy(){
   console.log('checkAndHidePrivacy');
   if ($('.customization2_booking-terms').length > 0 ){
     $('.customization2_booking-terms .customization2_booking-terms_checkbox').trigger('click');
-    $('.customization2_booking-terms .vv-checkbox__indicator').css('display', 'none');
+  //  $('.customization2_booking-terms .vv-checkbox__indicator').css('display', 'none');
     $('.customization2_booking-terms .vv-checkbox').on('click', function(e){
       e.preventDefault();
     });
@@ -11,3 +11,49 @@ function checkAndHidePrivacy(){
 }
 checkAndHidePrivacy();
 
+    var styles = `
+  .customization2_booking-terms .vv-checkbox__indicator{
+    display:none;
+  }
+`
+    var styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.innerText = styles;
+
+
+    styleSheet.onreadystatechange = handlerS;
+    styleSheet.onload = handlerS;
+
+    document.head.appendChild(styleSheet);
+
+    function handlerS() {
+        console.log('style added ');
+    }
+function startCustomizationPage3(){
+  checkAndHidePrivacy();
+    const observerThisPage = new MutationObserver((mutations, obs) => {
+        const page3 = document.getElementsByClassName('customization-booking-area-wrapper-page3');
+
+        if ($(page3).is(':visible')) {
+            startCustomizationPage3();
+            obs.disconnect();
+            return;
+        }
+    });
+    const observerOtherPage = new MutationObserver((mutations, obs) => {
+        const page3 = document.getElementsByClassName('customization-booking-area-wrapper-page3');
+
+        if (!$(page3).is(':visible')) {
+            observerThisPage.observe(document, {
+                childList: true,
+                subtree: true
+            });
+            obs.disconnect();
+            return;
+        }
+    });
+    observerOtherPage.observe(document, {
+        childList: true,
+        subtree: true
+    });
+}
