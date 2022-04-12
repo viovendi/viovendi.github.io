5314f0e3a87e61cdf9c8ce1373d4f522dd851d92
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-12 16:20:10 +0200

    Update customization-library.js

--- customizations/organization/15314/customization-library.js
+++ customizations/organization/15314/customization-library.js
@@ -166,31 +166,77 @@ this.addErrorStyles = function() {
     }
 }
 
-this.disableWhenEmpty = function(field) {
+this.disableWhenEmpty = function(field){
+        var inputOfField = $(field).find('.vv-selection-input__value.m-ellipsis').get(0);
+                console.log("inputOfField: "+inputOfField)
 
-    if (!$(field).find('.customization2_attendee_further-data_custom-question_input').hasClass("error-state")) {
-        $("<div class='error-message'>Erforderlich</div>").insertAfter($(field).find('.customization2_attendee_further-data_custom-question_input'));
-    }
+        if(inputOfField!=undefined){
+            $(field).find('.customization2_attendee_further-data_custom-question_dropdown').addClass('error-state');
+            if (!$(field).find('.customization2_attendee_further-data_custom-question_dropdown').next().hasClass("error-message")) {
+                $("<div class='error-message'> Please complete </div>").insertAfter( $(field).find('.customization2_attendee_further-data_custom-question_dropdown'));
+            }
+            
+        $(field).on("DOMSubtreeModified", ".vv-selection-input__value.m-ellipsis", function () {  
+            console.log("change detected: "+$(this).text())
+           if ($(this).text().trim() == "Please select"||$(this).text().trim() == "Bitte auswählen") {
+                $(field).find('.customization2_attendee_further-data_custom-question_dropdown').addClass('error-state');
+                $(field).find('.error-message').show();
+                $('.customization2_attendee_edit-action_save').prop("disabled", true);
 
-    $(field).find('.customization2_attendee_further-data_custom-question_input').addClass('error-state');
-    $('.customization2_attendee_edit-action_save').prop("disabled", true);
-
-    $(field).find('.customization2_attendee_further-data_custom-question_input').on('input', function () {
-
-        if ($(this).val().trim().length == 0) {
-            $(this).addClass('error-state');
-            $(field).find('.error-message').show();
-            $('.customization2_attendee_edit-action_save').prop("disabled", true);
-
-        } else {
-            $(this).removeClass('error-state');
-            $(field).find('.error-message').hide();
-            $('.customization2_attendee_edit-action_save').prop("disabled", false);
+            } else {
+                $(field).find('.customization2_attendee_further-data_custom-question_dropdown').removeClass('error-state');
+                $(field).find('.error-message').hide();
+             //   $(".error-state").each(function(){console.log($(this))});
+                if($(".error-state").length==0)
+                    $('.customization2_attendee_edit-action_save').prop("disabled", false);
+            }
+        });
+            
+        return;
         }
-    });
-}
-  return this
-}
+    
+         inputOfField = $(field).find('.customization2_attendee_further-data_custom-question_input');   
+
+          if(typeof $(inputOfField).get(0) === 'undefined'){
+               //         console.log('is date')
+            inputOfField = $(field).find('.customization2_attendee_further-data_custom-question_date');
+        }else{
+            if (!$(inputOfField).next().hasClass("error-message")) {
+                $("<div class='error-message'> Please complete </div>").insertAfter($(inputOfField));
+            }
+        }
+        
+       
+        
+        $(inputOfField).addClass('error-state');
+        
+        $(inputOfField).on("focusout blur", function () {
+            myTimeout = setTimeout(function(){
+            $(inputOfField).get(0).dispatchEvent(new Event('change'));
+          $(inputOfField).get(0).click();
+           //     console.log('fired click and change')
+            }, 50);
+        });
+    
+        $(inputOfField).on("click change input", function (event) {
+            
+            if ($(this).val().trim().length == 0) {
+                $(this).addClass('error-state');
+                $(field).find('.error-message').show();
+                $('.customization2_attendee_edit-action_save').prop("disabled", true);
+
+            } else {
+                $(this).removeClass('error-state');
+                $(field).find('.error-message').hide();
+             //   $(".error-state").each(function(){console.log($(this))});
+                if($(".error-state").length==0)
+                    $('.customization2_attendee_edit-action_save').prop("disabled", false);
+            }
+        });
+    
+       
+       
+    }
 
 /*
  selector - is the CSS class of the parent input field that you want to change
