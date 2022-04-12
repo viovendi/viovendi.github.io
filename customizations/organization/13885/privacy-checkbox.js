5c9c8ab5044b7926ce336740cf14d8004dc5345f
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-12 11:14:13 +0200

    Update privacy-checkbox.js

--- customizations/organization/13885/privacy-checkbox.js
+++ customizations/organization/13885/privacy-checkbox.js
@@ -3,8 +3,8 @@ function checkAndHidePrivacy(){
   if ($('.customization2_booking-terms').length > 0 ){
     $('.customization2_booking-terms .customization2_booking-terms_checkbox').trigger('click');
   //  $('.customization2_booking-terms .vv-checkbox__indicator').css('display', 'none');
-    $('.customization2_booking-terms .vv-checkbox').on('click', function(e){
-      e.preventDefault();
+ //   $('.customization2_booking-terms .vv-checkbox').on('click', function(e){
+ //     e.preventDefault();
     });
   }
 }
