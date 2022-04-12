2b50bb1b844227a519694047a6bb4af8352a86fd
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-12 09:45:03 +0200

    Update changeTextPage4.js

--- customizations/organization/13885/changeTextPage4.js
+++ customizations/organization/13885/changeTextPage4.js
@@ -1,2 +1,9 @@
-console.log($('div.ew-confirmation__summary > p').text())
+console.log($('.customization-confirmation-description').text().trim())
 console.log("git");
+const firtstTextElement=$('.customization-confirmation-description');
+const text = firtstTextElement.text().trim();
+if(text == "Vielen Dank für Ihre Buchung. In Kürze erhalten Sie Ihre Bestätigung per E-Mail."){
+  var clone = firtstTextElement.clone();
+  clone.text("Blablieblub")
+  $(clone).insertAfter(firtstTextElement);
+}
