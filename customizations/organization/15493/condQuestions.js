e7948d297da47ba8a1cf62ade945e3f5a9b68064
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-13 14:55:01 +0200

    Update condQuestions.js

--- customizations/organization/15493/condQuestions.js
+++ customizations/organization/15493/condQuestions.js
@@ -138,6 +138,7 @@ function disableWhenEmpty(field) {
 function customTerms() {
     if (!$('.custom_terms_checkbox').length) {
         console.log("Terms added");
+        console.log("Checkbox selected: "+checkboxSelected)
         var customBookingTerms = $(".customization2_booking-terms .vv-mb-xxs vv-checkbox").clone();
         $('.customization2_booking-terms').prepend('<div class="custom_terms_checkbox" style="margin-bottom:10px"></div>');
         $('.custom_terms_checkbox').append(customBookingTerms);
