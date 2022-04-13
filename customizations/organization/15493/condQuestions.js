99e55514a286c051308b263c4dbb76de55f2704b
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-13 14:34:27 +0200

    Update condQuestions.js

--- customizations/organization/15493/condQuestions.js
+++ customizations/organization/15493/condQuestions.js
@@ -136,17 +136,14 @@ function disableWhenEmpty(field) {
 };
 
 function customTerms() {
-    console.log(checkboxSelected)
     if (!$('.custom_terms_checkbox').length) {
         console.log("Terms added");
-        console.log(checkboxSelected)
         var customBookingTerms = $(".customization2_booking-terms .vv-mb-xxs vv-checkbox").clone();
         $('.customization2_booking-terms').prepend('<div class="custom_terms_checkbox" style="margin-bottom:10px"></div>');
         $('.custom_terms_checkbox').append(customBookingTerms);
         $('.custom_terms_checkbox p.customization2_booking-terms_text').text('Ich habe zur Kenntnis genommen, dass die Veranstaltung nach den zum Zeitpunkt der Veranstaltung gültigen Corona-Verordnungen des Landes Sachsen durchgeführt wird. Ich trage dafür Sorge, alle notwendigen Zertifikate am Veranstaltungstag vorweisen zu können.');
-        console.log($('.custom_terms_checkbox').length)
         $('.custom_terms_checkbox').on('click',function(e){
-            if(e=>e.target.name!=undefined&&e.target.name=='isTermsAccepted'){
+            if(e.target.name!=undefined&&e.target.name=='isTermsAccepted'){
                 checkboxSelected=!checkboxSelected;
                 console.log(checkboxSelected);
             }
