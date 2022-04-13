2792d9da8c5bc9d5ef6fbb367d2330aac0ed6f41
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-13 12:03:31 +0200

    Update condQuestions.js

--- customizations/organization/15493/condQuestions.js
+++ customizations/organization/15493/condQuestions.js
@@ -1,6 +1,6 @@
 var ticketNumber;
 var pleaseSelect;
-
+var checkboxSelected;
 
 const url = window.location.href;
 
@@ -29,7 +29,7 @@ if (url.includes('booking-15493-33318') || url.includes('booking-15493-33313')) 
 }
 console.log("translating")
 
-let checkboxSelected=true;
+checkboxSelected=true;
 console.log(checkboxSelected)
 
 function findDropDownByLabel(label) {
@@ -136,10 +136,10 @@ function disableWhenEmpty(field) {
 };
 
 function customTerms() {
-    console.log(pleaseSelect)
+    console.log(checkboxSelected)
     if (!$('.custom_terms_checkbox').length) {
         console.log("Terms added");
-        console.log(pleaseSelect)
+        console.log(checkboxSelected)
         var customBookingTerms = $(".customization2_booking-terms .vv-mb-xxs vv-checkbox").clone();
         $('.customization2_booking-terms').prepend('<div class="custom_terms_checkbox" style="margin-bottom:10px"></div>');
         $('.custom_terms_checkbox').append(customBookingTerms);
@@ -147,7 +147,7 @@ function customTerms() {
         $('.custom_terms_checkbox').on('click',function(){
             console.log('clicked');
             checkboxSelected=!checkboxSelected;
-            console.log(pleaseSelect)
+            console.log(checkboxSelected)
         })
     }
 };
