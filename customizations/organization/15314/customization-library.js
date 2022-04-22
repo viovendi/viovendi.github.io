67389c07d6ef56d49a98085263b71802e5202a8e
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-22 16:53:35 +0200

    Update customization-library.js

--- customizations/organization/15314/customization-library.js
+++ customizations/organization/15314/customization-library.js
@@ -110,15 +110,15 @@ function myHelpers(elements) {
     this.condQuestionDropdown = function (dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected) {
 
         var dropdown = this.findDropDown(dropdownLable);
-
+        console.log(dropdown)
         const questionsToShow = [];
         for (let i = 0; i < arrayOfLabelsToShow.length; i++)
             questionsToShow[i] = this.findQuestionByLabel(arrayOfLabelsToShow[i]);
-
+        console.log(questionsToShow)
         const questionsToHide = [];
         for (let i = 0; i < arrayOfLabelsToHide.length; i++)
             questionsToHide[i] = this.findQuestionByLabel(arrayOfLabelsToHide[i]);
-
+        console.log(questionsToHide)
         $(dropdown).on("DOMSubtreeModified", ".vv-selection-input__value.m-ellipsis", function () {
             if ($(this).text().trim() == value) {
 
