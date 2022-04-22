abc65ee0f19d0f1858e640d0057795d7bab2bea5
jonas-hitter-doo <92020389+jonas-hitter-doo@users.noreply.github.com>
2022-04-22 16:10:46 +0200

    Update testing.js

--- customizations/organization/15314/testing.js
+++ customizations/organization/15314/testing.js
@@ -5,4 +5,4 @@ var value = "A";
 var arrayOfLabelsToShow = ["A","A B","A C","A B C"];
 var arrayOfLabelsToHide = ["B","C","B C"];
 var disableSaveOnValueSelected = true;
-$$().condQuestionDropdown(dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected)
+$$('').condQuestionDropdown(dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
