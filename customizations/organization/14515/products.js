12a8f793c75716b3b67413177e7e2f76621e9a41
vbodend <69503064+vbodend@users.noreply.github.com>
2022-04-26 14:06:27 +0200

    Update products.js

--- customizations/organization/14515/products.js
+++ customizations/organization/14515/products.js
@@ -51,23 +51,23 @@ $('.customization2_attendee_further-data_product').each(function(i, element) {
                                                       )};
 
 function disablenaduncheck(shortName){
-$('.customization2_attendee_further-data_product').each(function(i, element) {
-     var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
-     var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-     if(checkboxName.indexOf(shortName) >= 0){
+    $('.customization2_attendee_further-data_product').each(function(i, element) {
+          var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+          var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
+          
+          //disable
+             if(checkboxName.indexOf(disable) >= 0){
                 checkbox.prop( "disabled", true )
-             });
-
-     if(checkbox.is(':checked')){
+             }
+      //uncheck:
+      if(checkbox.is(':checked')){
          if(checkboxName.indexOf(shortName) >= 0){
             checkbox.trigger('click');
            console.log(checkboxName + "clicked / unchecked");
          }
        }
- }
-                                                      )
-  
-};
+    });
+   }
 
 
 
