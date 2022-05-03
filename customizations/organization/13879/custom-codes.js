b571c67f211a81beb9c92a401afbbb781f526d2e
Oleg Stetsko <oleh.stetsko@gmail.com>
2022-05-03 17:07:01 +0300

    custom codes test

---
+++ customizations/organization/13879/custom-codes.js
@@ -0,0 +1,55 @@
+
+async function getPage(page) {
+    var pages = {
+      page1: "customization-booking-area-wrapper-page1",
+      page2: "customization-booking-area-wrapper-page2",
+      page3: "customization2_payment_title",
+      page4: "customization-confirmation-label",
+    };
+    return new Promise(function (resolve, reject) {
+      try {
+        const element = document.querySelector("body");
+
+        var observer = new MutationObserver(pageLoaded);
+
+        function pageLoaded(mutations) {
+          mutations.forEach((mutation) => {
+            var classList = mutation.target.classList
+              ? [...mutation.target.classList]
+              : [];
+            if (
+              mutation.type === "childList" &&
+              classList.indexOf(pages[page]) != -1
+            ) {
+              resolve({
+                selector: document.querySelector("." + pages[page]),
+                dataLayer: dataLayer,
+              });
+            }
+          });
+        }
+
+        observer.observe(element, {
+          characterData: true,
+          subtree: true,
+          childList: true,
+        });
+      } catch (error) {
+        reject(new Error(error));
+      }
+    });
+  }
+
+
+
+  async function hendler() {
+    if (document.readyState !== "loading") {
+      console.log("document is already ready");
+
+      await getPage("page2");
+      console.log('custom codes')
+
+    }
+  }
+
+  hendler();
