6bfa6239cb931105458af93cfd7c1498d575db4b
vbodend <69503064+vbodend@users.noreply.github.com>
2022-05-04 14:06:30 +0200

    Update products.js

--- customizations/organization/14515/products.js
+++ customizations/organization/14515/products.js
@@ -73,18 +73,18 @@ function disablenaduncheck(shortName){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
           var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
           var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
-          
-          //disable
-             if(checkboxName.indexOf(shortName) >= 0){
-                checkbox.prop( "disabled", true )
-             }
-      //uncheck:
+          //uncheck:
       if(checkbox.is(':checked')){
          if(checkboxName.indexOf(shortName) >= 0){
             checkbox.trigger('click');
            console.log(checkboxName + "clicked / unchecked");
          }
        }
+          //disable
+             if(checkboxName.indexOf(shortName) >= 0){
+                checkbox.prop( "disabled", true )
+             }
+      
     });
    }
 
