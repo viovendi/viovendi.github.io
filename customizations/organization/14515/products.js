936451eb370f546ab0798c216a52c243bd6e86d9
vbodend <69503064+vbodend@users.noreply.github.com>
2022-05-04 14:34:08 +0200

    Update products.js

--- customizations/organization/14515/products.js
+++ customizations/organization/14515/products.js
@@ -20,7 +20,7 @@ handler();
 
 
 
-function showProductGroup(shortName){
+function showProductGroup2(shortName){
 $('.customization2_attendee_further-data_product').each(function(i, element) {
      var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
@@ -34,7 +34,7 @@ $('.customization2_attendee_further-data_product').each(function(i, element) {
        }
  }
                                                       )};
-function showProductGroup2(shortName){
+function showProductGroup(shortName){
 $('.customization2_attendee_further-data_product').each(function(i, element) {
      var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
      var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
