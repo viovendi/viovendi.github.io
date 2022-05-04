47a1bb5a252f2421c1bb1c7c832c329f895a790f
vbodend <69503064+vbodend@users.noreply.github.com>
2022-05-04 11:22:43 +0200

    Update product-checkbox.js

--- customizations/organization/14515/product-checkbox.js
+++ customizations/organization/14515/product-checkbox.js
@@ -11,7 +11,7 @@ console.log('Start working, Google Tag Manager Tag CUSTOM-1434')
     });
    }
 
-   function enableProd(disable){
+   function enableProd2(disable){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
           var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
          var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
@@ -21,7 +21,7 @@ console.log('Start working, Google Tag Manager Tag CUSTOM-1434')
     });
    }
 
-   function isCheked(){
+   function isCheked2(){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
       var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
       var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
@@ -31,7 +31,7 @@ console.log('Start working, Google Tag Manager Tag CUSTOM-1434')
     })
    }
 
-   function prodCheked(){
+   function prodCheked2(){
     $('.customization2_attendee_further-data_product').each(function(i, element) {
       var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
       var checkboxName = $(this).find('.customization2_attendee_further-data_product_name').text().trim()
@@ -40,7 +40,7 @@ console.log('Start working, Google Tag Manager Tag CUSTOM-1434')
           console.log('checked')
              disableProd2(checkboxName)
         }else if(!checkbox.is(':checked')){
-             enableProd(checkboxName)
+             enableProd2(checkboxName)
           }
       })
     })
@@ -54,8 +54,8 @@ function handler2(){
   }
   else{
   console.log("call functions");
-  isCheked();
-  prodCheked();
+  isCheked2();
+  prodCheked2();
   }
 }
 handler2();
