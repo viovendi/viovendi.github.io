2abf5e0ce43e184d581847b6a296adc3791dfc0a
Oleg Stetsko <oleh.stetsko@gmail.com>
2022-05-06 16:51:53 +0300

    test

--- customizations/organization/13547/condquestions.js
+++ customizations/organization/13547/condquestions.js
@@ -222,6 +222,15 @@ var insertionListener = function (event) {
     var question = $$('.customization2_attendee_further-data_custom-question').findField('Wie viele Mitglieder vertreten Sie?');
     var dropDownQuestion = $(question).find('.customization2_attendee_further-data_custom-question_dropdown')
     console.log(dropDownQuestion)
+
+    var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        if ($(mutation.target).hasClass('vv-selection-input__control--focus')) {
+console.log("mutation")
+        }
+      });
+    });
+    observer.observe(dropDownQuestion, {attributes:true});
   }
 
 }
