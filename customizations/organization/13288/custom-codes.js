4d881859ddf3dbda831e088fc14916d4e1324ca4
Oleg Stetsko <oleh.stetsko@gmail.com>
2022-05-10 18:00:08 +0300

    test

---
+++ customizations/organization/13288/custom-codes.js
@@ -0,0 +1,48 @@
+  async function getCode(key) {
+    return $.ajax({
+      url: 'https://cs.doo.net/v1/integrations/custom-qr-codes/get-code?key=' + key,
+      type: 'get',
+      dataType: 'json',
+    });
+  }
+
+
+  function markCodeAsUsed(code, key) {
+    $.ajax({
+      url: 'https://cs.doo.net/v1/integrations/custom-qr-codes/mark-code-as-used',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      type: 'post',
+      data: JSON.stringify({
+        "customCode": code,
+        "key": key,
+      }),
+    })
+  }
+
+  function addCode(code, input) {
+    let event;
+    input.value = code
+      if(typeof(Event) === 'function') {
+        event = new Event('input'); // for Chrome
+      }else{
+        event = document.createEvent('Event');
+        event.initEvent('input', true, true); // for IE
+      }
+      input.dispatchEvent(event)
+      return input.value;
+  }
+
+
+  async function hendler() {
+    if (document.readyState !== "loading") {
+      console.log("document is already ready");
+    //   const code = await getCode("13288")13879
+      const code = await getCode("13879")
+    //   addCode(code, input)
+      console.log(code)
+     }
+  }
+
+  hendler();
