a62723a2559f18cc7f88ae31d8e1236c5fe2087c
christiankrausse7 <86656535+christiankrausse7@users.noreply.github.com>
2022-05-10 16:24:59 +0200

    Update bannergeneratorDE.js

--- customizations/organization/15342/bannergenerator/bannergeneratorDE.js
+++ customizations/organization/15342/bannergenerator/bannergeneratorDE.js
@@ -27,10 +27,10 @@ function setTexts() {
     const value = $('#format_inp').val();
 
     if (value == 'horizontal') {
-        ctx.fillText($('#halle_inp').val(), 397 - $('#halle_inp').val().length * 4, 37);
+        ctx.fillText($('#halle_inp').val(), 397 - $('#halle_inp').val().length * 4, 35);
         ctx.fillText($('#stand_inp').val(), 435 - $('#stand_inp').val().length * 4, 45);
     } else if (value == 'vertikal') {
-        ctx.fillText($('#halle_inp').val(), 78 - $('#halle_inp').val().length * 6, 495);
+        ctx.fillText($('#halle_inp').val(), 78 - $('#halle_inp').val().length * 6, 497);
         ctx.fillText($('#stand_inp').val(), 78 - $('#stand_inp').val().length * 5, 565);
     } else if (value == 'email') {
         ctx.fillText($('#halle_inp').val(), 24 - $('#halle_inp').val().length * 6, 44);
