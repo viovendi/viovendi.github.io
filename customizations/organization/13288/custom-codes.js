a524f41b5be8008907de173d96636e4967ff479a
Oleg Stetsko <oleh.stetsko@gmail.com>
2022-05-11 11:15:25 +0300

    TEST

--- customizations/organization/13288/custom-codes.js
+++ customizations/organization/13288/custom-codes.js
@@ -39,7 +39,7 @@
       if (document.readyState !== "loading") {
           console.log("document is already ready");
           //   const code = await getCode("13288")13879
-          const code = await getCode("13879")
+          const code = await getCode("TEST")
           const qrCode = $$('.customization2_attendee_further-data_custom-question').findField('QR-Code Nummer');
           const input = $(qrCode).find('input')
 
