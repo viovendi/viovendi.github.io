34b028dd0c5fcc59166c6154ddaed6639654a668
Oleg Stetsko <oleh.stetsko@gmail.com>
2022-05-11 11:03:13 +0300

    test

--- customizations/organization/13288/custom-codes.js
+++ customizations/organization/13288/custom-codes.js
@@ -1,34 +1,34 @@
   async function getCode(key) {
-    return $.ajax({
-      url: 'https://cs.doo.net/v1/integrations/custom-qr-codes/get-code?key=' + key,
-      type: 'get',
-      dataType: 'json',
-    });
+      return $.ajax({
+          url: 'https://cs.doo.net/v1/integrations/custom-qr-codes/get-code?key=' + key,
+          type: 'get',
+          dataType: 'json',
+      });
   }
 
 
   function markCodeAsUsed(code, key) {
-    $.ajax({
-      url: 'https://cs.doo.net/v1/integrations/custom-qr-codes/mark-code-as-used',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      type: 'post',
-      data: JSON.stringify({
-        "customCode": code,
-        "key": key,
-      }),
-    })
+      $.ajax({
+          url: 'https://cs.doo.net/v1/integrations/custom-qr-codes/mark-code-as-used',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          type: 'post',
+          data: JSON.stringify({
+              "customCode": code,
+              "key": key,
+          }),
+      })
   }
 
   function addCode(code, input) {
-    let event;
-    input.value = code
-      if(typeof(Event) === 'function') {
-        event = new Event('input'); // for Chrome
-      }else{
-        event = document.createEvent('Event');
-        event.initEvent('input', true, true); // for IE
+      let event;
+      input.value = code
+      if (typeof (Event) === 'function') {
+          event = new Event('input'); // for Chrome
+      } else {
+          event = document.createEvent('Event');
+          event.initEvent('input', true, true); // for IE
       }
       input.dispatchEvent(event)
       return input.value;
@@ -36,22 +36,23 @@
 
 
   async function hendler() {
-    if (document.readyState !== "loading") {
-      console.log("document is already ready");
-    //   const code = await getCode("13288")13879
-      const code = await getCode("13879")
-      const qrCode = $$('.customization2_attendee_further-data_custom-question').findField('QR-Code Nummer');
-     const input = $(qrCode).find('input')
-      console.log(input)
-      console.log(code)
-    addCode(code.payload.code, input[0])
-     }
+      if (document.readyState !== "loading") {
+          console.log("document is already ready");
+          //   const code = await getCode("13288")13879
+          const code = await getCode("13879")
+          const qrCode = $$('.customization2_attendee_further-data_custom-question').findField('QR-Code Nummer');
+          const input = $(qrCode).find('input')
+
+
+          addCode(code.payload.customCode, input[0])
+      }
   }
 
-  var insertionListener = function(event) {
-    if (event.animationName === "nodeInserted") {
-        hendler();
-    }}
+  var insertionListener = function (event) {
+      if (event.animationName === "nodeInserted") {
+          hendler();
+      }
+  }
   document.addEventListener("animationstart", insertionListener, false); // standard + firefox
   document.addEventListener("MSAnimationStart", insertionListener, false); // IE
   document.addEventListener("webkitAnimationStart", insertionListener, false); // Chrome + Safari
