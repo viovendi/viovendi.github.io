5b62551bbb8986a9c0f6d4e8126a66f9c10e0f9b
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-05-23 08:47:34 +0200

    Update hideQuelle.js

--- customizations/organization/13288/hideQuelle.js
+++ customizations/organization/13288/hideQuelle.js
@@ -9,7 +9,7 @@ handler();
 //Insert here, what should run once
 //
 var insertionListener = function(event) {  
-  if (event.animationName === "nodeInserted2") {    
+  if (event.animationName === "nodeInserted") {    
     console.log("Node has been inserted: ", event.target);
     $($$('.customization2_attendee_further-data_custom-question').findField('Quelle')).hide();
     $($$('.customization2_booker_further-data_custom-question').findField('Quelle')).hide();
