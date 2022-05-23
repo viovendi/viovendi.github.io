11605fcdb57e69c384e09d7de910f5f73e48d088
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-05-23 07:45:45 +0200

    Update hideQuelle.js

--- customizations/organization/13288/hideQuelle.js
+++ customizations/organization/13288/hideQuelle.js
@@ -5,31 +5,6 @@ function handler(){
   $($$('.customization2_booker_further-data_custom-question').findField('Quelle')).hide();
   };
 
-/*const observer1 = new MutationObserver((mutations, obs) => {
-    const page2attendee = document.getElementsByClassName('customization2_attendee-state_edit');
-
-    if ($(page2attendee).is(':visible')) {
-        handler();
-        obs.disconnect();
-        return;
-    }
-});
-
-observer1.observe(document, {
-    childList: true,
-    subtree: true
-});
-
-const observer2 = new MutationObserver((mutations, obs) => {
-    const page2booker = document.getElementsByClassName('customization2_booker-state_edit');
-
-    if ($(page2booker).is(':visible')) {
-        handler();
-        obs.disconnect();
-        return;
-    }
-});
-
 observer2.observe(document, {
     childList: true,
     subtree: true
@@ -41,9 +16,7 @@ handler();
 var insertionListener = function(event) {  
   if (event.animationName === "nodeInserted") {    
     console.log("Node has been inserted: ", event.target);
-    console.log("hide attendee Quelle");
     $($$('.customization2_attendee_further-data_custom-question').findField('Quelle')).hide();
-    console.log("hide booker Quelle");
     $($$('.customization2_booker_further-data_custom-question').findField('Quelle')).hide();
   }
 }
