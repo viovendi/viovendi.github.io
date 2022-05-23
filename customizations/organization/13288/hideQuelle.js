3a82abc33c89a1f957dd54f75bdf37ac68bc22f1
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-05-23 07:40:22 +0200

    Update hideQuelle.js

--- customizations/organization/13288/hideQuelle.js
+++ customizations/organization/13288/hideQuelle.js
@@ -2,10 +2,10 @@
 function handler(){
   console.log("hide Quelle");
   $($$('.customization2_attendee_further-data_custom-question').findField('Quelle')).hide();
-  $($$('.customization2_booker_further-data_custom-question').findField('Quelle')).hide()
-  }
+  $($$('.customization2_booker_further-data_custom-question').findField('Quelle')).hide();
+  };
 
-const observer1 = new MutationObserver((mutations, obs) => {
+/*const observer1 = new MutationObserver((mutations, obs) => {
     const page2attendee = document.getElementsByClassName('customization2_attendee-state_edit');
 
     if ($(page2attendee).is(':visible')) {
@@ -33,7 +33,7 @@ const observer2 = new MutationObserver((mutations, obs) => {
 observer2.observe(document, {
     childList: true,
     subtree: true
-});
+});*/
 
 handler();
 //Insert here, what should run once
