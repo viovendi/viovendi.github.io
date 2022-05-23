1a8c15d827619aa352ac209ef15604ff650590e6
vbodend <69503064+vbodend@users.noreply.github.com>
2022-05-23 13:47:36 +0200

    Update sap50years.js

--- customizations/organization/15195/sap50years.js
+++ customizations/organization/15195/sap50years.js
@@ -13,8 +13,9 @@ if (getWidgetLang() == 'de') {
 
 
 async function updateTicket(ticket) {
-  await $(ticket).find(".vv-selection-input__control").click();
-  await $(ticket).find(".vv-single-select-option").filter(el => el == '0').click();
+  // !!!!!!! TEST !!!!! uncomment
+ // await $(ticket).find(".vv-selection-input__control").click();
+ // await $(ticket).find(".vv-single-select-option").filter(el => el == '0').click();
   return
 }
 
