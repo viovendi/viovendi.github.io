be1abfe49ff25b55edc3699e34dd3488766024a3
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-05-25 17:07:08 +0200

    Update sap50years.js

--- customizations/organization/15195/sap50years.js
+++ customizations/organization/15195/sap50years.js
@@ -338,7 +338,7 @@ function changeMobileNumber() {
 oberserverPhoneNumber = new MutationObserver((mutations, obs) => {
     const phoneNumber = document.getElementsByClassName('.customization2_attendee_further-data_custom-question_phone');
 
-    if ($(phoneNumber).is(':visible')) {
+    if (!$(phoneNumber).is(':visible')) {
       console.log("observerChangeMobilenumber");
       $(".customization2_attendee_further-data_custom-question_phone input").attr("placeholder", "1234 56789");
       obs.disconnect();
