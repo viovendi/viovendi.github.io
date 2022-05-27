0925308b8f2d1f9ec0924aef52a929492c622bf2
vbodend <69503064+vbodend@users.noreply.github.com>
2022-05-27 14:17:17 +0200

    Update sap50years.js

--- customizations/organization/15195/sap50years.js
+++ customizations/organization/15195/sap50years.js
@@ -242,6 +242,8 @@ function addstuff(name) {
   console.log("click");
   if (getWidgetLang() == 'de') {
     console.log("de");
+    $("#widgetContainer > vv-widget-master > div > div > vv-booking-short > div > vv-information-step-master > div > vv-information-step > form > div:nth-child(2) > vv-questions > vv-attendees > div > vv-attendee > div > vv-collapsible-wrapper > div > div.collapsible-wrapper_header.customization3_collapsible-wrapper_header > div.d-none.d-sm-flex.collapsible-wrapper__view-actions.collapsible-wrapper__view-actions--desktop.customization3_collapsible-wrapper_view-actions.customization2_attendee_view-action > div > vv-tooltip > div > vv-button > button").click();
+    
  // $("#widgetContainer > vv-widget-master > div > div > vv-booking-short > div > vv-information-step-master > div > vv-information-step > form > div:nth-child(2)").after("Nach Abschluss der Buchung kannst du keine weiteren Personen mehr zu deiner Buchung hinzufügen. Falls du später weitere Personen hinzufügen oder stornieren möchtest, sende bitte eine Email an 50years-wdf@sap.com");
 }
   var ticketTitle = $('.customization2_attendee_title').text().trim()
