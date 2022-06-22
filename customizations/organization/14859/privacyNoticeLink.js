20cdfeaaffdb5cdf7bee6e13de7542e5e5bef858
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-06-22 08:32:52 +0200

    Update privacyNoticeLink.js

--- customizations/organization/14859/privacyNoticeLink.js
+++ customizations/organization/14859/privacyNoticeLink.js
@@ -5,7 +5,7 @@ function handler(){
 
   $($(privacyNotice).find(".customization2_attendee_further-data_custom-question_checkbox-line_label")).before('<p style = "font-size: .875rem; line-height: 1.37rem; color: #343a3f;"> I want to have my contact details shared within the Oerlikon Group and with Oerlikon\'s partners of the 2022 AMTC conference listed in the <a href="https://doo-product-consulting-uploads.s3.eu-central-1.amazonaws.com/Oerlikon/2022+AMTC+Conference+privacy+noticefor+website_AF.pdf" target="_blank"> Privacy Notice </a> in order to receive information about their services, products and invites to their events and conferences.</p>');
 
-  $(".customization2_attendee_further-data_custom-question-8 .customization2_attendee_further-data_custom-question_checkbox-line_label").hide();
+  $(".customization2_attendee_further-data_custom-question_checkbox-line_label:contains(I want to have my contact details shared within the Oerlikon Group)").hide();
 }
 handler();
 //Insert here, what should run once
