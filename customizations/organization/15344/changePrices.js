c3c8c7b5a1785f53ba05e8f404077e756192c9d0
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-06-29 09:46:14 +0200

    Update changePrices.js

--- customizations/organization/15344/changePrices.js
+++ customizations/organization/15344/changePrices.js
@@ -1,6 +1,37 @@
 //What runs on every new attendee form
+function changeTextTo(selector, originalText, newText) {
+    $(selector).each(function () {
+        if ($(this).text().trim() == originalText) {
+            $(this).text(newText);
+        }
+    })
+};
+
+function changePriceTags() {
+    changeTextTo('.event-category__price', '236,25 EUR', '1.890 EUR');
+    changeTextTo('.event-category__price', '273,75 EUR', '2.190 EUR');
+};
+
+function changeTicketAmount() {
+    changeTextTo('.vv-single-select-option', '8', '1');
+
+    document.querySelectorAll(".vv-selection-input__value").forEach(el => {
+        if (el.innerText === '8') { el.innerText = '1' }
+    });
+
+    addEventListener('click', e => {
+        if (e.target.classList.contains('vv-single-select-option')) {
+            let selectContainer = e.target.closest('.vv-selection');
+            let valueElement = selectContainer.getElementsByClassName('vv-selection-input__value')[0];
+            valueElement.innerText = e.target.textContent.trim().replace('<br>', '');
+        }
+    });
+
+};
+
 function handler(){
-  
+  changePriceTags();
+  changeTicketAmount();
   }
 handler();
 //Insert here, what should run once
