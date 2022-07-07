24b3000ecfbac47b9a7fadac9134160fb84e0403
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-07-07 10:32:52 +0200

    Update hideZeroEuroPrice.js

--- customizations/organization/14137/hideZeroEuroPrice.js
+++ customizations/organization/14137/hideZeroEuroPrice.js
@@ -1,18 +1,19 @@
-console.log("git")
-function handler(){
+console.log("git");
 
-//What runs on every new attendee form
-
+function handler() {
+  $(".customization-category-price:contains(0,00)").hide();
+  $('.customization2_summary_item_price:contains(0,00)').hide()
 }
+
 handler();
-//Insert here, what should run once
-//
-var insertionListener = function(event) {
-           if (event.animationName === "nodeInserted") {
-           console.log("Node has been inserted: ", event.target);
-           handler();
-      }
-}
+
+var insertionListener = function (event) {
+  if (event.animationName === "nodeInserted" || event.animationName === "nodeInserted2") {
+    console.log("Node has been inserted: ", event.target);
+    handler();
+  }
+};
+
 document.addEventListener("animationstart", insertionListener, false);
-document.addEventListener("MSAnimationStart", insertionListener, false); 
+document.addEventListener("MSAnimationStart", insertionListener, false);
 document.addEventListener("webkitAnimationStart", insertionListener, false);
