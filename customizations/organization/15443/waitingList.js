002fb6a87d66974aca8b0faea889d893353fd857
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-07-13 16:19:39 +0200

    Update waitingList.js

--- customizations/organization/15443/waitingList.js
+++ customizations/organization/15443/waitingList.js
@@ -20,61 +20,52 @@ function showCat(catName) {
     });
 }
 
+function checkCat() {
+    var catArr = [];
+    var i = 0;
+
+    $('.customization-booking-area-wrapper .event-categories li:visible').each(function () {
+
+        var catName = $(this).find('.event-category__name').text().replace(/^\s+|\s+$/g, ""),
+            openBooked = $(this).find('.event-category__amount--sold').length;
+
+        if (catName != '') {
+            var catObj = {};
+            catObj.name = catName;
+            catObj.booked = openBooked;
+
+            catArr[i] = catObj;
+            i++;
+        }
+
+    });
+
+    var bookItem = 0;
+    for (var j = 0; j < catArr.length; j++) {
+        bookItem = bookItem + catArr[j].booked;
+    }
+
+    if (bookItem == catArr.length) {
+        showCat('Warteliste');
+    }
+
+}
+
 function handler() {
-    var id = null;
-    function stop_checking() {
-        cancelAnimationFrame(id);
-    }
-
-    function checking() {
-        id = requestAnimationFrame(checking);
-        if ($('.event-categories').length > 0) {
-            /*
-              calling a functions 
-            */
+    $('.customization-booking-area-wrapper .event-categories li').each(function () {
+        var currentCat = $(this).find('.event-category__name').text().replace(/^\s+|\s+$/g, "");
+        if (currentCat === "Warteliste" ) {
+            console.log("Warteliste found")
             hideCat('Warteliste');
+
             checkCat();
-
-            //stop_checking();
         }
-    }
-    checking();
+    });
+    
 
-    /*
-    hideCat('Warteliste');
-    checkCat();
-    */
 
-    function checkCat() {
-        var catArr = [];
-        var i = 0;
 
-        $('.customization-booking-area-wrapper .event-categories li:visible').each(function () {
-
-            var catName = $(this).find('.event-category__name').text().replace(/^\s+|\s+$/g, ""),
-                openBooked = $(this).find('.event-category__amount--sold').length;
-
-            if (catName != '') {
-                var catObj = {};
-                catObj.name = catName;
-                catObj.booked = openBooked;
-
-                catArr[i] = catObj;
-                i++;
-            }
-
-        });
-
-        var bookItem = 0;
-        for (var j = 0; j < catArr.length; j++) {
-            bookItem = bookItem + catArr[j].booked;
-        }
-
-        if (bookItem == catArr.length) {
-            showCat('Warteliste');
-        }
-
-    }
+    
 
 }
 handler();
