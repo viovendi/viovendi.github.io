7d1d2300eb8ef8a926395678aa293567bc7381ae
vbodend <69503064+vbodend@users.noreply.github.com>
2022-07-15 18:01:27 +0200

    Update test.js

--- customizations/organization/13653/test.js
+++ customizations/organization/13653/test.js
@@ -141,27 +141,27 @@ async function hendler() {
     if (JSON.parse(order).attendees[0]?.ticket.checkin_link.length){
     var img0 = document.createElement("img");
     img0.setAttribute('src', 'https://chart.googleapis.com/chart?chs=190x190&cht=qr&choe=UTF-8&chl='+JSON.parse(order).attendees[0].ticket.checkin_link);
-    img0.setAttribute('style','display: block;')
+    img0.setAttribute('style','display: block;page-break-before: always;')
     }
     if (JSON.parse(order).attendees[1]?.ticket.checkin_link.length){
     var img1 = document.createElement("img");
     img1.setAttribute('src', 'https://chart.googleapis.com/chart?chs=190x190&cht=qr&choe=UTF-8&chl='+JSON.parse(order).attendees[1].ticket.checkin_link);
-    img1.setAttribute('style','display: block;')
+    img1.setAttribute('style','display: block;page-break-before: always;')
     }
     if (JSON.parse(order).attendees[2]?.ticket.checkin_link.length){
     var img2 = document.createElement("img");
     img2.setAttribute('src', 'https://chart.googleapis.com/chart?chs=190x190&cht=qr&choe=UTF-8&chl='+JSON.parse(order).attendees[2].ticket.checkin_link);
-    img2.setAttribute('style','display: block;')
+    img2.setAttribute('style','display: block;page-break-before: always;')
     }
     if (JSON.parse(order).attendees[3]?.ticket.checkin_link.length){
     var img3 = document.createElement("img");
     img3.setAttribute('src', 'https://chart.googleapis.com/chart?chs=190x190&cht=qr&choe=UTF-8&chl='+JSON.parse(order).attendees[3].ticket.checkin_link);
-    img3.setAttribute('style','display: block;')
+    img3.setAttribute('style','display: block; page-break-before: always;')
     }
     if (JSON.parse(order).attendees[4]?.ticket.checkin_link.length){
     var img4 = document.createElement("img");
     img4.setAttribute('src', 'https://chart.googleapis.com/chart?chs=190x190&cht=qr&choe=UTF-8&chl='+JSON.parse(order).attendees[4].ticket.checkin_link);
-    img4.setAttribute('style','display: block;')
+    img4.setAttribute('style','display: block; page-break-before: always;')
     }
     if (JSON.parse(order).attendees[5]?.ticket.checkin_link.length){
     var img5 = document.createElement("img");
