1a033ca3d907a807610dd6fa5a3059d8fdc05cce
Alex <olexiy.movchan@gmail.com>
2022-07-28 07:18:07 -0700

    fix4

--- customizations/organization/13675/custom.js
+++ customizations/organization/13675/custom.js
@@ -9,17 +9,12 @@ function hideError(){
   console.log('HIDE ERROR');
 }
 
-$('.customization2_attendee-state_edit .customization2_attendee_edit-action_save').on('click', function(e){
+$('.customization2_attendee_edit-action_save').on('click', function(e){
   
   console.log('save clicked');
   
-  /*
-  console.log($('.customization2_attendee-state_edit .customization2_attendee_contact-data_first-name_input').val());
-  console.log($('.customization2_attendee-state_edit .customization2_attendee_contact-data_last-name_input').val());
-  */
-  
-  var firstName = $('.customization2_attendee-state_edit .customization2_attendee_contact-data_first-name_input').val();
-  var lastName = $('.customization2_attendee-state_edit .customization2_attendee_contact-data_last-name_input').val();
+  var firstName = $(this).closest('.customization2_attendee_contact-data_first-name_input').val();
+  var lastName = $(this).closest('.customization2_attendee_contact-data_last-name_input').val();
   console.log(firstName);
   console.log(lastName);
   
