d8e26ae4ccb9d93c0da723768786a177267addf8
Alex <olexiy.movchan@gmail.com>
2022-07-28 04:28:21 -0700

    cp-3093

---
+++ customizations/organization/13675/custom.js
@@ -0,0 +1,19 @@
+// get localstorage
+console.log(123456798);
+var attendeesData = [];
+//localStorage.setItem("attendeeData", attendeesData);
+
+//localStorage.setItem("lastname", "Smith");
+//localStorage.getItem("lastname");
+
+
+// update localstorage
+
+
+
+// clear localstorage
+
+
+// page2 - 
+
+
