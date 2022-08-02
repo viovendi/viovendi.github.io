fdb4ce8fcbfc174c8c67427c72dc637a5432f9c0
vbodend <69503064+vbodend@users.noreply.github.com>
2022-08-02 15:00:20 +0200

    Update bannergeneratorDE.js

--- customizations/organization/15520/bannergenerator/bannergeneratorDE.js
+++ customizations/organization/15520/bannergenerator/bannergeneratorDE.js
@@ -39,11 +39,11 @@ function setTexts() {
         ctx.fillText($('#halle_inp').val(), 330 - $('#halle_inp').val().length * 6, 1053);
         ctx.fillText($('#stand_inp').val(), 745 - $('#stand_inp').val().length * 9, 1053);
     } else if (value == 'instagramstory') {
-        ctx.fillText($('#halle_inp').val(), 330 - $('#halle_inp').val().length * 6, 1233);
-        ctx.fillText($('#stand_inp').val(), 745 - $('#stand_inp').val().length * 9, 1233);
+        ctx.fillText($('#halle_inp').val(), 380 - $('#halle_inp').val().length * 6, 1233);
+        ctx.fillText($('#stand_inp').val(), 775 - $('#stand_inp').val().length * 9, 1233);
     } else if (value == 'facebook') {
-        ctx.fillText($('#halle_inp').val(), 330 - $('#halle_inp').val().length * 6, 1233);
-        ctx.fillText($('#stand_inp').val(), 745 - $('#stand_inp').val().length * 9, 1233);
+        ctx.fillText($('#halle_inp').val(), 430 - $('#halle_inp').val().length * 6, 1233);
+        ctx.fillText($('#stand_inp').val(), 755 - $('#stand_inp').val().length * 9, 1233);
     }
 }
 
