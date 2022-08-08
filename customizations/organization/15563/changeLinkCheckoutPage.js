9911e8b51863fcec855ffbd9781c900a637aeec3
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-08-08 09:37:51 +0200

    Create changeLinkCheckoutPage.js

---
+++ customizations/organization/15563/changeLinkCheckoutPage.js
@@ -0,0 +1,2 @@
+// Change booking portal link on checkout page
+$(".customization-client-portal-link").attr("href", "https://www.bundespraesident.de/DE/Bundespraesident/Reisen-und-Termine/Berichte/Buergerfest-des-Bundespraesidenten-2022/Tickets/buergerfest-2022-tickets-node.html?doo_mode=booker-login")
