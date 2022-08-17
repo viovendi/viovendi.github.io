759e2d704781135ab98c59bda80d2cd877f0a52a
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-08-17 16:25:28 +0200

    Update ewe.js

--- customizations/organization/14010/ewe.js
+++ customizations/organization/14010/ewe.js
@@ -1,4 +1,4 @@
-
+/*
 function addClassToField(shortName, className, wrapperClass) {
   $('.' + wrapperClass + ' .customization2_attendee_further-data .vv-nl-mb-lg').each(function (i, element) {
     var item = $(this).find('.customization2_attendee_further-data_custom-question_label').text().replace(/^\s+|\s+$/g, "");
