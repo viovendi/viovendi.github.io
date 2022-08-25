a1d4dcd829c4a01cd0f270e57f3b929311675d16
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-08-25 15:51:20 +0200

    Create page4AddText.js

---
+++ customizations/organization/15831/page4AddText.js
@@ -0,0 +1,19 @@
+console.log("Start Git");
+
+//What runs on every new attendee form
+function handler(){
+  
+  }
+handler();
+//Insert here, what should run once
+//
+var insertionListener = function(event) {  
+  if (event.animationName === "nodeInserted") {    
+    console.log("Node has been inserted: ", event.target);    
+    handler(); 
+  }
+}
+document.addEventListener("animationstart", insertionListener, false); 
+// standard + firefox  
+document.addEventListener("MSAnimationStart", insertionListener, false); // IE  
+document.addEventListener("webkitAnimationStart", insertionListener, false); // 
