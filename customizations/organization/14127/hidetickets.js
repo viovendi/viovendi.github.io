fce232dfa4d1d3a23ebf4bfbe65e7035973108bc
jonas-hitter-doo <jonas.hitter@doo.net>
2022-09-14 14:38:15 +0200

    Update hidetickets.js

--- customizations/organization/14127/hidetickets.js
+++ customizations/organization/14127/hidetickets.js
@@ -35,6 +35,6 @@ function logCat(){
     $('.event-category').each(function() {
         console.log('i');
         console.log($( this ).find('.event-category__name')[0])
-        console.log($( this ).find('.event-category__name')[0].val())
+        console.log($( this ).find('.event-category__name').first().text())
       });
 }
