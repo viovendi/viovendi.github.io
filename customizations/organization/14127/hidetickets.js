b98af493dabc14d501a10d94b78e06b24124e2e1
jonas-hitter-doo <jonas.hitter@doo.net>
2022-09-14 14:21:30 +0200

    Update hidetickets.js

--- customizations/organization/14127/hidetickets.js
+++ customizations/organization/14127/hidetickets.js
@@ -1,5 +1,34 @@
 console.log("hide tickets");
-$('.event-category').each(function() {
-    console.log('i');
-    console.log($( this ).find('.event-category__name')[0].text())
-  });
+const observerThisPage = new MutationObserver((mutations, obs) => {
+    const page1 = document.getElementsByClassName('customization-booking-area-wrapper-page1');
+
+    if ($(page1).is(':visible')) {
+        console.log('on page 1')
+        logCat();
+        obs.disconnect();
+        return;
+    }
+});
+const observerOtherPage = new MutationObserver((mutations, obs) => {
+    const page1 = document.getElementsByClassName('customization-booking-area-wrapper-page1');
+
+    if (!$(page1).is(':visible')) {
+        observerThisPage.observe(document, {
+            childList: true,
+            subtree: true
+        });
+        obs.disconnect();
+        return;
+    }
+});
+observerOtherPage.observe(document, {
+    childList: true,
+    subtree: true
+});
+
+function logCat(){
+    $('.event-category').each(function() {
+        console.log('i');
+        console.log($( this ).find('.event-category__name')[0].text())
+      });
+}
