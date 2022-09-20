dcbd4f266abf6c786e8397b85ceb15de4c2e2138
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-09-20 15:10:02 +0200

    Update conditionalQuestions115152.js

--- customizations/organization/16004/conditionalQuestions115152.js
+++ customizations/organization/16004/conditionalQuestions115152.js
@@ -1,31 +1,91 @@
+function condRadioQuestion (radioSelector, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected) {
+
+    addErrorStyles();
+
+    var radioGroup = $$('.customization2_attendee_further-data_custom-question').findRadioGroup('Wie reisen Sie an?');
+
+    const questionsToShow = [];
+    for (let i = 0; i < arrayOfLabelsToShow.length; i++)
+        questionsToShow[i] = $$('.customization2_attendee_further-data_custom-question').findQuestionByLabel(arrayOfLabelsToShow[i]);
+    //console.log(questionsToShow)
+    const questionsToHide = [];
+    for (let i = 0; i < arrayOfLabelsToHide.length; i++)
+        questionsToHide[i] = $$('.customization2_attendee_further-data_custom-question').findQuestionByLabel(arrayOfLabelsToHide[i]);
+    //console.log(questionsToHide)
+
+    $(radioSelector).on("DOMSubtreeModified", ".vv-radio__input", function () {
+        if ($(radioSelector).find('input.vv-radio__input').is(':checked')) {
+
+            for (let i = 0; i < questionsToShow.length; i++) {
+                $(questionsToShow[i]).show()
+                $(questionsToShow[i]).find('vv-optional-text').css("display", "none");
+                $$('.customization2_attendee_further-data_custom-question').disableWhenEmpty(questionsToShow[i]);
+            }
+            for (let i = 0; i < questionsToHide.length; i++) {
+                $(questionsToHide[i]).hide()
+                $(questionsToHide[i]).find('.customization2_attendee_further-data_custom-question_input').val('')
+            }
+            if (disableSaveOnValueSelected)
+                $('.customization2_attendee_edit-action_save').prop("disabled", false);
+        };
+    });
+}
+/*
+Adds the error styles needed for the conditional questions
+*/
+function addErrorStyles () {
+    var styles = `
+    .error-state{
+        border-color: #ea674d!important;
+        background-color: #fff1ef!important;
+    }
+    .error-message{
+        display: block;
+        color: #ea674d;
+        font-size: 12px;
+        line-height: 1;
+        margin-bottom: 10px;
+        left: 0;
+        top: 3px;
+    }
+`
+    var styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.innerText = styles;
+
+    /*
+    styleSheet.onreadystatechange = handlerS;
+    styleSheet.onload = handlerS;
+    function handlerS() {
+      console.log('style added');
+    }
+  */
+    document.head.appendChild(styleSheet);
+}
+
 function handler(){
   
-  console.log('Start conditional questions');
-  
+  /*
+  How to use the conditional questions functions, example:
+  */
   $$('.customization2_attendee_further-data_custom-question').hideMultipleQuestionsByLabel(["Benötigen Sie einen Parkplatz?","Abreise-Bahnhof","Abflughafen"])
 
-  var dropdownLable = "Wie reisen Sie an?";
   var disableSaveOnValueSelected = true;
   
-  value = "Bitte auswählen";
-  arrayOfLabelsToShow = [];
-  arrayOfLabelsToHide = ["Benötigen Sie einen Parkplatz?","Abreise-Bahnhof","Abflughafen"];
-  $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown(dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
-  
-  var value = "Auto";
+  // Auto
   var arrayOfLabelsToShow = ["Benötigen Sie einen Parkplatz?"];
   var arrayOfLabelsToHide = ["Abreise-Bahnhof","Abflughafen"];
-  $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown(dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
+  $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown('.customization2_attendee_further-data_custom-question_radio-line:contains(Auto)', arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
   
-  value = "Bahn";
+  // Bahn
   arrayOfLabelsToShow = ["Abreise-Bahnhof"];
   arrayOfLabelsToHide = ["Benötigen Sie einen Parkplatz?","Abflughafen"];
-  $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown(dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
+  $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown('.customization2_attendee_further-data_custom-question_radio-line:contains(Bahn)', arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
 
-  value = "Flugzeug";
+  // Flugzeug
   arrayOfLabelsToShow = ["Abflughafen"];
   arrayOfLabelsToHide = ["Benötigen Sie einen Parkplatz?","Abreise-Bahnhof"];
-  $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown(dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
+  $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown('.customization2_attendee_further-data_custom-question_radio-line:contains(Flugzeug)', arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
 
   /*
     example
