55412eebe0c5df7036fc8ee025507a6460e0c022
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-09-20 10:28:20 +0200

    Update conditionalQuestions115152.js

--- customizations/organization/16004/conditionalQuestions115152.js
+++ customizations/organization/16004/conditionalQuestions115152.js
@@ -3,35 +3,35 @@ function handler(){
   /*
   How to use the conditional questions functions, example:
   */
-  $$('.customization2_attendee_further-data_custom-question').hideMultipleQuestionsByLabel(["3","6","1","2","4","5","7"])
+  $$('.customization2_attendee_further-data_custom-question').hideMultipleQuestionsByLabel(["Benötigen Sie einen Parkplatz?","Abreise-Bahnhof","Abflughafen"])
 
-  var dropdownLable = "Dropdown1";
+  var dropdownLable = "Wie reisen Sie an?";
+  var disableSaveOnValueSelected = true;
   
   value = "Bitte auswählen";
   arrayOfLabelsToShow = [];
-  arrayOfLabelsToHide = ["3","6","1","2","4","5","7"];
+  arrayOfLabelsToHide = ["Benötigen Sie einen Parkplatz?","Abreise-Bahnhof","Abflughafen"];
   $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown(dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
   
-  var value = "A";
-  var arrayOfLabelsToShow = ["1","3","5","7"];
-  var arrayOfLabelsToHide = ["2","4","6"];
-  var disableSaveOnValueSelected = true;
+  var value = "Auto";
+  var arrayOfLabelsToShow = ["Benötigen Sie einen Parkplatz?"];
+  var arrayOfLabelsToHide = ["Abreise-Bahnhof","Abflughafen"];
   $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown(dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
   
-  value = "B";
-  arrayOfLabelsToShow = ["2","4","6"];
-  arrayOfLabelsToHide = ["1","3","5","7"];
+  value = "Bahn";
+  arrayOfLabelsToShow = ["Abreise-Bahnhof"];
+  arrayOfLabelsToHide = ["Benötigen Sie einen Parkplatz?","Abflughafen"];
   $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown(dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
 
-  value = "C";
-  arrayOfLabelsToShow = ["3","6"];
-  arrayOfLabelsToHide = ["1","2","4","5","7"];
+  value = "Flugzeug";
+  arrayOfLabelsToShow = ["Abflughafen"];
+  arrayOfLabelsToHide = ["Benötigen Sie einen Parkplatz?","Abreise-Bahnhof"];
   $$('.customization2_attendee_further-data_custom-question').condQuestionDropdown(dropdownLable, value, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
 
   /*
     example
   */
-  $$('.customization2_attendee_further-data_custom-question').setValueToDropdownArray(["Dropdown2","Dropdown3","Dropdown4"],["selected","selected","selected"])
+  //$$('.customization2_attendee_further-data_custom-question').setValueToDropdownArray(["Dropdown2","Dropdown3","Dropdown4"],["selected","selected","selected"])
 
 }
 
