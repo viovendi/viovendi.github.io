40d6ff2bae069938e75d100b13a8a66566374bf0
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-09-20 23:56:11 +0200

    Update conditionalQuestions115152.js

--- customizations/organization/16004/conditionalQuestions115152.js
+++ customizations/organization/16004/conditionalQuestions115152.js
@@ -1,146 +1,92 @@
-function condQuestion(selector, arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected) {
-    
+function condQuestion(selector, arrayOfLabelsToShow, arrayOfLabelsToHide) {
+
     console.log("Start condQuestions for: " + selector);
 
-    const questionsToShow = [];
-    for (let i = 0; i < arrayOfLabelsToShow.length; i++)
-        questionsToShow[i] = $$('.customization2_attendee_further-data_custom-question').findQuestionByLabel(arrayOfLabelsToShow[i]);
+    $(selector).on("change", function () {
 
-    const questionsToHide = [];
-    for (let i = 0; i < arrayOfLabelsToHide.length; i++)
-        questionsToHide[i] = $$('.customization2_attendee_further-data_custom-question').findQuestionByLabel(arrayOfLabelsToHide[i]);
-
-    $(selector).on('change', function() {
         console.log(selector + " clicked");
-        var input = $(selector).find('input')
-        if (input.is(':checked')) {
+
+        if ($(selector).find("input").is(":checked")) {
             
             console.log("is checked");
 
-            for (let i = 0; i < questionsToShow.length; i++) {
-                console.log('Show ' + questionsToShow[i])
-                $(questionsToShow[i]).show()
-                $(questionsToShow[i]).find('vv-optional-text').css("display", "none");
-                $$('.customization2_attendee_further-data_custom-question').disableWhenEmpty(questionsToShow[i]);
-            }
-            for (let i = 0; i < questionsToHide.length; i++) {
-                console.log('Hide ' + questionsToHide[i])
-                $(questionsToHide[i]).hide()
-                $(questionsToHide[i]).find('.customization2_attendee_further-data_custom-question_input').val('')
-            }
-            if (disableSaveOnValueSelected)
-                $('.customization2_attendee_edit-action_save').prop("disabled", false);
-        };
-        
-        var productSelected = false;
-        $('.customization2_attendee_further-data_product').each(function (i, element) {
-        var checkbox = $(this).find('.customization2_attendee_further-data_product_checkbox')
+            $.each(arrayOfLabelsToShow, function(key, value) {
+                var question = $$(".customization2_attendee_further-data_custom-question").findQuestionByLabel(value);
+                console.log("Show " + question);
+                $(question).show();
+                $(question).find("vv-optional-text").css("display", "none");
+                $$(".customization2_attendee_further-data_custom-question").disableWhenEmpty(question);
+            });
 
-        if (checkbox.is(':checked')) {
-            productSelected = true;
+            $.each(arrayOfLabelsToHide, function(key, value) {
+                var question = $$(".customization2_attendee_further-data_custom-question").findQuestionByLabel(value);
+                console.log("Hide " + question);
+                $(question).hide();
+                $(question).find(".customization2_attendee_further-data_custom-question_input").val("");
+            });
         }
-        })
-        
-        if(!productSelected) {
-            $('.customization2_attendee_further-data_custom-question:contains(Ihr Handicap)').hide();
-            $('.customization2_attendee_further-data_custom-question:contains(Ihre Schuhgröße)').hide();
+
+        if(selector.includes('customization2_attendee_further-data_product')){
+            hideQuestionsIfNoProductSelected();
         }
         
     });
 }
 
+function hideQuestionsIfNoProductSelected(){
+    var productSelected = false;
+    $(".customization2_attendee_further-data_product").each(function (i, element) {
+        var checkbox = $(this).find(".customization2_attendee_further-data_product_checkbox");
+        if (checkbox.is(":checked")) {
+            productSelected = true;
+        }
+    });
 
-/*
-Adds the error styles needed for the conditional questions
-*/
-function addErrorStyles () {
-    var styles = `
-    .error-state{
-        border-color: #ea674d!important;
-        background-color: #fff1ef!important;
+    if (!productSelected) {
+        $(".customization2_attendee_further-data_custom-question:contains(Ihr Handicap)").hide();
+        $(".customization2_attendee_further-data_custom-question:contains(Ihre Schuhgröße)").hide();
     }
-    .error-message{
-        display: block;
-        color: #ea674d;
-        font-size: 12px;
-        line-height: 1;
-        margin-bottom: 10px;
-        left: 0;
-        top: 3px;
-    }
-`
-    var styleSheet = document.createElement("style");
-    styleSheet.type = "text/css";
-    styleSheet.innerText = styles;
-
-    /*
-    styleSheet.onreadystatechange = handlerS;
-    styleSheet.onload = handlerS;
-    function handlerS() {
-      console.log('style added');
-    }
-  */
-    document.head.appendChild(styleSheet);
 }
 
-function handler(){
-    
-  addErrorStyles();
-  
-  /*
-  How to use the conditional questions functions, example:
-  */
-  $$('.customization2_attendee_further-data_custom-question').hideMultipleQuestionsByLabel(["Benötigen Sie einen Parkplatz?","Abreise-Bahnhof","Abflughafen","Ihr Handicap", "Ihre Schuhgröße"])
+function handler() {
 
-  var disableSaveOnValueSelected = true;
-  
-  // Auto
-  var arrayOfLabelsToShow = ["Benötigen Sie einen Parkplatz?"];
-  var arrayOfLabelsToHide = ["Abreise-Bahnhof","Abflughafen"];
-  condQuestion('.customization2_attendee_further-data_custom-question_radio-line:contains(Auto)', arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
-  
-  // Bahn
-  arrayOfLabelsToShow = ["Abreise-Bahnhof"];
-  arrayOfLabelsToHide = ["Benötigen Sie einen Parkplatz?","Abflughafen"];
-  condQuestion('.customization2_attendee_further-data_custom-question_radio-line:contains(Bahn)', arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
+    $$(".customization2_attendee_further-data_custom-question").hideMultipleQuestionsByLabel(["Benötigen Sie einen Parkplatz?", "Abreise-Bahnhof", "Abflughafen", "Ihr Handicap", "Ihre Schuhgröße"]);
 
-  // Flugzeug
-  arrayOfLabelsToShow = ["Abflughafen"];
-  arrayOfLabelsToHide = ["Benötigen Sie einen Parkplatz?","Abreise-Bahnhof"];
-  condQuestion('.customization2_attendee_further-data_custom-question_radio-line:contains(Flugzeug)', arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
+    // Auto
+    var arrayOfLabelsToShow = ["Benötigen Sie einen Parkplatz?"];
+    var arrayOfLabelsToHide = ["Abreise-Bahnhof", "Abflughafen"];
+    condQuestion(".customization2_attendee_further-data_custom-question_radio-line:contains(Auto)", arrayOfLabelsToShow, arrayOfLabelsToHide);
 
-  // Golf
-  arrayOfLabelsToShow = ["Ihr Handicap"];
-  arrayOfLabelsToHide = ["Ihre Schuhgröße"];
-  condQuestion('.customization2_attendee_further-data_product:contains(Golf)', arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
+    // Bahn
+    arrayOfLabelsToShow = ["Abreise-Bahnhof"];
+    arrayOfLabelsToHide = ["Benötigen Sie einen Parkplatz?", "Abflughafen"];
+    condQuestion(".customization2_attendee_further-data_custom-question_radio-line:contains(Bahn)", arrayOfLabelsToShow, arrayOfLabelsToHide);
 
-  // Laufen
-  arrayOfLabelsToShow = ["Ihre Schuhgröße"];
-  arrayOfLabelsToHide = ["Ihr Handicap"];
-  condQuestion('.customization2_attendee_further-data_product:contains(Laufen)', arrayOfLabelsToShow, arrayOfLabelsToHide, disableSaveOnValueSelected);
+    // Flugzeug
+    arrayOfLabelsToShow = ["Abflughafen"];
+    arrayOfLabelsToHide = ["Benötigen Sie einen Parkplatz?", "Abreise-Bahnhof"];
+    condQuestion(".customization2_attendee_further-data_custom-question_radio-line:contains(Flugzeug)", arrayOfLabelsToShow, arrayOfLabelsToHide);
 
-  /*
-    example
-  */
-  //$$('.customization2_attendee_further-data_custom-question').setValueToDropdownArray(["Dropdown2","Dropdown3","Dropdown4"],["selected","selected","selected"])
+    // Golf
+    arrayOfLabelsToShow = ["Ihr Handicap"];
+    arrayOfLabelsToHide = ["Ihre Schuhgröße"];
+    condQuestion(".customization2_attendee_further-data_product:contains(Golf)", arrayOfLabelsToShow, arrayOfLabelsToHide);
 
+    // Laufen
+    arrayOfLabelsToShow = ["Ihre Schuhgröße"];
+    arrayOfLabelsToHide = ["Ihr Handicap"];
+    condQuestion(".customization2_attendee_further-data_product:contains(Laufen)", arrayOfLabelsToShow, arrayOfLabelsToHide);
 }
 
 handler();
 
-var insertionListener = function(event) {
-  if (event.animationName === "nodeInserted") {
-    console.log("Node has been inserted: ", event.target);
-    //Inser your code here.
-    handler();
+var insertionListener = function (event) {
+    if (event.animationName === "nodeInserted") {
+        console.log("Node has been inserted: ", event.target);
+        handler();
+    }
+};
 
-    $('customization2_attendee_view-action_edit').on( "click", function() {
-        console.log( "clicked" );
-    });
-  }
-}
-
-  
-  document.addEventListener("animationstart", insertionListener, false); // standard + firefox
-  document.addEventListener("MSAnimationStart", insertionListener, false); // IE
-  document.addEventListener("webkitAnimationStart", insertionListener, false); //
+document.addEventListener("animationstart", insertionListener, false); // standard + firefox
+document.addEventListener("MSAnimationStart", insertionListener, false); // IE
+document.addEventListener("webkitAnimationStart", insertionListener, false); //
