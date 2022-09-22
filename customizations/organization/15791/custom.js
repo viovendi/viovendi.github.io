7cd84be66f0da7f7eac8e25f9960de60ad13a9dd
Alex <olexiy.movchan@gmail.com>
2022-09-22 06:59:52 -0700

    test get params

--- customizations/organization/15791/custom.js
+++ customizations/organization/15791/custom.js
@@ -1,1 +1,8 @@
 console.log('git-code');
+
+function getFrameParams(){
+  console.log('getFrameParams');
+  var frameUrl = $('.viovendi_iframe').attr('src');
+  console.log(frameUrl);
+}
+getFrameParams();
