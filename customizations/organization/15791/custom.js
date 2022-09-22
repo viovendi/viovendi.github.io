bd22210220e7112456b2d5be66db6b93e413d5eb
Alex <olexiy.movchan@gmail.com>
2022-09-22 07:08:30 -0700

    get redirect url

--- customizations/organization/15791/custom.js
+++ customizations/organization/15791/custom.js
@@ -1,10 +1,4 @@
 console.log('git-code');
 
-function getFrameParams(){
-  console.log('getFrameParams');
-  var frameUrl = $('.viovendi_iframe').attr('src');
-  console.log(frameUrl);
-}
-getFrameParams();
 
-console.log(is_prod);
+console.log(redirectUrl);
