ce881ec4b1252fb4230d2a1c3c26c4c1835c20b5
vbodend <69503064+vbodend@users.noreply.github.com>
2022-09-23 16:41:41 +0200

    Update redirect.js

--- customizations/organization/15118/redirect.js
+++ customizations/organization/15118/redirect.js
@@ -1,6 +1,7 @@
-console.log("page3");
-$('.customization-button-next').on('click', function(){
+console.log("git works");
+
+//TODO redirect to https://neuheiten.messe-dortmund.com/Danke if booking is made so the checkout page is not shown
+/*$('.customization-button-next').on('click', function(){
 console.log("clicked");
 location.href = "https://neuheiten.messe-dortmund.com/Danke";
-
-})
+})*/
