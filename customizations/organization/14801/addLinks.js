f99640e16a7ac686fad35ca91734524b4f805d82
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-09-29 10:35:44 +0200

    Update addLinks.js

--- customizations/organization/14801/addLinks.js
+++ customizations/organization/14801/addLinks.js
@@ -4,6 +4,10 @@ function handler(){
     $( ".customization2_attendee_further-data_custom-question_label:contains('Datenschutz-, bzw. Einwilligungserklärung *für Speaker* (Moderatoren, Vortragende und Teilnehmende der Podiumsdiskussion) im Hinblick auf das Vortraghalten in Präsenz, bzw. auf die zusätzliche Online-Teilnahme, z.B. Diskussions-Chat (Teil 1)')" ).append( '<a href="http://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2022/einverstaendnis-ds-speaker-vortrag.html" target="_blank"> - Öffnen</a>');
 
     $( ".customization2_attendee_further-data_custom-question_label:contains('Datenschutz-, bzw. Einwilligungserklärung *für Speaker* im Hinblick auf Anfertigung, Speicherung und Veröffentlichung von Videoaufnahmen (Teil 2)')" ).append( '<a href="http://www.destatis.de/DE/Ueber-uns/Kolloquien-Tagungen/Kolloquien/2022/einverstaendnis-ds-speaker-video.html" target="_blank"> - Öffnen</a>');
+    
+    if ($(".customization2_attendee_title").text().trim() == 'Präsenz Teilnahme (Speaker)') {
+        $("vv-booking-terms").hide();
+    }
   }
 
 handler();
