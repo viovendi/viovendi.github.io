fe8bbfe477551a3ff5637e3487312206eb50e99e
Alex <olexiy.movchan@gmail.com>
2022-09-30 07:06:01 -0700

    clean up + ready for prod

--- customizations/organization/15791/custom.js
+++ customizations/organization/15791/custom.js
@@ -6,7 +6,6 @@ console.log(redirectUrl);
 var isFuncUsed = false;
 
 function getXMLHttpRequest (open) {
-  console.log('getXMLHttpRequest');
     XMLHttpRequest.prototype.open = function() {
       this.addEventListener("readystatechange", function() {
         if(this.__zone_symbol__xhrURL == "https://api.doo.net/v1/orders" ){
@@ -39,69 +38,6 @@ function getXMLHttpRequest (open) {
 };
 
 function sendRedirectRequest(bodyData){
-  
-  var windowRedirectUrl = redirectUrl + bodyData;
-  
-  console.log(windowRedirectUrl);
-  
+  var windowRedirectUrl = redirectUrl + bodyData;  
   window.top.location.href=windowRedirectUrl;
 }
-
-
-/********************/
-function testRedirect(data){
-  
-  //var reqUrl = 'https://doo.ovas2-branch.w14.westermann.de/doo/anmeldung/return?id=8edadc8f-f8d6-447f-ae60-388ef22c5838';
-  
-  /*
-  var order_id = 123456;
-  var ticket_category_id = 456789;
-  var price = 6655;
-  var artikelnummer = 91117021;
-  */
-  
-  console.log(data);
-  console.log(data.price);
-  console.log('before form submit');
-  
-var form = $('<form action="' + redirectUrl + '" target="_top" method="post">' +
-  '<input type="text" name="order_id" value="' + data.order_id + '" />' +
-  '<input type="text" name="ticket_category_id" value="' + data.ticket_category_id + '" />' +
-  '<input type="text" name="price" value="' + data.price + '" />' +
-  '<input type="text" name="artikelnummer" value="' + data.artikelnummer + '" />' +
-  '</form>');
-$('body').append(form);
-form.submit();
-  
-}
-/********************/
-
-
-// add listener to the post request on 2nd page
-getXMLHttpRequest(XMLHttpRequest.prototype.open);
-
-const observer = new MutationObserver((mutations, obs) => {
-    const page = document.getElementsByClassName(
-    'customization-booking-area-wrapper-page3'
-    );
-
-    if ($(page).is(':visible')) {
-        console.log('page 3 visible');
-        // run function of the page3  
-        console.log('run page 3 func!');
-        // add listener to the post request
-      
-        //testRedirect();
-      
-        // send redirect when request received 
-      
-        obs.disconnect();
-        return;
-    }
-});
-
-observer.observe(document, {
-    childList: true,
-    subtree: true
-});
-
