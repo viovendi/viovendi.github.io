ce6852281f43030b8be21550b50c7dd15c5b76f6
vbodend <69503064+vbodend@users.noreply.github.com>
2022-10-24 11:07:01 +0200

    Update bannergeneratorDE.js

--- customizations/organization/15064/bannergenerator/bannergeneratorDE.js
+++ customizations/organization/15064/bannergenerator/bannergeneratorDE.js
@@ -30,7 +30,7 @@ function setTexts() {
         ctx.fillText($('#halle_inp').val(), 397 - $('#halle_inp').val().length * 4, 35);
         ctx.fillText($('#stand_inp').val(), 435 - $('#stand_inp').val().length * 4, 45);
     } else if (value == 'vertikal') {
-        ctx.fillText($('#halle_inp').val(), 78 - $('#halle_inp').val().length * 6, 507);
+        ctx.fillText($('#halle_inp').val(), 78 - $('#halle_inp').val().length * 6, 514);
         ctx.fillText($('#stand_inp').val(), 78 - $('#stand_inp').val().length * 5, 575);
     } else if (value == 'email') {
         ctx.fillText($('#halle_inp').val(), 24 - $('#halle_inp').val().length * 6, 44);
