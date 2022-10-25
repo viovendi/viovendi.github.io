2c3035d866591bb46f543569e64b3cce639648ea
vbodend <69503064+vbodend@users.noreply.github.com>
2022-10-25 13:27:16 +0200

    Update bannergeneratorEN.js

--- customizations/organization/14334/bannergenerator/bannergeneratorEN.js
+++ customizations/organization/14334/bannergenerator/bannergeneratorEN.js
@@ -33,8 +33,8 @@ function setTexts() {
         ctx.fillText($('#halle_inp').val(), 110 - $('#halle_inp').val().length * 6, 515);
         ctx.fillText($('#stand_inp').val(), 110 - $('#stand_inp').val().length * 5, 555);
     } else if (value == 'email') {
-        ctx.fillText($('#halle_inp').val(), 24 - $('#halle_inp').val().length * 6, 46);
-        ctx.fillText($('#stand_inp').val(), 43, 46);
+        ctx.fillText($('#halle_inp').val(), 36, 29);
+        ctx.fillText($('#stand_inp').val(), 36, 45);
     } else if (value == 'instagrampost') {
         ctx.fillText($('#halle_inp').val(), 330 - $('#halle_inp').val().length * 6, 993);
         ctx.fillText($('#stand_inp').val(), 745 - $('#stand_inp').val().length * 9, 993);
@@ -74,7 +74,7 @@ function setupBannerImage() {
     } else if (value == 'vertikal') {
         ctx.font = "13pt Verdana";
     } else if (value == 'email') {
-        ctx.font = "13pt Verdana";
+        ctx.font = "7pt Verdana";
     } else if (value == 'instagrampost') {
         ctx.font = "26pt Verdana";
     } else if (value == 'instagramstory') {
