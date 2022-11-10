9cf9b5fd33f1b094278db9b0e8a938534feeec9b
jonas-hitter-doo <jonas.hitter@doo.net>
2022-11-10 17:07:19 +0100

    Update linkedin.js

--- customizations/organization/15314/linkedin.js
+++ customizations/organization/15314/linkedin.js
@@ -1,4 +1,4 @@
-
+console.log("running linkedIn customization");
 $( '<div style="margin-bottom:50px"id="linkedInConsent"><div>I consent to the........Lorem ipsum dolor sit amet, consectetur adipiscing elit. In magna felis, maximus ut sagittis vitae, pellentesque non tellus. Morbi lobortis felis sed volutpat vehicula. Integer nec metus sed mi sollicitudin viverra eu vel felis. Duis fringilla augue in diam bibendum, sed accumsan massa dapibus. Fusce eget iaculis nunc, quis interdum arcu </div><div><button style="width:150px"class="button pull-right float-right col-xs-12 col-12"onclick="insertLinkedIn()">consent</button></div></div>').insertBefore(".container")
 function insertLinkedIn(){
     $('#linkedInConsent').hide();
