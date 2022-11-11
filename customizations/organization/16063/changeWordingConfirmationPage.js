3d21bc3fa156656d926bbbff9cb0318421177cfd
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-11-11 14:53:53 +0100

    Update changeWordingConfirmationPage.js

--- customizations/organization/16063/changeWordingConfirmationPage.js
+++ customizations/organization/16063/changeWordingConfirmationPage.js
@@ -1,2 +1,3 @@
+console.log("Change wording confirmation page");
 $(".ew-confirmation__summary p:contains(Freigabe)").text("Vielen Dank für Ihre Buchung. Diese wird nun auf Freigabe geprüft.");
 $(".ew-confirmation__summary p:contains(Eine Bestätigung über den Eingang der Buchung sowie weitere Informationen erhalten Sie in Kürze per E-Mail. Sobald Ihre Buchung freigegeben wird, werden Sie und alle Teilnehmer ebenfalls per E-Mail informiert.)").text("Eine Bestätigung über den Eingang der Buchung sowie weitere Informationen erhälst du in Kürze per E-Mail. Sobald deine Buchung freigegeben wird, wirst du und alle Teilnehmer ebenfalls per E-Mail informiert.");
