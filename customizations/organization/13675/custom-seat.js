fe482a3cd50c06b609f0c26ad8d703f033f99713
Alex <olexiy.movchan@gmail.com>
2022-11-15 19:05:13 +0200

    custom-seat test code

---
+++ customizations/organization/13675/custom-seat.js
@@ -0,0 +1,8 @@
+
+console.log('github code');
+
+var head = document.getElementsByTagName('head')[0];
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
