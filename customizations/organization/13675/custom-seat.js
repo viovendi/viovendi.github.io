cd7557bd3e7211de5e4c85ea870c2adb01103c6d
Alex <olexiy.movchan@gmail.com>
2022-11-15 19:20:43 +0200

    added seat code

--- customizations/organization/13675/custom-seat.js
+++ customizations/organization/13675/custom-seat.js
@@ -5,4 +5,26 @@ var head = document.getElementsByTagName('head')[0];
 
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+script.src = "https://cdn-eu.seatsio.net/chart.js";
+
+var div = document.createElement('div');
+div.id = 'chart';
+
+div.onload = createSeats;
+
+fubction createSeats(){
+    new seatsio.SeatingChart({
+        divId: 'chart',
+        workspaceKey: 'ef77668e-17cb-4bc3-b018-710cbb7d7469',
+        event: 'e1477cf4-5bf5-c684-8f98-daa43c878877',
+        session: 'continue',
+        pricing: [
+           {'category': 1, 'price': 30}, 
+           {'category': 2, 'price': 40}, 
+           {'category': 3, 'price': 50}
+        ],
+        priceFormatter: function(price) {
+            return '$' + price;
+        }
+    }).render();
+}
