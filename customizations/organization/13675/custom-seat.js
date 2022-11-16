1a7ed46d70d2194f3731c79d8fd97d6963b4cbb4
Alex <olexiy.movchan@gmail.com>
2022-11-16 14:07:39 +0200

    test

--- customizations/organization/13675/custom-seat.js
+++ customizations/organization/13675/custom-seat.js
@@ -1,13 +1,19 @@
 
 console.log('github code');
 
+document.load(function(){
+var wrapper = document.getElementsByClassName('customization-booking-area-wrapper')[0];
+console.log(wrapper);
+
+});
+/*
 var wrapper = document.getElementsByClassName('customization-booking-area-wrapper');
 console.log(wrapper);
 console.log(typeof wrapper);
 
 console.log(wrapper.item(0));
-
-
+*/
+function main(){
 var div = document.createElement('div');
 div.id = 'chart';
 wrapper.appendChild(div);
@@ -20,19 +26,21 @@ wrapper.appendChild(script);
 script.onload = createSeats;
 //script.onreadystatechange = createSeats;
 
-function createSeats(){
-    new seatsio.SeatingChart({
-        divId: 'chart',
-        workspaceKey: 'ef77668e-17cb-4bc3-b018-710cbb7d7469',
-        event: 'e1477cf4-5bf5-c684-8f98-daa43c878877',
-        session: 'continue',
-        pricing: [
-           {'category': 1, 'price': 30}, 
-           {'category': 2, 'price': 40}, 
-           {'category': 3, 'price': 50}
-        ],
-        priceFormatter: function(price) {
-            return '$' + price;
-        }
-    }).render();
+    function createSeats(){
+        new seatsio.SeatingChart({
+            divId: 'chart',
+            workspaceKey: 'ef77668e-17cb-4bc3-b018-710cbb7d7469',
+            event: 'e1477cf4-5bf5-c684-8f98-daa43c878877',
+            session: 'continue',
+            pricing: [
+               {'category': 1, 'price': 30}, 
+               {'category': 2, 'price': 40}, 
+               {'category': 3, 'price': 50}
+            ],
+            priceFormatter: function(price) {
+                return '$' + price;
+            }
+        }).render();
+    }
+ 
 }
