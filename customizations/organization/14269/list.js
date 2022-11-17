ef302c710e4fb40c3937a0617ad707d6407df085
jonas-hitter-doo <jonas.hitter@doo.net>
2022-11-17 15:42:12 +0100

    Update list.js

--- customizations/organization/14269/list.js
+++ customizations/organization/14269/list.js
@@ -2,7 +2,7 @@ console.log("list.js")
 
 function run(){
     $("li.ng-star-inserted").each(element => {
-        console.log($(element).find(".event-article__event-info__title"))
+        console.log($(element).find("h1").text())
     var title = $($(element).find(".event-article__event-info__title")).text()
     console.log(title)
     if(title.includes("Profi")){
