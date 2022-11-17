a22e439dd158039dc26a5693d07086cca92b30a9
jonas-hitter-doo <jonas.hitter@doo.net>
2022-11-17 13:06:59 +0100

    Update list.js

--- customizations/organization/14269/list.js
+++ customizations/organization/14269/list.js
@@ -1,1 +1,12 @@
 console.log("list.js")
+
+$("li .ng-star-inserted").forEach(element => {
+    var title = $(element).find(".event-article__event-info__title").text()
+    if(title.includes("Profi")){
+        console.log("Found Profi event")
+        console.log(element)
+        var but = $(element).find(".button")
+        console.log("Found the button with href: "+$(but).attr("href"))
+        console.log(but)
+    }
+});
