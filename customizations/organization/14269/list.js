e229400305ff50f55bf1a619edad065e0d629ed0
jonas-hitter-doo <jonas.hitter@doo.net>
2022-11-17 14:38:26 +0100

    Update list.js

--- customizations/organization/14269/list.js
+++ customizations/organization/14269/list.js
@@ -1,6 +1,7 @@
 console.log("list.js")
 
-$("li.ng-star-inserted").each(element => {
+function run(){
+    $("li.ng-star-inserted").each(element => {
     var title = $(element).find(".event-article__event-info__title").text()
     console.log(title)
     if(title.includes("Profi")){
@@ -11,3 +12,15 @@ $("li.ng-star-inserted").each(element => {
         console.log(but)
     }
 });
+}
+
+var insertionListener = function(event) {  
+  if (event.animationName === "nodeInserted") {    
+    console.log("Node has been inserted: ", event.target);    
+    run(); 
+  }
+}
+document.addEventListener("animationstart", insertionListener, false); 
+// standard + firefox  
+document.addEventListener("MSAnimationStart", insertionListener, false); // IE  
+document.addEventListener("webkitAnimationStart", insertionListener, false); // 
