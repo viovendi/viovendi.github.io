5831a953d8fd514b98f53af15e2166f397d4a1d2
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-11-18 14:01:29 +0100

    Update list.js

--- customizations/organization/14269/list.js
+++ customizations/organization/14269/list.js
@@ -4,7 +4,7 @@ function run(){
     $("li.ng-star-inserted").each(function() {
     var title = $($(this)).find(".event-article__event-info__title").text()
     if(title.includes("Schüler@HORNBACH")){
-        if($('#464413').length){
+        if($('#464413').length == 0){
         var but = $($(this)).find(".button:not(#464413)");
         $(but).after('<a class="button" id="464413" target="_blank" href="https://prjkt.ly/Schueler-at-HB-Markt">Mehr Infos</a>');
         $(but).hide();
