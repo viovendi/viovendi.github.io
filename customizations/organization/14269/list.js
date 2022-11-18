2815fdc27d5d30da2c2fa00692ad1d187a215d27
Yannik <92022876+yannikkunz@users.noreply.github.com>
2022-11-18 13:56:31 +0100

    Update list.js

--- customizations/organization/14269/list.js
+++ customizations/organization/14269/list.js
@@ -4,9 +4,11 @@ function run(){
     $("li.ng-star-inserted").each(function() {
     var title = $($(this)).find(".event-article__event-info__title").text()
     if(title.includes("Schüler@HORNBACH")){
+        if($('#464413').length()){
         var but = $($(this)).find(".button:not(#464413)");
         $(but).after('<a class="button" id="464413" target="_blank" href="https://prjkt.ly/Schueler-at-HB-Markt">Mehr Infos</a>');
         $(but).hide();
+        }
     }
 });
 }
