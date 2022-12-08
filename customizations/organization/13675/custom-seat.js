25081fafc0e1bcf1eef448efc62ed788b1738229
Alex <olexiy.movchan@gmail.com>
2022-12-08 11:56:04 +0200

    click

--- customizations/organization/13675/custom-seat.js
+++ customizations/organization/13675/custom-seat.js
@@ -169,7 +169,7 @@ function selectOptions(options, selectedInput) {
         
       tergetElement.closest('vv-single-select-option').click();
         
-      //tergetElement.parent().click();
+      tergetElement.closest('event-category').find('.event-category__info').click();
     }
   }
 }
