6240493ef7af193ac488fcb0517e24c6c3f84c8b
Alex <olexiy.movchan@gmail.com>
2022-12-09 15:35:31 +0200

    fix

--- customizations/organization/13675/custom-seat.js
+++ customizations/organization/13675/custom-seat.js
@@ -168,7 +168,7 @@ var mapObject = {
 
 
 function setTicketsFromPreviousChose(arr){
-    for(let i = 0; i < arr.length; i+=;){
+    for(let i = 0; i < arr.length; i++){
         console.log(arr[i]);
     }
 }
