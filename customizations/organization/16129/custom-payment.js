5b25a641669aa2cb9a8b2ebf447643199da894c7
Alex <olexiy.movchan@gmail.com>
2022-12-21 17:13:17 +0200

    event intercepter

--- customizations/organization/16129/custom-payment.js
+++ customizations/organization/16129/custom-payment.js
@@ -1,3 +1,87 @@
 console.log('github-js');
 
-alert(112233);
+async function handler() {
+  if (document.readyState !== 'loading') {
+
+    await getPage('page1');
+    console.log("page1!");
+
+    await getPage('page2');
+    console.log("page2!");
+    getXMLHttpRequest(XMLHttpRequest.prototype.open);
+    
+    await getPage('page3');
+    console.log("page3!");
+
+    await getPage('page4');
+    console.log("page4!");
+    
+  }
+}
+handler();
+
+
+
+function getXMLHttpRequest (open) {
+    XMLHttpRequest.prototype.open = function() {
+      this.addEventListener("readystatechange", function() {
+        if(this.__zone_symbol__xhrURL == "https://api.doo.net/v1/orders" ){
+          try {
+            var res = typeof JSON.parse(this.responseText) != "undefined" ? JSON.parse(this.responseText): undefined;
+          } catch (err) {}
+          if(res != undefined && res._embedded){
+
+           // send the request to Make (to confirm the seats booking)
+             const orders = res._embedded.orders;
+             const order_id = orders[0].id;
+             const organizer_id = orders[0].event.organizer_id;
+            
+            // snd the request to get the payment page URL
+          }
+        }
+      }, false);
+      open.apply(this, arguments);
+    };
+};
+  
+  
+async function getPage(page) {
+  var pages = {
+    page1: "customization-booking-area-wrapper-page1",
+    page2: "customization-booking-area-wrapper-page2",
+    page3: "customization-booking-area-wrapper-page3",
+    page4: "customization-booking-area-wrapper-page4",
+  };
+  return new Promise(function (resolve, reject) {
+    try {
+      const element = document.querySelector("body");
+
+      var observer = new MutationObserver(pageLoaded);
+
+      function pageLoaded(mutations) {
+        mutations.forEach((mutation) => {
+          var classList = mutation.target.classList
+            ? [...mutation.target.classList]
+            : [];
+          if (
+            mutation.type === "childList" &&
+            classList.indexOf(pages[page]) != -1
+          ) {
+            resolve({
+              selector: document.querySelector("." + pages[page]),
+              dataLayer: dataLayer,
+            });
+          }
+        });
+      }
+
+      observer.observe(element, {
+        characterData: true,
+        subtree: true,
+        childList: true,
+      });
+    } catch (error) {
+      reject(new Error(error));
+    }
+  });
+}
