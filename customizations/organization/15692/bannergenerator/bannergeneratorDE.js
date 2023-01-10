af0c4ee6c631177462c630c42176a817f3ec459d
christiankrausse7 <86656535+christiankrausse7@users.noreply.github.com>
2023-01-10 13:34:59 +0100

    Update bannergeneratorDE.js

--- customizations/organization/15692/bannergenerator/bannergeneratorDE.js
+++ customizations/organization/15692/bannergenerator/bannergeneratorDE.js
@@ -10,12 +10,12 @@ script.onload = handlerQ;
 head.appendChild(script);
 
 const imageNameAndURL = [
-['vertikal', 'https://viovendi.github.io/customizations/organization/14969/bannergenerator/2023_IL_Banner_Aussteller_160x600_D.png'],
-['horizontal', 'https://viovendi.github.io/customizations/organization/14969/bannergenerator/2023_IL_Banner_Aussteller_468x60_D.png'],
-['email', 'https://viovendi.github.io/customizations/organization/14969/bannergenerator/2023_IL_Banner_Aussteller_180x60_D_RZ.png'],
-['instagrampost', 'https://viovendi.github.io/customizations/organization/14969/bannergenerator/2023_IL_Banner_Aussteller_1080x1080_D_RZ.png'],
-['instagramstory', 'https://viovendi.github.io/customizations/organization/14969/bannergenerator/2023_IL_Banner_Aussteller_1080x1350_D_RZ.png'],
-['facebook', 'https://viovendi.github.io/customizations/organization/14969/bannergenerator/2023_IL_Banner_Aussteller_1080x1350_D_RZ.png']
+['vertikal', 'https://viovendi.github.io/customizations/organization/15692/bannergenerator/EVONXT_Ausstellerbanner_160x600_D.png'],
+['horizontal', 'https://viovendi.github.io/customizations/organization/15692/bannergenerator/EVONXT_Ausstellerbanner_468x60_D.png'],
+['email', 'https://viovendi.github.io/customizations/organization/15692/bannergenerator/EVONXT_Ausstellerbanner_180x60_D.png'],
+['instagrampost', 'https://viovendi.github.io/customizations/organization/15692/bannergenerator/EVONXT_Ausstellerbanner_1080x1080_D.png'],
+['instagramstory', 'https://viovendi.github.io/customizations/organization/15692/bannergenerator/EVONXT_Ausstellerbanner_1080x1350_D.png'],
+['facebook', 'https://viovendi.github.io/customizations/organization/15692/bannergenerator/EVONXT_Ausstellerbanner_1080x1350_D.png']
 ];
 
 function setTexts() {
