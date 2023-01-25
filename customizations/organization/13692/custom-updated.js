8c82e6f7d703959de2b66bd2aa09db1e620c5503
Alex <olexiy.movchan@gmail.com>
2023-01-25 15:31:03 +0200

    fix+debug

--- customizations/organization/13692/custom-updated.js
+++ customizations/organization/13692/custom-updated.js
@@ -221,7 +221,8 @@ function checking_page4() {
 
     stop_checking_page3();
 
-    var payment_method = localStorage.getItem("payment_method_upd");
+    //var payment_method = localStorage.getItem("payment_method_upd");
+    var payment_method = localStorage.getItem("payment_method");
 
     var free_order = localStorage.getItem("free_order");
 
@@ -241,13 +242,12 @@ function checking_page4() {
         console.log(localStorage.getItem("payment_method_upd").indexOf("Sofortbezahlung"));
       
       if (invoice_payment_method_arr_options.indexOf(payment_method) != -1) {
-              console.log("checking_page4 invoice");
         rebuildPageInvoiceConfirm();
       } else if (
         (!isFuncUsed && hub_payment_method_arr_options.indexOf(payment_method) != -1) ||
         (!isFuncUsed && localStorage.getItem("payment_method_upd").indexOf("Sofortbezahlung") != -1)
       ) {
-        console.log("checking_page4 payment hub");
+        console.log("checking_page4 payment hub!");
         loaderOn("on");
         sendRequest(obj, organizer_id, free_order);
       }
