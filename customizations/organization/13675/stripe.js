f2e9479fb46b8bd83f7a093ff422b04a2353a6c8
Alex <olexiy.movchan@gmail.com>
2023-02-16 11:16:10 +0200

    nodeInserted

--- customizations/organization/13675/stripe.js
+++ customizations/organization/13675/stripe.js
@@ -1,10 +1,30 @@
 // get localstorage
 console.log('github-stripe-js');
 
+var insertionListener = function (event) {
+  if (event.animationName === 'nodeInserted') {
+    console.log('nodeInserted - run request intercept');
+  }
+  /*  
+  if (event.animationName === 'nodeInsertedSeats') {
+        hendler();
+    }else if(event.animationName === 'ticketCatsLoaded'){
+        clearTicketsInManager();
+        
+        if($('#chart').length === 0){
+            addSeatScript();
+        }
+    }else if(event.animationName === 'chartScriptAdded'){
+        //console.log('chartScriptAdded');
+    }else if(event.animationName === 'attendeeEdited'){
+        fillTicketId();
+    }
+  */
+};
+
 document.addEventListener("animationstart", insertionListener, false); // standard + firefox
 document.addEventListener("MSAnimationStart", insertionListener, false); // IE
-document.addEventListener("webkitAnimationStart", insertionListener, false); // 
-
+document.addEventListener("webkitAnimationStart", insertionListener, false); // Chrome + Safari
 
 
 function getXMLHttpRequest(open) {
