6d64852d7636a9c74535b0e9a3cb42349d6e748a
Alex <olexiy.movchan@gmail.com>
2023-03-13 15:35:48 +0200

    status === 'active'

--- customizations/organization/13675/stripe.js
+++ customizations/organization/13675/stripe.js
@@ -45,14 +45,10 @@ function getXMLHttpRequest(open) {
           console.log(orders[0]);
           
           var checkoutSessionParameters = {
-            //organizer_id: orders[0].event.organizer_id,
-            //order_id: orders[0].id,
             order_tx_number: orders[0].invoice_id
           }
-          if(orders[0].status != 'pending_organizer_approval'){
+          if(orders[0].status === 'active'){
             sendRequest(checkoutSessionParameters);
-          }else{
-            console.log('booking approval!');
           }
         }
 
