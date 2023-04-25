52e86bbb2b847d59cfc92d4b389b5d52fc5525eb
christiankrausse7 <86656535+christiankrausse7@users.noreply.github.com>
2023-04-25 10:27:56 +0200

    Update bannergeneratorDE.js

--- customizations/organization/14127/bannergenerator_intertabac/bannergeneratorDE.js
+++ customizations/organization/14127/bannergenerator_intertabac/bannergeneratorDE.js
@@ -27,14 +27,14 @@ function setTexts() {
     const value = $('#format_inp').val();
 
     if (value == 'horizontal') {
-        ctx.fillText($('#halle_inp').val(), 423 - $('#halle_inp').val().length * 2.5, 38);
-        ctx.fillText($('#stand_inp').val(), 423 - $('#stand_inp').val().length * 2.5, 50);
+        ctx.fillText($('#halle_inp').val(), 400 - $('#halle_inp').val().length * 6, 45);
+        ctx.fillText($('#stand_inp').val(), 435 - $('#stand_inp').val().length * 5, 45);
     } else if (value == 'vertikal') {
         ctx.fillText($('#halle_inp').val(), 78 - $('#halle_inp').val().length * 4, 512);
         ctx.fillText($('#stand_inp').val(), 78 - $('#stand_inp').val().length * 5, 572);
     } else if (value == 'email') {
-        ctx.fillText($('#halle_inp').val(), 50 - $('#halle_inp').val().length * 2.5, 34);
-        ctx.fillText($('#stand_inp').val(), 50 - $('#stand_inp').val().length * 2.5, 50);
+        ctx.fillText($('#halle_inp').val(), 28, 43);
+        ctx.fillText($('#stand_inp').val(), 49, 43);
     } else if (value == 'instagrampost') {
         ctx.fillText($('#halle_inp').val(), 510 - $('#halle_inp').val().length * 6, 1020);
         ctx.fillText($('#stand_inp').val(), 835 - $('#stand_inp').val().length * 9, 1020);
@@ -70,17 +70,17 @@ function setupBannerImage() {
     const value = $('#format_inp').val();
     
     if (value == 'horizontal') {
-        ctx.font = "7pt Verdana";
+        ctx.font = "13pt Verdana";
     } else if (value == 'vertikal') {
-        ctx.font = "12pt Verdana";
+        ctx.font = "13pt Verdana";
     } else if (value == 'email') {
-        ctx.font = "10pt Verdana";
+        ctx.font = "13pt Verdana";
     } else if (value == 'instagrampost') {
-        ctx.font = "18pt Verdana";
+        ctx.font = "22pt Verdana";
     } else if (value == 'instagramstory') {
-        ctx.font = "18pt Verdana";
+        ctx.font = "22pt Verdana";
     } else if (value == 'facebook') {
-        ctx.font = "18pt Verdana";
+        ctx.font = "22pt Verdana";
     };
 }
 
