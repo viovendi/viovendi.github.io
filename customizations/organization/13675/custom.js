2117878c1a0cfef804469707e28342969dd7759d
Alex <olexiy.movchan@gmail.com>
2023-06-22 14:07:26 +0300

    Update custom.js

--- customizations/organization/13675/custom.js
+++ customizations/organization/13675/custom.js
@@ -1,6 +1,69 @@
 // get localstorage
 console.log('github-js');
 
+// add styles for animation
+var styleSheetStripe = document.createElement("style");
+styleSheetStripe.innerHTML = `
+@keyframes nodeInserted {
+    from { opacity: 0.99; }
+    to { opacity: 1; }
+}
+.event-booking-widget .customization-booking-area-wrapper-page3{
+    animation-duration: 0.1s;
+    animation-name: nodeInserted;
+}
+@keyframes nodeInsertedPage4 {
+    from { opacity: 0.99; }
+    to { opacity: 1; }
+}
+.event-booking-widget .customization-booking-area-wrapper-page4{
+    animation-duration: 0.1s;
+    animation-name: nodeInsertedPage4;
+}
+`;
+head.appendChild(styleSheetStripe);
+
+
+var insertionListener = function (event) {
+  if (event.animationName === 'nodeInserted') {
+    console.log('nodeInserted - page3');
+
+    console.log(window.location.href);
+    if(url.includes("133894")){
+      console.log('1111222333444');
+    }
+    // page3
+    /*
+    $('.customization-booking-area-wrapper-page3 .customization-button-next').on('click', function(){
+      console.log('bttn clicked!');
+      if(isEpayPayment()){
+        console.log('isisEpayPayment - true');
+        localStorage.setItem('payment_method', 'ePay');
+        getXMLHttpRequest(XMLHttpRequest.prototype.open);
+      }
+    });
+    */
+    
+  }else if(event.animationName === 'nodeInsertedPage4'){
+    console.log('page4 loaded');
+    
+    // page4
+    /*
+    if(localStorage.getItem('payment_method') === 'ePay'){
+      console.log('page4 ePay!!!');
+      loader("on");
+    }
+    */
+  }
+};
+
+document.addEventListener("animationstart", insertionListener, false); // standard + firefox
+document.addEventListener("MSAnimationStart", insertionListener, false); // IE
+document.addEventListener("webkitAnimationStart", insertionListener, false); // Chrome + Safari
+
+
+
+
 function showError(){
   $('.customization2_attendee_edit-action_save').attr('disabled', true);
   $('.customization2_attendee_edit-action').append('<span class="error-message">ERROR MESSAGE</span>');
