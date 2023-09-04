359b23c555297920f69b25033b1827fb441c1253
Alex <olexiy.movchan@gmail.com>
2023-09-04 13:21:43 +0100

    Update etermin-widget.js

--- customizations/organization/16574/etermin-widget.js
+++ customizations/organization/16574/etermin-widget.js
@@ -1,1 +1,31 @@
 
+let isMembership = false;
+  dmAPI.getLoggedInMember().then(result  => {
+    const email = result.memberInfo.email;
+    const first_name = result.memberInfo.firstname;
+    const last_name = result.memberInfo.lastname;
+    if(email){
+        isMembership = true;
+        addScript(first_name, last_name, email);
+    }else{
+        addScript();   
+    }
+  });
+
+function addScript(first_name, last_name, email){
+    console.log('addScript runs');
+    var urlParams = `servicegroupid=147654&calendarid=167153`;
+    if(isMembership){
+       urlParams = urlParams+`&firstname=${first_name}&lastname=${last_name}&email=${email}`; 
+    }
+    var frameElement = document.createElement("iframe");
+    console.log(urlParams);
+    frameElement.classList.add("etermin-frame");
+    frameElement.setAttribute("id","1923963275");
+    frameElement.setAttribute("height","1600px");
+    frameElement.setAttribute("width","100%");
+    frameElement.setAttribute("scrolling","no");
+    frameElement.setAttribute("frameborder","0");
+    frameElement.setAttribute("src","https://www.eTermin.net/Asmodee-events?"+urlParams+"");
+    document.getElementById('eterminWrapper').appendChild(frameElement);
+}
