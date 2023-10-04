6faf8b3cda9042054083178cd09321e190da1960
christiankrausse7 <86656535+christiankrausse7@users.noreply.github.com>
2023-10-04 13:07:56 +0200

    Update bannergeneratorDE.js

--- customizations/organization/14426/bannergenerator/bannergeneratorDE.js
+++ customizations/organization/14426/bannergenerator/bannergeneratorDE.js
@@ -30,20 +30,20 @@ function setTexts() {
         ctx.fillText($('#halle_inp').val(), 360 - $('#halle_inp').val().length * 6, 45);
         ctx.fillText($('#stand_inp').val(), 415 - $('#stand_inp').val().length * 5, 45);
     } else if (value == 'vertikal') {
-        ctx.fillText($('#halle_inp').val(), 78 - $('#halle_inp').val().length * 6, 485);
-        ctx.fillText($('#stand_inp').val(), 78 - $('#stand_inp').val().length * 5, 565);
+        ctx.fillText($('#halle_inp').val(), 78 - $('#halle_inp').val().length * 6, 495);
+        ctx.fillText($('#stand_inp').val(), 78 - $('#stand_inp').val().length * 5, 570);
     } else if (value == 'email') {
-        ctx.fillText($('#halle_inp').val(), 70 - $('#halle_inp').val().length * 6, 50);
-        ctx.fillText($('#stand_inp').val(), 80, 50);
+        ctx.fillText($('#halle_inp').val(), 88 - $('#halle_inp').val().length * 6, 50);
+        ctx.fillText($('#stand_inp').val(), 110, 50);
     } else if (value == 'instagrampost') {
-        ctx.fillText($('#halle_inp').val(), 450 - $('#halle_inp').val().length * 6, 990);
-        ctx.fillText($('#stand_inp').val(), 735 - $('#stand_inp').val().length * 9, 990);
+        ctx.fillText($('#halle_inp').val(), 300 - $('#halle_inp').val().length * 6, 985);
+        ctx.fillText($('#stand_inp').val(), 680 - $('#stand_inp').val().length * 9, 990);
     } else if (value == 'instagramstory') {
         ctx.fillText($('#halle_inp').val(), 510 - $('#halle_inp').val().length * 6, 1227);
         ctx.fillText($('#stand_inp').val(), 835 - $('#stand_inp').val().length * 9, 1227);
     } else if (value == 'facebook') {
-        ctx.fillText($('#halle_inp').val(), 300 - $('#halle_inp').val().length * 6, 1050);
-        ctx.fillText($('#stand_inp').val(), 600 - $('#stand_inp').val().length * 9, 1000);
+        ctx.fillText($('#halle_inp').val(), 280 - $('#halle_inp').val().length * 6, 1050);
+        ctx.fillText($('#stand_inp').val(), 600 - $('#stand_inp').val().length * 9, 1050);
     }
 }
 
@@ -70,17 +70,17 @@ function setupBannerImage() {
     const value = $('#format_inp').val();
     
     if (value == 'horizontal') {
-        ctx.font = "12pt Verdana";
+        ctx.font = "14pt Verdana";
     } else if (value == 'vertikal') {
         ctx.font = "17pt Verdana";
     } else if (value == 'email') {
         ctx.font = "10pt Verdana";
     } else if (value == 'instagrampost') {
-        ctx.font = "40pt Verdana";
+        ctx.font = "44pt Verdana";
     } else if (value == 'instagramstory') {
-        ctx.font = "40pt Verdana";
+        ctx.font = "44pt Verdana";
     } else if (value == 'facebook') {
-        ctx.font = "40pt Verdana";
+        ctx.font = "50pt Verdana";
     };
 }
 
