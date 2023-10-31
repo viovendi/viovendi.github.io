7878ffa44e2cb3b847691d4debb140d48ff62bd6
eliftavan <131448783+eliftavan@users.noreply.github.com>
2023-10-31 14:58:56 +0100

    Update changeLinkCheckoutPageDe.js

--- customizations/organization/16029/changeLinkCheckoutPageDe.js
+++ customizations/organization/16029/changeLinkCheckoutPageDe.js
@@ -1,2 +1,2 @@
 // Change booking portal link on checkout page
-$(".customization-client-portal-link").attr("href", "https://animationsinstitut.de/de/anmeldung-bestaetigung")
+window.location.href = "https://animationsinstitut.de/de/anmeldung-bestaetigung";
