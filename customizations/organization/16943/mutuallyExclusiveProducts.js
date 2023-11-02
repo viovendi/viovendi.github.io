a3d90b8edcadf90233141b8c5d5cc97b3e8af306
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2023-11-02 16:54:16 +0100

    Update mutuallyExclusiveProducts.js

--- customizations/organization/16943/mutuallyExclusiveProducts.js
+++ customizations/organization/16943/mutuallyExclusiveProducts.js
@@ -2,7 +2,7 @@ var lookup = { };
 var h = document.querySelector(".customization2_booker_further-data");
 var t = h.children[1];
 var groups = t.querySelectorAll("form>div");
-var select = [...groups[0].querySelectorAll("vv-checkbox")].map(function () { return false; })
+var select = [...groups[0].querySelectorAll("vv-checkbox")].map(function () { return false; });
 function trigger(gindex) {
   groups.forEach(function (gd, gdindex) {
     var products = gd.querySelectorAll("vv-checkbox");
@@ -11,6 +11,7 @@ function trigger(gindex) {
     })) return;
     
     products.forEach(function (p, pindex) {
+      if (lookup[p.innerText.trim()]) return;
       var input = p.querySelector(".customization2_booker_further-data_product_checkbox");
       if (select[pindex]) {
         p.classList.remove("ng-valid");
@@ -32,4 +33,6 @@ groups.forEach(function (g, gindex) {
       trigger(gindex);
     }
   });
+  q.querySelector("vv-button > button").onclick = function () { trigger(-1); };
 });
+
