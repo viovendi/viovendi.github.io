956cc6204d58a465901b8de041e9d97ae7c06477
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2023-11-08 18:08:00 +0100

    Update mutuallyExclusiveProducts.js

--- customizations/organization/16943/mutuallyExclusiveProducts.js
+++ customizations/organization/16943/mutuallyExclusiveProducts.js
@@ -5,20 +5,19 @@ var groups = t.querySelectorAll("div.question-group");
 var select = [...groups[0].querySelectorAll("vv-checkbox")].map(function () { return false; });
 function trigger() {
   groups.forEach(function (gd, gdindex) {
-    //var products = gd.querySelectorAll(".customization2_attendee_further-data_product");
-    var products = gd.querySelectorAll("vv-checkbox");
+    var products = gd.querySelectorAll(".customization2_attendee_further-data_product");
     if ([...products].some(function (p) {
       if (lookup[p.innerText.trim()]) return true;
     })) return;
     
     products.forEach(function (p, pindex) {
       var input = p.querySelector(".customization2_attendee_further-data_product_checkbox");
-      if (select[pindex]) { //&& p.querySelector(".customization2_attendee_further-data_product_capacity").innerText.trim() == "") {
-        p.classList.remove("ng-valid");
-        input.disabled = true;
-      } else {
+      if (!select[pindex] && p.querySelector(".customization2_attendee_further-data_product_capacity").innerText.trim() == "") {
         p.classList.add("ng-valid");
         input.disabled = false;
+      } else {
+        p.classList.remove("ng-valid");
+        input.disabled = true;
       }
     });
   });
