2e581dc29bb688481b330585b490ecd68cd924c4
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2023-11-08 18:05:37 +0100

    Update mutuallyExclusiveProducts-test.js

--- customizations/organization/16943/mutuallyExclusiveProducts-test.js
+++ customizations/organization/16943/mutuallyExclusiveProducts-test.js
@@ -13,12 +13,12 @@ function trigger() {
     
     products.forEach(function (p, pindex) {
       var input = p.querySelector(".customization2_attendee_further-data_product_checkbox");
-      if (select[pindex] && p.querySelector(".customization2_attendee_further-data_product_capacity").innerText.trim() == "") {
-        p.classList.remove("ng-valid");
-        input.disabled = true;
-      } else {
+      if (!select[pindex] && p.querySelector(".customization2_attendee_further-data_product_capacity").innerText.trim() == "") {
         p.classList.add("ng-valid");
         input.disabled = false;
+      } else {
+        p.classList.remove("ng-valid");
+        input.disabled = true;
       }
     });
   });
