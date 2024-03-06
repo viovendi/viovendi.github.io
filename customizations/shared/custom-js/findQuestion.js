95357905e5b52fc6239aa6b69023f7544dfdfccd
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-03-06 14:00:50 +0100

    Update findQuestion.js

--- customizations/shared/custom-js/findQuestion.js
+++ customizations/shared/custom-js/findQuestion.js
@@ -1,5 +1,5 @@
-async function run(exactLabel, dom) {
-  const qu = $(".customization2_attendee_further-data_custom-question");
+async function run(exactLabel, attendee) {
+  const qu = $(attendee ?? document).find(".customization2_attendee_further-data_custom-question");
   const results = await Promise.all(qu.map(async (i, q) => {
     const label = $(q).find(".customization2_attendee_further-data_custom-question_label");
     const allText = [...label.get(0).childNodes].map(c => $(c).text().trim()).join("");
@@ -7,7 +7,5 @@ async function run(exactLabel, dom) {
     if(label.find("vv-optional-text").length) match += label.find("vv-optional-text").text().trim();
     return await custom_js("match", match, allText);
   }));
-  const filtered = qu.filter((i, q) => results[i]).parent();
-  if (dom) return filtered.get(0);
-  else return filtered;
+  return qu.filter((i, q) => results[i]).parent();
 }
