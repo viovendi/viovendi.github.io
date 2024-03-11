a41d3bc48841639c55469be1fafef34991692e30
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-03-11 20:51:54 +0100

    Update noneCheckbox.js

--- customizations/shared/custom-js/noneCheckbox.js
+++ customizations/shared/custom-js/noneCheckbox.js
@@ -1,12 +1,19 @@
 async function run(attendee, question, none) {
   const q = await custom_js("findQuestion", question, attendee ?? document);
 
-  function disable() {
+  async function disable() {
     const t = q.find("input:checked").next(".vv-checkbox__label").find(".vv-checkbox__label-text").map((i, t) => $(t).text().trim()).get();
-    const d = t.includes(none);
-    q.find("input").forEach(function () {
+    let d = false;
+    for (const x of t) {
+      if (await custom_js("match", none, x)) {
+        d = true;
+        break;
+      }
+    }
+    q.find("input").forEach(async function () {
       const text = $(this).next(".vv-checkbox__label").find(".vv-checkbox__label-text").text().trim();
-      if ((d && text != none) || (!d && t.length && text == none)) {
+      const same = await custom_js("match", none, text);
+      if ((d && !same) || (!d && t.length && same)) {
         this.checked = false;
         this.disabled = true;
       } else {
