0e7fdbe228b575fd18bf5eae5137c5a796b444cd
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-03-11 18:33:25 +0100

    Update attendees.js

--- customizations/shared/custom-js/attendees.js
+++ customizations/shared/custom-js/attendees.js
@@ -1,45 +1,47 @@
-const attendee_states = { };
+const attendees_info = { };
 
 async function run(options) {
   console.log(attendee_states);
   
   const attendees = $(".customization3_booking-participant_attendee").parent().get();
-  for (const a_id in attendee_states) {
-    console.log("removing the old shit");
-    attendee_states[a_id].observer.disconnect();
+  for (const a_id in attendees_info) {
+    attendees_info[a_id].observer.disconnect();
     if (!attendees.some(a => a.id == a_id)) {
       options.remove?.(attendee, a_id, console.log("remove"));
-      delete attendee_states[a_id];
+      delete attendees_info[a_id];
     }
   }
   attendees.forEach(attendee => {
-    console.log(attendee);
-    
     const id = attendee.id;
     if (!(id in attendee_states)) {
       console.log("new attendee, wohoo!");
-      attendee_states[id] = { };
+      attendees_info[id] = { };
     }
-    const state = attendee_states[id];
+    const info = attendees_info[id];
     
     function execute() {
       const save = $(attendee).find(".customization2_attendee_edit-action_save").get(0);
-      const body = $(attendee).find(".customization3_collapsible-wrapper_body");
-      if (save != state.save_button) {
-        console.log("the save button changed \"-\"");
-        if (save) options.title_open?.(attendee, id, console.log("title_open"));
-        else options.title_close?.(attendee, id, console.log("title_close"));
+      const body = $(attendee).find(".customization3_collapsible-wrapper_body").get(0);
 
-        if (save) options.open?.(attendee, id, console.log("open"));
-        else if (body.length) options.close?.(attendee, id, console.log("close"));
-        else return;
-        state.save_button = save;
-      }
+      let new_state;
+      if (save && body) new_state = "open";
+      else if (body) new_state = "close";
+      else new_state = "title";
+
+      if (new_state == info.state && save == info.save_button) return;
+      info.state = new_state;
+      info.save_button = save_button;
+
+      if (save) options.title_open?.(attendee, id, console.log("title_open"));
+      else options.title_close?.(attendee, id, console.log("title_close"));
+
+      if (save) options.open?.(attendee, id, console.log("open"));
+      else if (body) options.close?.(attendee, id, console.log("close"));
     }
     execute();
     
-    state.observer = new MutationObserver(execute);
-    state.observer.observe(attendee, {
+    info.observer = new MutationObserver(execute);
+    info.observer.observe(attendee, {
       "childList": true,
       "subtree": true
     });
