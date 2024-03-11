5a4a3ea14d024805ce221f3b03d846e55a849278
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-03-11 17:02:03 +0100

    Update attendees.js

--- customizations/shared/custom-js/attendees.js
+++ customizations/shared/custom-js/attendees.js
@@ -1,5 +1,47 @@
-async function run(callback) {
-  $(".customization3_booking-participant_attendee").get().forEach((attendee, i) => {
-    callback(attendee, i);
+const attendee_states = { };
+
+async function run(options) {
+  const attendees = $(".customization3_booking-participant_attendee").parent().get();
+  for (const a_id in attendee_states) {
+    attendee_states[a_id].observer.disconnect();
+    if (!attendees.some(a => a.id == a_id)) {
+      options.remove?.(attendee, a_id);
+      delete attendee_states[a_id];
+    }
+  }
+  attendees.forEach(attendee => {
+    const id = attendee.id;
+    if (!(id in attendee_states)) {
+      attendee_states[id] = { };
+    }
+    const state = attendee_states[id];
+    
+    const save = $(attendee).find(".customization2_attendee_edit-action_save").get(0);
+    const body = $(attendee).find(".customization3_collapsible-wrapper_body");
+    
+    if (save) options.open?.(attendee, id);
+    else if (body.length) options.close?.(attendee, id);
+    
+    function execute() {
+      const save = $(attendee).find(".customization2_attendee_edit-action_save").get(0);
+      const body = $(attendee).find(".customization3_collapsible-wrapper_body");
+      if (save != state.save_button) {
+        if (save) options.title_open?.(attendee, id);
+        else options.title_close?.(attendee, id);
+
+        if (save) options.open?.(attendee, id);
+        else if (body.length) options.close?.(attendee, id);
+        else return;
+        state.save_button = save;
+      }
+    }
+    execute();
+    
+    state.observer = new MutationObserver(execute);
+    state.observer.observe(attendee, {
+      "childList": true,
+      "subtree": true
+    });
+    
   });
 }
