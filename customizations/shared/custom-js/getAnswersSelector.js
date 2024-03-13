934b31a059894eb56e965247a0189e16548a420a
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-03-13 11:47:55 +0100

    Create getAnswersSelector.js

---
+++ customizations/shared/custom-js/getAnswersSelector.js
@@ -0,0 +1,10 @@
+async function run(element) {
+  if (element.is("vv-additional-question-radio"))
+    return () => [element.find("input:checked").next(".customization2_attendee_further-data_custom-question_radio-line_label").find(".vv-radio__label-text").text().trim()];
+  else if (el.is("vv-additional-question-dropdown"))
+    return () => [element.find(".customization2_attendee_further-data_custom-question_dropdown .vv-selection-input__value").text().trim()];
+  else if (el.is("vv-additional-question-checkboxes"))
+    return () => element.find("input:checked").next(".vv-checkbox__label").find(".vv-checkbox__label-text").map((i, t) => $(t).text().trim()).get();
+  else if (el.is("vv-additional-question-text") || el.is("vv-additional-question-paragraph"))
+    return () => [element.find(".customization2_attendee_further-data_custom-question_input").val()];
+}
