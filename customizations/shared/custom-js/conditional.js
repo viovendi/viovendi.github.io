aff3b2795daff913aa766dcdb27c76ec93fa8426
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-03-13 12:46:17 +0100

    Update conditional.js

--- customizations/shared/custom-js/conditional.js
+++ customizations/shared/custom-js/conditional.js
@@ -33,7 +33,6 @@ async function conditional(attendee, cond, show, dict) {
                 break;
               }
             }
-            console.log(question, "has answer:", selected, "matches", possible, ":", same);
             await conditional(attendee, answers[possible], same, dict);
           }
         }
