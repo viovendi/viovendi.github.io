0a1c56f865e15c05c5293d83faf89fd2947b79b6
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-04-14 14:00:38 +0200

    Update loader.js

--- customizations/shared/custom-js/loader.js
+++ customizations/shared/custom-js/loader.js
@@ -1,12 +1,18 @@
 const cache = { };
 
 window.custom_js = async function (name, ...args) {
-  if (name in cache) {
+  if (name + "#loading" in cache) {
+    await cache[name + "#loading"];
     return await cache[name](...args);
   }
+  let loaded;
+  cache[name + "#loading"] = new Promise(res => {
+    loaded = res;
+  });
   return new Promise(resolve => {
     $.getScript("https://viovendi.github.io/customizations/shared/custom-js/" + name + ".js", async () => {
       cache[name] = run;
+      loaded();
       resolve(await run(...args));
     });
   });
