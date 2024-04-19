19d5a4b7f4b0484851d8255c7f93d6913f11cda9
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-04-19 22:00:13 +0200

    Update booker.js

--- customizations/shared/custom-js/booker.js
+++ customizations/shared/custom-js/booker.js
@@ -36,9 +36,14 @@ async function _run(booker, options) {
     else if (body && $(body).text().trim()) new_state = "close";
     else new_state = "title";
 
+    console.log("new state:", new_state);
+    
     if (new_state == "open" && !(booker.find("vv-additional-questions").find("form").length)) return;
+    console.log("pass 1");
     if (new_state == "open" && booker_info.state == "open" && save == booker_info.save_button) return;
+    console.log("pass 2");
     if (new_state == booker_info.state && !booker_info.first) return;
+    console.log("pass 3");
 
     booker_info.state = new_state;
     booker_info.save_button = save;
