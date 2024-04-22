a96e35056671574d6cb8a8b2d61e983ba49cac0c
Oleg <oleh.stetsko@gmail.com>
2024-04-22 13:40:42 +0300

    Update findQuestion.js

--- customizations/shared/custom-js/findQuestion.js
+++ customizations/shared/custom-js/findQuestion.js
@@ -11,6 +11,7 @@ async function run(exactLabel, attendee) {
   let p = qu.filter((i, q) => results[i]).parent();
   while (true) {
     const el = p.get(0);
+    console.log(el)
     if (!el || el.tagName.toLowerCase().startsWith("vv-additional-question-")) return p;
     p = p.parent();
   }
