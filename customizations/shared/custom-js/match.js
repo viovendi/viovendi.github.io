debe6f3a923cb90498ee155c6a8251f0aee77264
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-06-13 17:30:05 +0200

    Update match.js

--- customizations/shared/custom-js/match.js
+++ customizations/shared/custom-js/match.js
@@ -1,11 +1,14 @@
 function run(match, allText) {
-   const groups = match.split("{...}");
-    let i = 0;
-    for (const group of groups) {
-      const sub = allText.slice(i);
-      const idx = sub.indexOf(group);
-      if (idx == -1) return false;
-      i += idx + group.length;
-    }
-    return true;
+  if (match == "{...}" && allText == "") return false;
+  const groups = match.split("{...}");
+  let i = 0;
+  let lastEmpty = false;
+  for (const group of groups) {
+    const sub = allText.slice(i);
+    const idx = sub.indexOf(group);
+    if (idx == -1) return false;
+    i += idx + group.length;
+    lastEmpty = group.length == 0;
+  }
+  return lastEmpty || i == allText.length;
 }
