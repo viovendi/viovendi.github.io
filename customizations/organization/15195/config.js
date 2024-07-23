70de7570395b44dbe7245736c30e6407c40e85c9
vbodend <69503064+vbodend@users.noreply.github.com>
2024-07-23 09:20:21 +0200

    Update config.js

--- customizations/organization/15195/config.js
+++ customizations/organization/15195/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito:{
-    userPoolId: 'eu-central-1_N6OlPZKBe',
+    userPoolId: 'eu-central-1_bYQ5fnVPq',
     region: 'eu-central-1',
-    clientId: '132qeevu4fo2biee2gp40t623d'
+    clientId: '73ogb2etnl17m8v8srrfn7euvr'
   },
 };
