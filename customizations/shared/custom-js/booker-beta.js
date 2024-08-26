1f3941af6c4892576ea5816bf244c8f393b3ce85
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-08-26 14:17:33 +0200

    Create booker-beta.js

---
+++ customizations/shared/custom-js/booker-beta.js
@@ -0,0 +1,67 @@
+const booker_info = { };
+
+async function run(options, additional) {
+  booker_info.global_observer?.disconnect();
+  function execute() {
+    if ($(".customization2_booker_contact-data_headline").length) {
+      booker_info.global_observer?.disconnect();
+      const booker = $(".customization2_booker").parent().parent();
+      _run(booker, options, additional ?? true);
+      return true;
+    }
+    return false;
+  }
+  const f = execute();
+  if (!f) {
+    booker_info.global_observer = new MutationObserver(execute);
+    booker_info.global_observer.observe(document, {
+      "childList": true,
+      "subtree": true
+    });
+  }
+}
+
+async function _run(booker, options, additional) {
+  const be = booker.get(0);
+
+  options.create?.(be);
+  booker_info.first = true;
+  
+  function execute() {
+    const save = booker.find(".customization2_booker_edit-action_save").get(0);
+    const body = booker.find(".customization3_collapsible-wrapper_body").get(0);
+
+    let new_state;
+    if (save && body) new_state = "open";
+    else if (body && $(body).text().trim()) new_state = "close";
+    else new_state = "title";
+
+    if (new_state == "open") {
+      const defq = booker.find("vv-default-questions");
+      if (defq.length) {
+        if(!defq.children().length) return;
+      }
+    }
+
+    console.log("Additional questions there?", booker.find("vv-additional-questions").length);
+    
+    if (additional && new_state == "open" && !(booker.find("vv-additional-questions").find("form").length)) return;
+    if (new_state == "open" && booker_info.state == "open" && save == booker_info.save_button) return;
+    if (new_state == booker_info.state && !booker_info.first) return;
+
+    booker_info.state = new_state;
+    booker_info.save_button = save;
+    booker_info.first = false;
+
+    if (save) options.open?.(be);
+    else if (body && $(body).text().trim()) options.close?.(be);
+    else options.title?.(be);
+  }
+  execute();
+  
+  booker_info.observer = new MutationObserver(execute);
+  booker_info.observer.observe(be, {
+    "childList": true,
+    "subtree": true
+  });
+}
