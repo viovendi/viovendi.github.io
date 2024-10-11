3a732cc8808d761029b5d91c80c2c817f5fcb287
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-10-11 15:00:40 +0200

    Update css.js

--- customizations/shared/custom-js/css.js
+++ customizations/shared/custom-js/css.js
@@ -1,7 +1,7 @@
 async function run(css) {
   const styleSheet = document.createElement("style");
   styleSheet.type = "text/css";
-  styleSheet.innerText = css;
+  styleSheet.innerHTML = css;
   const wait = new Promise(resolve => {
     styleSheet.onload = resolve;
   });
