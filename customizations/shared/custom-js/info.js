24fdd1a31acc5fb79e7ba74bbf522586725a3b8d
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2024-11-20 19:18:15 +0100

    Create info.js

---
+++ customizations/shared/custom-js/info.js
@@ -0,0 +1,20 @@
+async function run() {
+  const url = new URL(window.location.href);
+  const eid = window.location.pathname.split("/")[3];
+  const oid = url.searchParams.get("organizerId");
+  const lang = url.searchParams.get("doo_locale");
+  return {
+
+    // short lowercase
+    eid, oid, lang,
+
+    // short uppercase
+    "EID": eid,
+    "OID": oid,
+
+    // long
+    "eventId": eid,
+    "organizerId": oid,
+    "locale": lang
+  };
+}
