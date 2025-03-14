c54253b79b0f99e68eeef912cec7c6847ce66a6b
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2025-03-14 14:51:48 +0100

    Fix Waiters

--- customizations/shared/custom-js/waitForCreate.js
+++ customizations/shared/custom-js/waitForCreate.js
@@ -2,7 +2,7 @@ async function run(selector, parent) {
     return await new Promise(async res => {
         const s = await selector();
         if (s.length) return res(s);
-        const observer = new MutationObserver(() => {
+        const observer = new MutationObserver(async () => {
             const s = await selector();
             if (s.length) {
                 observer.disconnect();
--- customizations/shared/custom-js/waitForDelete.js
+++ customizations/shared/custom-js/waitForDelete.js
@@ -2,7 +2,7 @@ async function run(selector, parent) {
     await new Promise(async res => {
         const s = await selector();
         if (!s.length) return res();
-        const observer = new MutationObserver(() => {
+        const observer = new MutationObserver(async () => {
             const s = await selector();
             if (!s.length) {
                 observer.disconnect();
