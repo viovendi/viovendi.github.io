cc3c77f7624dfd424a7015f074fa67e8484eb2b3
Alex <olexiy.movchan@gmail.com>
2025-09-02 08:37:43 +0100

    Update setTicket.js

--- customizations/shared/custom-js/setTicket.js
+++ customizations/shared/custom-js/setTicket.js
@@ -2,11 +2,11 @@ async function run(ticket, value) {
     const ctrl = ticket.find(".vv-selection-input__control");
     ctrl.find("p-overlay").hide();
     ctrl.click();
-    const panel = await custom_js.waitForCreate(() => ctrl.find("p-overlay .p-dropdown-panel"), ctrl);
+    const panel = await custom_js.waitForCreate(() => ctrl.find("p-overlay .p-select-overlay"), ctrl);
     const options = panel.find(".vv-selection-option");
     const search = value.toString();
     const select = options.filter((_, t) => $(t).text().trim() == search);
     (select.length ? select : options.find(".vv-selection-option--selected")).click();
-    await custom_js.waitForDelete(() => ctrl.find("p-overlay .p-dropdown-panel"), ctrl);
+    await custom_js.waitForDelete(() => ctrl.find("p-overlay .p-select-overlay"), ctrl);
     ctrl.find("p-overlay").show();
 }
