3484d0ca0e375d9f9eaeaee3608ede9c87d8392f
Alex <olexiy.movchan@gmail.com>
2026-01-16 08:59:04 +0000

    Update termsAndDoi.js

--- customizations/organization/13879/termsAndDoi.js
+++ customizations/organization/13879/termsAndDoi.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
         var attendeeEmail1 = $('.customization2_attendee_contact-data_email_input');
         var attendeeEmail2 = $('.customization2_attendee_contact-data_email-confirmation_input');
         attendeeEmail2.val(attendeeEmail1.val());
-        $('.customization2_attendee_contact-data_email-confirmation_input').focus().click();
-        $('.customization2_attendee_edit-action_save').focus().click();
+        $('.customization2_attendee_contact-data_email-confirmation_input').click();
+        $('.customization2_attendee_edit-action_save').click();
     };
 
     // Email Confirmation Booker
@@ -19,8 +19,8 @@ $(document).ready(function () {
         var box1 = $('.customization2_booker_contact-data_email_input');
         var box2 = $('.customization2_booker_contact-data_email-confirmation_input');
         box2.val(box1.val());
-        $('.customization2_booker_contact-data_email-confirmation_input').focus().click();
-        $('.customization2_booker_edit-action_save').focus().click();
+        $('.customization2_booker_contact-data_email-confirmation_input').click();
+        $('.customization2_booker_edit-action_save').click();
     };
 
     function hideEmail() {
