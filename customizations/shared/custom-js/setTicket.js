b33046cffb67dc08f3d03ae9cc8675b31b2e6c5f
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2026-01-22 18:58:15 +0100

    Update setTicket.js

--- customizations/shared/custom-js/setTicket.js
+++ customizations/shared/custom-js/setTicket.js
@@ -1,12 +1,12 @@
 async function run(ticket, value) {
     const ctrl = ticket.find(".event-category__ticket-count p-select");
     ctrl.find("p-overlay").hide();
-    ctrl.trigger("click");
+    ctrl.click();
     const panel = await custom_js.waitForCreate(() => ctrl.find("p-overlay .p-select-list"), ctrl);
     const options = panel.find(".p-select-option");
     const search = value.toString();
     const select = options.filter((_, t) => $(t).text().trim() == search);
-    (select.length ? select : options.find(".vv-selection-option--selected")).trigger("click");
+    (select.length ? select : options.find(".vv-selection-option--selected")).click();
     await custom_js.waitForDelete(() => ctrl.find("p-overlay .p-select-list"), ctrl);
     ctrl.find("p-overlay").show();
 }
