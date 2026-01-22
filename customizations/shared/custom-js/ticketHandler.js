757b3484662c80cbb889ab213b9cf74c616e418f
Dmitry <148325186+Dmitry-the-Werkstudent@users.noreply.github.com>
2026-01-22 17:39:38 +0100

    Update ticketHandler.js

--- customizations/shared/custom-js/ticketHandler.js
+++ customizations/shared/custom-js/ticketHandler.js
@@ -1,7 +1,7 @@
 function run(ticket, handler) {
     ticket.find(".event-category__ticket-count").click(() => {
         ticket.find("p-overlay p-selectitem").click(e => {
-            handler(parseInt($(target).text() ?? 0);
+            handler(parseInt($(target).text()) ?? 0);
         });
     });
 }
